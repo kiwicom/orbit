f944deb241bdcbcd290fc296802ac019aa7782ba
oreqizer <oreqizer@gmail.com>
2018-02-14 11:15:20 +0100

    general: Build, test things

--- src/Button/index.jsx
+++ src/Button/index.jsx
@@ -1,13 +1,18 @@
 /* @flow */
-import React from 'react';
-import styled from 'styled-components';
+import * as React from "react";
+import styled from "styled-components";
 
-type Props = {|
+const Container = styled.button`
+  background-color: dodgerblue;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  padding: 10px;
+  font-size: 16px;
+`;
 
-|};
+type Props = {};
 
-const Button = (props: Props) => (
-  <div {...props}>kek</div>
-);
+const Button = (props: Props) => <Container {...props}>Click</Container>;
 
 export default Button;
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { default as Button } from "./Button"; // eslint-disable-line import/prefer-default-export

5ad19ab4e539b48beac9e470b89f98ea946d1c7f
Jakub Žaba <jakub.zaba@kiwi.com>
2018-02-20 16:06:07 +0100

    BLKN: add Section with Header components

---
+++ blkn/src/Container/__tests__/Section.test.js
@@ -0,0 +1,18 @@
+// @flow
+
+import * as React from "react";
+import { shallow } from "enzyme";
+import Section from "../Section";
+
+describe("Section", () => {
+  it("should contain children elements", () => {
+    const component = shallow(
+      <Section>
+        <span>Test</span>
+      </Section>,
+    );
+
+    expect(component.find(".section")).toHaveLength(1);
+    expect(component.find("span")).toHaveLength(1);
+  });
+});

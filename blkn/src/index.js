bd3c54352d8b5058d3b35730aa8d498b0d0392a2
Simon Prochazka <simon@fallion.net>
2018-02-23 12:39:33 +0100

    BLKN: Add more exports to index.js

--- blkn/src/index.js
+++ blkn/src/index.js
@@ -1,5 +1,8 @@
 // @flow
 
 export { default as Button } from "./Button";
-export { default as Input } from "./Input";
+export { default as InputText } from "./InputText";
 export { default as Typography } from "./Typography";
+export { default as Header } from "./Container/Header";
+export { default as Section } from "./Container/Section";
+export { default as Loader } from "./Loader";

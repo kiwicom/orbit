a96219d968906a283480a7424fdf24c725e9982e
Toni.U <antoni.peya@kiwi.com>
2018-02-26 15:40:07 +0100

    Finished stories. Added tests

--- blkn/src/Select/index.js
+++ blkn/src/Select/index.js
@@ -1,95 +1,112 @@
 // @flow
 import * as React from "react";
+import css from "styled-jsx/css";
 import Typography from "./../Typography";
 
-type OptionType = {
-  value: string | number,
+const HASERROR_CLASS = "hasError";
+const ERROR_COLOR = "#d21c1c";
+const LIGHTGRAY_COLOR = "#bac7d5";
+const DARKGRAY_COLOR = "#46515e";
+
+const WHITE_COLOR = "white";
+
+type Option = {
+  value: string,
   label?: string,
   disabled?: boolean,
-  selected?: boolean,
   visible?: boolean,
-}
-type Option = OptionType | string | number;
+};
 type Props = {
-  //children: React.Node,
   disabled?: boolean,
-  error: any,
+  error?: string,
   label?: string,
-  onChange: (SyntheticInputEvent<HTMLInputElement>) => any,
+  onChange: (SyntheticInputEvent<HTMLSelectElement>) => any,
   options: Array<Option>,
   placeholder?: string,
   required?: boolean,
   value: string,
 };
+type State = {
+  value: string,
+};
 
-//const renderOption = ({ value, label, disabled, selected, visible } : Option) => {
-const renderOption = ( option: Option) => {
-  const { value: optValue, label, disabled, selected, visible } = option;
-  const value = typeof option === 'object' ? optValue : option;
-
-  if(true){
-    return(
-      <option
-        key={`option-${value}`}
-        value={value}
-        label={label || value}
-        disabled={!!disabled}
-        />
-    )
+const style = css`
+  select {
+    border-radius: 3px;
+    background-color: ${WHITE_COLOR};
+    border: solid 1px ${LIGHTGRAY_COLOR};
+    line-height: 1.25;
+    padding: 12px 16px;
+    color: ${DARKGRAY_COLOR};
   }
-}
-const Select = ({
-  disabled,
-  error,
-  label,
-  onChange,
-  options,
-  placeholder,
-  required = true,
-  value,
-}: Props) => {
-  return(
-    <div>
-      <label>
-        {label && (
-          <div>
-            <Typography type="secondary">{label}</Typography>
-          </div>
-        )}
-        <select value={value} required={!!required} onChange={onChange}>
-          {placeholder && <option disabled value='' label={placeholder} className='placeholder'/>}
-          {options.map(renderOption)}
-        </select>
-      </label>
-      {error && (
-        <div>
-          <Typography size="small" type="error">
-            {error}
-          </Typography>
-        </div>
-      )}
-      <style jsx>{`
-        select {
-          border-radius: 3px;
-          background-color: white;
-          border: solid 1px #bac7d5;
-          //font-size: 16px;
-          line-height: 1.25;
-          padding: 12px 16px;
-        }
-        label div {
-          font-size: 14px;
-          line-height: 1.43;
-        }
-        option.placeholder{
-          color: red;
-        }
-        option[value=""][disabled] {
-          display: none;
-        }
-      `}</style>
-    </div>
-  );
+  select.${HASERROR_CLASS} {
+    border-color: ${ERROR_COLOR};
+  }
+  label div {
+    font-size: 14px;
+    line-height: 1.43;
+  }
+  option[value=""][disabled] {
+    display: none;
+  }
+`;
+
+class Select extends React.Component<Props, State> {
+  state = {
+    value: this.props.value,
+  };
+  handleChange = (e: SyntheticInputEvent<HTMLSelectElement>) => {
+    this.setState({ value: e.target.value });
+    this.props.onChange(e);
+  };
+  feedbackLine = () =>
+    this.props.error && (
+      <div>
+        <Typography size="small" type="error">
+          {this.props.error}
+        </Typography>
+      </div>
+    );
+  renderOption = ({ value, label, disabled, visible = true }: Option) => {
+    if (visible) {
+      return (
+        <option
+          key={`option-${value}`}
+          value={value}
+          label={label || value}
+          disabled={!!disabled}
+        />
+      );
+    }
+    return null;
+  };
+  render() {
+    const { disabled, label, options, error, placeholder, required = true } = this.props;
+    const { value } = this.state;
+    return (
+      <div>
+        <label>
+          {label && (
+            <div>
+              <Typography type="secondary">{label}</Typography>
+            </div>
+          )}
+          <select
+            disabled={disabled}
+            value={value}
+            required={!!required}
+            onChange={this.handleChange}
+            className={error && HASERROR_CLASS}
+          >
+            {<option disabled value="" label={placeholder} className="placeholder" />}
+            {options.map(this.renderOption)}
+          </select>
+        </label>
+        {this.feedbackLine()}
+        <style jsx>{style}</style>
+      </div>
+    );
+  }
 }
 
 export default Select;
--- blkn/stories/Select.js
+++ blkn/stories/Select.js
@@ -4,50 +4,47 @@ import { action } from "@storybook/addon-actions";
 
 import Select from "../src/Select";
 
-const numberOptions =[ 1, 2, 3, 4, 5];
-const stringOptions =['one', 'two', 'three'];
-const objectOptions =[
-  { value: 1, label: 'Uno'},
-  { value: 2, label: 'Dos'},
-  { value: 3, label: 'Tres'},
-  { value: 4, label: 'Cuatro'},
+const objectOptions = [
+  { value: 1, label: "One" },
+  { value: 2, label: "Two" },
+  { value: 3, label: "Three" },
+  { value: 4, label: "Four" },
 ];
 
-const hiddenObjectOptions =[
-  { value: 'one', label: 'Uno'},
-  { value: 'two', label: 'Dos'},
-  { value: 'three', label: 'Tres'},
-  { value: 'four', label: 'Cuatro'},
-  { value: 'hidden four', label: 'Hidden Cuatro'},
+const hiddenOptions = [
+  { value: "one", label: "One" },
+  { value: "two", label: "Two" },
+  { value: "three", label: "Three" },
+  { visible: false, value: "hidden-four", label: "Hidden Four" },
+];
+const disabledOptions = [
+  { value: "one", label: "One" },
+  { value: "two", label: "Two" },
+  { value: "three", label: "Three" },
+  { disabled: true, value: "hidden-four", label: "Hidden Four" },
 ];
 
 storiesOf("Select", module)
-  .add("default", () => <Select 
-                          value="" 
-                          options={objectOptions}
-                          onChange={action("onChange")} />)
-  .add("default with placeholder", () => {
-    const placeholder = 'MyPlaceholder';
-    return(
-      <Select 
-        value="" 
-        placeholder={placeholder}
-        options={objectOptions}
-        onChange={action("onChange")} />
-    );
-  })
-//  .add("default with placeholder", () => <Select 
-//                          value="" 
-//                          options={objectOptions}
-//                          onChange={action("onChange")} />);
-  //.add("label", () => (
-  //  <Input label="Text label" value="input value" onChange={action("onChange")} />
-  //))
-  //.add("error", () => (
-  //  <Input
-  //    label="Text label"
-  //    error="Error message"
-  //    value="input value"
-  //    onChange={action("onChange")}
-  //  />
-  //));
+  .add("Default", () => <Select value="" options={objectOptions} onChange={action("onChange")} />)
+  .add("Default with placeholder", () => (
+    <Select
+      value=""
+      placeholder="MyPlaceHolder"
+      options={objectOptions}
+      onChange={action("onChange")}
+    />
+  ))
+  .add("Hidden 'Four' option", () => (
+    <Select value="" options={hiddenOptions} onChange={action("onChange")} />
+  ))
+  .add("Disabled 'Four' option", () => (
+    <Select value="" options={disabledOptions} onChange={action("onChange")} />
+  ))
+  .add("With error feedback", () => (
+    <Select
+      value=""
+      options={objectOptions}
+      error="Error message here"
+      onChange={action("onChange")}
+    />
+  ));

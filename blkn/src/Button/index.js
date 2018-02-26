e49432de32c3435c33ffc35a2490ddcc473f7a40
Przemek Trzepinski <przemektrzepinski@Przemeks-MacBook-Pro.local>
2018-02-26 18:16:32 +0100

    created button

--- blkn/src/Button/index.js
+++ blkn/src/Button/index.js
@@ -3,23 +3,35 @@ import * as React from "react";
 
 type Props = {
   title: string,
-  onClick: (e: SyntheticEvent<HTMLButtonElement>) => void,
+  onClick?: (e: SyntheticEvent<HTMLButtonElement>) => void,
+  disabled?: boolean,
+  secondary?: boolean,
+  size?: string,
+  icon?: object
 };
 
-const Button = ({ title, onClick }: Props) => (
-  <button onClick={onClick}>
-    {title}
+const Button = (Props) => {
+  console.log(Props)
+  return(
+  <button onClick={Props.onClick} disabled={Props.disabled}>
+      {Props.icon && <Props.icon fill={Props.secondary ? '#46515e' : 'white'} height={Props.size === 'normal'? '20px' : Props.size === 'small' ? '20px' : '26px'} width='20px' style={{position: 'absolute', top: '50%', left: '12px', transform: 'translateY(-50%)'}}/>} 
+      {Props.title}
     <style jsx>{`
       button {
-        background-color: dodgerblue;
-        color: white;
+        opacity: ${Props.disabled ? '0.3' : '1'}
+        cursor: ${Props.disabled ? 'default' : 'pointer'}
+        background-color: ${Props.secondary ? '#e8edf1' : '#00a991'}
+        color: ${Props.secondary ? '#46515e' : 'white'}
         border: none;
         border-radius: 3px;
-        padding: 10px;
-        font-size: 16px;
+        font-weight: 500;
+        padding: ${Props.size === 'normal' ? '12px 16px' : Props.size === 'small' ? '8px 12px' : '14px 28px'};
+        padding-left: ${Props.icon && '40px'}
+        font-size: ${Props.size === 'large'? '16px' : '14px' }
+        position: relative;
       }
     `}</style>
   </button>
-);
+)};
 
 export default Button;

98c54f6d6a350f72afcab5c733ba9b6392b618ea
Przemek Trzepinski <przemektrzepinski@Przemeks-MacBook-Pro.local>
2018-02-27 11:47:00 +0100

    created icon component

--- blkn/src/Button/index.js
+++ blkn/src/Button/index.js
@@ -7,31 +7,40 @@ type Props = {
   disabled?: boolean,
   secondary?: boolean,
   size?: string,
-  icon?: object
+  icon?: Function,
 };
 
-const Button = (Props) => {
-  console.log(Props)
-  return(
-  <button onClick={Props.onClick} disabled={Props.disabled}>
-      {Props.icon && <Props.icon fill={Props.secondary ? '#46515e' : 'white'} height={Props.size === 'normal'? '20px' : Props.size === 'small' ? '20px' : '26px'} width='20px' style={{position: 'absolute', top: '50%', left: '12px', transform: 'translateY(-50%)'}}/>} 
-      {Props.title}
-    <style jsx>{`
+const Button = (props: Props) => {
+  return (
+    <button onClick={props.onClick} disabled={props.disabled}>
+      {props.icon && (
+        <props.icon
+          fill={props.secondary ? "#46515e" : "white"}
+          height={props.size === "normal" ? "20px" : props.size === "small" ? "20px" : "26px"}
+          width="20px"
+          style={{ position: "absolute", top: "50%", left: "12px", transform: "translateY(-50%)" }}
+        />
+      )}
+      {props.title}
+      <style jsx>{`
       button {
-        opacity: ${Props.disabled ? '0.3' : '1'}
-        cursor: ${Props.disabled ? 'default' : 'pointer'}
-        background-color: ${Props.secondary ? '#e8edf1' : '#00a991'}
-        color: ${Props.secondary ? '#46515e' : 'white'}
+        opacity: ${props.disabled ? "0.3" : "1"}
+        cursor: ${props.disabled ? "default" : "pointer"}
+        background-color: ${props.secondary ? "#e8edf1" : "#00a991"}
+        color: ${props.secondary ? "#46515e" : "white"}
         border: none;
         border-radius: 3px;
         font-weight: 500;
-        padding: ${Props.size === 'normal' ? '12px 16px' : Props.size === 'small' ? '8px 12px' : '14px 28px'};
-        padding-left: ${Props.icon && '40px'}
-        font-size: ${Props.size === 'large'? '16px' : '14px' }
+        padding: ${
+          props.size === "normal" ? "12px 16px" : props.size === "small" ? "8px 12px" : "14px 28px"
+        };
+        padding-left: ${props.icon && "40px"}
+        font-size: ${props.size === "large" ? "16px" : "14px"}
         position: relative;
       }
     `}</style>
-  </button>
-)};
+    </button>
+  );
+};
 
 export default Button;

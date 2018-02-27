2043f69b1b58391d7795aeee76a659b7ea0ef56e
Przemek Trzepinski <przemektrzepinski@Przemeks-MacBook-Pro.local>
2018-02-27 14:57:46 +0100

    BLKN: added different states of button to storybook

--- blkn/stories/Button.js
+++ blkn/stories/Button.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import {Money} from '@kiwicom/icons';
+import { Money } from "@kiwicom/icons";
 
-import Button from "../src/Button";
+import Button from "../src/Button/";
 
 storiesOf("Button", module).add("normal primary", () => (
-  <Button title="Your Label" onClick={action("clicked")} size="normal"/>
+  <Button title="Your Label" onClick={action("clicked")} size="normal" />
 ));
 
 storiesOf("Button", module).add("normal primary disabled", () => (
   <Button title="Your Label" disabled={true} size="normal" />
-))
+));
 
 storiesOf("Button", module).add("small primary", () => (
   <Button title="Your Label" onClick={action("clicked")} size="small" />
@@ -19,7 +19,7 @@ storiesOf("Button", module).add("small primary", () => (
 
 storiesOf("Button", module).add("small primary disabled", () => (
   <Button title="Your Label" disabled={true} size="small" />
-))
+));
 
 storiesOf("Button", module).add("large primary", () => (
   <Button title="Your Label" onClick={action("clicked")} size="large" />
@@ -27,32 +27,42 @@ storiesOf("Button", module).add("large primary", () => (
 
 storiesOf("Button", module).add("large primary disabled", () => (
   <Button title="Your Label" disabled={true} size="large" />
-))
+));
 
 storiesOf("Button", module).add("normal secondary", () => (
-  <Button title="Your Label" onClick={action("clicked")} size="normal" secondary={true}/>
+  <Button title="Your Label" onClick={action("clicked")} size="normal" secondary={true} />
 ));
 
 storiesOf("Button", module).add("normal secondary disabled", () => (
-  <Button title="Your Label" disabled={true} size="normal" secondary={true}/>
-))
+  <Button title="Your Label" disabled={true} size="normal" secondary={true} />
+));
 
 storiesOf("Button", module).add("small secondary", () => (
-  <Button title="Your Label" onClick={action("clicked")} size="small" secondary={true}/>
+  <Button title="Your Label" onClick={action("clicked")} size="small" secondary={true} />
 ));
 
 storiesOf("Button", module).add("small secondary disabled", () => (
-  <Button title="Your Label" disabled={true} size="small" secondary={true}/>
-))
+  <Button title="Your Label" disabled={true} size="small" secondary={true} />
+));
 
 storiesOf("Button", module).add("large secondary", () => (
-  <Button title="Your Label" onClick={action("clicked")} size="large" secondary={true}/>
+  <Button title="Your Label" onClick={action("clicked")} size="large" secondary={true} />
 ));
 
 storiesOf("Button", module).add("large secondary disabled", () => (
-  <Button title="Your Label" disabled={true} size="large" secondary={true}/>
-))
+  <Button title="Your Label" disabled={true} size="large" secondary={true} />
+));
 
 storiesOf("Button", module).add("normal primary with icon", () => (
-  <Button title="Your Label" onClick={action("clicked")} size="normal" icon={Money}/>
+  <Button title="Your Label" onClick={action("clicked")} size="normal" icon={Money} />
+));
+
+storiesOf("Button", module).add("normal secondary with icon", () => (
+  <Button
+    title="Your Label"
+    onClick={action("clicked")}
+    size="normal"
+    secondary={true}
+    icon={Money}
+  />
 ));

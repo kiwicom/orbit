b32e5190003a1ae48956185d0ec5f55286fa921d
Simon Prochazka <simon@fallion.net>
2018-02-27 15:01:23 +0100

    BLKN: remove loader
    Current implementation breaks npm modules.

--- blkn/src/index.js
+++ blkn/src/index.js
@@ -5,4 +5,3 @@ export { default as InputText } from "./InputText";
 export { default as Typography } from "./Typography";
 export { default as Header } from "./Container/Header";
 export { default as Section } from "./Container/Section";
-export { default as Loader } from "./Loader";

bb7dc81ba5e0741f3459836a05f5fcfced6f0d08
Toni.U <antoni.peya@kiwi.com>
2018-02-27 12:08:07 +0100

    Added Select to src/index.js

--- blkn/src/index.js
+++ blkn/src/index.js
@@ -1,8 +1,10 @@
 // @flow
 
 export { default as Button } from "./Button";
-export { default as InputText } from "./InputText";
 export { default as Checkbox } from "./Checkbox";
-export { default as Typography } from "./Typography";
 export { default as Header } from "./Container/Header";
+export { default as InputText } from "./InputText";
+export { default as Loader } from "./Loader";
 export { default as Section } from "./Container/Section";
+export { default as Select } from "./Select";
+export { default as Typography } from "./Typography";

b6a5834b507e63cfa6577fc08ecfe6a07c99fffc
Przemek Trzepinski <przemektrzepinski@Przemeks-MacBook-Pro.local>
2018-02-27 14:56:48 +0100

    BLKN: added Icon export to srx/index.js

--- blkn/src/index.js
+++ blkn/src/index.js
@@ -1,6 +1,7 @@
 // @flow
 
 export { default as Button } from "./Button";
+export { default as Icon } from "./Button/Icon";
 export { default as InputText } from "./InputText";
 export { default as Typography } from "./Typography";
 export { default as Header } from "./Container/Header";

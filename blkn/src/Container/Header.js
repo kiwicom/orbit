f2d3200987c184e2b498992d65d5dd5642365b26
Vaclav Sir <vaclav.sir@gmail.com>
2018-02-28 18:58:16 +0100

    BLKN: Add icon to the Header

--- blkn/src/Container/Header.js
+++ blkn/src/Container/Header.js
@@ -4,13 +4,15 @@ import * as React from "react";
 import Typography from "../Typography";
 
 type Props = {
+  icon?: React.Node,
   title: string,
   description?: string,
 };
 
-const Header = ({ title, description }: Props) => (
+const Header = ({ icon, title, description }: Props) => (
   <div className="header">
     <h3>
+      {icon && <span className="icon">{icon}</span>}
       <Typography size="header">{title}</Typography>
     </h3>
     {description && <Typography>{description}</Typography>}
@@ -18,7 +20,12 @@ const Header = ({ title, description }: Props) => (
       .header {
         margin-bottom: 1em;
       }
+      .icon {
+        margin-right: 8px;
+      }
       h3 {
+        display: flex;
+        align-items: center;
         font-weight: 500;
         line-height: 1.18;
         margin-top: 0;

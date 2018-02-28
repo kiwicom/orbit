60e2ad6d5c36f3fac55051b180ef936b23cfbac0
Przemek Trzepinski <przemektrzepinski@Przemeks-MacBook-Pro.local>
2018-02-28 14:59:10 +0100

    deleted unnecessary constants

--- blkn/src/Button/Icon.js
+++ blkn/src/Button/Icon.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from "react";
+import * as constants from "../constants";
 
 type Props = {
   size: "normal" | "small" | "large",
@@ -7,20 +8,15 @@ type Props = {
   icon: Function,
 };
 
-const fills = {
-  primary: "#46515e",
-  secondary: "white",
-};
-const sizes = {
-  normal: "20px",
-  small: "20px",
-  large: "26px",
-};
 const Icon = (props: Props) => {
   const { icon, type } = props;
   return (
     <span>
-      <icon fill={fills[type]} height={sizes[props.size]} width={sizes[props.size]} />
+      <icon
+        fill={constants.iconColors[type]}
+        height={constants.iconSizes[props.size]}
+        width={constants.iconSizes[props.size]}
+      />
       <style xml>{`
         svg {
           position: absolute;

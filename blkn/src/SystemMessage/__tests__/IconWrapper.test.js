e326564df94a49083114beffa5a925652704256a
Przemek Trzepinski <przemektrzepinski@Przemeks-MacBook-Pro.local>
2018-03-01 12:49:05 +0100

    BLKN: build SystemMessage and helper components

---
+++ blkn/src/SystemMessage/__tests__/IconWrapper.test.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { shallow } from "enzyme";
+import IconWrapper from "../IconWrapper";
+
+describe("System Message", () => {
+  const component = shallow(<IconWrapper type="success" icon="Icon" />);
+
+  it("Should contain Icon", () => {
+    expect(component.find("Icon").exists()).toBe(true);
+  });
+});

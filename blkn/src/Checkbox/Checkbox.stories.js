6f2477dd5620647b297bed7f20877a2fd17db1e6
Filip Messa <filip.messa@kiwi.com>
2018-03-05 22:11:37 +0100

    BLKN: relocate components stories for the storybook.

---
+++ blkn/src/Checkbox/Checkbox.stories.js
@@ -0,0 +1,40 @@
+// @flow
+
+import React from "react";
+import { storiesOf, setAddon } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import chaptersAddon from "react-storybook-addon-chapters";
+import { withKnobs, text, boolean } from "@storybook/addon-knobs/react";
+import Checkbox from "./index";
+
+setAddon(chaptersAddon);
+
+const options = {
+  showSource: false,
+  allowSourceToggling: true,
+  showPropTables: false,
+  allowPropTablesToggling: false,
+};
+
+storiesOf("Checkbox", module)
+  .addDecorator(withKnobs)
+  .addWithChapters("default", () => {
+    const isChecked = boolean("isChecked", false);
+    const label = text("Label", "Text label");
+
+    return {
+      info: "You can change props in knobs",
+      chapters: [
+        {
+          sections: [
+            {
+              sectionFn: () => (
+                <Checkbox label={label} checked={isChecked} onChange={action("changed")} />
+              ),
+              options,
+            },
+          ],
+        },
+      ],
+    };
+  });
--- blkn/src/InputText/InputText.stories.js
+++ blkn/src/InputText/InputText.stories.js
@@ -1,15 +1,17 @@
+// @flow
+
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
-import InputText from "../src/InputText";
+import InputText from "./index";
 
 storiesOf("InputText", module)
   .add("default", () => <InputText value="" onChange={action("onChange")} />)
   .add("label", () => (
     <InputText label="Text label" value="input value" onChange={action("onChange")} />
   ))
-  .add("empty", () => <InputText label="Text label" onChange={action("onChange")} />)
+  .add("empty", () => <InputText label="Text label" onChange={action("onChange")} value="" />)
   .add("error", () => (
     <InputText
       label="Text label"

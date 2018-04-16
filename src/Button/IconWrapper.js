e305213c593f141ff4866a80340a7460432e8a5f
william.kolmacka <william.kolmacka@kiwi.com>
2018-04-16 16:36:54 +0200

    Add new button component

--- src/Button/IconWrapper.js
+++ src/Button/IconWrapper.js
@@ -1,41 +1,56 @@
 // @flow
-
 import * as React from "react";
+import * as tokens from "orbit-design-token";
 
-import { colors } from "../constants";
-
-const sizes = {
-  normal: "20px",
-  small: "20px",
-  large: "26px",
+const sizeIcon = {
+  small: tokens.heightIconSmall,
+  normal: tokens.heightIconMedium,
+  large: tokens.heightIconMedium,
 };
 
-const fillColors = {
-  primary: colors.white.normal,
-  secondary: colors.ink.normal,
+const marginRight = {
+  small: tokens.marginButtonIconSmall,
+  normal: tokens.marginButtonIconNormal,
+  large: tokens.marginButtonIconLarge,
+};
+
+const colorIcon = {
+  primary: tokens.colorIconButtonPrimary,
+  secondary: tokens.colorIconButtonSecondary,
+  link: tokens.colorIconButtonLink,
+  facebook: tokens.colorIconButtonFacebook,
+  google: tokens.colorIconButtonGoogle,
+  destructive: tokens.colorIconButtonDestructive,
+};
+
+const colorIconBordered = {
+  primary: tokens.colorIconButtonPrimaryBordered,
+  secondary: tokens.colorIconButtonSecondaryBordered,
+  link: tokens.colorIconButtonLinkBordered,
+  facebook: tokens.colorIconButtonFacebookBordered,
+  google: tokens.colorIconButtonGoogleBordered,
+  destructive: tokens.colorIconButtonDestructiveBordered,
 };
 
 type Props = {
-  size: $Keys<typeof sizes>,
-  type: $Keys<typeof fillColors>,
+  bordered?: boolean,
+  size: $Keys<typeof sizeIcon>,
+  type: $Keys<typeof colorIcon> | $Keys<typeof colorIconBordered>,
   Icon: React.ComponentType<*>,
 };
 
 const IconWrapper = (props: Props) => {
-  const { Icon, type, size } = props;
+  const { Icon, type, size, bordered } = props;
 
   return (
-    <span>
-      <Icon fill={fillColors[type]} height={sizes[size]} />
-      <style jsx>{`
-        span {
-          position: absolute;
-          top: 50%;
-          left: 12px;
-          transform: translateY(-50%);
+    <React.Fragment>
+      <Icon color={bordered ? colorIconBordered[type] : colorIcon[type]} size={sizeIcon[size]} />
+      <style xml>{`
+        svg {
+          margin-right: ${marginRight[props.size]};
         }
       `}</style>
-    </span>
+    </React.Fragment>
   );
 };
 

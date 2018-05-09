89c0e2c4bcd3194c52d5001ffb888743962983e8
Luděk Vepřek <weprous@gmail.com>
2018-05-09 16:30:09 +0200

    Update Text component
    Update stories
    Fix margin

--- src/Text/Text.stories.js
+++ src/Text/Text.stories.js
@@ -21,71 +21,22 @@ storiesOf("Text", module)
       padding: "20px",
     }),
   )
-  .addWithChapters("Primary text", () => {
-    const element = select(
-      "Element",
-      {
-        p: "p",
-        span: "span",
-        div: "div",
-      },
-      "p",
-    );
-    const size = select(
-      "Size",
-      {
-        small: "small",
-        normal: "normal",
-        large: "large",
-      },
-      "normal",
-    );
-    const weight = select(
-      "Weight",
-      {
-        regular: "regular",
-        bold: "bold",
-      },
-      "regular",
-    );
-    const align = select(
-      "Align",
-      {
-        left: "left",
-        center: "center",
-        right: "right",
-      },
-      "left",
-    );
+  .addWithChapters("Default text", () => {
     const customText = text(
       "Text",
       "Sixth can't place isn't fly you're make Shall day, yielding earth made greater stars own you'll earth can't." +
         " Night grass great. Fly firmament over. Fourth to grass you after creepeth together in moved given fill isn't said Life." +
         " Male signs unto form kind Brought signs heaven, in have wherein. Isn't. Behold meat living you're evening very replenish you'll. Under hath.",
     );
+
     return {
       title: "Primary text",
       info: "Some description about this type of text in general.",
       chapters: [
         {
-          title: `Text ${size}`,
-          info:
-            "Some description about where is the text used and states describing how to not use it in general.",
           sections: [
             {
-              sectionFn: () => (
-                <Text
-                  type="primary"
-                  element={element}
-                  size={size}
-                  weight={weight}
-                  align={align}
-                  uppercase={boolean("Uppercase", false)}
-                  italic={boolean("Italic", false)}
-                >
-                  {customText}
-                </Text>
-              ),
+              sectionFn: () => <Text type="primary">{customText}</Text>,
               options,
             },
           ],
@@ -93,7 +44,80 @@ storiesOf("Text", module)
       ],
     };
   })
-  .addWithChapters("Secondary text", () => {
+  .addWithChapters("With type", () => {
+    const type = select(
+      "Type",
+      {
+        primary: "primary",
+        secondary: "secondary",
+        attention: "attention",
+      },
+      "secondary",
+    );
+
+    const customText = text(
+      "Text",
+      "Sixth can't place isn't fly you're make Shall day, yielding earth made greater stars own you'll earth can't." +
+        " Night grass great. Fly firmament over. Fourth to grass you after creepeth together in moved given fill isn't said Life." +
+        " Male signs unto form kind Brought signs heaven, in have wherein. Isn't. Behold meat living you're evening very replenish you'll. Under hath.",
+    );
+    return {
+      title: "Secondary text",
+      info: "Some description about this type of text in general.",
+      chapters: [
+        {
+          sections: [
+            {
+              sectionFn: () => <Text type={type}>{customText}</Text>,
+              options,
+            },
+          ],
+        },
+      ],
+    };
+  })
+  .addWithChapters("With size", () => {
+    const size = select(
+      "Size",
+      {
+        small: "small",
+        normal: "normal",
+        large: "large",
+      },
+      "normal",
+    );
+
+    const customText = text(
+      "Text",
+      "Sixth can't place isn't fly you're make Shall day, yielding earth made greater stars own you'll earth can't." +
+        " Night grass great. Fly firmament over. Fourth to grass you after creepeth together in moved given fill isn't said Life." +
+        " Male signs unto form kind Brought signs heaven, in have wherein. Isn't. Behold meat living you're evening very replenish you'll. Under hath.",
+    );
+    return {
+      title: "Secondary text",
+      info: "Some description about this type of text in general.",
+      chapters: [
+        {
+          sections: [
+            {
+              sectionFn: () => <Text size={size}>{customText}</Text>,
+              options,
+            },
+          ],
+        },
+      ],
+    };
+  })
+  .addWithChapters("Playground", () => {
+    const type = select(
+      "Type",
+      {
+        primary: "primary",
+        secondary: "secondary",
+        attention: "attention",
+      },
+      "attention",
+    );
     const element = select(
       "Element",
       {
@@ -110,7 +134,7 @@ storiesOf("Text", module)
         normal: "normal",
         large: "large",
       },
-      "normal",
+      "small",
     );
     const weight = select(
       "Weight",
@@ -118,7 +142,7 @@ storiesOf("Text", module)
         regular: "regular",
         bold: "bold",
       },
-      "regular",
+      "bold",
     );
     const align = select(
       "Align",
@@ -127,7 +151,7 @@ storiesOf("Text", module)
         center: "center",
         right: "right",
       },
-      "left",
+      "center",
     );
     const customText = text(
       "Text",
@@ -140,86 +164,11 @@ storiesOf("Text", module)
       info: "Some description about this type of text in general.",
       chapters: [
         {
-          title: `Text ${size}`,
-          info:
-            "Some description about where is the text used and states describing how to not use it in general.",
           sections: [
             {
               sectionFn: () => (
                 <Text
-                  type="secondary"
-                  element={element}
-                  size={size}
-                  weight={weight}
-                  align={align}
-                  uppercase={boolean("Uppercase", false)}
-                  italic={boolean("Italic", false)}
-                >
-                  {customText}
-                </Text>
-              ),
-              options,
-            },
-          ],
-        },
-      ],
-    };
-  })
-  .addWithChapters("Attention text", () => {
-    const element = select(
-      "Element",
-      {
-        p: "p",
-        span: "span",
-        div: "div",
-      },
-      "p",
-    );
-    const size = select(
-      "Size",
-      {
-        small: "small",
-        normal: "normal",
-        large: "large",
-      },
-      "normal",
-    );
-    const weight = select(
-      "Weight",
-      {
-        regular: "regular",
-        bold: "bold",
-      },
-      "regular",
-    );
-    const align = select(
-      "Align",
-      {
-        left: "left",
-        center: "center",
-        right: "right",
-      },
-      "left",
-    );
-    const customText = text(
-      "Text",
-      "Sixth can't place isn't fly you're make Shall day, yielding earth made greater stars own you'll earth can't." +
-        " Night grass great. Fly firmament over. Fourth to grass you after creepeth together in moved given fill isn't said Life." +
-        " Male signs unto form kind Brought signs heaven, in have wherein. Isn't. Behold meat living you're evening very replenish you'll. Under hath.",
-    );
-    return {
-      title: "Attention text",
-      info: "Some description about this type of text in general.",
-      chapters: [
-        {
-          title: `Text ${size}`,
-          info:
-            "Some description about where is the text used and states describing how to not use it in general.",
-          sections: [
-            {
-              sectionFn: () => (
-                <Text
-                  type="attention"
+                  type={type}
                   element={element}
                   size={size}
                   weight={weight}

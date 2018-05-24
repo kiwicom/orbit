74629408e5bbf0b4da9b6e2c8d7072c77e3f0c4f
william.kolmacka <william.kolmacka@kiwi.com>
2018-05-24 16:23:31 +0200

    Update TextLink support Theming
    Added support for theming
    Rewrited to styled components
    Changed prop url to href
    Updated tests
    Added flow strict type
    Added constants for type prop

--- src/TextLink/index.js
+++ src/TextLink/index.js
@@ -1,63 +1,6 @@
 // @flow
-import * as React from "react";
-import * as tokens from "@kiwicom/orbit-design-tokens";
+import { withTheme } from "theming";
 
-const colorTextLink = {
-  primary: tokens.colorLinkPrimary,
-  secondary: tokens.colorLinkSecondary,
-};
-const fontSizeTextLink = {
-  small: tokens.fontSizeTextSmall,
-  normal: tokens.fontSizeTextNormal,
-  large: tokens.fontSizeTextLarge,
-};
+import TextLink from "./TextLink";
 
-type Props = {
-  children: React.Node,
-  url: string,
-  onClick: (SyntheticEvent<HTMLLinkElement>) => any,
-  newTab: boolean,
-  type: $Keys<typeof colorTextLink>,
-  size: $Keys<typeof fontSizeTextLink>,
-};
-
-const TextLink = (props: Props) => {
-  const { children, url, onClick, newTab, type, size } = props;
-
-  return (
-    <a href={url} target={newTab ? "_blank" : undefined} onClick={onClick}>
-      {children}
-      <style jsx>{`
-        a {
-          font-size: ${fontSizeTextLink[size]};
-          color: ${colorTextLink[type]};
-          font-weight: ${tokens.fontWeightLinks};
-          text-decoration: ${type === "secondary"
-            ? tokens.textDecorationLinkSecondary
-            : tokens.textDecorationLinkPrimary};
-          cursor: pointer;
-        }
-        a:hover {
-          text-decoration: ${type === "secondary"
-            ? tokens.textDecorationLinkSecondaryHover
-            : tokens.textDecorationLinkPrimaryHover};
-          color: ${tokens.colorLinkPrimaryHover};
-        }
-        a:visited {
-          //nothing needs to be here, but this state will be used in the future probably
-        }
-        a:focus {
-          outline-width: 3px;
-        }
-      `}</style>
-    </a>
-  );
-};
-
-TextLink.defaultProps = {
-  type: "primary",
-  size: "normal",
-  newTab: false,
-};
-
-export default TextLink;
+export default withTheme(TextLink);

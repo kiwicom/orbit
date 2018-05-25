0372c4b3dd1d5638548e83870c331f8e0b67e9e8
william.kolmacka <william.kolmacka@kiwi.com>
2018-05-25 12:04:20 +0200

    Support Theming in CarrierLogo

---
+++ src/CarrierLogo/consts.js
@@ -0,0 +1,15 @@
+// @flow
+
+export const SIZE_OPTIONS = {
+  SMALL: "small",
+  MEDIUM: "medium",
+  LARGE: "large",
+};
+
+export const CARRIER_TYPE_OPTIONS = {
+  AIRLINE: "airline",
+  BUS: "bus",
+  TRAIN: "train",
+};
+
+export const baseURL = "//images.kiwi.com";

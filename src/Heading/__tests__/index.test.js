118bc651d87d0273e41720ffec757b03d79feaac
Luděk Vepřek <weprous@gmail.com>
2018-06-14 19:20:31 +0200

    Separate withTheme from Heading component
    Separated withTheme from Heading component.
    Deleted ThemeProvider and unnecessary options from stories.
    Updated test.
    Added constants for element and type options.
    Added flow strict types.

--- src/Heading/__tests__/index.test.js
+++ src/Heading/__tests__/index.test.js
@@ -2,25 +2,20 @@
 import * as React from "react";
 import { shallow } from "enzyme";
 
-import Heading from "../index";
-import ThemeProvider from "../../Theming/ThemeProvider";
+import Heading from "../Heading";
 
 const children = "Children title";
-const component = shallow(
-  <ThemeProvider>
-    <Heading>{children}</Heading>
-  </ThemeProvider>,
-);
 
 describe("Heading with defaultProps", () => {
-  // it("should contain children", () => {
-  //   expect(
-  //     component
-  //       .find("Heading__StyledHeading")
-  //       .render()
-  //       .text(),
-  //   ).toBe(children);
-  // });
+  const component = shallow(<Heading>{children}</Heading>);
+  it("should contain children", () => {
+    expect(
+      component
+        .find("Heading__StyledHeading")
+        .render()
+        .text(),
+    ).toBe(children);
+  });
   it("should match snapshot", () => {
     expect(component).toMatchSnapshot();
   });

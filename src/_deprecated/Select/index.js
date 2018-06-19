716d0aca5fe00621168930c699dcc8fad86f2d19
william.kolmacka <william.kolmacka@kiwi.com>
2018-06-19 09:41:10 +0200

    NEW: Select

--- src/_deprecated/Select/index.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import Select from "./Select";
-import DeprecatedComponent from "../DeprecatedComponent";
-
-export default DeprecatedComponent(
-  Select,
-  "Select is deprecated and will be removed in next major release",
-);

75a3ea7d261d70a24a243b6e6583faaef072a7e5
Luděk Vepřek <weprous@gmail.com>
2018-07-03 15:53:25 +0200

    NEW: ServiceLogo component

---
+++ src/ServiceLogo/ServiceLogo.stories.js
@@ -0,0 +1,39 @@
+// @flow
+
+import * as React from "react";
+import { storiesOf, setAddon } from "@storybook/react";
+import styles from "@sambego/storybook-styles";
+import chaptersAddon from "react-storybook-addon-chapters";
+import { withKnobs, select, boolean } from "@storybook/addon-knobs/react";
+
+import { NAME_OPTIONS, SIZE_OPTIONS } from "./consts";
+
+import ServiceLogo from "./index";
+
+setAddon(chaptersAddon);
+
+storiesOf("ServiceLogo", module)
+  .addDecorator(withKnobs)
+  .addDecorator(
+    styles({
+      padding: "20px",
+    }),
+  )
+  .addWithChapters("Playground", () => {
+    const name = select("Type", Object.values(NAME_OPTIONS), NAME_OPTIONS.AIRHELP);
+    const size = select("Size", Object.values(SIZE_OPTIONS), SIZE_OPTIONS.MEDIUM);
+    const grayScale = boolean("GrayScale", false);
+
+    return {
+      info: "Some description about this type of component.",
+      chapters: [
+        {
+          sections: [
+            {
+              sectionFn: () => <ServiceLogo name={name} size={size} grayScale={grayScale} />,
+            },
+          ],
+        },
+      ],
+    };
+  });

8e082464288b30755d75668a5e8073628a19c1b5
Tomáš Hapl <tomas.hapl@gmail.com>
2018-10-01 12:26:37 +0200

    FIX: Illustration ThemeProvider

--- src/Illustration/__tests__/index.test.js
+++ src/Illustration/__tests__/index.test.js
@@ -9,15 +9,22 @@ const size = SIZE_OPTIONS.SMALL;
 const name = "Accommodation";
 const dataTest = "test";
 
+const URL = "//images.kiwi.com/illustrations/0x90/Accommodation.png";
+const URL_RETINA = "//images.kiwi.com/illustrations/0x180/Accommodation.png 2x";
+
 describe(`Illustration of ${name}`, () => {
   const component = shallow(<Illustration size={size} name={name} dataTest={dataTest} />);
   it("should have passed props", () => {
     expect(component.prop("size")).toBe(size);
-    expect(component.prop("src")).toContain(name);
-    expect(component.prop("srcSet")).toContain(name);
-    expect(component.prop("alt")).toBe(name);
+    expect(component.render().prop("alt")).toBe(name);
     expect(component.render().prop("data-test")).toBe(dataTest);
   });
+
+  it("should render proper image", () => {
+    expect(component.render().attr("src")).toContain(URL);
+    expect(component.render().attr("srcset")).toContain(URL_RETINA);
+  });
+
   it("should match snapshot", () => {
     expect(component).toMatchSnapshot();
   });

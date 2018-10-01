f653b6886f732badd3ece7acb9cc447a2322d3d4
Tomáš Hapl <tomas.hapl@gmail.com>
2018-10-01 15:03:41 +0200

    FIX: InputField ThemeProvider

--- src/InputField/index.js
+++ src/InputField/index.js
@@ -3,12 +3,35 @@ import * as React from "react";
 import styled from "styled-components";
 
 import defaultTokens from "../defaultTokens";
-import { SIZE_OPTIONS, TYPE_OPTIONS } from "./consts";
+import { SIZE_OPTIONS, TYPE_OPTIONS, TOKENS } from "./consts";
 import FormFeedback from "../FormFeedback";
 import DefaultFormLabel from "../FormLabel";
 import { StyledServiceLogo } from "../ServiceLogo";
 
 import type { Props } from "./index";
+
+const getToken = name => ({ theme, size }) => {
+  const tokens = {
+    [TOKENS.heightInput]: {
+      [SIZE_OPTIONS.SMALL]: theme.orbit.heightInputSmall,
+      [SIZE_OPTIONS.NORMAL]: theme.orbit.heightInputNormal,
+    },
+    [TOKENS.fontSizeInput]: {
+      [SIZE_OPTIONS.SMALL]: theme.orbit.fontSizeInputSmall,
+      [SIZE_OPTIONS.NORMAL]: theme.orbit.fontSizeInputNormal,
+    },
+    [TOKENS.paddingInput]: {
+      [SIZE_OPTIONS.SMALL]: theme.orbit.paddingInputSmall,
+      [SIZE_OPTIONS.NORMAL]: theme.orbit.paddingInputNormal,
+    },
+    [TOKENS.iconSize]: {
+      [SIZE_OPTIONS.SMALL]: theme.orbit.widthIconSmall,
+      [SIZE_OPTIONS.NORMAL]: theme.orbit.widthIconMedium,
+    },
+  };
+
+  return tokens[name][size];
+};
 
 const Field = styled.label`
   font-family: ${({ theme }) => theme.orbit.fontFamily};
@@ -28,7 +51,7 @@ export const FakeInput = styled(({ children, className }) => (
   position: absolute;
   z-index: 1;
   box-sizing: border-box;
-  height: ${({ tokens, size }) => tokens.heightInput[size]};
+  height: ${getToken(TOKENS.heightInput)};
   border-radius: ${({ theme }) => theme.orbit.borderRadiusNormal};
   box-shadow: inset 0 0 0
     ${({ theme, error }) =>
@@ -37,7 +60,7 @@ export const FakeInput = styled(({ children, className }) => (
       }`};
   background-color: ${({ disabled, theme }) =>
     disabled ? theme.orbit.backgroundInputDisabled : theme.orbit.backgroundInput};
-  font-size: ${({ size, tokens }) => tokens.fontSizeInput[size]};
+  font-size: ${getToken(TOKENS.fontSizeInput)};
   transition: all ${({ theme }) => theme.orbit.durationFast} ease-in-out;
 `;
 
@@ -55,11 +78,11 @@ export const InputContainer = styled(({ children, className }) => (
   justify-content: space-between;
   box-sizing: border-box;
   width: 100%;
-  height: ${({ tokens, size }) => tokens.heightInput[size]};
+  height: ${getToken(TOKENS.heightInput)};
   border-radius: ${({ theme }) => theme.orbit.borderRadiusNormal};
   color: ${({ disabled, theme }) =>
     disabled ? theme.orbit.colorTextInputDisabled : theme.orbit.colorTextInput};
-  font-size: ${({ size, tokens }) => tokens.fontSizeInput[size]};
+  font-size: ${getToken(TOKENS.fontSizeInput)};
   cursor: ${({ disabled }) => (disabled ? "not-allowed" : "text")};
 
   &:hover > ${FakeInput} {
@@ -85,7 +108,7 @@ const StyledInlineLabel = styled.div`
 
   ${DefaultFormLabel} {
     margin-bottom: 0;
-    font-size: ${({ size, tokens }) => tokens.fontSizeInput[size]};
+    font-size: ${getToken(TOKENS.fontSizeInput)};
     line-height: normal;
     z-index: 3;
     white-space: nowrap;
@@ -107,8 +130,8 @@ const Prefix = styled(({ children, className }) => <div className={className}>{c
   z-index: 3;
 
   & > svg {
-    width: ${({ tokens, size }) => tokens.iconSize[size]};
-    height: ${({ tokens, size }) => tokens.iconSize[size]};
+    width: ${getToken(TOKENS.iconSize)};
+    height: ${getToken(TOKENS.iconSize)};
     color: ${({ theme }) => theme.orbit.colorIconInput};
   }
 `;
@@ -118,7 +141,7 @@ Prefix.defaultProps = {
 };
 
 const Suffix = styled(({ children, className }) => <div className={className}>{children}</div>)`
-  height: ${({ tokens, size }) => tokens.heightInput[size]};
+  height: ${getToken(TOKENS.heightInput)};
   color: ${({ theme }) => theme.orbit.colorIconInput};
   display: flex;
   align-items: center;
@@ -139,13 +162,13 @@ Suffix.defaultProps = {
   theme: defaultTokens,
 };
 
-export const Input = styled(({ theme, tokens, size, suffix, error, help, ...props }) => (
+export const Input = styled(({ theme, size, suffix, error, help, ...props }) => (
   <input {...props} />
 ))`
   appearance: none;
   font-family: inherit;
   border: none;
-  padding: 0 0 ${({ tokens, size }) => tokens.paddingInput[size]};
+  padding: 0 0 ${getToken(TOKENS.paddingInput)};
   font-size: inherit;
   color: inherit;
   background-color: inherit;
@@ -153,7 +176,7 @@ export const Input = styled(({ theme, tokens, size, suffix, error, help, ...prop
   flex: 1 auto;
   width: 100%;
   height: 100%;
-  line-height: ${({ tokens, size }) => tokens.heightInput[size]};
+  line-height: ${getToken(TOKENS.heightInput)};
   border-radius: ${({ theme }) => theme.orbit.borderRadiusNormal};
   z-index: 2;
 
@@ -201,79 +224,60 @@ const FormLabel = ({
   </DefaultFormLabel>
 );
 
-const InputField = (props: Props) => {
-  const {
-    size = SIZE_OPTIONS.NORMAL,
-    type = TYPE_OPTIONS.TEXT,
-    theme = defaultTokens,
-    dataTest,
-    required,
-  } = props;
-
-  const tokens = {
-    heightInput: {
-      [SIZE_OPTIONS.SMALL]: theme.orbit.heightInputSmall,
-      [SIZE_OPTIONS.NORMAL]: theme.orbit.heightInputNormal,
-    },
-    fontSizeInput: {
-      [SIZE_OPTIONS.SMALL]: theme.orbit.fontSizeInputSmall,
-      [SIZE_OPTIONS.NORMAL]: theme.orbit.fontSizeInputNormal,
-    },
-    paddingInput: {
-      [SIZE_OPTIONS.SMALL]: theme.orbit.paddingInputSmall,
-      [SIZE_OPTIONS.NORMAL]: theme.orbit.paddingInputNormal,
-    },
-    iconSize: {
-      [SIZE_OPTIONS.SMALL]: theme.orbit.widthIconSmall,
-      [SIZE_OPTIONS.NORMAL]: theme.orbit.widthIconMedium,
-    },
-  };
-
-  return (
-    <Field data-test={dataTest}>
-      {props.label &&
-        !props.inlineLabel && (
-          <FormLabel label={props.label} isFilled={!!props.value} required={required} />
-        )}
-      <InputContainer tokens={tokens} size={size} disabled={props.disabled} error={props.error}>
-        {props.prefix && (
-          <Prefix tokens={tokens} size={size}>
-            {props.prefix}
-          </Prefix>
-        )}
-        {props.inlineLabel && (
-          <StyledInlineLabel tokens={tokens} size={size}>
-            <FormLabel label={props.label} isFilled={!!props.value} required={required} />
-          </StyledInlineLabel>
-        )}
-        <Input
-          onChange={props.onChange}
-          onFocus={props.onFocus}
-          onBlur={props.onBlur}
-          name={props.name}
-          type={type}
-          value={props.value}
-          placeholder={props.placeholder}
-          disabled={props.disabled}
-          min={props.minValue}
-          max={props.maxValue}
-          minLength={props.minLength}
-          maxLength={props.maxLength}
-          size={size}
-          tokens={tokens}
-          error={props.error}
-        />
-        {props.suffix && (
-          <Suffix tokens={tokens} size={size}>
-            {props.suffix}
-          </Suffix>
-        )}
-        <FakeInput tokens={tokens} size={size} disabled={props.disabled} error={props.error} />
-      </InputContainer>
-      {props.help && !props.error && <FormFeedback type="help">{props.help}</FormFeedback>}
-      {props.error && <FormFeedback type="error">{props.error}</FormFeedback>}
-    </Field>
-  );
-};
+const InputField = ({
+  disabled,
+  size = SIZE_OPTIONS.NORMAL,
+  type = TYPE_OPTIONS.TEXT,
+  label,
+  inlineLabel,
+  dataTest,
+  required,
+  error,
+  name,
+  prefix,
+  onChange,
+  onFocus,
+  onBlur,
+  placeholder,
+  minValue,
+  maxValue,
+  minLength,
+  maxLength,
+  suffix,
+  help,
+  value,
+}: Props) => (
+  <Field data-test={dataTest}>
+    {label && !inlineLabel && <FormLabel label={label} isFilled={!!value} required={required} />}
+    <InputContainer size={size} disabled={disabled} error={error}>
+      {prefix && <Prefix size={size}>{prefix}</Prefix>}
+      {inlineLabel && (
+        <StyledInlineLabel size={size}>
+          <FormLabel label={label} isFilled={!!value} required={required} />
+        </StyledInlineLabel>
+      )}
+      <Input
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        name={name}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        disabled={disabled}
+        min={minValue}
+        max={maxValue}
+        minLength={minLength}
+        maxLength={maxLength}
+        size={size}
+        error={error}
+      />
+      {suffix && <Suffix size={size}>{suffix}</Suffix>}
+      <FakeInput size={size} disabled={disabled} error={error} />
+    </InputContainer>
+    {help && !error && <FormFeedback type="help">{help}</FormFeedback>}
+    {error && <FormFeedback type="error">{error}</FormFeedback>}
+  </Field>
+);
 
 export default InputField;

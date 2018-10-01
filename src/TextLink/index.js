b97d1875e70eaf8cfab3699739c4538ff31cc684
Tomáš Hapl <tomas.hapl@gmail.com>
2018-10-01 15:08:58 +0200

    FIX: TextLink ThemeProvider

--- src/TextLink/index.js
+++ src/TextLink/index.js
@@ -7,10 +7,19 @@ import TYPE_OPTIONS from "./consts";
 
 import type { Props } from "./index";
 
+const getColor = ({ theme, type }) => {
+  const tokens = {
+    [TYPE_OPTIONS.PRIMARY]: theme.orbit.colorTextLinkPrimary,
+    [TYPE_OPTIONS.SECONDARY]: theme.orbit.colorTextLinkSecondary,
+  };
+
+  return tokens[type];
+};
+
 const IconContainer = styled(({ children, className }) => (
   <div className={className}>{children}</div>
 ))`
-  color: ${({ tokens, type }) => tokens.colorTextLink[type]};
+  color: ${getColor};
   transition: color ${({ theme }) => theme.orbit.durationFast} ease-in-out;
 
   & svg {
@@ -23,84 +32,66 @@ IconContainer.defaultProps = {
   theme: defaultTokens,
 };
 
-export const StyledTextLink = styled(({ tokens, theme, type, ...props }) => (
+export const StyledTextLink = styled(({ theme, type, ...props }) => (
   <a {...props}>{props.children}</a>
 ))`
-      color: ${({ tokens, type }) => tokens.colorTextLink[type]};
-      font-weight: ${({ theme }) => theme.orbit.fontWeightLinks}; 
-      text-decoration: ${({ theme, type }) =>
-        type === [TYPE_OPTIONS.SECONDARY]
-          ? theme.orbit.textDecorationTextLinkSecondary
-          : theme.orbit.textDecorationTextLinkPrimary};
-      cursor: pointer;
-      display: inline-flex;
-      align-items: center;
-      transition: color ${({ theme }) => theme.orbit.durationFast} ease-in-out;
-    }
-    
-    &:hover {
-      text-decoration: ${({ theme, type }) =>
-        type === [TYPE_OPTIONS.SECONDARY]
-          ? theme.orbit.textDecorationTextLinkSecondaryHover
-          : theme.orbit.textDecorationTextLinkPrimaryHover};
+  color: ${getColor};
+  font-weight: ${({ theme }) => theme.orbit.fontWeightLinks};
+  text-decoration: ${({ theme, type }) =>
+    type === [TYPE_OPTIONS.SECONDARY]
+      ? theme.orbit.textDecorationTextLinkSecondary
+      : theme.orbit.textDecorationTextLinkPrimary};
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  transition: color ${({ theme }) => theme.orbit.durationFast} ease-in-out;
+
+  &:hover {
+    text-decoration: ${({ theme, type }) =>
+      type === [TYPE_OPTIONS.SECONDARY]
+        ? theme.orbit.textDecorationTextLinkSecondaryHover
+        : theme.orbit.textDecorationTextLinkPrimaryHover};
+    color: ${({ theme, type }) =>
+      type === [TYPE_OPTIONS.SECONDARY]
+        ? theme.orbit.colorTextLinkSecondaryHover
+        : theme.orbit.colorTextLinkPrimaryHover};
+
+    ${IconContainer} {
       color: ${({ theme, type }) =>
         type === [TYPE_OPTIONS.SECONDARY]
           ? theme.orbit.colorTextLinkSecondaryHover
           : theme.orbit.colorTextLinkPrimaryHover};
-      
-      ${IconContainer} {
-        color: ${({ theme, type }) =>
-          type === [TYPE_OPTIONS.SECONDARY]
-            ? theme.orbit.colorTextLinkSecondaryHover
-            : theme.orbit.colorTextLinkPrimaryHover};
-      }
     }
-    &:focus {
-      outline-width: 3px;
-    }        
-  `;
+  }
+  &:focus {
+    outline-width: 3px;
+  }
+`;
 
 StyledTextLink.defaultProps = {
   theme: defaultTokens,
 };
 
-const TextLink = (props: Props) => {
-  const {
-    type = TYPE_OPTIONS.PRIMARY,
-    children,
-    href,
-    external = false,
-    theme = defaultTokens,
-    rel,
-    icon,
-    onClick,
-    dataTest,
-  } = props;
-  const tokens = {
-    colorTextLink: {
-      [TYPE_OPTIONS.PRIMARY]: theme.orbit.colorTextLinkPrimary,
-      [TYPE_OPTIONS.SECONDARY]: theme.orbit.colorTextLinkSecondary,
-    },
-  };
-
-  return (
-    <StyledTextLink
-      type={type}
-      href={href}
-      target={external ? "_blank" : undefined}
-      rel={rel}
-      tokens={tokens}
-      onClick={onClick}
-      data-test={dataTest}
-    >
-      {children}
-      {icon && (
-        <IconContainer type={type} tokens={tokens}>
-          {icon}
-        </IconContainer>
-      )}
-    </StyledTextLink>
-  );
-};
-
+const TextLink = ({
+  type = TYPE_OPTIONS.PRIMARY,
+  children,
+  href,
+  external = false,
+  rel,
+  icon,
+  onClick,
+  dataTest,
+}: Props) => (
+  <StyledTextLink
+    type={type}
+    href={href}
+    target={external ? "_blank" : undefined}
+    rel={rel}
+    onClick={onClick}
+    data-test={dataTest}
+  >
+    {children}
+    {icon && <IconContainer type={type}>{icon}</IconContainer>}
+  </StyledTextLink>
+);
 export default TextLink;

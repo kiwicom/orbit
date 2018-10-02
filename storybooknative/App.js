f1545c02285a6fb150ca268f428b5cbf74badaa4
Robin Cussol <me@robincussol.com>
2018-10-02 15:25:41 +0200

    Basic storybook setup for react-native

---
+++ storybooknative/App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import * as React from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+const instructions = Platform.select({
+  ios: "Press Cmd+R to reload,\nCmd+D or shake for dev menu",
+  android: "Double tap R on your keyboard to reload,\nShake or press menu button for dev menu",
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5,
+  },
+});
+
+const App = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>Welcome to React Native!</Text>
+    <Text style={styles.instructions}>To get started, edit App.js</Text>
+    <Text style={styles.instructions}>{instructions}</Text>
+  </View>
+);
+
+export default App;

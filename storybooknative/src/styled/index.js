877ab5a4c3d7df76fb9f2e57ff47b6b840a3fbfd
Trond Bergquist <trond_bergquist@hotmail.com>
2018-10-08 09:04:38 +0200

    Wrap styled and use flow to validate styles passed to styled

---
+++ storybooknative/src/styled/index.js
@@ -0,0 +1,21 @@
+// @flow
+
+import styled from "styled-components";
+
+import type { StylePropType } from "../types/StylesPropType";
+
+/**
+ * Now we can use styled like:
+ * const Pinky = styled(View, {
+ *  backgroundColor: 'pink'
+ * });
+ *
+ * const ColoredView = styled(View, (props) => ({ backgroundColor: props.color}))
+ *
+ * and we get flow validating our styles
+ */
+
+export default (
+  Component: React.Node,
+  styles: StylePropType | ((props: Object) => StylePropType),
+) => styled(Component)([], styles);

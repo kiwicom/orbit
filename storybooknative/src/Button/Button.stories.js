359c4a8b6f67c4a1c8ae969b3aa370ad899fb37d
denisbalogh <denis.balogh@kiwi.com>
2018-10-10 15:33:51 +0200

    Add: react-native Button component Fix: Weird error from Babel/Flow interaction cf. https://github.com/babel/babel/issues/8417#issuecomment-430007587

--- storybooknative/src/Button/Button.stories.js
+++ storybooknative/src/Button/Button.stories.js
@@ -3,22 +3,48 @@
 /* eslint-disable jsx-a11y/accessible-emoji, import/no-unresolved, import/extensions */
 
 import React from "react";
-import { Text } from "react-native";
 import { action } from "@storybook/addon-actions";
+import { number, boolean, select, text, withKnobs } from "@storybook/addon-knobs";
 
 import { storiesOf } from "../../helpers/storiesOf";
 import Button from "../../src/Button";
 import CenterView from "../../src/CenterView";
+import { SIZE_OPTIONS, TYPE_OPTIONS } from "./consts";
 
 storiesOf("Button", module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
-  .add("with text", () => (
-    <Button onPress={action("clicked-text")}>
-      <Text>Hello Button</Text>
-    </Button>
-  ))
-  .add("with some emoji", () => (
-    <Button onPress={action("clicked-emoji")}>
-      <Text>😀 😎 👍 💯</Text>
-    </Button>
-  ));
+  .addDecorator(withKnobs)
+  .add("Default", () => <Button onPress={action("clicked")}>Default button</Button>)
+  .add("Bordered", () => {
+    const type = select("Type", [TYPE_OPTIONS.PRIMARY, TYPE_OPTIONS.SECONDARY], "primary");
+    return (
+      <Button bordered onPress={action("clicked")} type={type}>
+        {"Default button"}
+      </Button>
+    );
+  })
+  .add("Playground", () => {
+    const title = text("Title", "Basic button");
+    const type = select("Type", Object.values(TYPE_OPTIONS), "primary");
+    const size = select("Size", Object.values(SIZE_OPTIONS), "normal");
+    const disabled = boolean("Disabled", false);
+    const block = boolean("Block", false);
+    const width = number("Width", 0);
+    const bordered = boolean("Bordered", false);
+    const circled = boolean("Circled", false);
+
+    return (
+      <Button
+        onPress={action("clicked")}
+        type={type}
+        size={size}
+        disabled={disabled}
+        block={block}
+        width={width}
+        bordered={bordered}
+        circled={circled}
+      >
+        {title}
+      </Button>
+    );
+  });
---
+++ storybooknative/src/Button/consts.js
@@ -0,0 +1,29 @@
+// @flow
+export const TYPE_OPTIONS = {
+  PRIMARY: "primary",
+  SECONDARY: "secondary",
+  INFO: "info",
+  SUCCESS: "success",
+  WARNING: "warning",
+  CRITICAL: "critical",
+  FACEBOOK: "facebook",
+  GOOGLE: "google",
+};
+
+export const SIZE_OPTIONS = {
+  SMALL: "small",
+  NORMAL: "normal",
+  LARGE: "large",
+};
+
+export const TOKENS = {
+  // Size tokens
+  heightButton: "heightButton",
+  fontSizeButton: "fontSizeButton",
+  paddingButton: "paddingButton",
+  // Type tokens
+  backgroundButton: "backgroundButton",
+  colorTextButton: "colorTextButton",
+  colorTextButtonBordered: "colorTextButtonBordered",
+  borderColorButton: "borderColorButton",
+};

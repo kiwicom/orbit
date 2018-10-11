7839434a04a641495e0aa765e957a6cd257d2127
Luděk Vepřek <weprous@gmail.com>
2018-10-11 12:44:16 +0200

    ADD: Staging

---
+++ config/deploymentUtils.js
@@ -0,0 +1,43 @@
+// @flow
+// const { resolve } = require("path");
+const octokit = require("@octokit/rest")();
+// require("dotenv").config({ path: resolve(".env") });
+
+const repo = "orbit-components";
+
+export const getPr = async (branchName: string) => {
+  const res = await octokit.pullRequests.getAll({
+    owner: "kiwicom",
+    repo,
+    head: `kiwicom:${branchName}`,
+  });
+  return res.data[0];
+};
+
+export const updateLiveURL = async (branchName: string, lastUrl: string) => {
+  if (!branchName) throw new Error("Missing branch parameter");
+  const pr = await getPr(branchName);
+  octokit.authenticate({
+    type: "integration",
+    token: process.env.GITHUB_TOKEN,
+  });
+  const res = await octokit.pullRequests.get({
+    owner: "kiwicom",
+    repo,
+    number: pr.number,
+  });
+  let newBody;
+  if (res.data.body.match(/<url>/)) {
+    newBody = res.data.body.replace(/<url>(.*)(<\/url>)?/, `<url>LiveURL: ${lastUrl}</url>`);
+  } else {
+    newBody = res.data.body.concat(`<br/><br/><br/><url>LiveURL: ${lastUrl}</url>`);
+  }
+  await octokit.pullRequests.update({
+    owner: "kiwicom",
+    repo,
+    number: pr.number,
+    body: newBody,
+  });
+};
+
+require("make-runnable");

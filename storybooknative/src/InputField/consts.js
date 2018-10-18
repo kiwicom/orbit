ca0aa0be8356c60d83997e7ab81fb2761d4d74c8
denisbalogh <denis.balogh@kiwi.com>
2018-10-18 16:19:27 +0200

    Basic Orbit styles.

---
+++ storybooknative/src/InputField/consts.js
@@ -0,0 +1,18 @@
+// @flow
+export const TYPE_OPTIONS = {
+  TEXT: "text",
+  NUMBER: "number",
+  EMAIL: "email",
+  PASSWORD: "password",
+};
+
+export const SIZE_OPTIONS = {
+  SMALL: "small",
+  NORMAL: "normal",
+};
+
+export const TOKENS = {
+  heightInput: "heightInput",
+  fontSizeInput: "fontSizeInput",
+  paddingInput: "paddingInput",
+};
--- storybooknative/src/InputField/index.js
+++ storybooknative/src/InputField/index.js
@@ -1,30 +1,113 @@
-// @flow strict
+// @flow
 
 import * as React from "react";
-import { TextInput } from "react-native"; // eslint-disable-line no-restricted-imports
+import { TextInput, View } from "react-native";
 
-import Text from "../Text";
 import styled from "../styled";
+import defaultTokens from "../defaultTokens";
+import { SIZE_OPTIONS, TYPE_OPTIONS, TOKENS } from "./consts";
 
-type Props = {|
-  +placeholder?: string,
-  +label?: React.Element<typeof Text>,
-  +defaultValue?: string,
-  +autoFocus?: boolean,
-  +onChangeText?: (text: string) => void,
-  +keyboardType?: "email-address" | "numeric" | "number-pad",
-  +secureTextEntry?: boolean,
-  +maxLength?: number,
-|};
+import type { Props } from "./index";
 
-const StyledTextInput = styled(TextInput, props => {});
+const getToken = (name, { theme, size }) => {
+  const tokens = {
+    [TOKENS.heightInput]: {
+      [SIZE_OPTIONS.SMALL]: theme.orbit.heightInputSmall,
+      [SIZE_OPTIONS.NORMAL]: theme.orbit.heightInputNormal,
+    },
+    [TOKENS.fontSizeInput]: {
+      [SIZE_OPTIONS.SMALL]: theme.orbit.fontSizeInputSmall,
+      [SIZE_OPTIONS.NORMAL]: theme.orbit.fontSizeInputNormal,
+    },
+    [TOKENS.paddingInput]: {
+      [SIZE_OPTIONS.SMALL]: theme.orbit.paddingInputSmall,
+      [SIZE_OPTIONS.NORMAL]: theme.orbit.paddingInputNormal,
+    },
+  };
+
+  return tokens[name][size];
+};
+
+const StyledInputContainer = styled(View, props => {
+  const { theme } = props;
+  return {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    height: getToken(TOKENS.heightInput, props),
+    borderRadius: theme.orbit.borderRadiusNormal,
+  };
+});
+
+StyledInputContainer.defaultProps = {
+  theme: defaultTokens,
+};
+
+const StyledFakeInput = styled(View, props => {
+  const { theme, error, disabled } = props;
+  return {
+    width: "100%",
+    position: "absolute",
+    zIndex: 1,
+    height: getToken(TOKENS.heightInput, props),
+    borderRadius: theme.orbit.borderRadiusNormal,
+    borderWidth: "1px",
+    borderColor: error ? theme.orbit.borderColorInputError : theme.orbit.borderColorInput,
+    backgroundColor: disabled ? theme.orbit.backgroundInputDisabled : theme.orbit.backgroundInput,
+  };
+});
+
+StyledFakeInput.defaultProps = {
+  theme: defaultTokens,
+};
+
+const StyledTextInput = styled(TextInput, props => {
+  const { theme, disabled } = props;
+  return {
+    padding: getToken(TOKENS.paddingInput, props),
+    borderRadius: theme.orbit.borderRadiusNormal,
+    color: disabled ? theme.orbit.colorTextInputDisabled : theme.orbit.colorTextInput,
+    fontSize: getToken(TOKENS.fontSizeInput, props),
+    width: "100%",
+    height: "100%",
+    lineHeight: getToken(TOKENS.heightInput, props),
+    zIndex: 2,
+  };
+});
+
+StyledTextInput.defaultProps = {
+  theme: defaultTokens,
+};
 
 const InputField = (props: Props) => {
-  const { label, ...rest } = props;
+  const {
+    size = SIZE_OPTIONS.NORMAL,
+    type = TYPE_OPTIONS.TEXT,
+    label,
+    help,
+    error,
+    inlineLabel,
+    prefix,
+    suffix,
+    disabled,
+    required,
+    ...rest
+  } = props;
   return (
     <React.Fragment>
       {label !== undefined && label}
-      <StyledTextInput underlineColorAndroid="transparent" autoCorrect={false} {...rest} />
+      <StyledInputContainer size={size} disabled={disabled} error={error}>
+        <StyledTextInput
+          underlineColorAndroid="transparent"
+          autoCorrect={false}
+          placeholderTextColor={defaultTokens.orbit.colorPlaceholderInput}
+          size={size}
+          type={type}
+          {...rest}
+        />
+        <StyledFakeInput size={size} disabled={disabled} error={error} />
+      </StyledInputContainer>
     </React.Fragment>
   );
 };

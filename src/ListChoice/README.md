033b78ceccc8f5291360d682d23529c27159a57f
Luděk Vepřek <weprous@gmail.com>
2018-11-02 19:19:12 +0100

    NEW: ListChoice component

---
+++ src/ListChoice/README.md
@@ -0,0 +1,21 @@
+# ListChoice
+To implement ListChoice component into your project you'll need to add the import:
+```jsx
+import ListChoice from "@kiwicom/orbit-components/lib/ListChoice";
+```
+After adding import into your project you can use it simply like:
+```jsx
+<ListChoice title="My Choice" />
+```
+## Props
+Table below contains all types of the props available in the ListChoice component.
+
+| Name              | Type          | Default | Description                      |
+| :---------------- | :------------ | :------ | :------------------------------- |
+| dataTest          | `string`      |         | Optional prop for testing purposes.
+| description       | `string`      |         | The additional info about the ListChoice.
+| icon              | `React.Node`  |         | The icon on the left of the ListChoice.
+| onClick           | `func`        |         | Function for handling onClick event.
+| selectable        | `boolean`     | `false` | If `true`, the check box glyph appears on the right size and it will be possible to select the ListChoice.
+| selected          | `boolean`     | `false` | If `true`, the check box glyph will be checked.
+| **title**         | `string`      |         | The title of the ListChoice.
---
+++ src/ListChoice/_tests_/index.test.js
@@ -0,0 +1,58 @@
+// @flow
+import * as React from "react";
+import { shallow } from "enzyme";
+
+import ListChoice from "../index";
+import Accommodation from "../../icons/Accommodation";
+
+describe("ListChoice", () => {
+  const title = "Choice Title";
+  const description = "Further description";
+  const dataTest = "test";
+  const selected = true;
+  const selectable = true;
+  const onClick = jest.fn();
+
+  const component = shallow(
+    <ListChoice
+      title={title}
+      description={description}
+      selectable={selectable}
+      selected={selected}
+      icon={<Accommodation />}
+      onClick={onClick}
+      dataTest={dataTest}
+    />,
+  );
+  const content = component.find("ListChoice__StyledListChoiceContent");
+  it("should render icon", () => {
+    expect(component.find("Accommodation").exists()).toBe(true);
+  });
+  it("should render data-test", () => {
+    expect(component.render().prop("data-test")).toBe(dataTest);
+  });
+  it("should render title and description", () => {
+    expect(
+      content
+        .find("Heading")
+        .children()
+        .text(),
+    ).toBe(title);
+    expect(
+      content
+        .find("Text")
+        .children()
+        .text(),
+    ).toBe(description);
+  });
+  it("should render checkbox with checked false", () => {
+    expect(component.find("Checkbox").prop("checked")).toBe(true);
+  });
+  it("should execute onClick method", () => {
+    component.simulate("click");
+    expect(onClick).toHaveBeenCalled();
+  });
+  it("should match snapshot", () => {
+    expect(component).toMatchSnapshot();
+  });
+});

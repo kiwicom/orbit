97592a5260099ec555fd6f10fc958fdd4a5b74c1
lukewalczak <lukasz.walczak.pwr@gmail.com>
2018-11-07 16:16:43 +0100

    Fix flow errors

--- storybooknative/src/Icon/index.js
+++ storybooknative/src/Icon/index.js
@@ -1,4 +1,4 @@
-// @flow strict
+// @flow
 
 import * as React from "react";
 import { Text as RNText } from "react-native";
@@ -9,13 +9,20 @@ import iconsMap from "./icons.json";
 
 import type { Props } from "./index";
 
-const StyledText = styled(RNText, () => ({
-  fontFamily: "orbit-icons",
-  includeFontPadding: false,
-  textAlignVertical: "center",
-  color: defaultTokens.orbit.colorIconPrimary,
-  fontSize: 20,
-}));
+const StyledText = styled(RNText, props => {
+  const { theme } = props;
+  return {
+    fontFamily: "orbit-icons",
+    includeFontPadding: false,
+    textAlignVertical: "center",
+    color: theme.orbit.colorIconPrimary,
+    fontSize: 20,
+  };
+});
+
+StyledText.defaultProps = {
+  theme: defaultTokens,
+};
 
 const getIconCharacter = name => {
   if (/^E(.{3})$/.test(iconsMap[name].character)) {

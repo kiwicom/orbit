935758b2e2dab48c6f55cd9c9e6decc979e8e625
Tomáš Hapl <tomas.hapl@gmail.com>
2019-01-02 15:57:51 +0100

    FIX: Storybook IE 11

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,6 +5,22 @@
 // IMPORTANT
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
+const babelOptions = {
+  presets: [
+    "@babel/react",
+    "@babel/flow",
+    ["@babel/env", { modules: false, targets: { browsers: ["last 2 versions", "ie >= 11", "Edge >= 38"] } }],
+  ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-proposal-nullish-coalescing-operator",
+    "@babel/plugin-proposal-optional-chaining",
+    "babel-plugin-styled-components"
+  ],
+  babelrc: false,
+};
+
 module.exports = {
   resolve: {
     extensions: [".js", ".json"],
@@ -12,8 +28,11 @@ module.exports = {
   module: {
     rules: [{
       test: /\.js?$/,
-      use: "babel-loader",
-      exclude: /node_modules/,
+      use: [{
+        options: babelOptions,
+        loader: "babel-loader"
+      }],
+      exclude: /node_modules\/(?!(loki)\/).*/, // Loki is not transpilled, throws error in IE 11
     }]
   },
   optimization: {

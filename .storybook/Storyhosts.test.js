c9dd2acc38f0800590f8e4a3ad2953a4cbd83e18
Tomáš Hapl <tomas.hapl@gmail.com>
2019-02-25 14:49:44 +0100

    DEV: Bump versions, upgrade node ci, removed storyshots

--- .storybook/Storyhosts.test.js
+++
@@ -1,9 +0,0 @@
-//@flow
-import ReactDOM from "react-dom";
-import initStoryshots, { multiSnapshotWithOptions } from "@storybook/addon-storyshots";
-
-// https://github.com/facebook/react/issues/11565
-// $FlowExpected
-ReactDOM.createPortal = node => node;
-
-initStoryshots({ test: multiSnapshotWithOptions({}) });

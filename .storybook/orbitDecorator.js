ccd977b903c5ad3b5be7fbb464e2d0c2ccb75b99
Tomáš Hapl <tomas.hapl@gmail.com>
2019-03-14 20:34:35 +0100

    update source

--- .storybook/orbitDecorator.js
+++ .storybook/orbitDecorator.js
@@ -1,13 +1,32 @@
 import * as React from "react"
 import Heading from "../src/Heading"
 import Text from "../src/Text"
+import Node from "@storybook/addon-info/dist/components/Node"
+import Pre from "@storybook/addon-info/dist/components/markdown/pre/pre"
 
 const orbitDecorator = (storyFn, context) => {
+
+  const children = storyFn(context)
+
   return  (
     <div style={{ padding: "20px" }}>
       <Heading spaceAfter="medium">{context.kind}</Heading>
       <Text spaceAfter="largest">{context.parameters?.info}</Text>
-      {storyFn(context)}
+      {children}
+      <Pre>
+        {React.Children.map(children, (root, idx) => (
+          <Node
+            key={idx}
+            node={root}
+            depth={0}
+            maxPropsIntoLine={3}
+            maxPropObjectKeys={1}
+            maxPropArrayLength={3}
+            maxPropStringLength={50}
+          />
+        ))}
+      </Pre>
+
     </div>
   )
 }

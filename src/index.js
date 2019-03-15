MouhamedAymen/orbit-components

new: FloatingCard

src/index.js
@@ -23,6 +23,7 @@ export { default as DeprecatedCardContent } from "./_deprecated/Card/CardContent
 export { default as Card } from "./Card";
 export { default as CardHeader } from "./Card/CardHeader";
 export { default as CardSection } from "./Card/CardSection";
+export { default as CardFloating } from "./FloatingCard";
 
 // Modal
 export { default as Portal } from "./Portal";

kiwicom/orbit

src/index.js
@@ -25,6 +25,7 @@ export { default as DeprecatedCardContent } from "./_deprecated/Card/CardContent
 export { default as Card } from "./Card";
 export { default as CardHeader } from "./Card/CardHeader";
 export { default as CardSection } from "./Card/CardSection";
+export { default as Sticky } from "./Sticky";
 export { default as DestinationCard } from "./DestinationCard";
 
 // Modal
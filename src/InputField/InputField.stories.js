b66401fc8c1e0450f4eccc64bfa107278385357c
Luděk Vepřek <ludek.veprek@kiwi.com>
2019-03-15 16:18:39 +0100

    FIX: InputField autoComplete (#922)

--- src/InputField/InputField.stories.js
+++ src/InputField/InputField.stories.js
@@ -449,7 +449,7 @@ storiesOf("InputField", module)
     const maxLength = number("maxLength", undefined);
     const minLength = number("minLength", undefined);
     const readOnly = boolean("readOnly", false);
-    const autocomplete = text("autocomplete", "off");
+    const autoComplete = text("autoComplete", "off");
     const dataTest = text("dataTest", "test");
 
     return {
@@ -489,7 +489,7 @@ storiesOf("InputField", module)
                   maxLength={maxLength}
                   minLength={minLength}
                   readOnly={readOnly}
-                  autocomplete={autocomplete}
+                  autoComplete={autoComplete}
                   onChange={action("change")}
                   onFocus={action("focus")}
                   onBlur={action("blur")}

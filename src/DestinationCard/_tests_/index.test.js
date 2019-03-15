e35933981c56be777acd0c4d37a2037618474405
Filip Danisko <filip@danisko.cz>
2019-03-15 16:10:13 +0100

    ADD: DestinationCard (#895)

---
+++ src/DestinationCard/_tests_/index.test.js
@@ -0,0 +1,82 @@
+// @flow
+import * as React from "react";
+import { shallow } from "enzyme";
+
+import DestinationCard from "../index";
+
+describe("DestinationCard one-way", () => {
+  const dataTest = "test";
+  const departureCity = "Prague";
+  const destinationCity = "Dubai";
+  const destinationCountry = "United Arab Emirates";
+  const image = "dubai_ae";
+  const height = 500;
+  const price = "5,563 Kč";
+  const onClick = jest.fn();
+  const outbound = { text: "One-way", type: "Direct", duration: "6h 10m" };
+  const component = shallow(
+    <DestinationCard
+      dataTest={dataTest}
+      departureCity={departureCity}
+      destinationCity={destinationCity}
+      destinationCountry={destinationCountry}
+      image={image}
+      height={height}
+      price={price}
+      outbound={outbound}
+      onClick={onClick}
+    />,
+  );
+
+  const departure = component.find("DestinationCard__StyledDestinationCardHeader");
+  const destination = component.find("DestinationCard__StyledDestination");
+
+  it("should have props", () => {
+    expect(component.render().prop("data-test")).toBe(dataTest);
+    expect(component.prop("height")).toBe(height);
+  });
+
+  it("should render departure", () => {
+    expect(
+      departure
+        .find("Heading")
+        .first()
+        .children()
+        .text(),
+    ).toBe(departureCity);
+    expect(departure.find("DestinationCard__ArrowUp").exists()).toBe(true);
+  });
+  it("should render destination city and country", () => {
+    expect(
+      destination
+        .find("Heading")
+        .children()
+        .text(),
+    ).toBe(destinationCity);
+    expect(
+      component
+        .find("DestinationCard__Shown")
+        .find("Heading")
+        .at(1)
+        .children()
+        .text(),
+    ).toBe(destinationCountry);
+  });
+  it("should render price", () => {
+    expect(
+      component
+        .find("DestinationCard__StyledDestinationCardContent")
+        .find("Heading")
+        .at(3)
+        .children()
+        .text(),
+    ).toBe(price);
+  });
+  it("should execute onClick method", () => {
+    component.simulate("click");
+    expect(onClick).toHaveBeenCalled();
+  });
+  it("should match snapshot", () => {
+    expect(component).toMatchSnapshot();
+  });
+});
---
+++ src/LazyImage/index.js
@@ -0,0 +1,89 @@
+// @flow
+import * as React from "react";
+import styled, { css } from "styled-components";
+
+import defaultTokens from "../defaultTokens";
+
+import type { Props, State, PictureProps } from "./";
+
+const FORMATS = {
+  WEBP: "webp",
+  JPEG: "jpg",
+};
+
+export const StyledLazyImage = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+
+const Image = styled.img`
+  z-index: ${({ visible }) => (visible ? "1" : "0")};
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  min-width: 100%;
+  min-height: 100%;
+  opacity: ${({ visible }) => (visible ? "1" : "0")};
+  transition: opacity ${({ theme }) => theme.orbit.durationFast} ease-in-out;
+  ${({ lowRes }) =>
+    lowRes &&
+    css`
+      filter: blur(3px);
+    `};
+`;
+
+Image.defaultProps = {
+  theme: defaultTokens,
+};
+
+const getPictureType = picture => {
+  const TYPES = {
+    [FORMATS.WEBP]: "image/webp",
+    [FORMATS.JPEG]: "image/jpg",
+  };
+  return TYPES[picture];
+};
+
+const Picture = ({ pictures, name, loaded, onLoad, lowRes }: PictureProps) => (
+  <picture>
+    {Object.keys(pictures).map(picture => (
+      <React.Fragment key={picture}>
+        <source srcSet={pictures[picture]} type={getPictureType(picture)} />
+        {picture === "jpg" && (
+          <Image
+            onLoad={onLoad}
+            src={pictures[picture]}
+            alt={name}
+            visible={loaded}
+            lowRes={lowRes}
+          />
+        )}
+      </React.Fragment>
+    ))}
+  </picture>
+);
+
+class LazyImage extends React.PureComponent<Props, State> {
+  state = {
+    loaded: false,
+  };
+  fullResLoaded = () => {
+    this.setState({ loaded: true });
+  };
+  render() {
+    const { placeholder, original, name } = this.props;
+    const { loaded } = this.state;
+
+    return (
+      <StyledLazyImage>
+        <Picture pictures={original} name={name} loaded={loaded} onLoad={this.fullResLoaded} />
+        {placeholder && <Picture pictures={placeholder} lowRes name={name} loaded={!loaded} />}
+      </StyledLazyImage>
+    );
+  }
+}
+
+export default LazyImage;

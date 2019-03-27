c38f53e0da4e7f73dff8336b7caf1d6754b05eaa
Luděk Vepřek <ludek.veprek@kiwi.com>
2019-03-27 14:57:39 +0100

    FIX: Stepper and InputStepper onChange callback (#950)

--- src/Stepper/index.js
+++ src/Stepper/index.js
@@ -45,31 +45,32 @@ class Stepper extends React.PureComponent<Props, State> {
     value: this.props.defaultValue || 0,
   };
 
-  componentDidUpdate() {
+  setValueAndInjectCallback = (value: number) => {
     const { onChange } = this.props;
-    const { value } = this.state;
     if (onChange) {
       onChange(value);
     }
-  }
+    this.setState({ value });
+  };
 
   incrementCounter = () => {
     const { value } = this.state;
-    const { maxValue, step = 1 } = this.props;
+    const { maxValue = Number.POSITIVE_INFINITY, step = 1 } = this.props;
     const newValue = value + step;
-    this.setState({
-      value: newValue >= +maxValue ? maxValue : newValue,
-    });
+    const stateValue = newValue >= +maxValue ? maxValue : newValue;
+    if (stateValue !== value) {
+      this.setValueAndInjectCallback(stateValue);
+    }
   };
 
   decrementCounter = () => {
     const { value } = this.state;
-    const { minValue, step = 1 } = this.props;
+    const { minValue = Number.NEGATIVE_INFINITY, step = 1 } = this.props;
     const newValue = value - step;
-
-    this.setState({
-      value: newValue <= +minValue ? minValue : newValue,
-    });
+    const stateValue = newValue <= +minValue ? minValue : newValue;
+    if (stateValue !== value) {
+      this.setValueAndInjectCallback(stateValue);
+    }
   };
 
   handleKeyDown = (ev: SyntheticKeyboardEvent<HTMLInputElement>) => {

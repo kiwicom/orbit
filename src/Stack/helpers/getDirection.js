3fab682999e66d88ef8c41cf58bb16817ee123f9
Tomáš Hapl <tomas.hapl@gmail.com>
2019-03-27 15:53:09 +0100

    FIX: Stack behavior (#961)

--- src/Stack/helpers/getDirection.js
+++ src/Stack/helpers/getDirection.js
@@ -3,6 +3,6 @@ import { DIRECTIONS } from "../consts";
 import type { GetDirection } from "./getDirection";
 
 const getDirection: GetDirection = direction =>
-  Object.values(DIRECTIONS).indexOf(direction) !== -1 ? direction : DIRECTIONS.COLUMN;
+  Object.values(DIRECTIONS).indexOf(direction) !== -1 ? direction : DIRECTIONS.ROW;
 
 export default getDirection;

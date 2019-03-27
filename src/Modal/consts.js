423d0d6057ab1c539bba388ee8f53b39e4805e40
Filip Danisko <filip@danisko.cz>
2019-03-27 15:10:56 +0100

    UPDATE: Modal accessibility (#958)

--- src/Modal/consts.js
+++ src/Modal/consts.js
@@ -6,3 +6,6 @@ export const SIZES = {
 };
 
 export const CLOSE_BUTTON_DATA_TEST = "ModalCloseButton";
+
+export const FOCUSABLE_ELEMENT_SELECTORS =
+  'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, [tabindex="0"], [contenteditable]';
---
+++ src/common/keyMaps.js
@@ -0,0 +1,5 @@
+// @flow
+
+export default {
+  TAB: 9,
+};

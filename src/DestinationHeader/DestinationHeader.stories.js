07c4e30ef101005f0a8412769e65d91e4d937692
Luděk Vepřek <ludek.veprek@kiwi.com>
2019-03-28 10:57:45 +0100

    DEV: Fix orbitDecorator and loki references (#963)

--- src/DestinationHeader/DestinationHeader.stories.js
+++ src/DestinationHeader/DestinationHeader.stories.js
@@ -7,22 +7,23 @@ import { text } from "@storybook/addon-knobs/react";
 
 import DestinationHeader from "./index";
 
-storiesOf("DestinationHeader", module).add("Playground", () => {
-  const image = text("image", "dubai_ae");
-  const destinationName = text("destinationName", "Dubai");
-  const dataTest = text("dataTest", "test");
-  return (
-    (
+storiesOf("DestinationHeader", module).add(
+  "Playground",
+  () => {
+    const image = text("image", "dubai_ae");
+    const destinationName = text("destinationName", "Dubai");
+    const dataTest = text("dataTest", "test");
+    return (
       <DestinationHeader
         image={image}
         destinationName={destinationName}
         goBack={action("goBack")}
         dataTest={dataTest}
       />
-    ),
-    {
-      info:
-        "DestinationHeader is a component providing better visual recognition of city you have chosen. It is placed on the result page and users can easily get back to the serch page by clicking on the back button",
-    }
-  );
-});
+    );
+  },
+  {
+    info:
+      "DestinationHeader is a component providing better visual recognition of city you have chosen. It is placed on the result page and users can easily get back to the serch page by clicking on the back button",
+  },
+);

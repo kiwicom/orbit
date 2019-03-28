f0da9f69d9b305256eeffe735ea069ce218d48ba
Tomáš Hapl <tomas.hapl@gmail.com>
2019-03-28 06:49:55 +0100

    prettier

--- src/CountryFlag/CountryFlag.stories.js
+++ src/CountryFlag/CountryFlag.stories.js
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
-import { text, select } from "@storybook/addon-knobs";
+import { text } from "@storybook/addon-knobs";
 
 import { CODES } from "./consts";
 

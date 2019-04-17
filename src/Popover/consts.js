92ecc7f57d0117642bd5606da54f3e74e7e7d24a
Filip Danisko <filip@danisko.cz>
2019-04-17 15:35:02 +0200

    NEW: Popover component (#894)

---
+++ src/Popover/consts.js
@@ -0,0 +1,14 @@
+// @flow
+
+// order of POSITIONS or ANCHOR is important, the first possible value will be applied
+export const POSITIONS = {
+  BOTTOM: "bottom",
+  TOP: "top",
+};
+
+export const ANCHORS = {
+  START: "start",
+  END: "end",
+};
+
+export const POPOVER_SPACE_BETWEEN = 4;

c204605edc55fe3413c78712a51c98ca066a3281
filipdanisko <filip@danisko.cz>
2019-04-25 14:56:24 +0200

    Updating tests

--- src/SkipNavigation/_tests_/index.test.js
+++ src/SkipNavigation/_tests_/index.test.js
@@ -5,23 +5,26 @@ import { mount } from "enzyme";
 import SkipNavigation from "..";
 
 describe("SkipNavigation", () => {
-  const component = mount(
-    <SkipNavigation
-      actions={[
-        {
-          link: "https://www.kiwi.com/cz/pages/content/terms",
-          name: "Got to terms and condition",
-        },
-        {
-          name: "Add baggage",
-        },
-        {
-          name: "Reguest refund",
-        },
-      ]}
-      feedbackUrl="#"
-    />,
-  );
+  const actions = [
+    {
+      link: "https://www.kiwi.com/cz/pages/content/terms",
+      name: "Got to terms and condition",
+    },
+    {
+      name: "Add baggage",
+    },
+    {
+      name: "Reguest refund",
+    },
+  ];
+  const feedbackUrl = "https://www.kiwi.com/en/";
+
+  const component = mount(<SkipNavigation actions={actions} feedbackUrl={feedbackUrl} />);
+
+  it("Should pass props", () => {
+    expect(component.prop("feedbackUrl")).toBe(feedbackUrl);
+    expect(component.prop("actions")).toBe(actions);
+  });
 
   it("should match snapshot", () => {
     expect(component).toMatchSnapshot();

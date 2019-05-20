b85366a8ee9c75670bb1ba46b0af5848940266ed
Filip Danisko <filip@danisko.cz>
2019-05-20 13:41:03 +0200

    UPDATE: InputStepper exports stateless version (#983)
    * UPDATE: InputStepper exports Stateless version
    
    * Updating flow, Stories and Reamde
    
    * inputStepper updating conditions
    
    * Updating ussage of helpers functions
    
    * moving helpers functions to utils and updating readme
    
    * updating readme
    
    * updating readme
    
    * Updating flow
    
    * updating readme

--- src/InputStepper/index.js
+++ src/InputStepper/index.js
@@ -1,59 +1,13 @@
 // @flow
 import * as React from "react";
-import styled from "styled-components";
 
 import { SIZE_OPTIONS } from "../InputField/consts";
-import PlusCircle from "../icons/PlusCircle";
-import MinusCircle from "../icons/MinusCircle";
-import ButtonLink from "../ButtonLink";
-import InputField, { Input, Prefix } from "../InputField";
-import defaultTheme from "../defaultTheme";
 import KEY_CODE_MAP from "../common/keyMaps";
-import getSpacingToken from "../common/getSpacingToken";
+import InputStepperStateless from "./InputStepperStateless";
+import validateIncrement from "../utils/validateIncrement";
+import validateDecrement from "../utils/validateDecrement";
 
 import type { Props, State, ForwardedRef } from "./index";
-
-const PrefixSuffix = styled(({ type, ...props }) => <div {...props} />)`
-  flex-shrink: 0;
-  z-index: 3;
-  cursor: ${({ disabled }) => disabled && "not-allowed"};
-`;
-
-PrefixSuffix.defaultProps = {
-  theme: defaultTheme,
-};
-
-const StyledInputStepper = styled.div`
-  width: 100%;
-  margin-bottom: ${getSpacingToken};
-  ${Input} {
-    text-align: center;
-  }
-  ${Prefix} {
-    padding: 0;
-    pointer-events: auto;
-  }
-`;
-
-StyledInputStepper.defaultProps = {
-  theme: defaultTheme,
-};
-
-const StyledButtonWrapper = styled.div`
-  &:focus {
-    outline: none;
-    box-shadow: 0 0 1px 1px ${({ theme }) => theme.orbit.colorTextButtonWhiteBordered},
-      0 0 1px 3px rgba(1, 118, 210, 0.6); //TODO: Create token
-  }
-
-  &:focus:active {
-    box-shadow: none;
-  }
-`;
-
-StyledButtonWrapper.defaultProps = {
-  theme: defaultTheme,
-};
 
 class InputStepper extends React.Component<Props & ForwardedRef, State> {
   state = {
@@ -71,20 +25,45 @@ class InputStepper extends React.Component<Props & ForwardedRef, State> {
   incrementCounter = () => {
     const { value } = this.state;
     const { maxValue = Number.POSITIVE_INFINITY, step = 1 } = this.props;
-    const newValue = value + step;
-    const stateValue = newValue >= +maxValue ? maxValue : newValue;
-    if (stateValue !== value) {
-      this.setValueAndInjectCallback(stateValue);
-    }
+    this.setValueAndInjectCallback(validateIncrement({ value, maxValue, step }));
   };
 
   decrementCounter = () => {
     const { value } = this.state;
     const { minValue = Number.NEGATIVE_INFINITY, step = 1 } = this.props;
-    const newValue = value - step;
-    const stateValue = newValue <= +minValue ? minValue : newValue;
-    if (stateValue !== value) {
-      this.setValueAndInjectCallback(stateValue);
+
+    this.setValueAndInjectCallback(validateDecrement({ value, minValue, step }));
+  };
+
+  handleIncrementCounter = (
+    ev?: SyntheticEvent<HTMLButtonElement> | SyntheticKeyboardEvent<HTMLButtonElement>,
+  ) => {
+    if (ev && ev.type === "click") {
+      this.incrementCounter();
+    }
+    if (ev && ev.type === "keydown") {
+      if (ev.keyCode === KEY_CODE_MAP.SPACE) {
+        ev.preventDefault();
+        this.incrementCounter();
+      } else if (ev.keyCode === KEY_CODE_MAP.ENTER) {
+        this.incrementCounter();
+      }
+    }
+  };
+
+  handleDecrementCounter = (
+    ev?: SyntheticEvent<HTMLButtonElement> | SyntheticKeyboardEvent<HTMLButtonElement>,
+  ) => {
+    if (ev && ev.type === "click") {
+      this.decrementCounter();
+    }
+    if (ev && ev.type === "keydown") {
+      if (ev.keyCode === KEY_CODE_MAP.SPACE) {
+        ev.preventDefault();
+        this.decrementCounter();
+      } else if (ev.keyCode === KEY_CODE_MAP.ENTER) {
+        this.decrementCounter();
+      }
     }
   };
 
@@ -102,27 +81,14 @@ class InputStepper extends React.Component<Props & ForwardedRef, State> {
   handleChange = (ev: SyntheticInputEvent<HTMLInputElement>) => {
     const { minValue = Number.NEGATIVE_INFINITY, maxValue = Number.POSITIVE_INFINITY } = this.props;
     const value = ev && parseInt(ev.target.value, 10);
+    const prevValue = this.state.value;
 
-    if (Number.isInteger(value) && value >= minValue && value <= maxValue) {
-      this.setState({ value });
+    if (prevValue <= value) {
+      this.setState({ value: validateIncrement({ value, maxValue, step: 0 }) });
     }
-  };
 
-  handleKeyDecrement = (ev: SyntheticKeyboardEvent<HTMLInputElement>) => {
-    if (ev.keyCode === KEY_CODE_MAP.SPACE) {
-      ev.preventDefault();
-      this.decrementCounter();
-    } else if (ev.keyCode === KEY_CODE_MAP.ENTER) {
-      this.decrementCounter();
-    }
-  };
-
-  handleKeyIncrement = (ev: SyntheticKeyboardEvent<HTMLInputElement>) => {
-    if (ev.keyCode === KEY_CODE_MAP.SPACE) {
-      ev.preventDefault();
-      this.incrementCounter();
-    } else if (ev.keyCode === KEY_CODE_MAP.ENTER) {
-      this.incrementCounter();
+    if (prevValue >= value) {
+      this.setState({ value: validateDecrement({ value, minValue, step: 0 }) });
     }
   };
 
@@ -143,55 +109,35 @@ class InputStepper extends React.Component<Props & ForwardedRef, State> {
       tabIndex,
       forwardedRef,
       spaceAfter,
+      titleIncrement,
+      titleDecrement,
     } = this.props;
     const { value } = this.state;
     return (
-      <StyledInputStepper spaceAfter={spaceAfter}>
-        <InputField
-          dataTest={dataTest}
-          size={size}
-          label={label}
-          disabled={disabled}
-          required={required}
-          name={name}
-          error={error}
-          help={help}
-          type="number"
-          onChange={this.handleChange}
-          onBlur={onBlur}
-          onFocus={onFocus}
-          onKeyDown={this.handleKeyDown}
-          value={value || 0}
-          minValue={minValue}
-          maxValue={maxValue}
-          tabIndex={tabIndex}
-          ref={forwardedRef}
-          prefix={
-            <StyledButtonWrapper role="button" tabIndex="0" onKeyDown={this.handleKeyDecrement}>
-              <ButtonLink
-                disabled={disabled || value <= +minValue}
-                iconLeft={<MinusCircle color="secondary" />}
-                size={size}
-                onClick={this.decrementCounter}
-                transparent
-                component={PrefixSuffix}
-              />
-            </StyledButtonWrapper>
-          }
-          suffix={
-            <StyledButtonWrapper role="button" tabIndex="0" onKeyDown={this.handleKeyIncrement}>
-              <ButtonLink
-                disabled={disabled || value >= +maxValue}
-                iconLeft={<PlusCircle color="secondary" />}
-                size={size}
-                onClick={this.incrementCounter}
-                transparent
-                component={PrefixSuffix}
-              />
-            </StyledButtonWrapper>
-          }
-        />
-      </StyledInputStepper>
+      <InputStepperStateless
+        dataTest={dataTest}
+        size={size}
+        label={label}
+        disabled={disabled}
+        required={required}
+        name={name}
+        error={error}
+        help={help}
+        onChange={this.handleChange}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        onKeyDown={this.handleKeyDown}
+        value={value || 0}
+        minValue={minValue}
+        maxValue={maxValue}
+        tabIndex={tabIndex}
+        forwardedRef={forwardedRef}
+        spaceAfter={spaceAfter}
+        onDecrement={this.handleDecrementCounter}
+        onIncrement={this.handleIncrementCounter}
+        titleIncrement={titleIncrement}
+        titleDecrement={titleDecrement}
+      />
     );
   }
 }
@@ -204,3 +150,4 @@ const ForwardedInputStepper = React.forwardRef((props, ref) => (
 ForwardedInputStepper.displayName = "InputStepper";
 
 export default ForwardedInputStepper;
+export { default as InputStepperStateless } from "./InputStepperStateless";

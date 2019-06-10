55e40543edf1b031a2fa08f80d38d91f8b67b6f9
Tomáš Hapl <tomas.hapl@gmail.com>
2019-06-10 15:13:11 +0200

    prepush hook testing

--- config/documentation.js
+++ config/documentation.js
@@ -2,7 +2,11 @@
 import fs from "fs";
 import path from "path";
 import table from "markdown-table";
-import { NAMES as ILLUSTRATION_NAMES, SIZE_OPTIONS as ILLUSTRATION_SIZES } from "../src/Illustration/consts"
+
+import {
+  NAMES as ILLUSTRATION_NAMES,
+  SIZE_OPTIONS as ILLUSTRATION_SIZES,
+} from "../src/Illustration/consts";
 
 const generateDocumentation = async (templatePath, replacements) => {
   const TEMPLATE = await fs.readFileSync(templatePath, "utf8");
@@ -18,11 +22,22 @@ const generateDocumentation = async (templatePath, replacements) => {
 const illustrationEnums = {
   name: ILLUSTRATION_NAMES,
   size: Object.values(ILLUSTRATION_SIZES),
-}
+};
 
 // Add table header
-const illustrationResults = [].concat([], [Object.keys(illustrationEnums)])
+let illustrationResults = [];
+
+Object.keys(illustrationEnums).map((property, propertyIndex) => {
+  illustrationEnums[property].map((value, rowIndex) => {
+    if (!illustrationResults[rowIndex]) {
+      illustrationResults[rowIndex] = [];
+    }
+    illustrationResults[rowIndex][propertyIndex] = `\`"${value}"\``;
+  });
+});
+
+illustrationResults = [Object.keys(illustrationEnums), ...illustrationResults];
 
 generateDocumentation(path.join(__dirname, "..", "src", "Illustration", "README_TEMPLATE.md"), {
-  ENUM: "test",
+  ENUM: table(illustrationResults),
 });

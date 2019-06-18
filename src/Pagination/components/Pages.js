a30cdf3eb5a1b1fe6a6b1de2c816971032fb9cdd
Luděk Vepřek <weprous@gmail.com>
2019-06-18 15:13:38 +0200

    NEW: Pagination component (#703)
    * NEW: Pagination component
    
    * refactor a bit
    
    * Add mobile version, sizes and hideLabels prop
    
    * add Stack component property
    
    * fix flow
    
    * fix flow

---
+++ src/Pagination/components/Pages.js
@@ -0,0 +1,26 @@
+// @flow
+import * as React from "react";
+
+import ActiveButton from "./ActiveButton";
+import PageButtonLink from "./PageButtonLink";
+import type { Props } from "./Pages";
+
+const Pages = ({
+  pageCount,
+  selectedPage,
+  onPageChange,
+  enlargement = 1,
+  size,
+}: Props): React$Node =>
+  Array(...Array(pageCount)).map((_, index) => {
+    const key = index + enlargement;
+    return selectedPage === key ? (
+      <ActiveButton size={size}>{key}</ActiveButton>
+    ) : (
+      <PageButtonLink onPageChange={onPageChange} size={size}>
+        {key}
+      </PageButtonLink>
+    );
+  });
+
+export default Pages;
---
+++ src/Pagination/consts.js
@@ -0,0 +1,7 @@
+// @flow
+export const MAXIMUM_PAGES = 7;
+
+export const SIZES = {
+  SMALL: "small",
+  NORMAL: "normal",
+};

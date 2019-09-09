6782badb9d359990f6a96d33199315c64928ae7a
Tomáš Hapl <tomas.hapl@gmail.com>
2019-09-09 14:46:43 +0200

    init

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  plugins: ["@babel/plugin-proposal-class-properties", "@babel/plugin-proposal-object-rest-spread", "@babel/plugin-proposal-nullish-coalescing-operator", "@babel/plugin-proposal-optional-chaining", "babel-plugin-styled-components"],
+  presets: ["@babel/react", "@babel/flow", "@babel/env"]
+};

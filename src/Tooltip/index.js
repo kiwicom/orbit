0e15249baec0239aecec19ed9daae3f5914dcf6c
Luděk Vepřek <weprous@gmail.com>
2019-09-12 21:23:50 +0200

    fix tooltip children display

--- src/Tooltip/index.js
+++ src/Tooltip/index.js
@@ -14,6 +14,7 @@ import TooltipContent from "./components/TooltipContent";
 import type { Props } from "./index";
 
 const StyledTooltipChildren = styled.span`
+  display: inline-block;
   &:focus:active {
     outline: none;
   }

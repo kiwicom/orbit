4df07bd87cdd7ac9d8d5ca4a90e7d4b102d50671
Luděk Vepřek <weprous@gmail.com>
2019-09-23 17:06:26 +0200

    fix lint

--- src/Button/Button.stories.js
+++ src/Button/Button.stories.js
@@ -2,7 +2,7 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { text, number, boolean, select } from "@storybook/addon-knobs";
+import { text, boolean, select } from "@storybook/addon-knobs";
 
 import * as Icons from "../icons";
 import { TYPE_OPTIONS, SIZE_OPTIONS } from "./consts";

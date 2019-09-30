49a7a22dd03269dea09ce55765516b71a4681d1f
Filip Messa <messa.filip@gmail.com>
2019-09-30 09:23:07 +0200

    DEV: import right sizes const for the ButtonLink component (#1284)

--- src/ButtonLink/helpers/getButtonLinkSpacing.js
+++ src/ButtonLink/helpers/getButtonLinkSpacing.js
@@ -1,6 +1,5 @@
 // @flow
-import { TOKENS } from "../consts";
-import { SIZE_OPTIONS } from "../../Button/consts";
+import { TOKENS, SIZES } from "../consts";
 import { rtlSpacing } from "../../utils/rtl";
 import type { GetButtonLinkSpacing } from "./getButtonLinkSpacing";
 
@@ -14,24 +13,24 @@ const getButtonLinkSpacing: GetButtonLinkSpacing = () => ({
   if (onlyIcon) return rtlSpacing(theme.orbit.paddingButtonWithoutText)({ theme });
   const tokens = {
     [TOKENS.paddingButton]: {
-      [SIZE_OPTIONS.LARGE]: theme.orbit.paddingButtonLarge,
-      [SIZE_OPTIONS.NORMAL]: theme.orbit.paddingButtonNormal,
-      [SIZE_OPTIONS.SMALL]: theme.orbit.paddingButtonSmall,
+      [SIZES.LARGE]: theme.orbit.paddingButtonLarge,
+      [SIZES.NORMAL]: theme.orbit.paddingButtonNormal,
+      [SIZES.SMALL]: theme.orbit.paddingButtonSmall,
     },
     [TOKENS.paddingButtonWithIcons]: {
-      [SIZE_OPTIONS.LARGE]: theme.orbit.paddingButtonLargeWithIcons,
-      [SIZE_OPTIONS.NORMAL]: theme.orbit.paddingButtonNormalWithIcons,
-      [SIZE_OPTIONS.SMALL]: theme.orbit.paddingButtonSmallWithIcons,
+      [SIZES.LARGE]: theme.orbit.paddingButtonLargeWithIcons,
+      [SIZES.NORMAL]: theme.orbit.paddingButtonNormalWithIcons,
+      [SIZES.SMALL]: theme.orbit.paddingButtonSmallWithIcons,
     },
     [TOKENS.paddingButtonWithLeftIcon]: {
-      [SIZE_OPTIONS.LARGE]: theme.orbit.paddingButtonLargeWithLeftIcon,
-      [SIZE_OPTIONS.NORMAL]: theme.orbit.paddingButtonNormalWithLeftIcon,
-      [SIZE_OPTIONS.SMALL]: theme.orbit.paddingButtonSmallWithLeftIcon,
+      [SIZES.LARGE]: theme.orbit.paddingButtonLargeWithLeftIcon,
+      [SIZES.NORMAL]: theme.orbit.paddingButtonNormalWithLeftIcon,
+      [SIZES.SMALL]: theme.orbit.paddingButtonSmallWithLeftIcon,
     },
     [TOKENS.paddingButtonWithRightIcon]: {
-      [SIZE_OPTIONS.LARGE]: theme.orbit.paddingButtonLargeWithRightIcon,
-      [SIZE_OPTIONS.NORMAL]: theme.orbit.paddingButtonNormalWithRightIcon,
-      [SIZE_OPTIONS.SMALL]: theme.orbit.paddingButtonSmallWithRightIcon,
+      [SIZES.LARGE]: theme.orbit.paddingButtonLargeWithRightIcon,
+      [SIZES.NORMAL]: theme.orbit.paddingButtonNormalWithRightIcon,
+      [SIZES.SMALL]: theme.orbit.paddingButtonSmallWithRightIcon,
     },
   };
   if (iconLeft && iconRight) {

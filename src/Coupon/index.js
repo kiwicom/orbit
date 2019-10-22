33e3cf2db7ba7a792078b89b02016ce5204652c1
Luděk Vepřek <weprous@gmail.com>
2019-10-22 11:15:16 +0200

    UPDATE: Coupon component styles (#1329)

--- src/Coupon/index.js
+++ src/Coupon/index.js
@@ -7,13 +7,14 @@ import defaultTheme from "../defaultTheme";
 import type { Props } from ".";
 
 const StyledCoupon = styled.mark`
-  display: inline-block;
+  display: inline;
   background: none;
-  font-size: inherit;
+  font-size: ${({ theme }) => theme.orbit.fontSizeTextSmall};
+  letter-spacing: 0.75px;
   text-transform: uppercase;
   border: 1px dashed ${({ theme }) => theme.orbit.paletteInkLighter};
-  padding: 0 ${({ theme }) => theme.orbit.spaceXXSmall};
-  line-height: 22px;
+  padding: 2px ${({ theme }) => theme.orbit.spaceXXSmall};
+  line-height: inherit;
   font-weight: ${({ theme }) => theme.orbit.fontWeightMedium};
   border-radius: ${({ theme }) => theme.orbit.borderRadiusNormal};
   color: ${({ theme }) => theme.orbit.paletteInkNormal};

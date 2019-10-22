e7855a38afb111e96e666d4dc2a9e57e5ecd5ed1
Luděk Vepřek <weprous@gmail.com>
2019-10-22 16:11:08 +0200

    unify declaration

--- src/Card/CardSection/CardSectionContext.js
+++ src/Card/CardSection/CardSectionContext.js
@@ -1,9 +1,9 @@
 // @flow
-import { createContext } from "react";
+import * as React from "react";
 
 import type { CardSectionContextType } from "./CardSectionContext";
 
-const CardSectionContext: CardSectionContextType = createContext({
+const CardSectionContext: CardSectionContextType = React.createContext({
   expandable: false,
   expanded: false,
   handleToggleSection: () => {},

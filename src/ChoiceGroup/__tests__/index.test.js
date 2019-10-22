e7748f1100cf50f47f1d95edecf74e4338aff272
filipdanisko <filip@danisko.cz>
2019-10-22 17:07:18 +0200

    removing snapshot

--- src/ChoiceGroup/__tests__/index.test.js
+++ src/ChoiceGroup/__tests__/index.test.js
@@ -106,7 +106,4 @@ describe("FormFeedback", () => {
   it("should have data-test", () => {
     expect(component.render().prop("data-test")).toBe(dataTest);
   });
-  it("should match snapshot", () => {
-    expect(component).toMatchSnapshot();
-  });
 });

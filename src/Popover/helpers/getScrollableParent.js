250ca82953e427b99737092fb26d298d68a78ab0
filipdanisko <filip@danisko.cz>
2019-10-24 15:10:14 +0200

    Flow fixes

--- src/Popover/helpers/getScrollableParent.js
+++ src/Popover/helpers/getScrollableParent.js
@@ -3,7 +3,12 @@ import type { GetScrollableParent } from "./getScrollableParent";
 
 const regex = /(auto|scroll)/;
 
-const style = (node, prop) => getComputedStyle(node).getPropertyValue(prop);
+const style = (node, prop) => {
+  if (node instanceof HTMLElement) {
+    return getComputedStyle(node).getPropertyValue(prop);
+  }
+  return "";
+};
 
 const scroll = node =>
   regex.test(style(node, "overflow") + style(node, "overflow-y") + style(node, "overflow-x"));
@@ -15,7 +20,10 @@ const getScrollableParent: GetScrollableParent = node => {
   if (scroll(node)) {
     return node;
   }
-  return getScrollableParent(node.parentNode);
+  if (node && node.parentNode) {
+    return getScrollableParent(node.parentNode);
+  }
+  return null;
 };
 
 export default getScrollableParent;

87b747cf0317cddccb87a9b6b31f9540c8d6be5f
Luděk Vepřek <weprous@gmail.com>
2019-10-31 11:07:28 +0100

    FIX: Collapse missing import in export index (#1354)

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ export { default as CallOutBanner } from "./CallOutBanner";
 export { default as CarrierLogo } from "./CarrierLogo";
 export { default as Checkbox } from "./Checkbox";
 export { default as ChoiceGroup } from "./ChoiceGroup";
+export { default as Collapse } from "./Collapse";
 export { default as CountryFlag } from "./CountryFlag";
 export { default as DestinationHeader } from "./DestinationHeader";
 export { default as FeatureIcon } from "./FeatureIcon";

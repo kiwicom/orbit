3ebed39e85076d340b6e715aa6ce13c2032585e4
Filip Danisko <filip@danisko.cz>
2019-11-05 14:05:18 +0100

    FIX: PricingTable double render on desktop (#1357)

--- src/PricingTable/index.js
+++ src/PricingTable/index.js
@@ -26,48 +26,52 @@ const PricingTable = ({ children, defaultActiveElement = 0 }: Props) => {
     return `100%`;
   };
   return (
-    <StyledPricingTable>
-      <Stack
-        flex
-        grow
-        spaceAfter="medium"
-        spacing="condensed"
-        align="stretch"
-        desktop={{ spacing: "natural", spaceAfter: "none" }}
-        justify="center"
-      >
-        {isDesktop
-          ? children
-          : React.Children.map(children, (child, i) =>
-              React.cloneElement(child, {
-                active: activeElement === i,
-                compact: true,
-                basis: resolveBasis(child),
-                onClick: handleOnClick(i),
-              }),
-            )}
-      </Stack>
-      {!isDesktop && children && (
-        <Stack spacing="condensed">
-          {React.Children.map(children, (child, i) => {
-            if (i === activeElement) {
-              return (
-                <>
-                  {child.props.mobileDescription && (
-                    <Text weight="bold" size="normal">
-                      {child.props.mobileDescription}
-                    </Text>
-                  )}
-                  {child.props.children && React.cloneElement(child.props.children)}
-                  {child.props.action && React.cloneElement(child.props.action)}
-                </>
-              );
-            }
-            return null;
-          })}
-        </Stack>
+    <>
+      {isDesktop !== null && (
+        <StyledPricingTable>
+          <Stack
+            flex
+            grow
+            spaceAfter="medium"
+            spacing="condensed"
+            align="stretch"
+            desktop={{ spacing: "natural", spaceAfter: "none" }}
+            justify="center"
+          >
+            {isDesktop
+              ? children
+              : React.Children.map(children, (child, i) =>
+                  React.cloneElement(child, {
+                    active: activeElement === i,
+                    compact: true,
+                    basis: resolveBasis(child),
+                    onClick: handleOnClick(i),
+                  }),
+                )}
+          </Stack>
+          {!isDesktop && children && (
+            <Stack spacing="condensed">
+              {React.Children.map(children, (child, i) => {
+                if (i === activeElement) {
+                  return (
+                    <>
+                      {child.props.mobileDescription && (
+                        <Text weight="bold" size="normal">
+                          {child.props.mobileDescription}
+                        </Text>
+                      )}
+                      {child.props.children && React.cloneElement(child.props.children)}
+                      {child.props.action && React.cloneElement(child.props.action)}
+                    </>
+                  );
+                }
+                return null;
+              })}
+            </Stack>
+          )}
+        </StyledPricingTable>
       )}
-    </StyledPricingTable>
+    </>
   );
 };
 

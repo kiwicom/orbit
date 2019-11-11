b13b5b6b33fc9bd83111f87bd3dc46db25da68dd
filipdanisko <filip@danisko.cz>
2019-11-11 17:53:27 +0100

    NEW: Script for generating icons in PNG

---
+++ config/generatePngIcons.js
@@ -0,0 +1,93 @@
+// @flow
+
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+const { defaultTokens } = require("@kiwicom/orbit-design-tokens");
+
+const DIR = path.join(__dirname, "../orbit-email-icons");
+const sizesToGenerate = [32, 48];
+const colors = [
+  ["paletteWhite", defaultTokens.paletteWhite],
+  ["paletteInkLight", defaultTokens.paletteInkLight],
+  ["paletteInkLighter", defaultTokens.paletteInkLighter],
+  ["paletteInkNormal", defaultTokens.paletteInkNormal],
+  ["paletteOrangeNormal", defaultTokens.paletteOrangeNormal],
+  ["paletteRedNormal", defaultTokens.paletteRedNormal],
+  ["paletteGreenNormal", defaultTokens.paletteGreenNormal],
+  ["paletteBlueNormal", defaultTokens.paletteBlueNormal],
+];
+
+async function readFile(pathToFile) {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line func-names
+    fs.readFile(pathToFile, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
+
+async function readDir(pathToDir) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(pathToDir, (err, files) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(files);
+    });
+  });
+}
+
+async function generateIcon(pathToFile, size, color, extraDir) {
+  const nameSplit = pathToFile.split("/");
+  const name = nameSplit[nameSplit.length - 1].replace(".svg", "");
+
+  const file = await readFile(`./src/icons/svg/${pathToFile}`);
+  const updateBuffer = Buffer.from(
+    file.toString().replace(`<svg `, `<svg fill="${color}" `),
+    "utf8",
+  );
+  await sharp(updateBuffer)
+    .resize(size, size)
+    .toFile(`${DIR}/${extraDir}/${size}/${name}.png`);
+
+  return `${DIR}/${extraDir}/${size}/${name}.png`;
+}
+
+(async () => {
+  // Sync paths
+  // TODO: make it nice and should be also awaited, can cause error of not being created before it want's to write.
+  if (!fs.existsSync(DIR)) {
+    fs.mkdirSync(DIR);
+  }
+  colors.forEach(el => {
+    if (!fs.existsSync(`${DIR}/${el[0]}`)) {
+      fs.mkdirSync(`${DIR}/${el[0]}`);
+    }
+  });
+  sizesToGenerate.forEach(size => {
+    colors.forEach(color => {
+      if (!fs.existsSync(`${DIR}/${color[0]}/${size}`)) {
+        fs.mkdirSync(`${DIR}/${color[0]}/${size}`);
+      }
+    });
+  });
+
+  const files = await readDir("./src/icons/svg/");
+
+  const promises = [];
+
+  // Generate every variant
+  files.forEach(file => {
+    colors.forEach(color => {
+      sizesToGenerate.forEach(size => {
+        promises.push(generateIcon(file, size, color[1], color[0]));
+      });
+    });
+  });
+
+  await Promise.all(promises);
+})();

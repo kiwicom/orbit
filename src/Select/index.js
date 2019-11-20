a96f5d4f9a9f382552380ee8ecc060fa9e6ad925
Filip Danisko <filip@danisko.cz>
2019-11-20 11:25:20 +0100

    FIX: Select autofill text color (#1374)

--- src/Select/index.js
+++ src/Select/index.js
@@ -146,6 +146,11 @@ const StyledSelect = styled(
       box-shadow: inset 0 0 0 1px ${({ theme }) => theme.orbit.borderColorInput};
     }
   }
+
+  &:-webkit-autofill,
+  &:-internal-autofill-selected {
+    -webkit-text-fill-color: transparent;
+  }
 `;
 
 StyledSelect.defaultProps = {

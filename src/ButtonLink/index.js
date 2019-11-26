d2f5acec9b5f71bb3c50c27a745c555605378ced
Jaroslav Kubíček <jaroslav.kubicek@gmail.com>
2019-11-26 17:17:35 +0100

    DEV: Use @adeira/js instead of @kiwicom/js (#1386)

--- src/ButtonLink/index.js
+++ src/ButtonLink/index.js
@@ -1,7 +1,7 @@
 // @flow
 import * as React from "react";
 import styled, { css } from "styled-components";
-import { warning } from "@kiwicom/js";
+import { warning } from "@adeira/js";
 
 import defaultTheme from "../defaultTheme";
 import { TYPES, SIZES, TOKENS, BUTTON_STATES } from "./consts";

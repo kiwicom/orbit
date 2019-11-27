43e91b72f7a402c0738d2721695aa6fe57c2bca7
filipdanisko <filip@danisko.cz>
2019-11-27 15:12:07 +0100

    Popover overflow

--- src/Popover/components/ContentWrapper.js
+++ src/Popover/components/ContentWrapper.js
@@ -45,7 +45,7 @@ StyledContentWrapper.defaultProps = {
 };
 
 const StyledActions = styled.div`
-  margin-top: ${({ theme }) => actionsSpace(theme)};
+  padding: ${({ noPadding, theme }) => (noPadding ? 0 : actionsSpace(theme))};
 `;
 
 StyledActions.defaultProps = {
@@ -63,7 +63,6 @@ const StyledPopoverParent = styled.div`
   border-top-left-radius: 9px; /* TODO: Add token */
   border-top-right-radius: 9px; /* TODO: Add token */
   background-color: ${({ theme }) => theme.orbit.backgroundModal}; // TODO: Add token
-  padding: ${({ noPadding, theme }) => (noPadding ? 0 : popoverPadding(theme))};
   box-shadow: ${({ theme }) => theme.orbit.boxShadowRaisedReverse};
   z-index: 1000;
   transition: ${transition(["opacity", "transform"], "fast", "ease-in-out")};
@@ -93,6 +92,15 @@ StyledPopoverParent.defaultProps = {
   theme: defaultTheme,
 };
 
+const StyledPopoverPadding = styled.div`
+  padding: ${({ noPadding, theme }) => (noPadding ? 0 : popoverPadding(theme))};
+  padding-bottom: 0;
+`;
+
+StyledPopoverPadding.defaultProps = {
+  theme: defaultTheme,
+};
+
 const StyledPopoverContent = styled.div``;
 
 const StyledOverlay = styled.div`
@@ -118,7 +126,7 @@ StyledOverlay.defaultProps = {
 
 const StyledPopoverClose = styled.div`
   padding: ${({ noPadding, theme }) => (noPadding ? popoverPadding(theme) : 0)};
-  padding-top: ${({ theme }) => popoverPadding(theme)};
+  /* padding-top: ${({ theme }) => popoverPadding(theme)}; */
 
   ${media.largeMobile(css`
     display: none;
@@ -194,8 +202,9 @@ const PopoverContentWrapper = ({
       >
         <StyledPopoverContent ref={content}>
           <StyledContentWrapper actionsHeight={actionsDimensions && actionsDimensions.height}>
-            {children}
+            <StyledPopoverPadding noPadding={noPadding}>{children}</StyledPopoverPadding>
           </StyledContentWrapper>
+
           {actions ? (
             <StyledActions ref={actionsRef}>{actions}</StyledActions>
           ) : (

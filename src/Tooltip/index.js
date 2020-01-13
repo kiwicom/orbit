37e2187c6c50e02e11ac2a4c304ee5da9e4d1978
Luděk Vepřek <weprous@gmail.com>
2020-01-13 09:14:18 +0100

    FIX: Not enabled Tooltip calling handlers (#1423)

--- src/Tooltip/index.js
+++ src/Tooltip/index.js
@@ -91,6 +91,7 @@ const Tooltip = ({
     setShownMobile(false);
     setRenderWithTimeout(false);
   }, [setRenderWithTimeout, setShownMobile]);
+  if (!enabled) return children;
   return (
     <React.Fragment>
       <StyledTooltipChildren

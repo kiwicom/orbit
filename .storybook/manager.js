0e6055cd66ed80c9eaaf93be0ba9b2ca8a66720b
Luděk Vepřek <weprous@gmail.com>
2020-01-17 14:29:18 +0100

    update theme

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,8 +1,10 @@
 import { create } from "@storybook/theming"
+import "@storybook/addon-actions/register";
+import "@storybook/addon-knobs/register";
 
 export default create({
-  base: "light",
   brandTitle: "Orbit",
   brandUrl: "https://orbit.kiwi",
   brandImage: "https://orbit.kiwi/files/2019/08/cropped-OrbitLogo-1.png"
 })
+

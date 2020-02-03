d8514dd3227c2ce15092690549a00c02d7e4cb5e
Luděk Vepřek <weprous@gmail.com>
2020-02-03 18:06:05 +0100

    DEV: Fix Storybook webpack build

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -7,17 +7,9 @@
 // to "React Create App". This only has babel loader to load JavaScript.
 const babelOptions = {
   presets: [
-    "@babel/react",
-    "@babel/flow",
-    ["@babel/env", { modules: false, targets: { browsers: ["last 2 versions", "ie >= 11", "Edge >= 38"] } }],
+    ["@adeira/babel-preset-adeira", { target: "js", environments: { browsers: ["last 2 versions", "ie >= 11", "Edge >= 38"] } }],
   ],
-  plugins: [
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-proposal-nullish-coalescing-operator",
-    "@babel/plugin-proposal-optional-chaining",
-    "babel-plugin-styled-components"
-  ],
+  plugins: ["babel-plugin-styled-components"],
   babelrc: false,
 }
 

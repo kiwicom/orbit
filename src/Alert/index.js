5addc6c4bf93aa4f34bdcdf3d72c8c36ffea9629
Luděk Vepřek <weprous@gmail.com>
2020-03-05 10:09:19 +0100

    DEV: Bump prettier from 1.16.4 to 1.19.1 (#1628)

--- src/Alert/index.js
+++ src/Alert/index.js
@@ -121,9 +121,7 @@ const StyledAlert = styled(StyledDiv)`
   padding: ${({ theme, closable }) =>
     closable
       ? rtlSpacing(
-          `${theme.orbit.spaceXSmall} ${theme.orbit.spaceXLarge} ${theme.orbit.spaceXSmall} ${
-            theme.orbit.spaceXSmall
-          }`,
+          `${theme.orbit.spaceXSmall} ${theme.orbit.spaceXLarge} ${theme.orbit.spaceXSmall} ${theme.orbit.spaceXSmall}`,
         )
       : theme.orbit.spaceXSmall};
 
@@ -131,9 +129,7 @@ const StyledAlert = styled(StyledDiv)`
     padding: ${({ theme, closable }) =>
       closable
         ? rtlSpacing(
-            `${theme.orbit.paddingAlert} ${theme.orbit.spaceXXLarge} ${theme.orbit.paddingAlert} ${
-              theme.orbit.paddingAlert
-            }`,
+            `${theme.orbit.paddingAlert} ${theme.orbit.spaceXXLarge} ${theme.orbit.paddingAlert} ${theme.orbit.paddingAlert}`,
           )
         : theme.orbit.paddingAlert};
   `)}

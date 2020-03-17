155c17d452711a8a9ac3e5bd16de1fad5f643b9b
filipdanisko <filip@danisko.cz>
2020-03-17 16:23:57 +0100

    removing duplicity

--- .eslintTS.js
+++ .eslintTS.js
@@ -1,20 +1,19 @@
-module.exports =  {
-  parser:  "@typescript-eslint/parser",
-  extends:  [
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
     "plugin:prettier/recommended",
-    'plugin:prettier/recommended',
   ],
   plugins: ["import", "react"],
-  parserOptions:  {
-    ecmaVersion:  2018,
-    sourceType:  "module",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
 };

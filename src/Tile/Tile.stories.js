34d35f73da07d6621b687f572bc96ae875f1d8b2
Luděk Vepřek <weprous@gmail.com>
2020-03-24 17:04:22 +0100

    feat(Tile): introduce noHeaderIcon property (#1710)

--- src/Tile/Tile.stories.js
+++ src/Tile/Tile.stories.js
@@ -46,12 +46,14 @@ storiesOf("Tile", module)
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
       );
       const Icon = getIcon(getIcons("Airplane"));
+      const noHeaderIcon = boolean("noHeaderIcon", false);
       return (
         <Tile
           onClick={action("clicked")}
           icon={Icon && <Icon />}
           title={title}
           description={description}
+          noHeaderIcon={noHeaderIcon}
         />
       );
     },

b01f82e0498637dfc682a7d01ebe1653534f0a94
vepor <weprous@gmail.com>
2020-04-06 12:35:04 +0200

    revert: use spread

--- src/hooks/useBoundingRect/index.js
+++ src/hooks/useBoundingRect/index.js
@@ -4,22 +4,17 @@ import * as React from "react";
 import type { UseBoundingRect } from ".";
 
 const useBoundingRect: UseBoundingRect = initialValue => {
-  /* eslint-disable prefer-object-spread */
-  const [state, setState] = React.useState(() =>
-    Object.assign(
-      {
-        x: 0,
-        y: 0,
-        width: 0,
-        height: 0,
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-      },
-      initialValue,
-    ),
-  );
+  const [state, setState] = React.useState(() => ({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    ...initialValue,
+  }));
 
   const ref = React.useRef<?HTMLElement>(null);
   React.useEffect(() => {

1a98f2957515197025ac494483519482bffb0cf3
vepor <weprous@gmail.com>
2020-04-06 10:12:23 +0200

    feat: introduce useBoundingRect hook

---
+++ src/hooks/useBoundingRect/index.js
@@ -0,0 +1,40 @@
+// @flow
+import * as React from "react";
+
+import type { UseBoundingRect } from ".";
+
+const DEFAULT_VALUE = {
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0,
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+};
+
+const useBoundingRect: UseBoundingRect = initialValue => {
+  const [state, setState] = React.useState(() => ({ ...DEFAULT_VALUE, ...initialValue }));
+
+  const ref = React.useRef<?HTMLElement>(null);
+  React.useEffect(() => {
+    const calculate = () => {
+      if (ref && ref.current) {
+        const dimensions = ref.current.getBoundingClientRect();
+        /* $FlowFixMe
+        Somehow the Flow is confused and returns type ClientRect instead of DOMRect and therefore it expects that it's a function.
+        */
+        setState(dimensions);
+      }
+    };
+    calculate();
+    window.addEventListener("resize", calculate);
+    return () => {
+      window.removeEventListener("resize", calculate);
+    };
+  }, []);
+  return [state, ref];
+};
+
+export default useBoundingRect;

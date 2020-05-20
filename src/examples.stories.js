84f01fbb5766a828055dae0db2f4485b2062a64a
Luděk Vepřek <weprous@gmail.com>
2020-05-20 15:23:45 +0200

    docs(Examples): add stories for live preview and visual regression (#1870)

---
+++ src/examples.stories.js
@@ -0,0 +1,17 @@
+// @flow
+
+import { storiesOf } from "@storybook/react";
+
+import * as examples from "./examples";
+
+const storyFn = storiesOf("Examples", module);
+
+Object.keys(examples).forEach(key => {
+  const {
+    Example,
+    info: { description },
+  } = examples[key];
+  storyFn.add(key, Example, {
+    info: description,
+  });
+});

42a9f8c2904a6b67a7d43cf00d6215a5fce93fae
vepor <weprous@gmail.com>
2020-05-22 15:23:51 +0200

    feat(Storybook): integration of Playroom

--- .storybook/orbitDecorator.js
+++ .storybook/orbitDecorator.js
@@ -3,6 +3,24 @@ import Heading from "../src/Heading"
 import Text from "../src/Text"
 import jsxToString from "react-element-to-jsx-string"
 import { Code } from "@storybook/addon-info/dist/components/markdown/"
+import { ActionBar } from "@storybook/components"
+import { ThemeProvider, convert } from "@storybook/theming";
+import styled from "styled-components";
+import lzString from "lz-string";
+
+const PlayroomButton = styled(ActionBar)`
+  &&& {
+    top: 1px;
+    right: 1px;
+    bottom: auto;
+
+    > button {
+      border-top: 0;
+      border-bottom: 1px solid rgba(0,0,0,.1);
+      border-radius: 0 0 0 4px;
+    }
+  }
+`;
 
 const orbitDecorator = (storyFn, context) => {
   const children = storyFn(context)
@@ -12,14 +30,27 @@ const orbitDecorator = (storyFn, context) => {
       return "function()";
     },
   };
+  const code = jsxToString(children, options);
+
+  const openInPlayroom = () => {
+    const compressed = lzString.compressToEncodedURIComponent(JSON.stringify({code}));
+    const basePath = process.env.NODE_ENV === 'production' ? '/playroom/' : "http://localhost:9000/";
+    const url = `${basePath}#?code=${compressed}`;
+    const win = window.open(url, '_blank');
+    win.focus();
+  };
+
   return  (
     <div style={{ padding: "20px" }}>
       <Heading spaceAfter="medium">{context.kind}</Heading>
       <Text spaceAfter="largest">{context.parameters?.info}</Text>
       {children}
-      <div style={{ marginTop: 20}}>
+      <div style={{ marginTop: 20, position: "relative" }}>
+        <ThemeProvider theme={convert()}>
+          <PlayroomButton actionItems={[{ title: "Open in Playroom", onClick: openInPlayroom }]} />
+        </ThemeProvider>
         <React.Fragment>
-          <Code code={jsxToString(children, options)} language="jsx" format={false} />
+          <Code code={code} language="jsx" format={false} />
         </React.Fragment>
       </div>
     </div>
---
+++ playroom.config.js
@@ -0,0 +1,16 @@
+// @flow
+module.exports = {
+  components: "./lib",
+  outputPath: "./.out/playroom",
+  widths: [320, 992],
+  title: "Playroom for Kiwi.com's Orbit components",
+  openBrowser: false,
+  exampleCode: `
+      <Card>
+    <CardSection>
+      <Heading type="title3" element="h3">
+        Start editing to see some magic happen!
+      </Heading>
+    </CardSection>
+  </Card>`,
+};

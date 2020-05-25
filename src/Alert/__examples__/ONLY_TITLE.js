d7e8e7b48c7439891ace5a98384fbd672e182cd3
Aaron Collier <aaron@collier.cz>
2020-05-25 15:42:31 +0200

    docs(Examples): update alert examples

--- src/Alert/__examples__/ONLY_TITLE.js
+++ src/Alert/__examples__/ONLY_TITLE.js
@@ -7,7 +7,6 @@ export default {
   Example: () => <Alert title="You're got mail" />,
   info: {
     title: "Alert with only a title",
-    description:
-      "If you have a short, clear message, use an alert with only a title.",
+    description: "If you have a short, clear message, use an alert with only a title.",
   },
 };

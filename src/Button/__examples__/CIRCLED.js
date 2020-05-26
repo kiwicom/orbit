e02da9ddf07d05ad18e84f645238d3536d950189
Aaron Collier <aaron@collier.cz>
2020-05-26 10:35:46 +0200

    docs(Examples): fix circled button numbers

--- src/Button/__examples__/CIRCLED.js
+++ src/Button/__examples__/CIRCLED.js
@@ -9,26 +9,20 @@ import * as Icons from "../../icons";
 export default {
   Example: () => (
     <Stack spacing="loose">
+      <Button iconLeft={<Icons.Edit />} circled title="Edit this page" />
       <Stack spacing="condensed">
         <Text>How much do you like Orbit?</Text>
         <Stack direction="row">
-          <Button type="primarySubtle" circled>
-            1
-          </Button>
-          <Button type="primarySubtle" circled>
-            2
-          </Button>
-          <Button type="primarySubtle" circled>
-            3
-          </Button>
+          <Button type="primarySubtle" circled iconLeft="1" />
+          <Button type="primarySubtle" circled iconLeft="2" />
+          <Button type="primarySubtle" circled iconLeft="3" />
         </Stack>
       </Stack>
-      <Button iconLeft={<Icons.Edit />} circled title="Edit this page" />
     </Stack>
   ),
   info: {
     title: "Circled button",
     description:
-      "Circled buttons should be used only with a single number or icon. If using only an icon, remember to include a title.",
+      "Circled buttons should be used only with an icon or a single number. If using only an icon, remember to include a title.",
   },
 };

03511b4f0f131a807f2c37123e97253f0d70448e
filipdanisko <filip@danisko.cz>
2020-06-01 12:02:26 +0200

    feat(TableCell): adding white-space and vertical align options

--- src/Table/TableCell/index.d.ts
+++ src/Table/TableCell/index.d.ts
@@ -7,9 +7,21 @@ import { SharedProps } from "../index";
 declare module "@kiwicom/orbit-components/lib/Table/TableCell";
 
 type Align = "left" | "center" | "right";
+type WhiteSpace = "nowrap" | "pre" | "pre-line" | "pre-wrap";
+type VerticalAlign =
+  | "baseline"
+  | "sub"
+  | "super"
+  | "text-top"
+  | "text-bottom"
+  | "middle"
+  | "top"
+  | "bottom";
 
 interface Props extends SharedProps {
   readonly align?: Align;
+  readonly whiteSpace?: WhiteSpace;
+  readonly verticalAlign?: VerticalAlign;
 }
 
 declare const TableCell: React.FunctionComponent<Props>;
--- src/Table/TableCell/index.js
+++ src/Table/TableCell/index.js
@@ -18,15 +18,28 @@ export const StyledTableCell = styled(({ children, className, dataTest }) => (
   font-size: ${({ theme }) => theme.orbit.fontSizeTextNormal};
   color: ${({ theme }) => theme.orbit.colorInkNormal};
   text-align: ${({ align }) => textAlign(align)};
+  white-space: ${({ whiteSpace }) => whiteSpace};
+  vertical-align: ${({ verticalAlign }) => verticalAlign};
 `;
 
 StyledTableCell.defaultProps = {
   theme: defaultTheme,
 };
 
-const TableCell = ({ align = ALIGN_OPTIONS.CENTER, dataTest, children }: Props) => {
+const TableCell = ({
+  align = ALIGN_OPTIONS.CENTER,
+  verticalAlign,
+  whiteSpace,
+  dataTest,
+  children,
+}: Props) => {
   return (
-    <StyledTableCell align={align} dataTest={dataTest}>
+    <StyledTableCell
+      verticalAlign={verticalAlign}
+      whiteSpace={whiteSpace}
+      align={align}
+      dataTest={dataTest}
+    >
       {children}
     </StyledTableCell>
   );

e8d72e6043396f219702c2d4c61285c49b2cfa00
Aaron Collier <aaron@collier.cz>
2020-06-10 18:06:15 +0200

    docs: add examples for badge (#1937)

---
+++ src/Badge/__examples__/WHITE.js
@@ -0,0 +1,30 @@
+// @flow
+import * as React from "react";
+
+import Badge from "../index";
+import PictureCard from "../../PictureCard";
+import * as Icons from "../../icons";
+
+export default {
+  Example: () => (
+    <PictureCard
+      image={{
+        code: "moscow_ru",
+        name: "moscow_ru",
+        original: "385x320",
+        placeholder: "385x320",
+      }}
+      height="300px"
+      width="400px"
+    >
+      <Badge type="white" icon={<Icons.Passengers />} ariaLabel="4 passengers">
+        4
+      </Badge>
+    </PictureCard>
+  ),
+  info: {
+    title: "White badges",
+    description:
+      "White badges are useful when presenting neutral information within a dark context.",
+  },
+};

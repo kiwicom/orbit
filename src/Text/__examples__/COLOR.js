1e268f814686d905954fc0e20e36b7b116ae17bc
Aaron Collier <aaron@collier.cz>
2020-06-18 10:45:11 +0200

    docs: add text examples

---
+++ src/Text/__examples__/COLOR.js
@@ -0,0 +1,19 @@
+// @flow
+import * as React from "react";
+
+import Text from "../index";
+import Stack from "../../Stack";
+
+export default {
+  Example: () => (
+    <Stack flex>
+      <Text>Primary text</Text>
+      <Text type="secondary">Secondary text</Text>
+    </Stack>
+  ),
+  info: {
+    title: "Text color",
+    description:
+      "There are two main colors to work with: primary (Ink / Normal) and secondary (Ink / Light).",
+  },
+};

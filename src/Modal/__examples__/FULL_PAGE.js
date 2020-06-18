fb573692ce4f6e2e5c3c8995496aef9105482282
Aaron Collier <aaron@collier.cz>
2020-06-18 20:35:34 +0200

    docs: add modal examples

---
+++ src/Modal/__examples__/FULL_PAGE.js
@@ -0,0 +1,42 @@
+// @flow
+import * as React from "react";
+
+import Button from "../../Button";
+import Modal from "../index";
+import ModalSection from "../ModalSection";
+
+export default {
+  Example: () => {
+    const [showModal, setShowModal] = React.useState(true);
+    return (
+      <>
+        {showModal && (
+          <Modal
+            isMobileFullPage
+            onClose={() => {
+              setShowModal(false);
+            }}
+          >
+            <ModalSection>
+              This modal will cover the entire screen when the window width is small.
+            </ModalSection>
+          </Modal>
+        )}
+        {!showModal && (
+          <Button
+            onClick={() => {
+              setShowModal(true);
+            }}
+          >
+            Show modal
+          </Button>
+        )}
+      </>
+    );
+  },
+  info: {
+    title: "Full page modal",
+    description:
+      "If you have lots to show users and the context isn't as important, set isModalFullPage to true and the modal will cover the whole screen on small devices.",
+  },
+};

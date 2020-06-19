bbe5692c5693b7aa2c2137d4f1871f4dd3aefb1c
Aaron Collier <aaron@collier.cz>
2020-06-19 11:27:26 +0200

    docs: add InputField examples

---
+++ src/InputField/__examples__/ERROR.js
@@ -0,0 +1,21 @@
+// @flow
+import * as React from "react";
+
+import InputField from "../index";
+
+export default {
+  Example: () => (
+    <InputField
+      label="Password"
+      placeholder="paSsw0rd"
+      type="password"
+      help="Use at least one uppercase letter and one number"
+      error="Your password must contain a number"
+    />
+  ),
+  info: {
+    title: "Error messages",
+    description:
+      "Include error messages when validation isn't passed, either on change in focus or form submission. Note that an error message will override any help text.",
+  },
+};
---
+++ src/InputField/__examples__/PREFIX.js
@@ -0,0 +1,25 @@
+// @flow
+import * as React from "react";
+
+import InputField from "../index";
+import Stack from "../../Stack";
+import * as Icons from "../../icons";
+
+export default {
+  Example: () => (
+    <Stack direction="column">
+      <InputField label="Maximum price" type="number" prefix="$" />
+      <InputField
+        label="Search"
+        placeholder="Search by keyword"
+        type="text"
+        inputMode="search"
+        prefix={<Icons.Search />}
+      />
+    </Stack>
+  ),
+  info: {
+    title: "Prefixes",
+    description: "If you need additional context, you can add it as a prefix.",
+  },
+};

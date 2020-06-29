4491246a67332a1a418ad51dff39de9d8490815b
Aaron Collier <aaron@collier.cz>
2020-06-29 16:52:18 +0200

    docs: add Popover examples (#1965)

---
+++ src/Popover/__examples__/OTHER_CONTENT.js
@@ -0,0 +1,51 @@
+// @flow
+import * as React from "react";
+
+import Button from "../../Button";
+import ListChoice from "../../ListChoice";
+import Popover from "../index";
+import Stack from "../../Stack";
+import * as Icons from "../../icons";
+
+export default {
+  Example: () => {
+    const [is2star, setIs2star] = React.useState(false);
+    const [is3star, setIs3star] = React.useState(false);
+    const [is4star, setIs4star] = React.useState(false);
+    return (
+      <Popover
+        content={
+          <Stack spacing="compact">
+            <ListChoice
+              title="2 star hotels"
+              selectable
+              selected={is2star}
+              onClick={() => setIs2star(!is2star)}
+              icon={<Icons.Accommodation />}
+            />
+            <ListChoice
+              title="3 star hotets"
+              selectable
+              selected={is3star}
+              onClick={() => setIs3star(!is3star)}
+              icon={<Icons.Accommodation />}
+            />
+            <ListChoice
+              title="4 star hotets"
+              selectable
+              selected={is4star}
+              onClick={() => setIs4star(!is4star)}
+              icon={<Icons.Accommodation />}
+            />
+          </Stack>
+        }
+      >
+        <Button circled title="Accommodation" iconLeft={<Icons.Accommodation />} />
+      </Popover>
+    );
+  },
+  info: {
+    title: "Other content",
+    description: "Popovers are very flexible in terms of the content they accept.",
+  },
+};

fd1323d6e31d4add25ad9d12e5213a4a333f4707
Aaron Collier <aaron@collier.cz>
2020-06-29 16:57:49 +0200

    docs: add Checkbox+Radio examples (#1963)

---
+++ src/Radio/__examples__/ERROR.js
@@ -0,0 +1,31 @@
+// @flow
+import * as React from "react";
+
+import Button from "../../Button";
+import Radio from "../index";
+import Stack from "../../Stack";
+
+export default {
+  Example: () => {
+    const [checked, setChecked] = React.useState(false);
+    return (
+      <Stack>
+        <Radio
+          label="Cabin bag"
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+          info="56 × 45 × 25 cm, 8 kg"
+          hasError
+        />
+        <Button type="secondary" onClick={() => setChecked(false)}>
+          Clear
+        </Button>
+      </Stack>
+    );
+  },
+  info: {
+    title: "Errors",
+    description:
+      "Show users there is an error with a radio by passing the hasError prop. Note that the radio will show the error only when checked and disabled are false.",
+  },
+};
---
+++ src/Radio/__examples__/SHAPE_ONLY.js
@@ -0,0 +1,25 @@
+// @flow
+import * as React from "react";
+
+import Button from "../../Button";
+import Radio from "../index";
+import Stack from "../../Stack";
+
+export default {
+  Example: () => {
+    const [checked, setChecked] = React.useState(false);
+    return (
+      <Stack>
+        <Radio checked={checked} onChange={() => setChecked(!checked)} />
+        <Button type="secondary" onClick={() => setChecked(false)}>
+          Clear
+        </Button>
+      </Stack>
+    );
+  },
+  info: {
+    title: "Shape only",
+    description:
+      "In some cases, you may want to include only the shape of the radio. Make sure any visual information about the radio is accessible to everyone.",
+  },
+};

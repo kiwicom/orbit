770b4d65a8bf609b59dfef8df80974815c55802c
Aaron Collier <aaron@collier.cz>
2020-06-29 17:05:17 +0200

    docs: add examples (#1958)
    * docs: add breadcrumbs examples
    
    * fix(Breadcrumbs): unnecessary optional event
    
    * docs: add text examples
    
    * docs: add modal examples
    
    * docs: hide modals for examples
    
    * docs: add Heading examples
    
    * docs: add List examples
    
    * chore: fixing flow
    
    Co-authored-by: vepor <weprous@gmail.com>
    Co-authored-by: filipdanisko <filip@danisko.cz>

---
+++ src/Heading/__examples__/DEFAULT.js
@@ -0,0 +1,12 @@
+// @flow
+import * as React from "react";
+
+import Heading from "../index";
+
+export default {
+  Example: () => <Heading>Orbit components</Heading>,
+  info: {
+    title: "Default heading",
+    description: "By default, headings are rendered as divs in the title1 format.",
+  },
+};

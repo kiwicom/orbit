c7411ddf4f75383ef9303b804e56bdbf62190a2e
Aaron Collier <aaron@collier.cz>
2020-06-29 16:33:53 +0200

    docs: add Select examples (#1964)

---
+++ src/Select/__examples__/DEFAULT.js
@@ -0,0 +1,30 @@
+// @flow
+import * as React from "react";
+
+import Select from "../index";
+
+export default {
+  Example: () => {
+    const [country, setCountry] = React.useState("");
+    return (
+      <Select
+        value={country}
+        placeholder="Choose your country"
+        options={[
+          { label: "Algeria", value: "dz" },
+          { label: "Bolivia", value: "bo" },
+          { label: "Croatia", value: "hr" },
+          { label: "Indonesia", value: "id" },
+          { label: "Mexico", value: "mx" },
+        ]}
+        label="Nationality"
+        onChange={event => setCountry(event.target.value)}
+      />
+    );
+  },
+  info: {
+    title: "Default select",
+    description:
+      "By default, selects offer users choices among listen options with an associated label to guide them.",
+  },
+};

1732b897be804d6105bc6944b5e8a65aaa2d6c2f
Aaron Collier <aaron@collier.cz>
2020-07-10 17:32:08 +0200

    docs: add illustration examples

---
+++ src/Illustration/__examples__/DEFAULT.js
@@ -0,0 +1,13 @@
+// @flow
+import * as React from "react";
+
+import Illustration from "../index";
+
+export default {
+  Example: () => <Illustration name="Accommodation" />,
+  info: {
+    title: "Default illustration",
+    description:
+      "Illustrations by default come in medium. By default, the name of the illustration is used for alt text. This might not be meaningful so include meaningful alt text when appropriate.",
+  },
+};

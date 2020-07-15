2a9e66a57400b628babe615f6665bda5bf8eabb3
vepor <weprous@gmail.com>
2020-07-15 12:19:45 +0200

    fix(Layout): inner usage of Card – edge to edge behavior

--- src/Layout/LayoutColumn/index.js
+++ src/Layout/LayoutColumn/index.js
@@ -1,14 +1,27 @@
 // @flow
 import * as React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import defaultTheme from "../../defaultTheme";
 import getViewportHideStyles from "../../Hide/helpers/getViewportHideStyles";
+import { StyledCard } from "../../Card";
+import mq from "../../utils/mediaQuery";
 
 import type { Props } from "./index";
 
 const StyledColumn = styled.div`
   ${({ hideOn }) => !!hideOn && getViewportHideStyles(hideOn)};
+
+  ${StyledCard} {
+    margin: 0 -${({ theme }) => theme.orbit.spaceMedium};
+    width: auto;
+  }
+  ${mq.tablet(css`
+    ${StyledCard} {
+      margin: auto;
+      width: 100%;
+    }
+  `)};
 `;
 
 StyledColumn.defaultProps = {

b49522c251ebe29aecba0c3c576130ff3f17a0f2
Filip Danisko <filip@danisko.cz>
2020-07-29 12:11:13 +0200

    feat(SocialButton): adding twitter type (#2057)

--- src/SocialButton/index.d.ts
+++ src/SocialButton/index.d.ts
@@ -8,7 +8,7 @@ import { ButtonCommonProps } from "../primitives/ButtonPrimitive/index";
 
 declare module "@kiwicom/orbit-components/lib/Button";
 
-type Type = "apple" | "facebook" | "google";
+type Type = "apple" | "facebook" | "google" | "twitter";
 
 type OmittedButtonCommonProps = Exclude<
   ButtonCommonProps,

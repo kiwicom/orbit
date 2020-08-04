17fa9ae6bf716a5021989a628cfcbfbb07e59930
Luděk Vepřek <weprous@gmail.com>
2020-08-04 14:05:32 +0200

    feat(Modal): adding extraSmall size option (#2078)

--- src/Modal/index.d.ts
+++ src/Modal/index.d.ts
@@ -8,8 +8,10 @@ import * as Common from "../common/common";
 
 declare module "@kiwicom/orbit-components/lib/Modal";
 
+type Size = "extraSmall" | "small" | "normal" | "large";
+
 export interface Props extends Common.Global {
-  readonly size?: Common.Size;
+  readonly size?: Size;
   readonly children: React.ReactNode;
   readonly onClose?: Common.Event<
     React.KeyboardEvent<HTMLDivElement> | React.SyntheticEvent<HTMLButtonElement> | React.MouseEvent

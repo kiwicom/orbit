ede11cff61bfb99d1947f2b82e4e71c9ba4c5803
Victor Genaev <rejuv@MacBook-Pro.local>
2020-08-05 19:53:04 +0200

    feat(Hooks): useIntersect

---
+++ src/hooks/useIntersect/index.js
@@ -0,0 +1,31 @@
+// @flow
+import { useState, useEffect, useRef } from "react";
+
+export default ({ root = null, rootMargin, threshold = 0 }: IntersectionObserverOptions) => {
+  if (typeof window === "undefined") {
+    return { ref: null, entry: null };
+  }
+
+  const [entry, updateEntry] = useState<IntersectionObserverEntry | null>(null);
+  const [node, setNode] = useState<Element | null>(null);
+
+  const observer = useRef<null | IntersectionObserver>(null);
+
+  useEffect(() => {
+    if (observer.current) observer.current.disconnect();
+
+    observer.current = new window.IntersectionObserver(([ent]) => updateEntry(ent), {
+      root,
+      rootMargin,
+      threshold,
+    });
+
+    const { current: currentObs } = observer;
+
+    if (node) currentObs?.observe(node);
+
+    return () => currentObs?.disconnect();
+  }, [node, root, rootMargin, threshold]);
+
+  return { ref: setNode, entry };
+};

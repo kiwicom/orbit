604168d4f2a8f9172a9c1cbd374be5cd0a6801da
Aaron Collier <aaron@collier.cz>
2020-08-20 13:12:32 +0200

    docs: add examples for drawer (#2079)

---
+++ src/Drawer/__examples__/POSITION.js
@@ -0,0 +1,78 @@
+// @flow
+import * as React from "react";
+
+import Button from "../../Button";
+import Drawer from "../index";
+import LinkList from "../../LinkList";
+import Stack from "../../Stack";
+import TextLink from "../../TextLink";
+import * as Icons from "../../icons";
+
+export default {
+  Example: () => {
+    const [showLeftDrawer, setShowLeftDrawer] = React.useState(false);
+    const [showRightDrawer, setShowRightDrawer] = React.useState(false);
+    const content = (
+      <LinkList>
+        <TextLink href="https://orbit.kiwi/components/drawer/" external type="secondary">
+          Drawer guidelines
+        </TextLink>
+        <TextLink href="https://orbit.kiwi/components/drawer/react/" external type="secondary">
+          Drawer React API
+        </TextLink>
+        <TextLink
+          href="https://github.com/kiwicom/orbit-components/tree/master/src/Drawer"
+          external
+          type="secondary"
+        >
+          Drawer on GitHub
+        </TextLink>
+      </LinkList>
+    );
+    return (
+      <>
+        {showLeftDrawer && (
+          <Drawer
+            position="left"
+            onClose={() => {
+              setShowLeftDrawer(false);
+            }}
+            shown={showLeftDrawer}
+          >
+            {content}
+          </Drawer>
+        )}
+        <Stack justify="center">
+          <Button
+            iconLeft={<Icons.ChevronLeft />}
+            title="Open left drawer"
+            onClick={() => {
+              setShowLeftDrawer(true);
+            }}
+          />
+          <Button
+            iconLeft={<Icons.ChevronRight />}
+            title="Open right drawer"
+            onClick={() => {
+              setShowRightDrawer(true);
+            }}
+          />
+        </Stack>
+        {showRightDrawer && (
+          <Drawer
+            onClose={() => {
+              setShowRightDrawer(false);
+            }}
+            shown={showRightDrawer}
+          >
+            {content}
+          </Drawer>
+        )}
+      </>
+    );
+  },
+  info: {
+    title: "Position",
+    description: "Drawers can appear on the left or the right (the default).",
+  },
+};

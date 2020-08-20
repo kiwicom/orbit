bbd285b5afc44f7b532ac84f573b3d3fcf244bc2
Aaron Collier <aaron@collier.cz>
2020-08-20 14:31:57 +0200

    docs: add social and mobile store button examples (#2108)

---
+++ src/SocialButton/__examples__/DEFAULT.js
@@ -0,0 +1,12 @@
+// @flow
+import * as React from "react";
+
+import SocialButton from "../index";
+
+export default {
+  Example: () => <SocialButton>Sign in with Apple </SocialButton>,
+  info: {
+    title: "Default social button",
+    description: "The default social button is an Apple button.",
+  },
+};

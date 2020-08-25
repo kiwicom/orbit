17bbec7b12fea1f4b88b8c506657cebbce2c727f
Albert Alises <albert.alises@gmail.com>
2020-08-25 11:11:23 +0200

    feat(MediaQuery): add Typescript types for consts (#2160)

--- src/utils/mediaQuery/index.d.ts
+++ src/utils/mediaQuery/index.d.ts
@@ -3,18 +3,10 @@
 // Project: http://github.com/kiwicom/orbit-components
 import { Interpolation } from "styled-components";
 import { ThemeShape } from "../../defaultTheme";
-
+import { Devices } from "./consts";
 declare module "@kiwicom/orbit-components/lib/utils/mediaQuery";
 
 type QueryFunction = (style: Interpolation<any>) => Interpolation<any>;
-
-export type Devices =
-  | "largeDesktop"
-  | "desktop"
-  | "tablet"
-  | "largeMobile"
-  | "mediumMobile"
-  | "smallMobile";
 
 declare const MediaQuery: { [key in Devices]: QueryFunction };
 declare const getBreakpointWidth: (name: string, theme: ThemeShape, pure?: boolean) => string;

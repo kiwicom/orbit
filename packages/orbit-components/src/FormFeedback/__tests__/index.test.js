b94c3e4ee3c3d1b6c9be26a21ece7b7ebdbcd009
filipdanisko <filip@danisko.cz>
2020-08-27 15:24:30 +0200

    chore: fixing tests

--- packages/orbit-components/src/FormFeedback/__tests__/index.test.js
+++ packages/orbit-components/src/FormFeedback/__tests__/index.test.js
@@ -4,11 +4,25 @@ import { shallow } from "enzyme";
 
 import FormFeedback from "../index";
 
-describe("FormFeedback", () => {
-  const error = "test";
-  const component = shallow(<FormFeedback error={error} />);
+describe("FormFeedback Error", () => {
+  const error = "error message";
+  const help = "help message";
+  const dataTest = "dataTest";
+  const component = shallow(<FormFeedback error={error} help={help} dataTest={dataTest} />);
 
   it("should have data-test", () => {
-    expect(component.render().prop("data-test")).toBe(error);
+    expect(component.render().prop("data-test")).toBe(dataTest);
+  });
+  it("should render error", () => {
+    expect(component.text()).toEqual(error);
+  });
+});
+
+describe("FormFeedback Help", () => {
+  const help = "test";
+  const component = shallow(<FormFeedback error={undefined} help={help} />);
+
+  it("should render help", () => {
+    expect(component.text()).toEqual(help);
   });
 });

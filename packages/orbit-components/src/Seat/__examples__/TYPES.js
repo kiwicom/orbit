04b15b3768f2d2b878ecdde806191f6d52c3c403
Aaron Collier <aaron@collier.cz>
2020-09-08 15:00:13 +0200

    docs: add examples for Seat (#2159)

---
+++ packages/orbit-components/src/Seat/__examples__/TYPES.js
@@ -0,0 +1,41 @@
+// @flow
+import * as React from "react";
+
+import Heading from "../../Heading";
+import Seat from "../index";
+import Stack from "../../Stack";
+
+export default {
+  Example: () => (
+    <Stack flex>
+      <Stack spacing="tight">
+        <Heading as="h3" type="title3">
+          Default
+        </Heading>
+        <Seat />
+      </Stack>
+      <Stack spacing="tight">
+        <Heading as="h3" type="title3">
+          Extra legroom
+        </Heading>
+        <Seat type="legroom" />
+      </Stack>
+      <Stack spacing="tight">
+        <Heading as="h3" type="title3">
+          Selected
+        </Heading>
+        <Seat type="selected" />
+      </Stack>
+      <Stack spacing="tight">
+        <Heading as="h3" type="title3">
+          Unavailable
+        </Heading>
+        <Seat type="unavailable" />
+      </Stack>
+    </Stack>
+  ),
+  info: {
+    title: "Types",
+    description: "The types of seats indicate information about the possible options.",
+  },
+};

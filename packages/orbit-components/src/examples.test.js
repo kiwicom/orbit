005e4181e48129ccf514f3147e7e3c3fd251e6e9
vepor <weprous@gmail.com>
2020-09-09 11:30:20 +0200

    docs: add examples for NavigationBar

--- packages/orbit-components/src/examples.test.js
+++ packages/orbit-components/src/examples.test.js
@@ -4,6 +4,18 @@ import { shallow } from "enzyme";
 
 import * as examples from "./examples";
 
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 describe("GitHub examples", () => {
   test.each(Object.keys(examples).map(k => [examples[k].info.title, examples[k].Example]))(
     "Rendering example %p",

5501aec47e7069284381a8caed497ca5b901b4ac
Aaron Collier <aaron@collier.cz>
2020-09-09 11:10:37 +0200

    docs: add examples for Table (#2197)

---
+++ packages/orbit-components/src/Table/__examples__/DEFAULT.js
@@ -0,0 +1,41 @@
+// @flow
+import * as React from "react";
+
+import Table from "../index";
+import TableCell from "../TableCell";
+import TableBody from "../TableBody";
+import TableHead from "../TableHead";
+import TableRow from "../TableRow";
+import * as Icons from "../../icons";
+
+export default {
+  Example: () => (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell as="th">Baggage</TableCell>
+          <TableCell as="th">Availability</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell>
+            <Icons.BaggageCabin /> Cabin baggage
+          </TableCell>
+          <TableCell>Free</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <Icons.BaggageChecked /> Checked baggage
+          </TableCell>
+          <TableCell>Paid-for option</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  ),
+  info: {
+    title: "Default table",
+    description:
+      "Tables require at least a few kinds of children. By default, the cells are center aligned and the rows alternate background colors.",
+  },
+};

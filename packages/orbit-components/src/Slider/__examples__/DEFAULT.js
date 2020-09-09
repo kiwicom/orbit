c18c306ca32193bf99736f77bb82797e79fbfed0
Aaron Collier <aaron@collier.cz>
2020-09-09 09:48:55 +0200

    docs: add examples for Slider (#2084)

---
+++ packages/orbit-components/src/Slider/__examples__/DEFAULT.js
@@ -0,0 +1,13 @@
+// @flow
+import * as React from "react";
+
+import Slider from "../index";
+
+export default {
+  Example: () => <Slider label="Volume" minValue={0} maxValue={100} defaultValue={33} />,
+  info: {
+    title: "Default slider",
+    description:
+      "A default slider presents a range in between given minimum and maximum values. Always include a label.",
+  },
+};

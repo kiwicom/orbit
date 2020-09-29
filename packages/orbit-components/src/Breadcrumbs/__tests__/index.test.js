0ac34605c61dfa9ee81c363da4731775f3f0be9c
Matija Marohnić <matija.marohnic@gmail.com>
2020-09-29 16:25:32 +0200

    test(Breadcrumbs): use Testing Library

--- packages/orbit-components/src/Breadcrumbs/__tests__/index.test.js
+++ packages/orbit-components/src/Breadcrumbs/__tests__/index.test.js
@@ -1,55 +1,52 @@
 // @flow
+/* eslint-disable no-restricted-syntax */
 
 import * as React from "react";
-import { shallow } from "enzyme";
-import { render } from "react-dom";
-import { nullthrows } from "@adeira/js";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Breadcrumbs from "../index";
 import BreadcrumbsItem from "../BreadcrumbsItem";
 
-let screen;
-const body = nullthrows(document.body, "Expected document to have a body");
-
-beforeEach(() => {
-  screen = document.createElement("div");
-  body.appendChild(screen);
-});
-
-afterEach(() => {
-  body.removeChild(screen);
-});
+// jsdom doesn't support @media queries, so for <Hide> the computed value of
+// "display" is "none", which @testing-library/react (impressively) detects as
+// inaccessible, so we're replacing <Hide> with a dummy component
+jest.mock("../../Hide", () => ({ children }) => children);
 
 describe("Breadcrumbs", () => {
   const dataTest = "test";
   const onGoBack = jest.fn();
-  const component = shallow(
-    <Breadcrumbs dataTest={dataTest} onGoBack={onGoBack}>
-      <BreadcrumbsItem href="https://kiwi.com">Kiwi.com</BreadcrumbsItem>
-    </Breadcrumbs>,
-  );
-  const wrapper = component.find("Breadcrumbs__StyledBreadcrumbs");
-  const list = component.find("Breadcrumbs__StyledBreadcrumbsList");
+  beforeEach(() => {
+    render(
+      <Breadcrumbs dataTest={dataTest} onGoBack={onGoBack}>
+        <BreadcrumbsItem href="https://kiwi.com">Kiwi.com</BreadcrumbsItem>
+      </Breadcrumbs>,
+    );
+  });
+  afterEach(() => {
+    onGoBack.mockClear();
+  });
   it("nav should contain label, role and data-test", () => {
-    expect(wrapper.render().prop("name")).toBe("nav");
-    expect(wrapper.render().prop("role")).toBe("navigation");
-    expect(wrapper.render().prop("aria-label")).toBe("Breadcrumb");
-    expect(wrapper.render().prop("data-test")).toBe(dataTest);
+    expect(screen.getByRole("navigation")).toBeInTheDocument();
+    expect(screen.getByLabelText("Breadcrumb")).toBeInTheDocument();
+    expect(screen.getByTestId(dataTest)).toBeInTheDocument();
   });
   it("ol should contain correct item type", () => {
-    expect(list.render().prop("name")).toBe("ol");
-    expect(list.render().prop("attribs").itemtype).toContain("BreadcrumbList");
+    expect(screen.getByRole("list")).toHaveAttribute(
+      "itemType",
+      expect.stringContaining("BreadcrumbList"),
+    );
   });
   it("children should contain active and contentKey", () => {
-    expect(list.find("BreadcrumbsItem").prop("active")).toBe(true);
-    expect(list.find("BreadcrumbsItem").prop("contentKey")).toBe(1);
+    expect(screen.getByRole("listitem")).toHaveAttribute("aria-current", "page");
+    expect(document.querySelector("meta")).toHaveAttribute("content", "1");
   });
   it("should execute onGoBack", () => {
-    component.find("GoBackButton").simulate("click");
-    expect(onGoBack).toHaveBeenCalled();
+    for (const backBtn of screen.getAllByRole("button", { name: "Back" })) {
+      userEvent.click(backBtn);
+    }
 
-    component.find("[dataTest='BreadcrumbsBack']").simulate("click");
-    expect(onGoBack).toHaveBeenCalled();
+    expect(onGoBack).toHaveBeenCalledTimes(2);
   });
 
   it("should render as a link when backHref is passed", () => {
@@ -57,10 +54,11 @@ describe("Breadcrumbs", () => {
       <Breadcrumbs backHref="https://orbit.kiwi" dataTest={dataTest} onGoBack={onGoBack}>
         <BreadcrumbsItem href="https://kiwi.com">Kiwi.com</BreadcrumbsItem>
       </Breadcrumbs>,
-      screen,
     );
 
-    expect(screen.querySelector('a[href="https://orbit.kiwi"]')).not.toBeNull();
+    for (const backLink of screen.getAllByRole("link", { name: "Back" })) {
+      expect(backLink).toHaveAttribute("href", "https://orbit.kiwi");
+    }
   });
 
   it("should render as a link when backHref is passed without onGoBack", () => {
@@ -68,10 +66,11 @@ describe("Breadcrumbs", () => {
       <Breadcrumbs backHref="https://orbit.kiwi" dataTest={dataTest}>
         <BreadcrumbsItem href="https://kiwi.com">Kiwi.com</BreadcrumbsItem>
       </Breadcrumbs>,
-      screen,
     );
 
-    expect(screen.querySelector('a[href="https://orbit.kiwi"]')).not.toBeNull();
+    for (const backLink of screen.getAllByRole("link", { name: "Back" })) {
+      expect(backLink).toHaveAttribute("href", "https://orbit.kiwi");
+    }
   });
 });
 
@@ -81,39 +80,52 @@ describe("Breadcrumbs", () => {
   const title = "Kiwi.com";
   const dataTest = "test";
   const id = "id";
-  const component = shallow(
-    <BreadcrumbsItem id={id} href={url} onClick={onClick} dataTest={dataTest} active contentKey={2}>
-      {title}
-    </BreadcrumbsItem>,
-  );
-  const anchor = component.find("BreadcrumbsItem__StyledBreadcrumbsItemAnchor");
+  beforeEach(() => {
+    render(
+      <BreadcrumbsItem
+        id={id}
+        href={url}
+        onClick={onClick}
+        dataTest={dataTest}
+        active
+        contentKey={2}
+      >
+        {title}
+      </BreadcrumbsItem>,
+    );
+  });
   it("li contains props", () => {
-    expect(component.render().prop("name")).toBe("li");
-    expect(component.render().prop("data-test")).toBe(dataTest);
-    expect(component.render().prop("attribs").itemprop).toBe("itemListElement");
-    expect(component.render().prop("attribs").itemtype).toContain("ListItem");
-    expect(component.render().prop("aria-current")).toBe("page");
-    expect(anchor.render().prop("itemid")).toBe(id);
+    expect(screen.getByRole("listitem")).toBeInTheDocument();
+    expect(screen.getByTestId(dataTest)).toBeInTheDocument();
+    expect(screen.getByRole("listitem")).toHaveAttribute("itemprop", "itemListElement");
+    expect(screen.getByRole("listitem")).toHaveAttribute(
+      "itemtype",
+      expect.stringContaining("ListItem"),
+    );
+    expect(screen.getByRole("listitem")).toHaveAttribute("aria-current", "page");
+    expect(screen.getByRole("link")).toHaveAttribute("itemid", id);
   });
   it("anchor contains props", () => {
-    expect(anchor.render().prop("href")).toBe(url);
-    expect(anchor.render().prop("itemprop")).toBe("item");
-    expect(anchor.render().prop("itemtype")).toContain("WebPage");
-    expect(anchor.children().find("span").render().prop("itemprop")).toBe("name");
-    expect(anchor.children().find("span").children().text()).toBe(title);
+    expect(screen.getByRole("link")).toHaveAttribute("href", url);
+    expect(screen.getByRole("link")).toHaveAttribute("itemprop", "item");
+    expect(screen.getByRole("link")).toHaveAttribute(
+      "itemtype",
+      expect.stringContaining("WebPage"),
+    );
+    expect(screen.getByText(title)).toHaveAttribute("itemprop", "name");
   });
   it("meta contains props", () => {
-    expect(component.children().find("meta").render().prop("itemprop")).toBe("position");
-    expect(component.children().find("meta").prop("content")).toBe(2);
+    expect(document.querySelector("meta")).toHaveAttribute("itemprop", "position");
+    expect(document.querySelector("meta")).toHaveAttribute("content", "2");
   });
   it("itemid defaults to href when no id attribute", () => {
-    const componentWithoutID = shallow(
+    cleanup();
+    render(
       <BreadcrumbsItem href={url} onClick={onClick} dataTest={dataTest} active contentKey={2}>
         {title}
       </BreadcrumbsItem>,
     );
-    const anchorWithoutID = componentWithoutID.find("BreadcrumbsItem__StyledBreadcrumbsItemAnchor");
 
-    expect(anchorWithoutID.render().prop("itemid")).toBe(url);
+    expect(screen.getByRole("link")).toHaveAttribute("itemid", url);
   });
 });

bb524cb5eb1cafb5f16d36790eafca672597c61e
Aaron Collier <aaron@collier.cz>
2020-10-12 12:48:32 +0200

    docs: add examples for ButtonPrimitive (#2250)
    * chore: generate examples in right locations

---
+++ packages/orbit-components/src/primitives/ButtonPrimitive/__examples__/DEFAULT.js
@@ -0,0 +1,13 @@
+// @flow
+import * as React from "react";
+
+import ButtonPrimitive from "../index";
+
+export default {
+  Example: () => <ButtonPrimitive>Click me</ButtonPrimitive>,
+  info: {
+    title: "Default button primitive",
+    description:
+      "The default button primitive appears as a &lt;button&gt; with other traits inherited or as the browser default.",
+  },
+};

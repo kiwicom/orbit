570a00f2ba52c4f9c77c17555382732028a31792
mainframev <vgenaev@gmail.com>
2020-10-13 15:24:16 +0500

    fix(tooltip): React to React.Node

--- packages/orbit-components/src/primitives/MobileDialogPrimitive/components/DialogContent.flow.js
+++ packages/orbit-components/src/primitives/MobileDialogPrimitive/components/DialogContent.flow.js
@@ -1,10 +1,12 @@
 // @flow
+import * as React from "react";
+
 import type { Globals } from "../../../common/common.js.flow";
 
 export type Props = {|
   shown: boolean,
   dialogId: ?string,
-  children: React$Node,
+  children: React.Node,
   onClose: () => void,
   ...Globals,
 |};

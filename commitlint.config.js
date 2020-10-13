f8ce488bdc88b7f440d6ccfc0925060f7e1e6a24
Albert Alises <albert.alises@gmail.com>
2020-10-13 12:18:29 +0200

    improve: add the improve type to commitlint (#2330)

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,23 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "scope-case": [2, "always", "pascal-case"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "improve",
+      ],
+    ],
   },
 };

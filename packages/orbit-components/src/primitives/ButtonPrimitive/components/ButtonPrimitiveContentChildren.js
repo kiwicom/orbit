19af53dcc935b9da429c011c9315f4ccd7877516
Matija Marohnić <matija.marohnic@gmail.com>
2020-10-15 15:55:58 +0200

    fix(ButtonPrimitive): align text to right in RTL

--- packages/orbit-components/src/primitives/ButtonPrimitive/components/ButtonPrimitiveContentChildren.js
+++ packages/orbit-components/src/primitives/ButtonPrimitive/components/ButtonPrimitiveContentChildren.js
@@ -2,13 +2,14 @@
 import * as React from "react";
 import styled from "styled-components";
 
+import { left } from "../../../utils/rtl";
 import defaultTheme from "../../../defaultTheme";
 import type { Props } from "./ButtonPrimitiveContentChildren";
 
 const StyledButtonPrimitiveContentChildren = styled.div`
   display: inline-block;
   width: ${({ contentWidth }) => contentWidth};
-  text-align: ${({ hasIcon }) => hasIcon && "left"};
+  text-align: ${({ hasIcon }) => hasIcon && left};
 `;
 
 StyledButtonPrimitiveContentChildren.defaultProps = {

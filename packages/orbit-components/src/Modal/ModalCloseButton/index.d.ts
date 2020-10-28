1210da376fc93ed992405e2564fe7519d3e63764
Viktor Borszeki <vborszeki@gmail.com>
2020-10-28 11:39:56 +0100

    chore(ModalCloseButton): add type definitions

---
+++ packages/orbit-components/src/Modal/ModalCloseButton/index.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for @kiwicom/orbit-components
+// Project: http://github.com/kiwicom/orbit
+import * as React from "react";
+
+import * as Common from "../../common/common";
+
+declare module "@kiwicom/orbit-components/lib/Modal/ModalCloseButton";
+
+export interface Props extends Common.Global {
+  readonly onClick?: Common.Event<React.SyntheticEvent<HTMLButtonElement>>;
+}
+
+declare class ModalCloseButton extends React.Component<Props> {}
+export { ModalCloseButton, ModalCloseButton as default };

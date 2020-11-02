9b0744d809e43dd25e40c56408754b94696e3b72
Matija Marohnić <matija.marohnic@gmail.com>
2020-11-02 15:44:51 +0100

    test(FeatureIcon): rewrite to Testing Library (#2448)

--- packages/orbit-components/src/FeatureIcon/__tests__/index.test.js
+++ packages/orbit-components/src/FeatureIcon/__tests__/index.test.js
@@ -1,22 +1,24 @@
 // @flow
 import * as React from "react";
-import { mount } from "enzyme/build";
+import { render, screen } from "@testing-library/react";
 
 import FeatureIcon from "../index";
-import { baseURL, NAME_OPTIONS } from "../consts";
+import { NAME_OPTIONS } from "../consts";
 
 const name = NAME_OPTIONS.TICKETSAVER;
 
 describe(`FeatureIcon: ${name}`, () => {
-  const dataTest = "test";
-  const alt = "alt-test";
-  const component = mount(<FeatureIcon alt={alt} name={name} dataTest={dataTest} />).find("img");
-  it("should have props", () => {
-    const IMAGE_PATH = `${baseURL}/feature-icons/52x52/${name}.png`;
-    const IMAGE_PATH_RETINA = `${baseURL}/feature-icons/104x104/${name}.png 2x,${baseURL}/feature-icons/156x156/${name}.png 3x`;
-    expect(component.prop("src")).toBe(IMAGE_PATH);
-    expect(component.prop("srcSet")).toBe(IMAGE_PATH_RETINA);
-    expect(component.prop("alt")).toBe(alt);
-    expect(component.prop("data-test")).toBe(dataTest);
+  describe(name, () => {
+    it("should have expected DOM output", () => {
+      render(<FeatureIcon alt="alt-test" name={name} dataTest="test" />);
+      expect(screen.getByAltText("alt-test"));
+      const img = screen.getByRole("img");
+      expect(img.getAttribute("src")).toMatchInlineSnapshot(
+        `"//images.kiwi.com/feature-icons/52x52/TicketSaver.png"`,
+      );
+      expect(img.getAttribute("srcset")).toMatchInlineSnapshot(
+        `"//images.kiwi.com/feature-icons/104x104/TicketSaver.png 2x,//images.kiwi.com/feature-icons/156x156/TicketSaver.png 3x"`,
+      );
+    });
   });
 });

6a408d1e6734ad451e02b5b2498a3e7756b40db7
Matija Marohnić <matija.marohnic@gmail.com>
2020-11-03 06:22:49 +0100

    test(Icon): rewrite to Testing Library (#2454)

--- packages/orbit-components/src/Icon/__tests__/index.test.js
+++ packages/orbit-components/src/Icon/__tests__/index.test.js
@@ -1,60 +1,31 @@
 // @flow
 import * as React from "react";
-import { mount } from "enzyme";
+import { render, screen } from "@testing-library/react";
 
 import Accommodation from "../../icons/Accommodation";
 import { ICON_SIZES, ICON_COLORS } from "../consts";
 import defaultTheme from "../../defaultTheme";
 
 describe("Icon", () => {
-  const color = ICON_COLORS.PRIMARY;
-  const component = mount(<Accommodation color={color} />);
-  const svg = component.find("svg");
-  it("should render SVG", () => {
-    expect(component.find("svg").exists()).toBe(true);
+  it("should have expected DOM output", () => {
+    render(
+      <Accommodation
+        size={ICON_SIZES.LARGE}
+        color={ICON_COLORS.PRIMARY}
+        dataTest="test"
+        ariaHidden
+        ariaLabel="Accommodation"
+      />,
+    );
+    expect(screen.getByTestId("test")).toBeInTheDocument();
+    const icon = screen.getByLabelText("Accommodation");
+    expect(icon.tagName.toLowerCase()).toBe("svg");
+    expect(icon).toHaveAttribute("aria-hidden", "true");
+    expect(icon).toHaveStyle({ color: defaultTheme.orbit.colorIconPrimary });
   });
-  it("should have default sizing", () => {
-    expect(svg).toHaveStyleRule("height", defaultTheme.orbit.widthIconMedium);
-    expect(svg).toHaveStyleRule("width", defaultTheme.orbit.widthIconMedium);
-  });
-  it("should have selected color", () => {
-    expect(svg).toHaveStyleRule("color", defaultTheme.orbit.colorIconPrimary);
-  });
-  it("should have CSS properties", () => {
-    expect(svg).toHaveStyleRule("vertical-align", "middle");
-    expect(svg).toHaveStyleRule("fill", "currentColor");
-  });
-});
 
-describe("Icon with custom props", () => {
-  const dataTest = "test";
-  const size = ICON_SIZES.LARGE;
-  const customColor = "#FF0000";
-  const ariaHidden = true;
-  const ariaLabel = "Accommodation";
-  const component = mount(
-    <Accommodation
-      size={size}
-      customColor={customColor}
-      dataTest={dataTest}
-      ariaHidden={ariaHidden}
-      ariaLabel={ariaLabel}
-    />,
-  );
-  const svg = component.find("svg");
-  it("should render SVG", () => {
-    expect(component.find("svg").exists()).toBe(true);
-  });
-  it("should render attributes", () => {
-    expect(svg.render().prop("data-test")).toBe(dataTest);
-    expect(svg.render().prop("aria-hidden")).toBe(String(ariaHidden));
-    expect(svg.render().prop("aria-label")).toBe(ariaLabel);
-  });
-  it("should have default sizing", () => {
-    expect(svg).toHaveStyleRule("height", defaultTheme.orbit.widthIconLarge);
-    expect(svg).toHaveStyleRule("width", defaultTheme.orbit.widthIconLarge);
-  });
-  it("should have selected color", () => {
-    expect(svg).toHaveStyleRule("color", customColor);
+  it("should support passing custom color", () => {
+    render(<Accommodation customColor="#FF0000" ariaLabel="Accommodation" />);
+    expect(screen.getByLabelText("Accommodation")).toHaveStyle({ color: "#FF0000" });
   });
 });

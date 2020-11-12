kiwicom/orbit

commit 153f27eef2633e168fb52ffd6133e7bd712c2e2c

feat(Tooltip): refactoring Tooltip into 2 separate components (#2230)
* refactor(Tooltip): separating Tooltip logic to primitive component

* chore: cleaning orignial tooltip

* chore: updating docs

* chore: updating types

* chore: adding readme to MobileDialogPrimitive

* chore: adding stories for MobileDialog

* chore: updates from CR

* chore: removing snapshots

* chore: removing fragment

* feat: move tests from enzyme to testing-lib

* fix(tooltip): React to React.Node

Co-authored-by: mainframev <vgenaev@gmail.com>
Co-authored-by: Luděk Vepřek <weprous@gmail.com>

packages/orbit-components/src/primitives/TooltipPrimitive/index.js
@@ -1,0 +1,125 @@
+// @flow
+import React, { useState, useRef, useMemo, useCallback } from "react";
+import styled, { css } from "styled-components";
+
+import { SIZE_OPTIONS } from "./consts";
+import { StyledText } from "../../Text";
+import Portal from "../../Portal";
+import randomID from "../../utils/randomID";
+import TooltipContent from "./components/TooltipContent";
+import useStateWithTimeout from "../../hooks/useStateWithTimeout";
+
+import type { Props } from "./index";
+
+export const StyledTooltipChildren = styled.span`
+  display: inline-flex;
+  &:focus:active {
+    outline: none;
+  }
+  ${({ enabled, removeUnderlinedText }) =>
+    enabled &&
+    !removeUnderlinedText &&
+    css`
+      ${StyledText} {
+        display: inline-block;
+        text-decoration: underline; // fallback for IE 10+
+        text-decoration: underline currentColor dotted;
+      }
+    `};
+  /* enable event bubbling for disabled children, e.g. buttons */
+  [disabled] {
+    pointer-events: none;
+  }
+`;
+
+const TooltipPrimitive = ({
+  children,
+  enabled = true,
+  tabIndex = "0",
+  dataTest,
+  size = SIZE_OPTIONS.SMALL,
+  content,
+  preferredPosition,
+  preferredAlign,
+  stopPropagation = false,
+  removeUnderlinedText,
+}: Props) => {
+  const [shown, setShown] = useState(false);
+  const [
+    render,
+    setRender,
+    setRenderWithTimeout,
+    clearRenderTimeout,
+  ] = useStateWithTimeout<boolean>(false, 200);
+
+  const tooltipId = useMemo(() => randomID("TooltipID"), []);
+  const container = useRef(null);
+  const handleIn = useCallback(() => {
+    setRender(true);
+    setShown(true);
+    clearRenderTimeout();
+  }, [clearRenderTimeout, setRender]);
+
+  const handleOut = useCallback(() => {
+    setShown(false);
+    setRenderWithTimeout(false);
+  }, [setRenderWithTimeout]);
+
+  const handleInMobile = useCallback(
+    ev => {
+      if (stopPropagation) {
+        ev.stopPropagation();
+      }
+      ev.preventDefault();
+      setRender(true);
+      clearRenderTimeout();
+    },
+    [clearRenderTimeout, setRender, stopPropagation],
+  );
+
+  const handleOutMobile = useCallback(() => {
+    setRenderWithTimeout(false);
+  }, [setRenderWithTimeout]);
+
+  if (!enabled) return children;
+
+  return (
+    <>
+      <StyledTooltipChildren
+        onMouseEnter={handleIn}
+        onMouseLeave={handleOut}
+        onClick={handleInMobile}
+        onFocus={handleIn}
+        onBlur={handleOut}
+        ref={container}
+        aria-describedby={enabled ? tooltipId : undefined}
+        tabIndex={enabled ? tabIndex : undefined}
+        enabled={enabled}
+        removeUnderlinedText={removeUnderlinedText}
+      >
+        {children}
+      </StyledTooltipChildren>
+      {enabled && render && (
+        <Portal renderInto="tooltips">
+          <TooltipContent
+            parent={children}
+            dataTest={dataTest}
+            shown={shown}
+            size={size}
+            tooltipId={tooltipId}
+            onClose={handleOut}
+            onCloseMobile={handleOutMobile}
+            onEnter={handleIn}
+            preferredPosition={preferredPosition}
+            preferredAlign={preferredAlign}
+            containerRef={container}
+          >
+            {content}
+          </TooltipContent>
+        </Portal>
+      )}
+    </>
+  );
+};
+
+export default TooltipPrimitive;

packages/orbit-components/src/Tooltip/index.js
@@ -1,39 +1,12 @@
 // @flow
-import React, { useState, useRef, useEffect, useCallback } from "react";
-import styled, { css } from "styled-components";
+import React from "react";
 
-import { getBreakpointWidth } from "../utils/mediaQuery";
 import { SIZE_OPTIONS } from "./consts";
-import { StyledText } from "../Text";
-import Portal from "../Portal";
-import RandomID from "../utils/randomID";
-import { QUERIES } from "../utils/mediaQuery/consts";
-import useTheme from "../hooks/useTheme";
-import TooltipContent from "./components/TooltipContent";
-import useStateWithTimeout from "../hooks/useStateWithTimeout";
+import TooltipPrimitive from "../primitives/TooltipPrimitive";
+import useMediaQuery from "../hooks/useMediaQuery";
+import MobileDialog from "../primitives/MobileDialogPrimitive";
 
 import type { Props } from "./index";
-
-const StyledTooltipChildren = styled.span`
-  display: inline-flex;
-  &:focus:active {
-    outline: none;
-  }
-  ${({ enabled, removeUnderlinedText }) =>
-    enabled &&
-    !removeUnderlinedText &&
-    css`
-      ${StyledText} {
-        display: inline-block;
-        text-decoration: underline; // fallback for IE 10+
-        text-decoration: underline currentColor dotted;
-      }
-    `};
-  /* enable event bubbling for disabled children, e.g. buttons */
-  [disabled] {
-    pointer-events: none;
-  }
-`;
 
 const Tooltip = ({
   children,
@@ -47,96 +20,31 @@ const Tooltip = ({
   stopPropagation = false,
   removeUnderlinedText,
 }: Props) => {
-  const theme = useTheme();
-  const [shown, setShown] = useState(false);
-  const [
-    render,
-    setRender,
-    setRenderWithTimeout,
-    clearRenderTimeout,
-  ] = useStateWithTimeout<boolean>(false, 200);
-  const [shownMobile, setShownMobile, setShownMobileWithTimeout] = useStateWithTimeout<boolean>(
-    false,
-    200,
-  );
-  const [tooltipId, setTooltipId] = useState(null);
-  const container = useRef(null);
-  const handleIn = useCallback(() => {
-    if (window.innerWidth > +getBreakpointWidth(QUERIES.LARGEMOBILE, theme, true)) {
-      setRender(true);
-      setShown(true);
-      clearRenderTimeout();
-    }
-  }, [clearRenderTimeout, setRender, theme]);
-
-  const handleOut = useCallback(() => {
-    if (window.innerWidth > +getBreakpointWidth(QUERIES.LARGEMOBILE, theme, true)) {
-      setShown(false);
-      setRenderWithTimeout(false);
-    }
-  }, [setRenderWithTimeout, theme]);
-
-  const handleInMobile = useCallback(
-    ev => {
-      if (stopPropagation) {
-        ev.stopPropagation();
-      }
-      if (window.innerWidth <= +getBreakpointWidth(QUERIES.LARGEMOBILE, theme, true)) {
-        ev.preventDefault();
-        setRender(true);
-        setShownMobileWithTimeout(true);
-        clearRenderTimeout();
-      }
-    },
-    [clearRenderTimeout, setRender, setShownMobileWithTimeout, stopPropagation, theme],
-  );
-
-  const handleOutMobile = useCallback(() => {
-    setShownMobile(false);
-    setRenderWithTimeout(false);
-  }, [setRenderWithTimeout, setShownMobile]);
-
-  useEffect(() => {
-    setTooltipId(RandomID("tooltip"));
-  }, []);
-  if (!enabled) return children;
-  return (
-    <>
-      <StyledTooltipChildren
-        onMouseEnter={handleIn}
-        onMouseLeave={handleOut}
-        onClick={handleInMobile}
-        onFocus={handleIn}
-        onBlur={handleOut}
-        ref={container}
-        aria-describedby={enabled ? tooltipId : undefined}
-        tabIndex={enabled ? tabIndex : undefined}
-        enabled={enabled}
-        removeUnderlinedText={removeUnderlinedText}
-      >
-        {children}
-      </StyledTooltipChildren>
-      {enabled && render && (
-        <Portal renderInto="tooltips">
-          <TooltipContent
-            parent={children}
-            dataTest={dataTest}
-            shownMobile={shownMobile}
-            shown={shown}
-            size={size}
-            tooltipId={tooltipId}
-            onClose={handleOut}
-            onCloseMobile={handleOutMobile}
-            onEnter={handleIn}
-            preferredPosition={preferredPosition}
-            preferredAlign={preferredAlign}
-            containerRef={container}
-          >
-            {content}
-          </TooltipContent>
-        </Portal>
-      )}
-    </>
+  const { isLargeMobile } = useMediaQuery();
+  return isLargeMobile ? (
+    <TooltipPrimitive
+      dataTest={dataTest}
+      tabIndex={tabIndex}
+      enabled={enabled}
+      content={content}
+      size={size}
+      preferredPosition={preferredPosition}
+      preferredAlign={preferredAlign}
+      stopPropagation={stopPropagation}
+      removeUnderlinedText={removeUnderlinedText}
+    >
+      {children}
+    </TooltipPrimitive>
+  ) : (
+    <MobileDialog
+      tabIndex={tabIndex}
+      enabled={enabled}
+      content={content}
+      removeUnderlinedText={removeUnderlinedText}
+      stopPropagation={stopPropagation}
+    >
+      {children}
+    </MobileDialog>
   );
 };
 

packages/orbit-components/src/primitives/MobileDialogPrimitive/index.js
@@ -1,0 +1,78 @@
+// @flow
+import React, { useCallback, useMemo } from "react";
+
+import randomID from "../../utils/randomID";
+import useStateWithTimeout from "../../hooks/useStateWithTimeout";
+import Portal from "../../Portal";
+import { StyledTooltipChildren } from "../TooltipPrimitive";
+import DialogContent from "./components/DialogContent";
+
+import type { Props } from "./index";
+
+const MobileDialog = ({
+  children,
+  enabled = true,
+  tabIndex = "0",
+  dataTest,
+  content,
+  stopPropagation = false,
+  removeUnderlinedText,
+}: Props) => {
+  const [
+    render,
+    setRender,
+    setRenderWithTimeout,
+    clearRenderTimeout,
+  ] = useStateWithTimeout<boolean>(false, 200);
+  const [shown, setshown, setshownWithTimeout] = useStateWithTimeout<boolean>(false, 200);
+  const mobileDialogID = useMemo(() => randomID("mobileDialogID"), []);
+
+  const handleInMobile = useCallback(
+    ev => {
+      ev.preventDefault();
+      if (stopPropagation) {
+        ev.stopPropagation();
+      }
+
+      setRender(true);
+      setshownWithTimeout(true);
+      clearRenderTimeout();
+    },
+    [clearRenderTimeout, setRender, setshownWithTimeout, stopPropagation],
+  );
+
+  const handleOutMobile = useCallback(() => {
+    setshown(false);
+    setRenderWithTimeout(false);
+  }, [setRenderWithTimeout, setshown]);
+
+  if (!enabled) return children;
+
+  return (
+    <>
+      <StyledTooltipChildren
+        onClick={handleInMobile}
+        aria-describedby={enabled ? mobileDialogID : undefined}
+        tabIndex={enabled ? tabIndex : undefined}
+        enabled={enabled}
+        removeUnderlinedText={removeUnderlinedText}
+      >
+        {children}
+      </StyledTooltipChildren>
+      {enabled && render && (
+        <Portal renderInto="dialogs">
+          <DialogContent
+            dataTest={dataTest}
+            shown={shown}
+            dialogId={mobileDialogID}
+            onClose={handleOutMobile}
+          >
+            {content}
+          </DialogContent>
+        </Portal>
+      )}
+    </>
+  );
+};
+
+export default MobileDialog;

commit 0aca8bbead6c6a77894d110735c83beff3643029

fix(Tooltip): only call `preventDefault` on mobile
Calling it aborts stuff like `change` event on chekcbox, so Checkbox
nested inside Tooltip doesn't work. This bug was introduced in #2230,
this functionality was only supposed to exist on mobile.

packages/orbit-components/src/primitives/TooltipPrimitive/index.js
@@ -65,16 +65,13 @@ const TooltipPrimitive = ({
     setRenderWithTimeout(false);
   }, [setRenderWithTimeout]);
 
-  const handleInMobile = useCallback(
+  const handleClick = useCallback(
     ev => {
       if (stopPropagation) {
         ev.stopPropagation();
       }
-      ev.preventDefault();
-      setRender(true);
-      clearRenderTimeout();
     },
-    [clearRenderTimeout, setRender, stopPropagation],
+    [stopPropagation],
   );
 
   const handleOutMobile = useCallback(() => {
@@ -88,7 +85,7 @@ const TooltipPrimitive = ({
       <StyledTooltipChildren
         onMouseEnter={handleIn}
         onMouseLeave={handleOut}
-        onClick={handleInMobile}
+        onClick={handleClick}
         onFocus={handleIn}
         onBlur={handleOut}
         ref={container}
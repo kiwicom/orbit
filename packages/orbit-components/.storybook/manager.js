98698622140082cc73c8a0f023b314fe3a6a4ff6
Victor Genaev <vgenaev@gmail.com>
2020-11-12 20:47:48 +0500

    improve: add storybook viewports (#2499)

--- packages/orbit-components/.storybook/manager.js
+++ packages/orbit-components/.storybook/manager.js
@@ -1,5 +1,8 @@
 // @flow
 import { create } from "@storybook/theming";
+import { addons } from "@storybook/addons";
+
+import orbitTheme from "./orbitTheme";
 import "@storybook/addon-knobs/register";
 import "@storybook/addon-actions/register";
 
@@ -7,4 +10,9 @@ export default create({
   brandTitle: "Orbit",
   brandUrl: "https://orbit.kiwi",
   brandImage: "https://orbit.kiwi/files/2019/08/cropped-OrbitLogo-1.png",
+});
+
+addons.setConfig({
+  theme: orbitTheme,
+  panelPosition: "bottom",
 });

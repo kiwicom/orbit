c147deba985c8428570a9ba40b70c46a31ee141b
Victor Genaev <vgenaev@gmail.com>
2020-11-12 20:36:49 +0500

    improve: preview changelog on release (#2500)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,49 @@
 // @noflow
+/* eslint-disable no-console */
 const { spawn } = require("child_process");
-const dotenv = require("dotenv-safe");
+const { series } = require("gulp");
+const conventionalChangelog = require("conventional-changelog");
+const { getPackages } = require("@lerna/project");
+const markdownChalk = require("markdown-chalk");
+const mergeStream = require("merge-stream");
+const transform = require("through2").obj;
+const path = require("path");
 
-dotenv.config();
+async function previewChangelog(done) {
+  const packages = await getPackages();
+  const streams = packages.map(pkg => {
+    return conventionalChangelog(
+      {
+        lernaPackage: pkg.name,
+        preset: "angular",
+      },
+      {
+        host: "https://github.com",
+        owner: "kiwicom",
+        repository: "orbit",
+      },
+      {
+        path: path.join("packages", pkg.name.replace("@kiwicom/", "")),
+      },
+    ).pipe(
+      transform((chunk, enc, cb) => {
+        const changelog = chunk.toString();
+        if (changelog.trim().includes("\n")) {
+          console.log(markdownChalk(changelog));
+        }
+        cb();
+      }),
+    );
+  });
+
+  mergeStream(...streams)
+    .on("done", () => {
+      done();
+    })
+    .on("error", err => {
+      done(err);
+    });
+}
 
 function publishPackages() {
   return spawn(
@@ -13,5 +54,5 @@ function publishPackages() {
 }
 
 module.exports = {
-  publish: publishPackages,
+  publish: series(previewChangelog, publishPackages),
 };

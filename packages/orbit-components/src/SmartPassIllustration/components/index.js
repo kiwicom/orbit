c80519a80d6b37bcfb8de233e8ea3c4bedae74c3
Victor Genaev <vgenaev@gmail.com>
2020-11-19 19:25:54 +0500

    feat: SmartPassIllustration component (#2504)

---
+++ packages/orbit-components/src/SmartPassIllustration/components/index.js
@@ -0,0 +1,7 @@
+// @flow
+export { default as SmartPassV1 } from "./SmartPassV1";
+export { default as SmartPassV2 } from "./SmartPassV2";
+export { default as SmartPassV3 } from "./SmartPassV3";
+export { default as SmartPassV4 } from "./SmartPassV4";
+// TODO: add after it will be fixed by design
+// export { default as SmartPassV5 } from "./SmartPassV5";

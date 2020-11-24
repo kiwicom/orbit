15aa5b4aaca6ef393bcb1b29e04744535e771cd1
Victor Genaev <vgenaev@gmail.com>
2020-11-25 00:24:18 +0500

    test(Text): rewrite to Testing library (#2524)

--- packages/orbit-components/src/Text/__tests__/index.test.js
+++ packages/orbit-components/src/Text/__tests__/index.test.js
@@ -1,38 +1,32 @@
 // @flow
 import * as React from "react";
-import { shallow, mount } from "enzyme";
+import { screen, render } from "@testing-library/react";
 
 import Text from "../index";
-import SPACINGS_AFTER from "../../common/getSpacingToken/consts";
 import { SIZE_OPTIONS, TYPE_OPTIONS } from "../consts";
 import defaultTheme from "../../defaultTheme";
 
 describe("Text", () => {
-  const text = "Children text";
-  const dataTest = "test";
-  const spaceAfter = SPACINGS_AFTER.NORMAL;
-  const type = TYPE_OPTIONS.PRIMARY;
-  const size = SIZE_OPTIONS.SMALL;
-  const id = "id";
-  const component = shallow(
-    <Text type={type} size={size} dataTest={dataTest} spaceAfter={spaceAfter} id={id}>
-      {text}
-    </Text>,
-  );
-  it("should have passed props", () => {
-    expect(component.prop("type")).toBe(type);
-    expect(component.prop("size")).toBe(size);
-    expect(component.prop("spaceAfter")).toBe(spaceAfter);
-    expect(component.prop("id")).toBe(id);
-  });
-  it("should contain children", () => {
-    expect(component.children().text()).toBe(text);
-  });
-  it("should have data-test", () => {
-    expect(component.render().prop("data-test")).toBe(dataTest);
-  });
-  it("should have margin-bottom", () => {
-    const mounted = mount(<Text spaceAfter={spaceAfter}>{text}</Text>);
-    expect(mounted).toHaveStyleRule("margin-bottom", defaultTheme.orbit.spaceSmall);
+  it("should have expected DOM output", () => {
+    const dataTest = "test";
+    const text = "Children text";
+    const type = TYPE_OPTIONS.PRIMARY;
+    const size = SIZE_OPTIONS.SMALL;
+    const id = "id";
+
+    render(
+      <Text dataTest={dataTest} type={type} size={size} id={id} spaceAfter="normal">
+        {text}
+      </Text>,
+    );
+
+    expect(screen.getByTestId(dataTest)).toHaveAttribute("id", id);
+    expect(screen.getByTestId(dataTest)).toBeInTheDocument();
+    expect(screen.getByText(text)).toBeInTheDocument();
+    expect(screen.getByTestId(dataTest)).toHaveStyle({
+      fontSize: defaultTheme.orbit.fontSizeTextSmall,
+      color: defaultTheme.orbit.colorTextPrimary,
+      marginBottom: defaultTheme.orbit.spaceSmall,
+    });
   });
 });

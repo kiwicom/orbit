644c254c9a0b3a40fd0ff9b1a1c181e4ea844e9b
Matija Marohnić <matija.marohnic@gmail.com>
2020-11-24 11:41:03 +0100

    test(Modal): simplify tests
    - render new JSX for each test to ensure that tests don't affect each
    other
    - render minimal JSX required for that test
    - merge related assertions into a single test
    - replace snapshot testing with exact assertions
    - remove unnecessary tests

--- packages/orbit-components/src/Modal/__tests__/index.test.js
+++ packages/orbit-components/src/Modal/__tests__/index.test.js
@@ -4,62 +4,49 @@ import { render, screen, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import Modal from "../index";
-import { SIZES, CLOSE_BUTTON_DATA_TEST } from "../consts";
+import { CLOSE_BUTTON_DATA_TEST } from "../consts";
 import ModalHeader from "../ModalHeader";
 import ModalSection from "../ModalSection";
 import ModalFooter from "../ModalFooter";
 import Illustration from "../../Illustration";
-import List from "../../List";
-import ListItem from "../../List/ListItem";
-import Wallet from "../../icons/Wallet";
-import Button from "../../Button";
-import ChevronLeft from "../../icons/ChevronLeft";
-
-const Div = (props: any) => <div>{props.children}</div>;
-const onClose = jest.fn();
-
-const ref = React.createRef();
-
-const modal = (
-  <Modal size={SIZES.LARGE} onClose={onClose} fixedFooter dataTest="modal" ref={ref}>
-    <ModalHeader
-      title="Title of Modal"
-      description="Description of Modal"
-      illustration={<Illustration name="Accommodation" size="extraSmall" dataTest="illustration" />}
-      suppressed
-      dataTest="header"
-    >
-      <List>
-        <ListItem icon={<Wallet />}>
-          To save you time, we have calculated your total possible refundable amount.
-        </ListItem>
-      </List>
-    </ModalHeader>
-    <ModalSection suppressed dataTest="section">
-      This is the content of the section
-    </ModalSection>
-    <ModalFooter flex={["0 0 auto", "1 1 100%"]} dataTest="footer">
-      <Button iconLeft={<ChevronLeft />} type="secondary">
-        Back
-      </Button>
-      <Button fullWidth>Continue to Payment</Button>
-    </ModalFooter>
-  </Modal>
-);
 
 jest.useFakeTimers("modern");
 
 beforeEach(() => {
-  onClose.mockClear();
   jest.clearAllTimers();
 });
 
 describe("Modal", () => {
-  it("should render correctly", () => {
-    const { container } = render(modal);
+  it("should have expected DOM output", () => {
+    render(
+      <Modal dataTest="container">
+        <ModalHeader
+          dataTest="header"
+          title="modal title"
+          description="modal description"
+          illustration={<Illustration name="Accommodation" dataTest="illustration" />}
+        >
+          header content
+        </ModalHeader>
+        <ModalSection dataTest="section">section content</ModalSection>
+        <ModalFooter dataTest="footer">footer content</ModalFooter>
+      </Modal>,
+    );
 
     expect(screen.getByRole("dialog")).toBeInTheDocument();
-    expect(container).toMatchSnapshot();
+    expect(screen.getByTestId("container")).toBeInTheDocument();
+    expect(screen.getByTestId("header")).toBeInTheDocument();
+    expect(screen.getByTestId("section")).toBeInTheDocument();
+    expect(screen.getByTestId("footer")).toBeInTheDocument();
+    expect(screen.getByTestId("illustration")).toBeInTheDocument();
+    // title appears twice in the DOM, the second one is for the sticky header,
+    // which is initially hidden and appears on scroll
+    // when original title is out from the viewport
+    expect(screen.getAllByText("modal title")).toHaveLength(2);
+    expect(screen.getByText("modal description")).toBeInTheDocument();
+    expect(screen.getByText("header content")).toBeInTheDocument();
+    expect(screen.getByText("section content")).toBeInTheDocument();
+    expect(screen.getByText("footer content")).toBeInTheDocument();
   });
 
   it("should be able to focus content", async () => {
@@ -80,70 +67,27 @@ describe("Modal", () => {
     expect(screen.getByRole("textbox")).toHaveFocus();
   });
 
-  it("should expose ref with setScrollPosition", () => {
-    render(modal);
-    expect(ref.current?.setScrollPosition).toBeDefined();
+  it("should expose ref scrolling API", () => {
+    const ref = React.createRef();
+    render(
+      <Modal ref={ref} dataTest="test">
+        content
+      </Modal>,
+    );
+
     ref.current?.setScrollPosition(20);
-    expect(screen.getByTestId("modal").scrollTop).toBe(20);
-  });
-
-  it("should render passed props correctly", () => {
-    render(modal);
-
-    // title appears twice in the DOM, the second one is for the sticky header,
-    // which is initially hidden and appears on scroll
-    // when original title is out from the viewport
-    expect(screen.getAllByText("Title of Modal")).toHaveLength(2);
-    expect(screen.getByText("Description of Modal")).toBeInTheDocument();
-    expect(screen.getByTestId("illustration")).toBeInTheDocument();
-    expect(screen.getByText("This is the content of the section")).toBeInTheDocument();
-  });
-
-  it("should contain passed data-test attributes", () => {
-    render(modal);
-
-    expect(screen.getByTestId("modal")).toBeInTheDocument();
-    expect(screen.getByTestId("header")).toBeInTheDocument();
-    expect(screen.getByTestId("section")).toBeInTheDocument();
-    expect(screen.getByTestId("footer")).toBeInTheDocument();
+    expect(screen.getByTestId("test").scrollTop).toBe(20);
   });
 
   it("should call callback function when clicking on close button", () => {
-    render(modal);
+    const onClose = jest.fn();
+    render(
+      <Modal hasCloseButton onClose={onClose}>
+        content
+      </Modal>,
+    );
 
     userEvent.click(screen.getByTestId(CLOSE_BUTTON_DATA_TEST));
-
     expect(onClose).toBeCalled();
-  });
-
-  it("should render ModalSection when passed as children", () => {
-    const { container } = render(
-      <Modal>
-        <ModalHeader title="Title of Modal" />
-        <Div>
-          <Div>
-            <Div>
-              <ModalSection>Content of Modal Section</ModalSection>
-            </Div>
-          </Div>
-        </Div>
-        <ModalFooter>It should render if needed</ModalFooter>
-      </Modal>,
-    );
-
-    expect(container.querySelector("section")).toBeInTheDocument();
-  });
-
-  it("should not render ModalSection when not passed", () => {
-    const { container } = render(
-      <Modal>
-        <ModalHeader title="Title of Modal" />
-        <ModalFooter>
-          <Button fullWidth>Continue to Payment</Button>
-        </ModalFooter>
-      </Modal>,
-    );
-
-    expect(container.querySelector("section")).not.toBeInTheDocument();
   });
 });

6f37baccea6b99de9b3ba7db5df0960e827b4f8c
Matija Marohnić <matija.marohnic@gmail.com>
2020-11-26 01:46:41 +0100

    fix(Modal): expose modalBody and modalContent
    These two ref objects are intended as internal API, but when Modal was a
    class people were able to access them, and Search is relying on them, so
    it broke after we refactored Modal to a function component. We'll expose
    them for now, but we won't document this.

--- packages/orbit-components/src/Modal/__typetests__/index.tsx
+++ packages/orbit-components/src/Modal/__typetests__/index.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import Modal from "../";
 
 export default function Test() {
-  const ref = React.useRef<React.ElementRef<typeof Modal> | null>(null);
+  const modalRef = React.useRef<React.ElementRef<typeof Modal> | null>(null);
   React.useEffect(() => {
-    ref.current?.getScrollPosition();
+    modalRef.current?.getScrollPosition();
+    modalRef.current?.setScrollPosition(20);
+    modalRef.current?.modalBody.current?.addEventListener("scroll", () => {
+      // do something
+    });
+    modalRef.current?.modalContent.current?.addEventListener("scroll", () => {
+      // do something
+    });
   }, []);
-  return <Modal ref={ref}>content</Modal>;
+  return <Modal ref={modalRef}>content</Modal>;
 }

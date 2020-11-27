2d1cb05735a60503557db47d68ccfed1c45519e1
mainframev <vgenaev@gmail.com>
2020-11-27 21:51:42 +0500

    refactor(Pagination): useMediaQuery instead of Hide, title

--- packages/orbit-components/src/Pagination/index.js
+++ packages/orbit-components/src/Pagination/index.js
@@ -2,9 +2,9 @@
 import * as React from "react";
 import styled from "styled-components";
 
+import useMediaQuery from "../hooks/useMediaQuery";
 import ButtonLink from "../ButtonLink";
 import Stack from "../Stack";
-import Hide from "../Hide";
 import ChevronLeft from "../icons/ChevronLeft";
 import ChevronRight from "../icons/ChevronRight";
 import { MAXIMUM_PAGES, SIZES } from "./consts";
@@ -12,6 +12,7 @@ import Pages from "./components/Pages";
 import CompactPages from "./components/CompactPages";
 import ActiveButton from "./components/ActiveButton";
 import Translate from "../Translate";
+import useTranslate from "../hooks/useTranslate";
 
 import type { Props } from "./index";
 
@@ -35,82 +36,75 @@ const Pagination = ({
   size = SIZES.NORMAL,
 }: Props) => {
   const pageChanged = handlePageChange(onPageChange, pageCount);
+  const { isTablet } = useMediaQuery();
+  const translate = useTranslate();
 
   return (
-    <Stack
-      direction="row"
-      spacing="XXSmall"
-      align="center"
-      grow={false}
-      shrink
-      dataTest={dataTest}
-      as={StyledNav}
-    >
-      <Hide on={["smallMobile", "mediumMobile", "largeMobile"]}>
-        <ButtonLink
-          onClick={() => pageChanged(selectedPage - 1)}
-          iconLeft={<ChevronLeft />}
-          type="secondary"
-          size={size}
-          disabled={selectedPage <= 1}
-        >
-          {!hideLabels && <Translate tKey="pagination_label_prev" />}
-        </ButtonLink>
-      </Hide>
-      <Hide on={["tablet", "desktop", "largeDesktop"]}>
-        <ButtonLink
-          onClick={() => pageChanged(selectedPage - 1)}
-          iconLeft={<ChevronLeft />}
-          type="secondary"
-          size={size}
-          disabled={selectedPage <= 1}
-        />
-      </Hide>
-      <Hide on={["smallMobile", "mediumMobile", "largeMobile"]}>
-        <Stack direction="row" spacing="XXSmall" align="center">
-          {pageCount <= MAXIMUM_PAGES ? (
-            <Pages
-              pageCount={pageCount}
-              selectedPage={selectedPage}
-              onPageChange={onPageChange}
-              size={size}
-            />
-          ) : (
-            <CompactPages
-              pageCount={pageCount}
-              selectedPage={selectedPage}
-              onPageChange={onPageChange}
-              size={size}
-            />
-          )}
-        </Stack>
-      </Hide>
-      <Hide on={["tablet", "desktop", "largeDesktop"]}>
-        <ActiveButton transparent size={size}>
-          {selectedPage} of {pageCount}
-        </ActiveButton>
-      </Hide>
-      <Hide on={["smallMobile", "mediumMobile", "largeMobile"]}>
-        <ButtonLink
-          onClick={() => pageChanged(selectedPage + 1)}
-          iconRight={!hideLabels && <ChevronRight />}
-          iconLeft={hideLabels && <ChevronRight />}
-          type="secondary"
-          size={size}
-          disabled={pageCount <= selectedPage}
-        >
-          {!hideLabels && <Translate tKey="pagination_label_next" />}
-        </ButtonLink>
-      </Hide>
-      <Hide on={["tablet", "desktop", "largeDesktop"]}>
-        <ButtonLink
-          onClick={() => pageChanged(selectedPage + 1)}
-          iconLeft={<ChevronRight />}
-          type="secondary"
-          size={size}
-          disabled={pageCount <= selectedPage}
-        />
-      </Hide>
+    <Stack spacing="XXSmall" align="center" grow={false} shrink dataTest={dataTest} as={StyledNav}>
+      {!isTablet ? (
+        <>
+          <ButtonLink
+            onClick={() => pageChanged(selectedPage - 1)}
+            iconLeft={<ChevronLeft />}
+            type="secondary"
+            title={translate("pagination_label_prev")}
+            size={size}
+            disabled={selectedPage <= 1}
+          />
+          <ActiveButton transparent size={size}>
+            {selectedPage} of {pageCount}
+          </ActiveButton>
+          <ButtonLink
+            onClick={() => pageChanged(selectedPage + 1)}
+            iconLeft={<ChevronRight />}
+            type="secondary"
+            title={translate("pagination_label_next")}
+            size={size}
+            disabled={pageCount <= selectedPage}
+          />
+        </>
+      ) : (
+        <>
+          <ButtonLink
+            onClick={() => pageChanged(selectedPage - 1)}
+            iconLeft={<ChevronLeft />}
+            type="secondary"
+            title={hideLabels ? translate("pagination_label_prev") : undefined}
+            size={size}
+            disabled={selectedPage <= 1}
+          >
+            {!hideLabels && <Translate tKey="pagination_label_prev" />}
+          </ButtonLink>
+          <Stack inline grow={false} spacing="XXSmall" align="center">
+            {pageCount <= MAXIMUM_PAGES ? (
+              <Pages
+                pageCount={pageCount}
+                selectedPage={selectedPage}
+                onPageChange={onPageChange}
+                size={size}
+              />
+            ) : (
+              <CompactPages
+                pageCount={pageCount}
+                selectedPage={selectedPage}
+                onPageChange={onPageChange}
+                size={size}
+              />
+            )}
+          </Stack>
+          <ButtonLink
+            onClick={() => pageChanged(selectedPage + 1)}
+            iconRight={!hideLabels && <ChevronRight />}
+            iconLeft={hideLabels && <ChevronRight />}
+            type="secondary"
+            title={hideLabels ? translate("pagination_label_next") : undefined}
+            size={size}
+            disabled={pageCount <= selectedPage}
+          >
+            {!hideLabels && <Translate tKey="pagination_label_next" />}
+          </ButtonLink>
+        </>
+      )}
     </Stack>
   );
 };

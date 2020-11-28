879420505709d93427fb3ec2bab3e7dc8ed98e0b
Victor Genaev <vgenaev@gmail.com>
2020-11-29 02:59:07 +0500

    refactor(Pagination): useMediaQuery instead of Hide, title (#2542)

--- packages/orbit-components/src/Pagination/__tests__/index.test.js
+++ packages/orbit-components/src/Pagination/__tests__/index.test.js
@@ -9,6 +9,16 @@ const pageCount = 9;
 const selectedPage = 3;
 const onPageChange = jest.fn();
 
+// temporary added to pass CI, will be resolved by another PR with
+// rewriting to testing-lib
+jest.mock("../../hooks/useMediaQuery", () => {
+  return () => {
+    return {
+      isTablet: false,
+    };
+  };
+});
+
 describe("Pagination", () => {
   const component = shallow(
     <Pagination

a134a9babb83376c7aa39724c2c7523944f53f0d
Victor Genaev <vgenaev@gmail.com>
2020-12-01 03:06:59 +0500

    test(Separator): rewrite to testing library (#2550)

--- packages/orbit-components/src/Separator/__tests__/index.test.js
+++ packages/orbit-components/src/Separator/__tests__/index.test.js
@@ -1,14 +1,20 @@
 // @flow
 import * as React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from "@testing-library/react";
 
+import theme from "../../defaultTheme";
 import Separator from "../index";
 import SPACINGS_AFTER from "../../common/getSpacingToken/consts";
 
 describe("Separator", () => {
-  const spaceAfter = SPACINGS_AFTER.LARGE;
-  const component = shallow(<Separator spaceAfter={spaceAfter} />);
-  it("should match snapshot", () => {
-    expect(component).toMatchSnapshot();
+  it("should have expected DOM output", () => {
+    const spaceAfter = SPACINGS_AFTER.LARGE;
+    render(<Separator spaceAfter={spaceAfter} />);
+    expect(screen.getByRole("separator")).toBeInTheDocument();
+    expect(screen.getByRole("separator")).toHaveStyle({
+      height: theme.orbit.heightSeparator,
+      background: theme.orbit.backgroundSeparator,
+      marginBottom: theme.orbit.spaceLarge,
+    });
   });
 });

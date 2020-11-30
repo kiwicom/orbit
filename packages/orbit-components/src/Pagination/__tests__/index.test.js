91d9791b0f3794afcaf7082bfd6e2329770c2358
Victor Genaev <vgenaev@gmail.com>
2020-11-30 18:53:10 +0500

    test(Pagination): rewrite to testing library (#2539)

--- packages/orbit-components/src/Pagination/__tests__/index.test.js
+++ packages/orbit-components/src/Pagination/__tests__/index.test.js
@@ -1,34 +1,62 @@
 // @flow
 import * as React from "react";
-import { shallow } from "enzyme";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Pagination from "../index";
 
-const dataTest = "test";
-const pageCount = 9;
-const selectedPage = 3;
-const onPageChange = jest.fn();
-
-// temporary added to pass CI, will be resolved by another PR with
-// rewriting to testing-lib
 jest.mock("../../hooks/useMediaQuery", () => {
   return () => {
     return {
-      isTablet: false,
+      isTablet: true,
     };
   };
 });
 
 describe("Pagination", () => {
-  const component = shallow(
-    <Pagination
-      dataTest={dataTest}
-      pageCount={pageCount}
-      selectedPage={selectedPage}
-      onPageChange={onPageChange}
-    />,
-  );
-  it("Stack should have passed dataTest", () => {
-    expect(component.prop("dataTest")).toBe(dataTest);
+  it("should have expected DOM output", () => {
+    const dataTest = "test";
+    const pageCount = 9;
+    const selectedPage = 3;
+    const onPageChange = jest.fn();
+
+    render(
+      <Pagination
+        dataTest={dataTest}
+        pageCount={pageCount}
+        selectedPage={selectedPage}
+        onPageChange={onPageChange}
+      />,
+    );
+
+    expect(screen.getByTestId(dataTest)).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: "Next" }));
+    expect(onPageChange).toHaveBeenCalled();
+  });
+
+  it("should have next button disabled", () => {
+    render(
+      <Pagination
+        dataTest="test"
+        hideLabels={false}
+        pageCount={9}
+        selectedPage={9}
+        onPageChange={() => {}}
+      />,
+    );
+    expect(screen.getByRole("button", { name: "Next" })).toBeDisabled();
+  });
+
+  it("should have first button disabled", () => {
+    render(
+      <Pagination
+        dataTest="test"
+        hideLabels={false}
+        pageCount={6}
+        selectedPage={1}
+        onPageChange={() => {}}
+      />,
+    );
+    expect(screen.getByRole("button", { name: "Previous" })).toBeDisabled();
   });
 });

467ab8564587341f6c51babbcd0e2eb898d3e52b
mainframev <vgenaev@gmail.com>
2020-11-30 18:42:28 +0500

    test(Select): rewrite to testing library

--- packages/orbit-components/src/Select/__tests__/Select.test.js
+++ packages/orbit-components/src/Select/__tests__/Select.test.js
@@ -1,81 +1,82 @@
 // @flow
 
 import * as React from "react";
-import { shallow, mount } from "enzyme";
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Select from "../index";
 import SPACINGS_AFTER from "../../common/getSpacingToken/consts";
 
-const mockChange = jest.fn();
-const placeholder = "Default placeholder";
-const dataTest = "test";
-const tabIndex = "-1";
-const name = "name";
-const id = "test-id";
-const objectOptions = [
-  { value: "1", label: "One" },
-  { value: "2", label: "Two" },
-  { value: "3", label: "Three" },
-  { value: "4", label: "Four" },
-  { value: "hidden-five", label: "Hidden Five" },
-  { disabled: true, value: "disabled-six", label: "Disabled Six" },
-];
-const spaceAfter = SPACINGS_AFTER.NORMAL;
-const dataAttrs = {
-  "data-recording-ignore": true,
-};
-
 describe("Select", () => {
-  const component = shallow(
-    <Select
-      id={id}
-      value="1"
-      name={name}
-      placeholder={placeholder}
-      options={objectOptions}
-      onChange={mockChange}
-      tabIndex={tabIndex}
-      dataTest={dataTest}
-      readOnly
-      spaceAfter={spaceAfter}
-      dataAttrs={dataAttrs}
-    />,
-  );
+  it("should have expected DOM output", () => {
+    const placeholder = "Default placeholder";
+    const dataTest = "test";
+    const label = "label";
+    const tabIndex = "-1";
+    const name = "name";
+    const id = "test-id";
+    const onBlur = jest.fn();
+    const onFocus = jest.fn();
+    const onChange = jest.fn();
+    const objectOptions = [
+      { value: "1", label: "One" },
+      { value: "2", label: "Two" },
+      { value: "3", label: "Three" },
+      { value: "4", label: "Four" },
+      { value: "hidden-five", label: "Hidden Five" },
+      { disabled: true, value: "disabled-six", label: "Disabled Six" },
+    ];
+    const spaceAfter = SPACINGS_AFTER.NORMAL;
+    const dataAttrs = {
+      "data-recording-ignore": true,
+    };
 
-  const select = component.find("Select__StyledSelect");
-  const label = component.find("Select__Label");
-  it("should have data-test", () => {
-    expect(select.render().prop("data-test")).toBe(dataTest);
-  });
-  it("should have data-state", () => {
-    expect(select.render().prop("data-state")).toBe("ok");
-    expect(select.render().prop("data-recording-ignore")).toBe("true");
-  });
-  it("should have name", () => {
-    expect(select.render().prop("attribs").name).toBe(name);
-  });
-  it("should have spaceAfter", () => {
-    expect(label.prop("spaceAfter")).toBe(spaceAfter);
-  });
-  it("should have tabindex", () => {
-    expect(select.render().prop("tabindex")).toBe(tabIndex);
-  });
-  it("should have placeholder", () => {
-    expect(select.childAt(0).text()).toBe(placeholder);
-    expect(select.render().prop("id")).toBe(id);
+    render(
+      <Select
+        id={id}
+        label={label}
+        value="1"
+        name={name}
+        placeholder={placeholder}
+        options={objectOptions}
+        onChange={onChange}
+        tabIndex={tabIndex}
+        dataTest={dataTest}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        readOnly
+        spaceAfter={spaceAfter}
+        dataAttrs={dataAttrs}
+      />,
+    );
+
+    const select = screen.getByRole("combobox");
+
+    expect(screen.getByTestId(dataTest)).toBeInTheDocument();
+    expect(select).toHaveAttribute("id", id);
+    expect(select).toHaveAttribute("readonly");
+    expect(select).toHaveAttribute("tabindex", "-1");
+    expect(select).toHaveAttribute("data-state", "ok");
+    expect(select).toHaveAttribute("data-recording-ignore");
+    expect(select).toHaveAttribute("name", name);
+    expect(screen.getByLabelText(label)).toBeInTheDocument();
+    expect(screen.getByText(placeholder)).toBeInTheDocument();
+
+    userEvent.selectOptions(select, screen.getByText("One"));
+    expect(onChange).toHaveBeenCalled();
+    userEvent.tab(select);
+    expect(onFocus).toHaveBeenCalled();
+    fireEvent.blur(select);
+    expect(onBlur).toHaveBeenCalled();
   });
 
-  it("should have readOnly", () => {
-    expect(select.render().prop("readonly")).toBe(true);
+  it("should have custom value text", () => {
+    render(<Select customValueText="blin" options={[{ value: "1", label: "One" }]} />);
+    expect(screen.getByText("blin")).toBeInTheDocument();
   });
-});
 
-describe(`Required field`, () => {
-  const label = "Label";
-  const component = mount(
-    <Select options={objectOptions} label={label} required onChange={mockChange} />,
-  );
-  it("should render asterisk", () => {
-    expect(component.find("FormLabel__StyledAsterisk").exists()).toBe(true);
+  it("should be disabled", () => {
+    render(<Select options={[{ value: "1", label: "One" }]} disabled />);
+    expect(screen.getByRole("combobox")).toBeDisabled();
   });
 });

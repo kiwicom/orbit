0963474a0ab6a3a83ce6201ac2a0c95980cd07e0
Matija Marohnić <matija.marohnic@gmail.com>
2020-12-01 15:46:51 +0100

    test(Pagination): test labels and breakpoints (#2556)
    - group common tests that should work both when Pagination is normal and
    compact
    - test visibility of labels

--- packages/orbit-components/src/Pagination/__tests__/index.test.js
+++ packages/orbit-components/src/Pagination/__tests__/index.test.js
@@ -3,60 +3,93 @@ import * as React from "react";
 import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
+import useMediaQuery from "../../hooks/useMediaQuery";
 import Pagination from "../index";
 
-jest.mock("../../hooks/useMediaQuery", () => {
-  return () => {
-    return {
-      isTablet: true,
-    };
-  };
+jest.mock("../../hooks/useMediaQuery", () => jest.fn());
+
+const useMediaQueryMock: JestMockFn<[], $Shape<$Call<typeof useMediaQuery>>> = useMediaQuery;
+
+const breakpoints = {
+  normal: { isTablet: true },
+  compact: { isLargeMobile: true },
+};
+
+afterEach(() => {
+  useMediaQueryMock.mockRestore();
 });
 
 describe("Pagination", () => {
-  it("should have expected DOM output", () => {
-    const dataTest = "test";
-    const pageCount = 9;
-    const selectedPage = 3;
-    const onPageChange = jest.fn();
+  describe.each(Object.entries(breakpoints))("%s", (breakpoint, queryObject) => {
+    it("should have expected DOM output", () => {
+      const dataTest = "test";
+      const pageCount = 9;
+      const selectedPage = 3;
+      const onPageChange = jest.fn();
 
-    render(
-      <Pagination
-        dataTest={dataTest}
-        pageCount={pageCount}
-        selectedPage={selectedPage}
-        onPageChange={onPageChange}
-      />,
-    );
+      useMediaQueryMock.mockImplementation(() => queryObject);
+      render(
+        <Pagination
+          dataTest={dataTest}
+          pageCount={pageCount}
+          selectedPage={selectedPage}
+          onPageChange={onPageChange}
+        />,
+      );
 
-    expect(screen.getByTestId(dataTest)).toBeInTheDocument();
-    userEvent.click(screen.getByRole("button", { name: "Next" }));
-    expect(onPageChange).toHaveBeenCalled();
+      expect(screen.getByTestId(dataTest)).toBeInTheDocument();
+      userEvent.click(screen.getByRole("button", { name: "Next" }));
+      expect(onPageChange).toHaveBeenCalled();
+    });
+
+    it("should have next button disabled", () => {
+      useMediaQueryMock.mockImplementation(() => queryObject);
+      render(
+        <Pagination
+          dataTest="test"
+          hideLabels={false}
+          pageCount={9}
+          selectedPage={9}
+          onPageChange={() => {}}
+        />,
+      );
+      expect(screen.getByRole("button", { name: "Next" })).toBeDisabled();
+    });
+
+    it("should have first button disabled", () => {
+      useMediaQueryMock.mockImplementation(() => queryObject);
+      render(
+        <Pagination
+          dataTest="test"
+          hideLabels={false}
+          pageCount={6}
+          selectedPage={1}
+          onPageChange={() => {}}
+        />,
+      );
+      expect(screen.getByRole("button", { name: "Previous" })).toBeDisabled();
+    });
   });
 
-  it("should have next button disabled", () => {
-    render(
-      <Pagination
-        dataTest="test"
-        hideLabels={false}
-        pageCount={9}
-        selectedPage={9}
-        onPageChange={() => {}}
-      />,
-    );
-    expect(screen.getByRole("button", { name: "Next" })).toBeDisabled();
+  describe("normal", () => {
+    it("should be able to hide labels", () => {
+      useMediaQueryMock.mockImplementation(() => breakpoints.normal);
+      render(<Pagination hideLabels pageCount={6} onPageChange={() => {}} />);
+      expect(screen.getByRole("button", { name: "Previous" })).not.toHaveTextContent("Previous");
+    });
+
+    it("should be able to show labels", () => {
+      useMediaQueryMock.mockImplementation(() => breakpoints.normal);
+      render(<Pagination hideLabels={false} pageCount={6} onPageChange={() => {}} />);
+      expect(screen.getByRole("button", { name: "Previous" })).toHaveTextContent("Previous");
+    });
   });
 
-  it("should have first button disabled", () => {
-    render(
-      <Pagination
-        dataTest="test"
-        hideLabels={false}
-        pageCount={6}
-        selectedPage={1}
-        onPageChange={() => {}}
-      />,
-    );
-    expect(screen.getByRole("button", { name: "Previous" })).toBeDisabled();
+  describe("compact", () => {
+    it("should always hide labels", () => {
+      useMediaQueryMock.mockImplementation(() => breakpoints.compact);
+      render(<Pagination hideLabels={false} pageCount={6} onPageChange={() => {}} />);
+      expect(screen.getByRole("button", { name: "Previous" })).not.toHaveTextContent("Previous");
+    });
   });
 });

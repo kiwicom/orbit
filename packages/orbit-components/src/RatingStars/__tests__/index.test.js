4f4dc9fc4577d9f23b37946cf36fb5d02be76b0d
Victor Genaev <vgenaev@gmail.com>
2020-12-02 18:00:26 +0500

    test(RatingStars): rewrite to testing library (#2559)

--- packages/orbit-components/src/RatingStars/__tests__/index.test.js
+++ packages/orbit-components/src/RatingStars/__tests__/index.test.js
@@ -1,40 +1,26 @@
 // @flow
 
 import * as React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from "@testing-library/react";
 
 import RatingStars from "../index";
-import StarFull from "../../icons/StarFull";
-import StarEmpty from "../../icons/StarEmpty";
 import { ICON_COLORS, ICON_SIZES } from "../../Icon/consts";
 
 describe("RatingStars", () => {
-  const dataTest = "test";
-  const rating = 2.4;
-  const color = ICON_COLORS.SECONDARY;
-  const size = ICON_SIZES.LARGE;
-  const component = shallow(
-    <RatingStars rating={rating} size={size} color={color} showEmpty dataTest={dataTest} />,
-  );
-  const hotel = shallow(
-    <RatingStars rating={rating} size={size} color={color} dataTest={dataTest} />,
-  );
-  it("should have data-test", () => {
-    expect(component.render().prop("data-test")).toBe(dataTest);
+  it("should have expected DOM output", () => {
+    const dataTest = "test";
+    const rating = 2.4;
+    const color = ICON_COLORS.SECONDARY;
+    const size = ICON_SIZES.LARGE;
+
+    render(<RatingStars rating={rating} size={size} color={color} dataTest={dataTest} />);
+
+    expect(screen.getByTestId(dataTest)).toBeInTheDocument();
+    expect(screen.getByLabelText("Rating 2 out of 2")).toBeInTheDocument();
   });
 
-  it("stars should have size and color", () => {
-    component.children().forEach(node => {
-      expect(node.prop("size")).toBe(size);
-      expect(node.prop("color")).toBe(color);
-    });
-  });
-  it("should render 2 full stars and 3 empty", () => {
-    component.children().forEach((node, key) => {
-      expect(node.type()).toBe(key <= Math.round(rating) - 1 ? StarFull : StarEmpty);
-    });
-  });
-  it("should render only 2 full stars", () => {
-    expect(hotel.children()).toHaveLength(Math.round(rating));
+  it("should show empty stars", () => {
+    render(<RatingStars rating={2.4} showEmpty />);
+    expect(screen.getByLabelText("Rating 2 out of 5")).toBeInTheDocument();
   });
 });

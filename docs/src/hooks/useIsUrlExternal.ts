044ac6bf1f53d8a270bd060b572443833d308f43
Matija Marohnić <matija.marohnic@gmail.com>
2020-12-04 17:44:34 +0100

    docs: refine MDX anchor style and handle external

---
+++ docs/src/hooks/useIsUrlExternal.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function useIsUrlExternal(url?: string): boolean {
+  return React.useMemo(() => {
+    if (!url) return false;
+    // https://stackoverflow.com/a/2911045/1247274
+    const parser = document.createElement("a");
+    parser.href = url;
+    return parser.hostname !== location.hostname;
+  }, [url]);
+}

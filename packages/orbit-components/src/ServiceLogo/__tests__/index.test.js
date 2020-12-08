646cdf37d76378aa5e876927f8e71e140b79e211
Victor Genaev <vgenaev@gmail.com>
2020-12-09 04:20:33 +0500

    test(ServiceLogo): rewrite to testing library (#2553)

--- packages/orbit-components/src/ServiceLogo/__tests__/index.test.js
+++ packages/orbit-components/src/ServiceLogo/__tests__/index.test.js
@@ -1,6 +1,6 @@
 // @flow
 import * as React from "react";
-import { mount } from "enzyme";
+import { render, screen } from "@testing-library/react";
 
 import defaultTheme from "../../defaultTheme";
 import { NAME_OPTIONS, SIZE_OPTIONS, baseURL } from "../consts";
@@ -8,35 +8,30 @@ import ServiceLogo from "../index";
 
 const name = NAME_OPTIONS.AIRHELP;
 const size = SIZE_OPTIONS.LARGE;
-const grayScale = true;
 
 const SIZE = defaultTheme.orbit.heightServiceLogoLarge;
 
-describe(`ServiceLogo: ${name}`, () => {
-  const component = mount(<ServiceLogo name={name} size={size} />).find("img");
-  it("should have props", () => {
+describe(`ServiceLogo`, () => {
+  it("should have expected DOM output", () => {
     const IMAGE_PATH = `${baseURL}/logos/0x${parseInt(SIZE, 10)}/${name}.png`;
     const IMAGE_PATH_RETINA = `${baseURL}/logos/0x${parseInt(SIZE, 10) * 2}/${name}.png`;
+    const dataTest = "test";
 
-    expect(component.prop("src")).toBe(IMAGE_PATH);
-    expect(component.prop("srcSet")).toBe(`${IMAGE_PATH_RETINA} 2x`);
-    expect(component.prop("alt")).toBe(name);
+    render(<ServiceLogo dataTest={dataTest} name={name} size={size} />);
+
+    expect(screen.getByRole("img")).toHaveAttribute("src", IMAGE_PATH);
+    expect(screen.getByRole("img")).toHaveAttribute("srcSet", `${IMAGE_PATH_RETINA} 2x`);
+    expect(screen.getByAltText(name)).toBeInTheDocument();
+    expect(screen.getByTestId(dataTest)).toBeInTheDocument();
   });
-});
 
-describe(`ServiceLogo: ${name}`, () => {
-  const dataTest = "test";
-  const component = mount(
-    <ServiceLogo name={name} size={size} grayScale={grayScale} dataTest={dataTest} />,
-  ).find("img");
+  it("should be grayscale", () => {
+    render(<ServiceLogo name={name} size={size} grayScale />);
 
-  const IMAGE_PATH = `${baseURL}/logos-grayscale/0x${parseInt(SIZE, 10)}/${name}.png`;
-  const IMAGE_PATH_RETINA = `${baseURL}/logos-grayscale/0x${parseInt(SIZE, 10) * 2}/${name}.png`;
+    const IMAGE_PATH = `${baseURL}/logos-grayscale/0x${parseInt(SIZE, 10)}/${name}.png`;
+    const IMAGE_PATH_RETINA = `${baseURL}/logos-grayscale/0x${parseInt(SIZE, 10) * 2}/${name}.png`;
 
-  it("should have props", () => {
-    expect(component.prop("src")).toBe(IMAGE_PATH);
-    expect(component.prop("srcSet")).toBe(`${IMAGE_PATH_RETINA} 2x`);
-    expect(component.prop("alt")).toBe(name);
-    expect(component.render().prop("data-test")).toBe(dataTest);
+    expect(screen.getByRole("img")).toHaveAttribute("src", IMAGE_PATH);
+    expect(screen.getByRole("img")).toHaveAttribute("srcSet", `${IMAGE_PATH_RETINA} 2x`);
   });
 });

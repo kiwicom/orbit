5f9955e5e18ef8c80ed4aaa3740d51b10c55d895
Victor Genaev <vgenaev@gmail.com>
2020-12-09 23:16:38 +0500

    test(PictureCard): rewrite to testing library (#2546)

--- packages/orbit-components/src/PictureCard/__tests__/index.test.js
+++ packages/orbit-components/src/PictureCard/__tests__/index.test.js
@@ -1,11 +1,12 @@
 // @flow
-import { mount } from "enzyme";
 import * as React from "react";
+import { render, screen } from "@testing-library/react";
 
 import PictureCard from "..";
 
 const props = {
   height: "400px",
+  dataTest: "test",
   width: "800px",
   title: "Dubai",
   label: "Family Fun",
@@ -20,35 +21,18 @@ const props = {
   },
 };
 
-const wrapper = mount(<PictureCard {...props} />);
-
-describe("#PictureCard", () => {
-  it("should have an image", () => {
-    expect(wrapper.find("img").exists()).toBe(true);
-  });
-
-  it("should set height and width", () => {
-    const StyledPictureCard = wrapper.find("PictureCard__StyledPictureCard");
-
-    expect(StyledPictureCard.exists()).toBe(true);
-    expect(StyledPictureCard.prop("height")).toBe(props.height);
-    expect(StyledPictureCard.prop("width")).toBe(props.width);
-    expect(StyledPictureCard).toHaveStyleRule("height", props.height);
-    expect(StyledPictureCard).toHaveStyleRule("width", props.width);
-  });
-
-  it("should have title, subTitel and label", () => {
-    expect(wrapper.contains(props.title)).toBe(true);
-    expect(wrapper.contains(props.subTitle)).toBe(true);
-    expect(wrapper.contains(props.label)).toBe(true);
-  });
-
-  it("should render an anchor element if href is present", () => {
-    expect(wrapper.find("a").exists()).toBe(true);
-    expect(wrapper.find("PictureCard__StyledPictureCard").prop("href")).toBe(props.href);
-  });
-
-  it("should have href", () => {
-    expect(wrapper.find("a").prop("href")).toBe("http://weslav.com");
+describe("PictureCard", () => {
+  it("should have expected DOM output", () => {
+    render(<PictureCard {...props} />);
+    expect(screen.getAllByRole("img")).toHaveLength(2);
+    expect(screen.getByTestId(props.dataTest)).toBeInTheDocument();
+    expect(screen.getByText(props.label)).toBeInTheDocument();
+    expect(screen.getByText(props.title)).toBeInTheDocument();
+    expect(screen.getByText(props.subTitle)).toBeInTheDocument();
+    expect(screen.getByRole("link")).toBeInTheDocument();
+    expect(screen.getByTestId(props.dataTest)).toHaveStyle({
+      height: props.height,
+      width: props.width,
+    });
   });
 });

7a3833a319c01175b1935bdb685ed12b87148f6c
Victor Genaev <vgenaev@gmail.com>
2020-12-14 21:24:34 +0500

    test(NotificationBadge): rewrite to testing library (#2545)

--- packages/orbit-components/src/NotificationBadge/__tests__/index.test.js
+++ packages/orbit-components/src/NotificationBadge/__tests__/index.test.js
@@ -1,8 +1,9 @@
 // @flow
 
 import * as React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from "@testing-library/react";
 
+import theme from "../../defaultTheme";
 import NotificationBadge from "../index";
 import Sightseeing from "../../icons/Sightseeing";
 
@@ -10,21 +11,25 @@ describe("NotificationBadge", () => {
   const content = "badge";
   const type = "info";
   const dataTest = "test";
-  const icon = <Sightseeing />;
   const ariaLabel = content;
 
-  const wrapped = shallow(
-    <NotificationBadge type={type} icon={icon} dataTest={dataTest} ariaLabel={ariaLabel}>
+  render(
+    <NotificationBadge type={type} dataTest={dataTest} ariaLabel={ariaLabel}>
       {content}
     </NotificationBadge>,
   );
 
-  const component = wrapped.find("Badge");
+  it("should have expected DOM output", () => {
+    expect(screen.getByLabelText(ariaLabel)).toBeInTheDocument();
+    expect(screen.getByTestId(dataTest)).toBeInTheDocument();
+    expect(screen.getByText(content)).toBeInTheDocument();
+    expect(screen.getByTestId(dataTest)).toHaveStyle({
+      background: theme.orbit.paletteBlueLight,
+    });
+  });
 
-  it("should have passed props", () => {
-    expect(component.prop("type")).toBe(type);
-    expect(component.render().prop("data-test")).toBe(dataTest);
-    expect(component.render().prop("aria-label")).toBe(ariaLabel);
-    expect(component.prop("icon")).toBe(icon);
+  it("should have icon", () => {
+    render(<NotificationBadge icon={<Sightseeing dataTest="icon" />} />);
+    expect(screen.getByTestId("icon")).toBeInTheDocument();
   });
 });

d8ef89ea3938fa016546eef36b6978a19ba4c9d2
Victor Genaev <vgenaev@gmail.com>
2020-12-15 16:23:50 +0500

    test(Slider): rewrite to testing library (#2565)

--- packages/orbit-components/src/Slider/__tests__/index.test.js
+++ packages/orbit-components/src/Slider/__tests__/index.test.js
@@ -1,497 +1,83 @@
 // @flow
 
 import * as React from "react";
-import { mount, shallow } from "enzyme";
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import { PureSlider as Slider } from "../index";
-
-function testKeyTriggeringEvent(instance, expect, initialState, keyCode, stateValue, onHandler) {
-  instance.setState(initialState);
-  instance.handleKeyDown({
-    stopPropagation: () => {},
-    preventDefault: () => {},
-    keyCode,
-  });
-  expect(instance.state.value).toEqual(stateValue);
-  expect(onHandler).toHaveBeenCalled();
-}
+import Slider from "../index";
 
 describe("Slider", () => {
-  beforeEach(() => {
-    // $FlowExpectedError
-    Element.prototype.getBoundingClientRect = jest.fn(() => {
-      return {
-        width: 400,
-        height: 0,
-        top: 0,
-        left: 28,
-        bottom: 0,
-        right: 428,
-      };
-    });
-  });
-  const onChange = jest.fn();
-  const onChangeBefore = jest.fn();
-  const onChangeAfter = jest.fn();
-  const component = mount(
-    <Slider onChange={onChange} onChangeBefore={onChangeBefore} onChangeAfter={onChangeAfter} />,
-  );
-  const instance = component.instance();
-  it("calculateValue should return proper value", () => {
-    expect(instance.calculateValue(0.52, false, false)).toEqual(52);
-    expect(instance.calculateValue(0.52, true, false)).toEqual(53);
-    expect(instance.calculateValue(0.52, false, true)).toEqual(51);
-    expect(instance.calculateValue(0.52, true, true)).toEqual(52);
-  });
-  it("calculateValueFromPosition should return proper value", () => {
-    expect(instance.calculateValueFromPosition(52)).toEqual(7);
-    expect(instance.calculateValueFromPosition(352)).toEqual(81);
-    expect(instance.calculateValueFromPosition(400)).toEqual(93);
-    expect(instance.calculateValueFromPosition(428)).toEqual(100);
-  });
-  it("findClosestKey should return proper key", () => {
-    expect(instance.findClosestKey(1, [1, 12])).toEqual(0);
-    expect(instance.findClosestKey(2, [1, 12])).toEqual(0);
-    expect(instance.findClosestKey(6, [1, 12])).toEqual(0);
-    expect(instance.findClosestKey(7, [1, 12])).toEqual(1);
-    expect(instance.findClosestKey(11, [1, 12])).toEqual(1);
-    expect(instance.findClosestKey(11, [1, 12, 24])).toEqual(1);
-    expect(instance.findClosestKey(20, [1, 12, 24])).toEqual(2);
-  });
-  it("moveValueByStep should return proper value", () => {
-    component.setProps({ step: 1 });
-    expect(instance.moveValueByStep(1)).toEqual(2);
-    expect(instance.moveValueByStep(-1)).toEqual(1);
-    expect(instance.moveValueByStep(5)).toEqual(6);
-    expect(instance.moveValueByStep(-5)).toEqual(1);
-    expect(instance.moveValueByStep(-10)).toEqual(1);
-    instance.setState({ value: 100 });
-    expect(instance.moveValueByStep(1)).toEqual(100);
-    expect(instance.moveValueByStep(5)).toEqual(100);
-  });
-  it("alignValueToStep should return proper value", () => {
-    component.setProps({ step: 5 });
-    expect(instance.alignValueToStep(92)).toEqual(90);
-    expect(instance.alignValueToStep(1)).toEqual(0);
-    expect(instance.alignValueToStep(105)).toEqual(105);
-    expect(instance.alignValueToStep(40)).toEqual(40);
-    expect(instance.alignValueToStep(98)).toEqual(100);
-  });
-  it("alignValueToMaxMin should return proper value", () => {
-    expect(instance.alignValueToMaxMin(105)).toEqual(100);
-    expect(instance.alignValueToMaxMin(95)).toEqual(95);
-    expect(instance.alignValueToMaxMin(0)).toEqual(1);
-  });
-  it("replaceValue should return plain number", () => {
-    expect(instance.replaceValue(10, null)).toEqual(10);
-  });
-  it("handleMove should return proper value", () => {
-    expect(instance.handleMove(1)).toEqual(1);
-    expect(instance.handleMove(null)).toEqual(null);
-    expect(instance.handleMove(101)).toEqual(100);
-    expect(instance.handleMove(-10)).toEqual(1);
-  });
-  it("injectCallbackAndSetState should call", () => {
-    instance.injectCallbackAndSetState(onChange, 95);
-    expect(onChange).toHaveBeenCalled();
-    instance.injectCallbackAndSetState(onChangeBefore, 90);
-    expect(onChangeBefore).toHaveBeenCalled();
-    instance.injectCallbackAndSetState(onChangeAfter, 90, true);
-    expect(onChangeAfter).toHaveBeenCalled();
-  });
-  it("handleBarMouseDown execute onChange", () => {
-    instance.setState({ value: 50 });
-    component.setProps({ step: 1 });
-    instance.handleBarMouseDown({ pageX: 52 });
-    expect(instance.state.value).toEqual(7);
-    expect(onChange).toHaveBeenCalled();
-  });
-  it("defaultValue changes the value state", () => {
-    component.setProps({ defaultValue: 20 });
-    expect(instance.state.value).toEqual(20);
-  });
-});
+  it("should have expected DOM output", () => {
+    const onChange = jest.fn();
+    const onChangeBefore = jest.fn();
+    const onChangeAfter = jest.fn();
+    const label = "bur";
+    const ariaLabel = "label";
+    const ariaValueText = "text";
+    const minValue = 0;
+    const maxValue = 100;
+    const defaultValue = 2;
+    const valueDescription = "description";
+    const dataTest = "test";
 
-describe("Range Slider", () => {
-  beforeEach(() => {
-    // $FlowExpectedError
-    Element.prototype.getBoundingClientRect = jest.fn(() => {
-      return {
-        width: 400,
-        height: 0,
-        top: 0,
-        left: 28,
-        bottom: 0,
-        right: 428,
-      };
-    });
-  });
-  const eventMap = {};
-  window.addEventListener = jest.fn((event, cb) => {
-    eventMap[event] = cb;
-  });
-  window.removeEventListener = jest.fn(event => {
-    eventMap[event] = null;
-  });
-  const min = 1;
-  const max = 100;
-  const onChange = jest.fn();
-  const onChangeBefore = jest.fn();
-  const onChangeAfter = jest.fn();
-  const component = mount(
-    <Slider
-      minValue={min}
-      maxValue={max}
-      defaultValue={[12, 12]}
-      label="Depart from Prague"
-      onChange={onChange}
-      onChangeBefore={onChangeBefore}
-      onChangeAfter={onChangeAfter}
-      histogramData={[
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
-        100,
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
-        100,
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
-        100,
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
-        100,
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
-        100,
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
-        100,
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
-        100,
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
-        100,
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
-        100,
-        10,
-        20,
-        30,
-        40,
-        50,
-        60,
-        70,
-        80,
-        90,
-        100,
-      ]}
-    />,
-  );
-  const instance = component.instance();
-  it("calculateValueFromPosition should return proper value", () => {
-    expect(instance.calculateValueFromPosition(52)).toEqual(7);
-    expect(instance.calculateValueFromPosition(150)).toEqual(31);
-    instance.setState({ value: [15, 12], handleIndex: 0 });
-    expect(instance.calculateValueFromPosition(140)).toEqual(28);
-    instance.setState({ handleIndex: null });
-    expect(instance.calculateValueFromPosition(140)).toEqual(29);
-    instance.setState({ value: [12, 15], handleIndex: 0 });
-    expect(instance.calculateValueFromPosition(140)).toEqual(29);
-    instance.setState({ handleIndex: null });
-    expect(instance.calculateValueFromPosition(140, true)).toEqual(28);
-    instance.setState({ handleIndex: 1 });
-    expect(instance.calculateValueFromPosition(140, true)).toEqual(28);
-  });
-  it("should have Hide component", () => {
-    expect(component.find("Hide").exists()).toBe(true);
-  });
-  it("moveValueByStep should return proper value", () => {
-    instance.setState({ handleIndex: 0 });
-    expect(instance.moveValueByStep(1)).toEqual([13, 15]);
-    expect(instance.moveValueByStep(-1)).toEqual([11, 15]);
-    expect(instance.moveValueByStep(3)).toEqual([15, 15]);
-    expect(instance.moveValueByStep(-3)).toEqual([9, 15]);
-    expect(instance.moveValueByStep(-15)).toEqual([1, 15]);
-    expect(instance.moveValueByStep(100)).toEqual([100, 15]);
-  });
-  it("defaultValue changes the value state", () => {
-    component.setProps({ defaultValue: [1, 100] });
-    expect(instance.state.value).toEqual([1, 100]);
-  });
-  it("should execute keyDown event with arrow up", () => {
-    testKeyTriggeringEvent(
-      instance,
-      expect,
-      { handleIndex: 0, value: [50, 60] },
-      38,
-      [51, 60],
-      onChange,
+    render(
+      <Slider
+        onChange={onChange}
+        dataTest={dataTest}
+        onChangeBefore={onChangeBefore}
+        onChangeAfter={onChangeAfter}
+        label={label}
+        defaultValue={defaultValue}
+        valueDescription={valueDescription}
+        ariaLabel={ariaLabel}
+        ariaValueText={ariaValueText}
+        minValue={minValue}
+        maxValue={maxValue}
+      />,
     );
-  });
-  it("should execute keyDown event with arrow right", () => {
-    testKeyTriggeringEvent(
-      instance,
-      expect,
-      { handleIndex: 0, value: [50, 60] },
-      39,
-      [51, 60],
-      onChange,
-    );
-  });
-  it("should execute keyDown event with arrow down", () => {
-    testKeyTriggeringEvent(
-      instance,
-      expect,
-      { handleIndex: 1, value: [50, 60] },
-      40,
-      [50, 59],
-      onChange,
-    );
-  });
-  it("should execute keyDown event with arrow left", () => {
-    testKeyTriggeringEvent(
-      instance,
-      expect,
-      { handleIndex: 1, value: [50, 60] },
-      37,
-      [50, 59],
-      onChange,
-    );
-  });
-  it("should execute keyDown event with home key", () => {
-    testKeyTriggeringEvent(
-      instance,
-      expect,
-      { handleIndex: 0, value: [50, 60] },
-      36,
-      [1, 60],
-      onChange,
-    );
-    testKeyTriggeringEvent(
-      instance,
-      expect,
-      { handleIndex: 1, value: [50, 60] },
-      36,
-      [50, 1],
-      onChange,
-    );
-  });
-  it("should execute keyDown event with end key", () => {
-    testKeyTriggeringEvent(
-      instance,
-      expect,
-      { handleIndex: 0, value: [50, 60] },
-      35,
-      [100, 60],
-      onChange,
-    );
-    testKeyTriggeringEvent(
-      instance,
-      expect,
-      { handleIndex: 1, value: [50, 60] },
-      35,
-      [50, 100],
-      onChange,
-    );
-  });
-  it("handleKeyDown should not change the state when ctrl, shift or alt key", () => {
-    instance.setState({ value: [50, 60] });
-    instance.handleKeyDown({
-      keyCode: 35,
-      ctrlKey: true,
-      stopPropagation: () => {},
-      preventDefault: () => {},
-    });
-    expect(instance.state.value).toEqual([50, 60]);
-    instance.handleKeyDown({
-      keyCode: 35,
-      shiftKey: true,
-      stopPropagation: () => {},
-      preventDefault: () => {},
-    });
-    expect(instance.state.value).toEqual([50, 60]);
-    instance.handleKeyDown({
-      keyCode: 35,
-      altKey: true,
-      stopPropagation: () => {},
-      preventDefault: () => {},
-    });
-    expect(instance.state.value).toEqual([50, 60]);
-  });
-  it("handleBarMouseDown should execute onChange", () => {
-    instance.setState({ value: [50, 60] });
-    instance.handleBarMouseDown({ pageX: 52 });
-    expect(instance.state.value).toEqual([7, 60]);
-    expect(onChange).toHaveBeenCalled();
-  });
-  it("handleOnFocus should execute onChangeBefore and add listeners", () => {
-    instance.setState({ value: [50, 60] });
-    instance.handleOnFocus(0)({});
-    expect(typeof eventMap.keydown === "function").toBe(true);
-    expect(typeof eventMap.focusout === "function").toBe(true);
-    expect(instance.state.focused).toBe(true);
-    expect(onChangeBefore).toHaveBeenCalled();
-  });
-  it("handleBlur should call onChangeAfter and remove listeners", () => {
-    instance.handleBlur();
-    expect(typeof eventMap.keydown !== "function").toBe(true);
-    expect(typeof eventMap.focusout !== "function").toBe(true);
-    expect(onChangeAfter).toHaveBeenCalled();
-  });
-  it("handleOnTouchStart should execute onChangeBefore and add listeners", () => {
-    instance.setState({ value: [50, 60] });
-    instance.handleOnTouchStart(0)({ touches: [{ pageX: 109 }] });
-    expect(typeof eventMap.touchmove === "function").toBe(true);
-    expect(typeof eventMap.touchend === "function").toBe(true);
-    expect(instance.state.focused).toBe(true);
-    expect(onChangeBefore).toHaveBeenCalled();
-  });
-  it("handleTouchEnd should call onChangeAfter and remove listeners", () => {
-    instance.handleTouchEnd();
-    expect(typeof eventMap.touchmove !== "function").toBe(true);
-    expect(typeof eventMap.touchend !== "function").toBe(true);
-    expect(instance.state.focused).toBe(false);
-    expect(onChangeAfter).toHaveBeenCalled();
-  });
-  it("handleTouchMove should call onChange and set value", () => {
-    instance.setState({ value: [50, 60], handleIndex: 0 });
-    instance.handleOnTouchMove({ stopPropagation: () => {}, touches: [{ pageX: 109 }] });
-    expect(instance.state.value).toEqual([21, 60]);
-    expect(onChange).toHaveBeenCalled();
-  });
-  it("handleTouchMove should not set value when touches > 1", () => {
-    instance.setState({ value: [50, 60] });
-    instance.handleOnTouchMove({
-      touches: [{ pageX: 109 }, { pageX: 110 }],
-    });
-    expect(instance.state.value).toEqual([50, 60]);
-  });
-  it("handleMouseDown should execute onChangeBefore and add listeners", () => {
-    instance.setState({ value: [50, 60] });
-    instance.handleMouseDown(0)({ button: 0, buttons: 1 });
-    expect(typeof eventMap.mousemove === "function").toBe(true);
-    expect(typeof eventMap.mouseup === "function").toBe(true);
-    expect(instance.state.focused).toBe(true);
-    expect(onChangeBefore).toHaveBeenCalled();
-  });
-  it("handleMouseUp should call onChangeAfter and remove listeners", () => {
-    instance.handleMouseUp();
-    expect(typeof eventMap.mousemove !== "function").toBe(true);
-    expect(typeof eventMap.mouseup !== "function").toBe(true);
-    expect(instance.state.focused).toBe(false);
-    expect(onChangeAfter).toHaveBeenCalled();
-  });
-  it("handleMouseMove should call onChange and set value", () => {
-    instance.setState({ value: [50, 60], handleIndex: 0 });
-    instance.handleMouseMove({ pageX: 152 });
-    expect(instance.state.value).toEqual([32, 60]);
-    expect(onChange).toHaveBeenCalled();
-  });
-  it("replaceValue should return number or array number", () => {
-    instance.setState({ value: [50, 60] });
-    expect(instance.replaceValue(10, 1)).toEqual([50, 10]);
-  });
-});
 
-describe("Range Slider in render", () => {
-  const min = 1;
-  const max = 24;
-  const dataTest = "test";
-  const component = shallow(
-    <Slider
-      minValue={min}
-      maxValue={max}
-      defaultValue={[1, 24]}
-      label="Depart from Prague"
-      dataTest={dataTest}
-    />,
-  );
-  const slider = component.find("Slider__StyledSlider");
-  it("should have data-test", () => {
-    expect(slider.render().prop("data-test")).toBe(dataTest);
+    const slider = screen.getByRole("slider");
+
+    expect(screen.getByTestId(dataTest)).toBeInTheDocument();
+    expect(screen.getByText(valueDescription)).toBeInTheDocument();
+    expect(screen.getByText(label)).toBeInTheDocument();
+    expect(screen.getByLabelText(ariaLabel)).toBeInTheDocument();
+    expect(slider).toHaveAttribute("tabindex", "0");
+    expect(slider).toHaveAttribute("aria-valuemax", maxValue.toString());
+    expect(slider).toHaveAttribute("aria-valuemin", minValue.toString());
+    expect(slider).toHaveAttribute("aria-valuenow", defaultValue.toString());
+    expect(slider).toHaveAttribute("aria-valuetext", ariaValueText.toString());
+
+    userEvent.tab(slider);
+    expect(onChangeBefore).toHaveBeenCalled();
+    fireEvent.blur(slider);
+    expect(onChangeAfter).toHaveBeenCalled();
+
+    fireEvent.mouseDown(slider, { button: 0, buttons: 0 });
+    expect(onChangeBefore).toHaveBeenCalledTimes(2);
+    expect(onChangeAfter).toHaveBeenCalled();
+
+    fireEvent.mouseMove(slider);
+    expect(onChange).toHaveBeenCalledTimes(1);
+
+    fireEvent.touchStart(slider);
+    expect(onChangeBefore).toHaveBeenCalledTimes(3);
   });
-  it("calculateValueFromPosition should return null", () => {
-    expect(component.instance().calculateValueFromPosition(52)).toBe(null);
+
+  it("should have histogram", () => {
+    const data = Array.from({ length: 100 }, () => Math.floor(Math.random() * 100));
+    render(<Slider histogramData={data} />);
+    expect(screen.getByRole("presentation")).toBeInTheDocument();
   });
-  it("should have two Handles", () => {
-    const children = slider.find("Slider__StyledSliderInput").find("Handle");
-    children.forEach((node, index) => {
-      expect(node.prop("valueMax")).toBe(max);
-      expect(node.prop("valueMin")).toBe(min);
-      expect(node.prop("value")).toEqual([1, 24]);
-      expect(node.prop("index")).toBe(index);
-    });
+
+  it("should have loading histogram", () => {
+    const data = Array.from({ length: 100 }, () => Math.floor(Math.random() * 100));
+    render(<Slider histogramData={data} histogramLoadingText="loading" histogramLoading />);
+    expect(screen.getByText("loading")).toBeInTheDocument();
   });
-  it("should have Bar component", () => {
-    const children = slider.find("Slider__StyledSliderInput").find("Bar");
-    children.forEach(node => {
-      expect(node.prop("max")).toBe(max);
-      expect(node.prop("min")).toBe(min);
-      expect(node.prop("value")).toEqual([1, 24]);
-    });
+
+  it("should be ranged slider", () => {
+    render(<Slider defaultValue={[1, 10]} maxValue={24} minValue={1} />);
+    expect(screen.getAllByRole("slider")).toHaveLength(2);
   });
 });
--- packages/orbit-components/src/Slider/components/Histogram/_tests_/index.test.js
+++ packages/orbit-components/src/Slider/components/Histogram/_tests_/index.test.js
@@ -1,36 +1,14 @@
 // @flow
 
 import * as React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from "@testing-library/react";
 
 import Histogram from "../index";
 
-describe("Histogram in Slider", () => {
-  const data = [11, 25, 37, 2];
-  const component = shallow(<Histogram data={data} value={2} min={1} step={1} />);
+describe("Histogram", () => {
   it("should have four columns", () => {
-    const dataHeight = [29.7, 67.6, 100, 5.4];
-    const dataActive = [true, true, false, false];
-    const children = component
-      .find("Histogram__StyledHistogram")
-      .find("Histogram__StyledHistogramColumn");
-    children.forEach((node, index) => {
-      expect(node.prop("height")).toBe(dataHeight[index]);
-      expect(node.prop("active")).toBe(dataActive[index]);
-    });
-  });
-  it("should have four columns (value: [3, 4])", () => {
-    component.setProps({ value: [3, 4] });
-    const dataActive = [false, false, true, true];
-    const children = component
-      .find("Histogram__StyledHistogram")
-      .find("Histogram__StyledHistogramColumn");
-    children.forEach((node, index) => {
-      expect(node.prop("active")).toBe(dataActive[index]);
-    });
-  });
-  it("should render loading", () => {
-    component.setProps({ data: undefined, loading: true, loadingText: "Loading..." });
-    expect(component.find("Histogram__StyledLoadingContainer").find("Loading").exists()).toBe(true);
+    const data = [11, 25, 37, 2];
+    render(<Histogram data={data} value={2} min={1} step={1} />);
+    expect(screen.getByRole("presentation").childElementCount).toBe(4);
   });
 });

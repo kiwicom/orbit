2bc2a589271b8805d660be8272f94847244376b2
Matija Marohnić <matija.marohnic@gmail.com>
2020-12-15 13:03:42 +0100

    test(IllustrationPrimitive): rewrite to Testing Library

--- packages/orbit-components/src/primitives/IllustrationPrimitive/__tests__/index.test.js
+++ packages/orbit-components/src/primitives/IllustrationPrimitive/__tests__/index.test.js
@@ -1,51 +1,34 @@
 // @flow
 import * as React from "react";
-import { shallow, mount } from "enzyme";
+import { render, screen } from "@testing-library/react";
 
 import IllustrationPrimitive from "../index";
 import { SIZE_OPTIONS } from "../consts";
 import SPACINGS_AFTER from "../../../common/getSpacingToken/consts";
 import defaultTheme from "../../../defaultTheme";
 
-const size = SIZE_OPTIONS.EXTRASMALL;
-const name = "Accommodation";
-const alt = "Alternative text";
-const dataTest = "test";
-
 const URL = "//images.kiwi.com/illustrations/0x90/Accommodation-Q85.png";
 const URL_RETINA = "//images.kiwi.com/illustrations/0x180/Accommodation-Q85.png 2x";
 
-describe(`IllustrationPrimitive of ${name}`, () => {
-  const component = shallow(
-    <IllustrationPrimitive
-      size={size}
-      name={name}
-      alt={alt}
-      dataTest={dataTest}
-      spaceAfter={SPACINGS_AFTER.NORMAL}
-    />,
-  );
+describe("IllustrationPrimitive", () => {
+  it("should have expected DOM output", () => {
+    render(
+      <IllustrationPrimitive
+        size={SIZE_OPTIONS.EXTRASMALL}
+        name="Accommodation"
+        alt="Alternative text"
+        dataTest="test"
+        spaceAfter={SPACINGS_AFTER.NORMAL}
+      />,
+    );
 
-  const mountedComponent = mount(
-    <IllustrationPrimitive
-      size={size}
-      name={name}
-      dataTest={dataTest}
-      spaceAfter={SPACINGS_AFTER.NORMAL}
-    />,
-  );
+    const img = screen.getByRole("img");
 
-  it("should have passed props", () => {
-    expect(component.prop("size")).toBe(size);
-    expect(component.render().prop("alt")).toBe(alt);
-    expect(component.render().prop("data-test")).toBe(dataTest);
-  });
-
-  it("should render proper image", () => {
-    expect(component.render().attr("src")).toContain(URL);
-    expect(component.render().attr("srcset")).toContain(URL_RETINA);
-  });
-  it("should have margin-bottom", () => {
-    expect(mountedComponent).toHaveStyleRule("margin-bottom", defaultTheme.orbit.spaceSmall);
+    expect(getComputedStyle(img).maxHeight).toMatchInlineSnapshot(`"90px"`);
+    expect(screen.getByAltText("Alternative text")).toBeInTheDocument();
+    expect(screen.getByTestId("test")).toBeInTheDocument();
+    expect(img).toHaveAttribute("src", URL);
+    expect(img).toHaveAttribute("srcset", expect.stringContaining(URL_RETINA));
+    expect(img).toHaveStyleRule("margin-bottom", defaultTheme.orbit.spaceSmall);
   });
 });

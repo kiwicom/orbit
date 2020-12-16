037d985ea59c27046a4a975cbdcd8c6e530421a3
Matija Marohnić <matija.marohnic@gmail.com>
2020-12-16 14:12:05 +0100

    test(DeprecatedDestinationCard): rewrite to Testing Library (#2596)

--- packages/orbit-components/src/deprecated/DestinationCard/_tests_/index.test.js
+++ packages/orbit-components/src/deprecated/DestinationCard/_tests_/index.test.js
@@ -1,63 +1,43 @@
 // @flow
 import * as React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import DestinationCard from "../index";
 
-describe("DestinationCard one-way", () => {
-  const dataTest = "test";
-  const departureCity = "Prague";
-  const destinationCity = "Dubai";
-  const destinationCountry = "United Arab Emirates";
-  const image = "dubai_ae";
-  const height = 500;
-  const price = "5,563 Kč";
-  const onClick = jest.fn();
-  const outbound = { text: "One-way", type: "Direct", duration: "6h 10m" };
-  const component = shallow(
-    <DestinationCard
-      dataTest={dataTest}
-      departureCity={departureCity}
-      destinationCity={destinationCity}
-      destinationCountry={destinationCountry}
-      image={image}
-      height={height}
-      price={price}
-      outbound={outbound}
-      onClick={onClick}
-    />,
-  );
+describe("DestinationCard", () => {
+  it("should have expected DOM output", () => {
+    const dataTest = "test";
+    const departureCity = "Prague";
+    const destinationCity = "Dubai";
+    const destinationCountry = "United Arab Emirates";
+    const image = "dubai_ae";
+    const height = 500;
+    const price = "5,563 Kč";
+    const onClick = jest.fn();
+    const outbound = { text: "One-way", type: "Direct", duration: "6h 10m" };
 
-  const departure = component.find("DestinationCard__StyledDestinationCardHeader");
-  const destination = component.find("DestinationCard__StyledDestination");
-
-  it("should have props", () => {
-    expect(component.render().prop("data-test")).toBe(dataTest);
-    expect(component.prop("height")).toBe(height);
-  });
-
-  it("should render departure", () => {
-    expect(departure.find("Heading").first().children().text()).toBe(departureCity);
-    expect(departure.find("DestinationCard__ArrowUp").exists()).toBe(true);
-  });
-  it("should render destination city and country", () => {
-    expect(destination.find("Heading").children().text()).toBe(destinationCity);
-    expect(component.find("DestinationCard__Shown").find("Heading").at(1).children().text()).toBe(
-      destinationCountry,
+    render(
+      <DestinationCard
+        dataTest={dataTest}
+        departureCity={departureCity}
+        destinationCity={destinationCity}
+        destinationCountry={destinationCountry}
+        image={image}
+        height={height}
+        price={price}
+        outbound={outbound}
+        onClick={onClick}
+      />,
     );
-  });
-  it("should render price", () => {
-    expect(
-      component
-        .find("DestinationCard__StyledDestinationCardContent")
-        .find("Heading")
-        .at(3)
-        .children()
-        .text(),
-    ).toBe(price);
-  });
-  it("should execute onClick method", () => {
-    component.simulate("click");
+
+    expect(screen.getByTestId(dataTest)).toHaveStyle({ height: `${height}px` });
+    screen.getByText(departureCity);
+    screen.getByText(destinationCity);
+    screen.getByText(destinationCountry);
+    screen.getByText(price);
+
+    userEvent.click(screen.getByTestId(dataTest));
     expect(onClick).toHaveBeenCalled();
   });
 });

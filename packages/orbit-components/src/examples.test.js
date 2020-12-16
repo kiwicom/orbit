eb13a90b6c0126932e4f2618cc95fd9cb1b7a6e0
Matija Marohnić <matija.marohnic@gmail.com>
2020-12-16 14:49:48 +0100

    test(examples): rewrite to Testing Library

--- packages/orbit-components/src/examples.test.js
+++ packages/orbit-components/src/examples.test.js
@@ -1,27 +1,29 @@
 // @flow
 import * as React from "react";
-import { shallow } from "enzyme";
+import { render } from "@testing-library/react";
+import MatchMediaMock from "jest-matchmedia-mock";
 
 import * as examples from "./examples";
 
-Object.defineProperty(window, "matchMedia", {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
+let matchMedia: MatchMediaMock;
+
+beforeAll(() => {
+  matchMedia = new MatchMediaMock();
+});
+
+afterEach(() => {
+  matchMedia.clear();
+});
+
+afterAll(() => {
+  matchMedia.destroy();
 });
 
 describe("GitHub examples", () => {
   it.each(Object.keys(examples).map(k => [examples[k].info.title, examples[k].Example]))(
     "Rendering example %p",
     (name, Example) => {
-      const wrapper = shallow(<Example />);
-      expect(wrapper).toHaveLength(1);
+      render(<Example />);
     },
   );
 });

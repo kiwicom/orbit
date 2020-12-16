b2ab5d2065d82044991cad26ad4d46b2356e0034
mainframev <vgenaev@gmail.com>
2020-12-16 23:43:45 +0500

    test(Tag): rewrite to testing library

--- packages/orbit-components/src/Tag/__tests__/index.test.js
+++ packages/orbit-components/src/Tag/__tests__/index.test.js
@@ -1,42 +1,29 @@
 // @flow
 import * as React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Tag from "../index";
-import Bus from "../../icons/Bus";
 
 describe("Tag", () => {
-  const content = "Brno";
-  const dataTest = "test";
-  const onRemove = jest.fn();
-  const onClick = jest.fn();
-  const selected = true;
-  const icon = <Bus />;
+  it("should have expected DOM output", () => {
+    const content = "Brno";
+    const dataTest = "test";
+    const onRemove = jest.fn();
+    const onClick = jest.fn();
 
-  const component = shallow(
-    <Tag selected={selected} icon={icon} dataTest={dataTest} onRemove={onRemove} onClick={onClick}>
-      {content}
-    </Tag>,
-  );
+    render(
+      <Tag selected dataTest={dataTest} onRemove={onRemove} onClick={onClick}>
+        {content}
+      </Tag>,
+    );
 
-  it("the onRemove should be called", () => {
-    const CloseContainer = component.find("Tag__CloseContainer");
-    CloseContainer.simulate("click", { stopPropagation() {} });
-    expect(onRemove).toHaveBeenCalled();
-  });
-
-  it("the onClick should be called", () => {
-    component.simulate("click");
+    expect(screen.getByTestId(dataTest)).toBeInTheDocument();
+    const tag = screen.getByRole("button", { name: content });
+    userEvent.click(tag);
     expect(onClick).toHaveBeenCalled();
-  });
-
-  it("should have passed props", () => {
-    expect(component.render().prop("data-test")).toBe(dataTest);
-  });
-  it("should contain a content", () => {
-    expect(component.render().text()).toBe(content);
-  });
-  it("should contain a icon", () => {
-    expect(component.find("Bus").exists()).toBe(true);
+    userEvent.click(screen.getByRole("button", { name: "close" }));
+    expect(onRemove).toHaveBeenCalled();
+    expect(screen.getByText(content)).toBeInTheDocument();
   });
 });

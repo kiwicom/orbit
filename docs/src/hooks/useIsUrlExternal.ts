2d77e10b1132a126be3599b25ea4d73d18e16f0c
Victor Genaev <vgenaev@gmail.com>
2020-12-18 18:12:05 +0500

    fix(docs): window and document SSR (#2613)

--- docs/src/hooks/useIsUrlExternal.ts
+++ docs/src/hooks/useIsUrlExternal.ts
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function useIsUrlExternal(url?: string): boolean {
   return React.useMemo(() => {
-    if (!url) return false;
+    if (!url || typeof document === "undefined") return false;
     // https://stackoverflow.com/a/2911045/1247274
     const parser = document.createElement("a");
     parser.href = url;

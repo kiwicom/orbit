6b582c0d12db912265d6aa74100273b66a003322
Matija Marohnić <matija.marohnic@gmail.com>
2021-01-11 09:01:01 +0100

    improve: build JSON files when changing examples (#2649)
    By using lint-staged's function syntax we can avoid passing file names
    as arguments that usually happens behind the scenes in object syntax, so
    we can be sure that lint-staged will run the script exactly as we see
    it, because we don't need to pass arguments.

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+// @flow
+
+module.exports = {
+  "*.{js,js.flow,ts}": "eslint --fix --report-unused-disable-directives",
+  "*.md": "prettier --write",
+  "*.svg": "yarn orbit-components check:icons",
+  ".browserslistrc": () => ["yarn update-supported-browsers"],
+  "**/__examples__/**/*.js": () => ["lerna run --scope @kiwicom/orbit-components build:examples"],
+};

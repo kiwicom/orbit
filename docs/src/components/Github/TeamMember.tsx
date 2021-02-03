c4fc6701af9bce6abc630ac8f55ff91f81163adb
Victor Genaev <vgenaev@gmail.com>
2021-02-03 21:40:46 +0500

    feat: contributors component (#2652)

---
+++ docs/src/components/Github/TeamMember.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Stack } from "@kiwicom/orbit-components";
+import styled from "styled-components";
+import Twitter from "@kiwicom/orbit-components/lib/icons/Twitter";
+import LinkIcon from "@kiwicom/orbit-components/lib/icons/Link";
+
+import { Member } from "./OrbitTeam";
+import Github from "../../images/github.svg";
+import Dribbble from "../../images/dribbble.svg";
+
+const StyledHeading = styled.h4`
+  color: ${({ theme }) => theme.orbit.colorHeading};
+  font-size: calc(1em + 2px);
+  font-weight: ${({ theme }) => theme.orbit.fontWeightBold};
+  width: 100%;
+  line-height: 1.2;
+`;
+
+const StyledLink = styled.a.attrs(({ href }) => ({
+  href,
+  rel: "noopener noreferrer",
+  target: "_blank",
+}))`
+  svg {
+    height: 16px;
+    width: 16px;
+  }
+`;
+
+const StyledInfo = styled.div`
+  height: 100%;
+  color: ${({ theme }) => theme.orbit.colorTextPrimary};
+`;
+
+interface Props extends Member {
+  image: React.ReactNode;
+}
+
+const TeamMember = ({ position, info, name, twitter, website, dribbble, github, image }: Props) => {
+  const links = [
+    { url: twitter, icon: <Twitter /> },
+    { url: website, icon: <LinkIcon /> },
+    { url: dribbble, icon: <Dribbble /> },
+    { url: github, icon: <Github /> },
+  ];
+
+  return (
+    <Stack
+      inline
+      direction="column"
+      spacing="large"
+      align="center"
+      largeMobile={{ direction: "row", align: "stretch" }}
+    >
+      {image}
+      <Stack flex shrink direction="column" spacing="medium">
+        <StyledHeading>
+          {name}, {position}
+        </StyledHeading>
+        <StyledInfo>{info}</StyledInfo>
+        <Stack inline spacing="medium">
+          {links.map(({ icon, url }) => {
+            return (
+              url && (
+                <StyledLink key={url} href={url}>
+                  {icon}
+                </StyledLink>
+              )
+            );
+          })}
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default TeamMember;

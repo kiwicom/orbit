4c1e3b4af885f0d4c7b4531326a2f638756501cc
Victor Genaev <vgenaev@gmail.com>
2021-02-04 14:39:10 +0500

    feat(eslint-orbit): add recommended config (#2712)

--- packages/eslint-plugin-orbit-components/src/index.ts
+++ packages/eslint-plugin-orbit-components/src/index.ts
@@ -1,7 +1,12 @@
 import buttonHasTitle from "./rules/buttonHasTitle";
 import unnecessaryText from "./rules/unnecessaryText";
+import recommended from "./configs/recommended";
 
 export const rules = {
   "button-has-title": buttonHasTitle,
   "unnecessary-text": unnecessaryText,
+};
+
+export const configs = {
+  recommended,
 };

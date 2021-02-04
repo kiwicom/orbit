3585fc86acfb227040b0a2f8e29bea6afbbc109b
Luděk Vepřek <weprous@gmail.com>
2021-02-04 13:02:07 +0100

    fix: lint only icons inside orbit-components (#2717)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   "*.{js,js.flow,ts}": "eslint --fix --report-unused-disable-directives",
   "*.md": "prettier --write",
-  "*.svg": "yarn orbit-components check:icons",
+  "packages/orbit-components/**/*.svg": "yarn orbit-components check:icons",
   ".browserslistrc": () => ["yarn update-supported-browsers"],
   "**/__examples__/**/*.js": () => ["lerna run --scope @kiwicom/orbit-components build:examples"],
 };

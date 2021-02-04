a80c95a33fe53e5bfc3ad3ec9eb7c6f05abbc786
Victor Genaev <vgenaev@gmail.com>
2021-02-04 14:39:10 +0500

    feat(eslint-orbit): add recommended config (#2712)

---
+++ packages/eslint-plugin-orbit-components/src/configs/recommended.ts
@@ -0,0 +1,9 @@
+const recommended = {
+  plugins: ["orbit-components"],
+  rules: {
+    "orbit-components/button-has-title": "error",
+    "orbit-components/unnecessary-text": "error",
+  },
+};
+
+export default recommended;

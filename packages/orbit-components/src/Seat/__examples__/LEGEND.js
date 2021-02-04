2f0b139e65bca73f79b74799695f8d45be3f5240
Aaron Collier <aaron@collier.cz>
2021-02-04 15:10:31 +0100

    docs: update guidance on seat size

--- packages/orbit-components/src/Seat/__examples__/LEGEND.js
+++ packages/orbit-components/src/Seat/__examples__/LEGEND.js
@@ -4,22 +4,26 @@ import * as React from "react";
 import Seat from "../index";
 import SeatLegend from "../components/SeatLegend";
 import Stack from "../../Stack";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 export default {
-  Example: () => (
-    <Stack direction="column">
-      <Stack inline>
-        <Seat />
-        <Seat type="legroom" />
-        <Seat type="unavailable" />
-      </Stack>
+  Example: () => {
+    const { isTablet } = useMediaQuery();
+    return (
       <Stack direction="column">
-        <SeatLegend label="Extra legroom ($ 5.99 – $ 12.98)" />
-        <SeatLegend type="legroom" label="Standard ($ 5.99 – $ 12.98)" />
-        <SeatLegend type="unavailable" label="Unavailable" />
+        <Stack inline>
+          <Seat size={isTablet ? "medium" : "small"} />
+          <Seat size={isTablet ? "medium" : "small"} type="legroom" />
+          <Seat size={isTablet ? "medium" : "small"} type="unavailable" />
+        </Stack>
+        <Stack direction="column">
+          <SeatLegend label="Extra legroom ($ 5.99 – $ 12.98)" />
+          <SeatLegend type="legroom" label="Standard ($ 5.99 – $ 12.98)" />
+          <SeatLegend type="unavailable" label="Unavailable" />
+        </Stack>
       </Stack>
-    </Stack>
-  ),
+    );
+  },
   info: {
     title: "Legend",
     description: "Including an explanation can help users quickly scan the available options.",
--- packages/orbit-components/src/Seat/__examples__/PRICE.js
+++ packages/orbit-components/src/Seat/__examples__/PRICE.js
@@ -2,11 +2,15 @@
 import * as React from "react";
 
 import Seat from "../index";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 export default {
-  Example: () => <Seat price="12$" />,
+  Example: () => {
+    const { isTablet } = useMediaQuery();
+    return <Seat price="$12" size={isTablet ? "medium" : "small"} />;
+  },
   info: {
-    title: "Seat with price",
-    description: "Seat can have a price title",
+    title: "Price",
+    description: "You can set the price to display with each seat.",
   },
 };

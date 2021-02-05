33fb1cb416f9c2107b0c73c9a5c0b547668afb62
Aaron Collier <aaron@collier.cz>
2021-02-05 12:40:47 +0100

    feat(docs): add link checks
    and fix some broken links

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,10 @@
 module.exports = {
   "*.{js,js.flow,ts}": "eslint --fix --report-unused-disable-directives",
   "*.md": "prettier --write",
-  "*.mdx": "eslint --fix --report-unused-disable-directives",
+  "*.mdx": [
+    "eslint --fix --report-unused-disable-directives",
+    "remark -qf -u validate-links --no-config",
+  ],
   "packages/orbit-components/**/*.svg": "yarn orbit-components check:icons",
   ".browserslistrc": () => ["yarn update-supported-browsers"],
   "**/__examples__/**/*.js": () => ["lerna run --scope @kiwicom/orbit-components build:examples"],

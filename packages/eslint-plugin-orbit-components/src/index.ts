d3de0c3933a77302750606304978afdd096a50be
mainframev <vgenaev@gmail.com>
2021-02-14 04:37:05 +0500

    feat(eslint-plugin-orbit): custom-colors

--- packages/eslint-plugin-orbit-components/src/index.ts
+++ packages/eslint-plugin-orbit-components/src/index.ts
@@ -2,11 +2,13 @@ import buttonHasTitle from "./rules/buttonHasTitle";
 import unnecessaryText from "./rules/unnecessaryText";
 import recommended from "./configs/recommended";
 import preferSingleDestructure from "./rules/preferSingleDestructure";
+import customColors from "./rules/customColors";
 
 export const rules = {
   "button-has-title": buttonHasTitle,
   "unnecessary-text": unnecessaryText,
   "prefer-single-destructure": preferSingleDestructure,
+  "custom-colors": customColors,
 };
 
 export const configs = {

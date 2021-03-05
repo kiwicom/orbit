2749c80a379108be073b9a469a24e0b1717d3c5b
vepor <weprous@gmail.com>
2021-03-05 11:31:35 +0100

    feat(tokens): deprecated tokens eslint rule

--- packages/eslint-plugin-orbit-internal/src/index.ts
+++ packages/eslint-plugin-orbit-internal/src/index.ts
@@ -2,7 +2,7 @@ import uniqueId from "./rules/unique-id";
 import recommended from "./configs/recommended";
 
 export const rules = {
-  "unique-id": uniqueId
+  "unique-id": uniqueId,
 };
 
 export const configs = {

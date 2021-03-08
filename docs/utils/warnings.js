ea29c8a9985406f9499520eeccc044a3d7a618cb
Matija Marohnić <matija.marohnic@gmail.com>
2021-03-08 17:50:52 +0100

    improve: avoid error when warning about token
    If `error.missing` doesn't exist, `error.missing.includes` would fail.

--- docs/utils/warnings.js
+++ docs/utils/warnings.js
@@ -1,4 +1,11 @@
 const warnMissingAccessToken = error => {
+  if (typeof error.missing === "undefined") {
+    console.error(error);
+    console.info(
+      "You may have forgotten to include the repo and read:org scopes for your GitHub access token.",
+    );
+    return;
+  }
   if (error.missing.includes("GH_TOKEN")) {
     console.warn("Missing an access token for GitHub. Please create one.");
     console.info(
@@ -6,11 +13,6 @@ const warnMissingAccessToken = error => {
       "Create a personal access token: https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token",
       "Don't forget to include the required scopes for the token: all repo scopes and read:org.",
       "After creating a token in GitHub, add it to a .env file in the root folder with GH_TOKEN=<YOUR TOKEN>",
-    );
-  } else if (typeof error.missing === "undefined") {
-    console.error(error);
-    console.info(
-      "You may have forgotten to include the repo and read:org scopes for your GitHub access token.",
     );
   }
 };

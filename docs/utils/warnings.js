1872d817e4ca0e43dbaf07e7d216d37458ff4fff
Matija Marohnić <matija.marohnic@gmail.com>
2021-03-08 15:35:33 +0100

    improve: check only needed environment vars
    Always check only for variables needed for performing that particular
    task, instead of checking for unnecessarily failing if someone doesn't
    have a variable needed for a task they never happen to perform.

--- docs/utils/warnings.js
+++ docs/utils/warnings.js
@@ -7,7 +7,7 @@ const warnMissingAccessToken = error => {
       "Don't forget to include the required scopes for the token: all repo scopes and read:org.",
       "After creating a token in GitHub, add it to a .env file in the root folder with GH_TOKEN=<YOUR TOKEN>",
     );
-  } else {
+  } else if (typeof error.missing === "undefined") {
     console.error(error);
     console.info(
       "You may have forgotten to include the repo and read:org scopes for your GitHub access token.",

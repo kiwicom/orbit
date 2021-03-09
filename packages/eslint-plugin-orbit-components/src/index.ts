6645da8873b7d23f96214dd8fff65ea1cb40cdab
Victor Genaev <vgenaev@gmail.com>
2021-03-09 17:46:21 +0100

    feat(eslint-plugin-orbit): prefer-single-destructure (#2739)

--- packages/eslint-plugin-orbit-components/src/index.ts
+++ packages/eslint-plugin-orbit-components/src/index.ts
@@ -1,10 +1,12 @@
 import buttonHasTitle from "./rules/buttonHasTitle";
 import unnecessaryText from "./rules/unnecessaryText";
 import recommended from "./configs/recommended";
+import preferSingleDestructure from "./rules/preferSingleDestructure";
 
 export const rules = {
   "button-has-title": buttonHasTitle,
   "unnecessary-text": unnecessaryText,
+  "prefer-single-destructure": preferSingleDestructure,
 };
 
 export const configs = {

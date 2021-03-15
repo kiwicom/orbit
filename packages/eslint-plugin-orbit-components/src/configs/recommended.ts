10159d32507d60165b04166a7cff4901766d0408
Victor Genaev <vgenaev@gmail.com>
2021-03-15 18:09:40 +0100

    feat(eslint-plugin-orbit): custom-typography (#2777)
    * feat(eslint-plugin-orbit): custom-typography
    
    * fix(eslint-plugin-orbit): after review

--- packages/eslint-plugin-orbit-components/src/configs/recommended.ts
+++ packages/eslint-plugin-orbit-components/src/configs/recommended.ts
@@ -2,11 +2,12 @@ const recommended = {
   plugins: ["orbit-components"],
   rules: {
     "orbit-components/button-has-title": "error",
-    "orbit-components/unnecessary-text": "error",
-    "orbit-components/prefer-single-destructure": "warn",
-    "orbit-components/custom-colors": "warn",
     "orbit-components/default-theme": "error",
+    "orbit-components/no-custom-colors": "warn",
+    "orbit-components/no-custom-typography": "warn",
+    "orbit-components/prefer-single-destructure": "warn",
     "orbit-components/rtl-utils": "error",
+    "orbit-components/unnecessary-text": "error",
   },
 };
 

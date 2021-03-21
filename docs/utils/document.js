6d316bd48496cce19d5774e645dcefa73d5fdf66
Matija Marohnić <matija.marohnic@gmail.com>
2021-03-21 23:34:29 +0100

    docs: implement a more accurate trail via Node API
    The trail implementation in Breadcrumbs was incomplete because not all
    names can be inferred from url, for example "kiwi-use" should be "For
    Kiwi.com use", not "Kiwi use". This trail will be used for side
    navigation later.
    
    As a bonus, meta.yml files are now being enforced as well as checked
    for required fields.

--- docs/utils/document.js
+++ docs/utils/document.js
@@ -1,37 +1,34 @@
-const fs = require("fs");
 const path = require("path");
-const yaml = require("js-yaml");
 
 const rePathNumbers = new RegExp(`(${path.sep})\\d+-`, "g");
 const omitNumbers = filePath => filePath.replace(rePathNumbers, "$1");
 
-const ROOT = path.resolve(__dirname, "../src/documentation");
-
-function getMetaPath(dir) {
-  return path.join(ROOT, dir, "meta.yml");
-}
-
-function getMetaFileData(dir) {
-  const metaPath = getMetaPath(dir);
-  return fs.existsSync(metaPath) ? yaml.load(fs.readFileSync(metaPath)) : {};
-}
-
-function doesPageHaveTabs(dir) {
-  const meta = getMetaFileData(dir);
-  return meta.type === "tabs";
-}
-
-function getDocumentUrlPath(dir, base, name) {
+function getDocumentUrl(fileUrl, hasTabs) {
+  const { dir, base, name } = path.parse(fileUrl);
   if (!base.startsWith("01-")) {
     return omitNumbers(path.join(dir, name, "/"));
   }
-  return doesPageHaveTabs(dir)
-    ? omitNumbers(path.join(dir, "/"))
-    : omitNumbers(path.join(dir, name, "/"));
+  return hasTabs ? omitNumbers(path.join(dir, "/")) : omitNumbers(path.join(dir, name, "/"));
+}
+
+function getParentUrl(url) {
+  return path.join(path.dirname(url), "/");
+}
+
+const metaFileDataMap = new Map();
+
+function getDocumentTrail(url, trail = []) {
+  if (url === "/") {
+    return trail;
+  }
+  const { title } = metaFileDataMap.get(url);
+  return getDocumentTrail(getParentUrl(url), [{ name: title, url }, ...trail]);
 }
 
 module.exports = {
-  doesPageHaveTabs,
-  getDocumentUrlPath,
-  getMetaFileData,
+  omitNumbers,
+  getDocumentUrl,
+  metaFileDataMap,
+  getParentUrl,
+  getDocumentTrail,
 };

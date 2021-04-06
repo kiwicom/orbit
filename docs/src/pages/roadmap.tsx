d7dc75b8c87db1ef48fdee40cd9ff6750b4d93a3
Aaron Collier <aaron@collier.cz>
2021-04-06 09:23:58 +0200

    feat(docs): rework roadmap based on design

---
+++ docs/src/pages/roadmap.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "gatsby";
+import { Text } from "@kiwicom/orbit-components";
+
+import DocLayout from "../components/DocLayout";
+import HeadingWithLink from "../components/HeadingWithLink";
+import Roadmap from "../components/Roadmap";
+
+export default ({ location, path }) => (
+  <DocLayout
+    description="All our planning under one roof."
+    title="Roadmap"
+    location={location}
+    noElevation
+    path={path}
+    tableOfContents={[]}
+  >
+    <Text>
+      Interested in what’s planned for Orbit in the coming weeks and months? This is a good place to
+      start.
+    </Text>
+    <Text>
+      This roadmap contains delivery estimates only by quarter. For a more detailed overview of
+      what&apos;s already designed and the state it&apos;s in, visit{" "}
+      <Link to="/component-status/">component status</Link>.
+    </Text>
+    <HeadingWithLink headingLevel={2}>Current quarter 🚀</HeadingWithLink>
+    <Text type="secondary">Our key results for this quarter.</Text>
+    <Roadmap roadmapQuarter="current" />
+    <HeadingWithLink headingLevel={2}>Next quarter or two 🏗</HeadingWithLink>
+    <Text type="secondary">
+      There&apos;s a larger chance of this being our possible focus for next two quarters.
+    </Text>
+    <Roadmap roadmapQuarter="next" />
+    <HeadingWithLink headingLevel={2}>Future 🔮</HeadingWithLink>
+    <Text type="secondary">
+      Items and ideas being considered, without any commitment. We’ll see what the priorities will
+      be once we get there.
+    </Text>
+    <Roadmap roadmapQuarter="future" />
+  </DocLayout>
+);

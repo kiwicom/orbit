133ee076736a4715394b34ca8975d905d5352c80
Aaron Collier <aaron@collier.cz>
2021-04-07 14:50:23 +0200

    feat(docs): update guidance on notification badges
    what types are recommended

--- docs/src/__examples__/NotificationBadge/DEFAULT.tsx
+++ docs/src/__examples__/NotificationBadge/DEFAULT.tsx
@@ -2,10 +2,9 @@ import * as React from "react";
 import { NotificationBadge } from "@kiwicom/orbit-components";
 
 export default {
-  Example: () => <NotificationBadge type="criticalInverted">3</NotificationBadge>,
+  Example: () => <NotificationBadge type="info">3</NotificationBadge>,
   info: {
     title: "Default notification badges",
-    description:
-      "Notification badges should be only an icon or a 2-digit number. For notifications, use critical inverted badges unless you have a specific reason not to.",
+    description: "Notification badges should be only an icon or a number up to 2 digits.",
   },
 };

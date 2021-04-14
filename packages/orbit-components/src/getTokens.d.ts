edb0a576551a04ce05acf323a49accb98e983703
Aaron Collier <aaron@collier.cz>
2021-04-14 14:10:54 +0200

    fix: add TypeScript definition of getTokens

---
+++ packages/orbit-components/src/getTokens.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for @kiwicom/orbit-components
+// Project: http://github.com/kiwicom/orbit
+
+import { getTokens } from "@kiwicom/orbit-design-tokens";
+
+export default getTokens;

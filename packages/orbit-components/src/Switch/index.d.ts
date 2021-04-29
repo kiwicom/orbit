5a8f87f707be073f63e1e3d16d27fc7f3a35a6b8
mainframev <vgenaev@gmail.com>
2021-04-29 15:24:19 +0200

    test(Switch): add test
    Update packages/orbit-components/src/Switch/README.md
    
    Co-authored-by: Matija Marohnić <matija.marohnic@gmail.com>
    
    Update packages/orbit-components/src/Switch/README.md
    
    Co-authored-by: Matija Marohnić <matija.marohnic@gmail.com>
    
    feat(Switch): disabled

--- packages/orbit-components/src/Switch/index.d.ts
+++ packages/orbit-components/src/Switch/index.d.ts
@@ -9,9 +9,10 @@ declare module "@kiwicom/orbit-components/lib/Switch";
 
 export interface Props extends Common.Global, Common.Ref {
   readonly icon?: React.ReactNode;
-  readonly checked?: boolean;
+  readonly checked: boolean;
+  readonly ariaLabelledby?: string;
   readonly disabled?: boolean;
-  readonly onChange?: Common.Event<React.SyntheticEvent<HTMLInputElement>>;
+  readonly onChange: Common.Event<React.SyntheticEvent<HTMLInputElement>>;
   readonly onFocus?: Common.Event<React.SyntheticEvent<HTMLInputElement>>;
   readonly onBlur?: Common.Event<React.SyntheticEvent<HTMLInputElement>>;
 }

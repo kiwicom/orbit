ce48664b018af75d6e8a46b30a83d5b56cd52405
mainframev <vgenaev@gmail.com>
2021-05-04 16:46:58 +0200

    fix(Popover): fix bug with Tooltip inside Popover on mobile

--- packages/orbit-components/src/hooks/useClickOutside/index.d.ts
+++ packages/orbit-components/src/hooks/useClickOutside/index.d.ts
@@ -2,6 +2,7 @@ declare const UseClickOutside: (
   ref: { readonly current: HTMLElement | null | undefined },
   // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
   handler: (ev: React.SyntheticEvent<HTMLLinkElement>) => void,
+  isOff?: boolean,
 ) => void;
 
 export { UseClickOutside, UseClickOutside as default };

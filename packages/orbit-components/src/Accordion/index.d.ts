41dbce0caad59ca31b0b0734f6fd5c7bd84af924
Victor Genaev <vgenaev@gmail.com>
2021-05-05 16:09:59 +0200

    chore: add docs generator (#2667)
    * chore: add docs generator
    
    * feat(Accordion): update d.ts for docs-gen
    
    * chore: update monstra
    
    * feat(Accordion): update d.ts for docs-gen

--- packages/orbit-components/src/Accordion/index.d.ts
+++ packages/orbit-components/src/Accordion/index.d.ts
@@ -8,11 +8,31 @@ import * as Common from "../common/common";
 
 declare module "@kiwicom/orbit-components/lib/Accordion";
 
+/*
+  DOCS:
+  To implement Accordion component into your project you'll need to the import the Accordion and the [AccordionSection](#Accordionsection):
+  ```jsx
+  import Accordion, { AccordionSection } from "@kiwicom/orbit-components/lib/Accordion";
+  ```
+
+  After adding import into your project you can use it simply like:
+
+  ```jsx
+  <Accordion>
+    <AccordionSection>Hello World!</AccordionSection>
+  </Accordion>
+  ```
+*/
+
 export interface Props extends Common.Global, Common.SpaceAfter {
+  /** The content of the Accordion. You can use only AccordionSection */
   readonly children?: React.ReactNode;
+  /** Optional prop to control which AccordionSection (by id) is expanded */
   readonly expandedSection?: string | number;
-  readonly onExpand?: (sectionId: string | number) => void | Promise<any>;
+  /** If true it will render the Loading component */
   readonly loading?: boolean;
+  /** Callback (along with sectionId) that is triggered when section is expanding */
+  readonly onExpand?: (sectionId: string | number) => void | Promise<any>;
 }
 
 declare const Accordion: React.FunctionComponent<Props>;

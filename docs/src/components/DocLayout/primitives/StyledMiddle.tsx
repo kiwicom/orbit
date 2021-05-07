ae31bb871825612722f3b9e6ae353cd5722a3cfd
Matija Marohnić <matija.marohnic@gmail.com>
2021-05-07 13:00:18 +0200

    docs: span document container always full-width

--- docs/src/components/DocLayout/primitives/StyledMiddle.tsx
+++ docs/src/components/DocLayout/primitives/StyledMiddle.tsx
@@ -4,6 +4,7 @@ import { CONTENT_PADDING, MAX_CONTENT_WIDTH } from "../../../consts";
 
 const StyledMiddle = styled.div`
   padding: ${CONTENT_PADDING} ${CONTENT_PADDING} 0;
+  width: calc(100% - (${CONTENT_PADDING} * 2));
   max-width: ${MAX_CONTENT_WIDTH};
   margin: 0 auto;
   box-sizing: content-box;

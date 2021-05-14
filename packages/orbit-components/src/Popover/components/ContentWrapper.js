fcf2081c633a56b37632b437bd76500ca010bf0f
mainframev <vgenaev@gmail.com>
2021-05-14 14:08:45 +0200

    test(Popover): test Tooltip inside Popover

--- packages/orbit-components/src/Popover/components/ContentWrapper.js
+++ packages/orbit-components/src/Popover/components/ContentWrapper.js
@@ -183,7 +183,6 @@ const PopoverContentWrapper = ({
   const { isInsideModal } = React.useContext(ModalContext);
   const { isTablet } = useMediaQuery();
   const popover: {| current: React.ElementRef<*> |} = React.useRef(null);
-  const overlay: {| current: React.ElementRef<*> |} = React.useRef(null);
   const content: {| current: React.ElementRef<*> |} = React.useRef(null);
   const intervalRef = React.useRef(null);
   const position = calculatePopoverPosition(preferredPosition, preferredAlign);
@@ -217,15 +216,6 @@ const PopoverContentWrapper = ({
     [actions],
   );
 
-  const handleClick = React.useCallback(
-    ev => {
-      if (onClose && overlay.current.contains(ev.target)) {
-        onClose(ev);
-      }
-    },
-    [onClose],
-  );
-
   React.useEffect(() => {
     const timer = setTimeout(() => {
       if (popover.current) {
@@ -248,12 +238,7 @@ const PopoverContentWrapper = ({
 
   return (
     <>
-      <StyledOverlay
-        shown={shown}
-        isInsideModal={isInsideModal}
-        onMouseDown={handleClick}
-        ref={overlay}
-      />
+      <StyledOverlay shown={shown} isInsideModal={isInsideModal} onMouseDown={onClose} />
       <StyledPopoverParent
         shownMobile={shown}
         shown={shown && verticalPosition && horizontalPosition}

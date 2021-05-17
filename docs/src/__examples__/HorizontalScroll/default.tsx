fd99c61e2adc5c4edec05c40b08a4defa1198527
Aaron Collier <aaron@collier.cz>
2021-05-17 09:52:31 +0200

    fix: prettier

--- docs/src/__examples__/HorizontalScroll/default.tsx
+++ docs/src/__examples__/HorizontalScroll/default.tsx
@@ -4,42 +4,42 @@ import { BaggageCabin, BaggageChecked30, BaggagePersonal } from "@kiwicom/orbit-
 
 export default {
   Example: () => (
-      <HorizontalScroll>
-        <Tile title="Complete baggage bundle">
-          <Stack>
-            <Inline>
-              <BaggageChecked30 />
-              <Text>30 kg checked baggage</Text>
-            </Inline>
-            <Inline>
-              <BaggageCabin />
-              <Text>10 kg cabin baggage</Text>
-            </Inline>
-            <Inline>
-              <BaggagePersonal />
-              <Text>8 kg personal item</Text>
-            </Inline>
-          </Stack>
-        </Tile>
-        <Tile title="Basic baggage bundle">
-          <Stack>
-            <Inline>
-              <BaggageCabin />
-              <Text>10 kg cabin baggage</Text>
-            </Inline>
-            <Inline>
-              <BaggagePersonal />
-              <Text>8 kg personal item</Text>
-            </Inline>
-          </Stack>
-        </Tile>
-        <Tile title="Personal item only">
+    <HorizontalScroll>
+      <Tile title="Complete baggage bundle">
+        <Stack>
+          <Inline>
+            <BaggageChecked30 />
+            <Text>30 kg checked baggage</Text>
+          </Inline>
+          <Inline>
+            <BaggageCabin />
+            <Text>10 kg cabin baggage</Text>
+          </Inline>
           <Inline>
             <BaggagePersonal />
             <Text>8 kg personal item</Text>
           </Inline>
-        </Tile>
-      </HorizontalScroll>
+        </Stack>
+      </Tile>
+      <Tile title="Basic baggage bundle">
+        <Stack>
+          <Inline>
+            <BaggageCabin />
+            <Text>10 kg cabin baggage</Text>
+          </Inline>
+          <Inline>
+            <BaggagePersonal />
+            <Text>8 kg personal item</Text>
+          </Inline>
+        </Stack>
+      </Tile>
+      <Tile title="Personal item only">
+        <Inline>
+          <BaggagePersonal />
+          <Text>8 kg personal item</Text>
+        </Inline>
+      </Tile>
+    </HorizontalScroll>
   ),
   info: {
     title: "Default icon",

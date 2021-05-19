a191eb18d87096216f20511e989ac5c09d72d1ef
Matija Marohnić <matija.marohnic@gmail.com>
2021-05-19 13:31:50 +0200

    fix: make base font size always the same

--- docs/src/components/BaseStyles/BaseStyles.tsx
+++ docs/src/components/BaseStyles/BaseStyles.tsx
@@ -1,18 +1,11 @@
-import { mediaQueries } from "@kiwicom/orbit-components";
 import { createGlobalStyle, css } from "styled-components";
 
 const BaseStyles = createGlobalStyle`
   ${({ theme }) => css`
     html {
       font-family: ${theme.orbit.fontFamily};
-      font-size: 14px;
+      font-size: 16px;
     }
-
-    ${mediaQueries.tablet(css`
-      html {
-        font-size: 16px;
-      }
-    `)};
   `}
 `;
 

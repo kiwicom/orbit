2376e8abbf6dcae3f4e4831fcb7b6d66daa2320a
Aaron Collier <aaron@collier.cz>
2021-05-20 12:37:39 +0200

    docs: document spacing of Alert buttons

--- docs/src/__examples__/Alert/ACTIONS_SIDEBYSIDE.tsx
+++ docs/src/__examples__/Alert/ACTIONS_SIDEBYSIDE.tsx
@@ -8,7 +8,7 @@ export default {
         <Stack shrink direction="column" spacing="XSmall">
           <Heading type="title5">Type: Success</Heading>
           <Alert type="success" title="Your payment was successful." icon>
-            <Stack flex>
+            <Stack flex spacing="XSmall">
               <AlertButton type="success">Success</AlertButton>
               <AlertButton type="successSubtle">Success</AlertButton>
             </Stack>
@@ -17,7 +17,7 @@ export default {
         <Stack shrink direction="column" spacing="XSmall">
           <Heading type="title5">Type: Info</Heading>
           <Alert type="info" title="Check your details." icon>
-            <Stack flex>
+            <Stack flex spacing="XSmall">
               <AlertButton type="info">More info</AlertButton>
               <AlertButton type="infoSubtle">Contact support</AlertButton>
             </Stack>
@@ -28,7 +28,7 @@ export default {
         <Stack shrink direction="column" spacing="XSmall">
           <Heading type="title5">Type: Warning</Heading>
           <Alert type="warning" title="Visa may be required." icon>
-            <Stack flex>
+            <Stack flex spacing="XSmall">
               <AlertButton type="warning">Check visa requirements</AlertButton>
               <AlertButton type="warningSubtle">Contact support</AlertButton>
             </Stack>
@@ -37,7 +37,7 @@ export default {
         <Stack shrink direction="column" spacing="XSmall">
           <Heading type="title5">Type: Critical</Heading>
           <Alert type="critical" title="Your payment failed." icon>
-            <Stack flex>
+            <Stack flex spacing="XSmall">
               <AlertButton type="critical">Retry</AlertButton>
               <AlertButton type="criticalSubtle">Contact support</AlertButton>
             </Stack>

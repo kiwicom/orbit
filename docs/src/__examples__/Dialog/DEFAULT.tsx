124891f26a482203cf136db71234977208e8e510
mainframev <vgenaev@gmail.com>
2021-05-25 13:46:28 +0200

    docs(Dialog): add insidePortal prop

--- docs/src/__examples__/Dialog/DEFAULT.tsx
+++ docs/src/__examples__/Dialog/DEFAULT.tsx
@@ -10,6 +10,7 @@ export default {
           <Dialog
             title="Accept notifications?"
             description="Stay up to date with all travel possibilities."
+            insidePortal={false}
             primaryAction={
               <Button
                 onClick={() => {

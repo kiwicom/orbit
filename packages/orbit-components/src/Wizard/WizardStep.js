61914b94b4c89f618f5b15073394663b3ab7b316
Matija Marohnić <matija.marohnic@gmail.com>
2021-06-15 14:31:10 +0200

    fix(Wizard): increase clickable area of steps
    An additional refactor here is replacing the CheckCircle Orbit icon with
    a custom one in order to avoid the hack of placing the background circle
    and simplify the code for Wizard step icon.

--- packages/orbit-components/src/Wizard/WizardStep.js
+++ packages/orbit-components/src/Wizard/WizardStep.js
@@ -35,6 +35,15 @@ StyledContainer.defaultProps = {
   theme: defaultTheme,
 };
 
+const StyledContent = styled.div`
+  /* place above progress bar */
+  position: relative;
+`;
+// $FlowFixMe: https://github.com/flow-typed/flow-typed/issues/3653#issuecomment-568539198
+StyledContent.defaultProps = {
+  theme: defaultTheme,
+};
+
 const StyledActiveMarker = styled.div`
   ${({ theme }) => css`
     position: absolute;
@@ -52,14 +61,14 @@ StyledActiveMarker.defaultProps = {
 };
 
 const StyledProgressBar = styled.div`
-  ${({ theme, status, nextStepStatus, iconHeight }) => css`
+  ${({ theme, status, nextStepStatus }) => css`
     position: relative;
     &:before,
     &:after {
       content: "";
       display: block;
       position: absolute;
-      top: ${parseFloat(iconHeight) / 2 - 1}px;
+      top: ${parseFloat(theme.orbit.heightIconSmall) / 2 - 1}px;
       width: 50%;
       height: 2px;
     }
@@ -82,37 +91,45 @@ StyledProgressBar.defaultProps = {
   theme: defaultTheme,
 };
 
-const StyledLink = styled.a`
+const StyledLabel = styled.div`
   ${({ theme, active }) => css`
     display: block;
-    text-decoration: none;
     span {
       display: block;
+      color: ${theme.orbit.colorTextSecondary};
     }
-    ${active
-      ? css`
-          span {
-            font-weight: ${theme.orbit.fontWeightMedium};
-          }
-        `
-      : // appear interactive only if it's not active
-        css`
-          cursor: pointer;
-          span {
-            color: ${theme.orbit.colorTextSecondary};
-          }
-          &:hover,
-          &:focus {
-            span {
-              color: ${theme.orbit.colorTextPrimary};
-              text-decoration: underline;
-            }
-          }
-        `}
+    ${active &&
+    css`
+      span {
+        font-weight: ${theme.orbit.fontWeightMedium};
+        color: ${theme.orbit.colorTextPrimary};
+      }
+    `}
   `};
 `;
 // $FlowFixMe: https://github.com/flow-typed/flow-typed/issues/3653#issuecomment-568539198
-StyledLink.defaultProps = {
+StyledLabel.defaultProps = {
+  theme: defaultTheme,
+};
+
+const StyledButtonWrapper = styled.div.attrs({ tabIndex: 0, role: "button" })`
+  ${({ theme }) => css`
+    cursor: pointer;
+    padding: 0 ${theme.orbit.spaceXSmall};
+
+    &:hover,
+    &:focus {
+      ${StyledLabel} {
+        span {
+          text-decoration: underline;
+          color: ${theme.orbit.colorTextPrimary};
+        }
+      }
+    }
+  `}
+`;
+// $FlowFixMe: https://github.com/flow-typed/flow-typed/issues/3653#issuecomment-568539198
+StyledButtonWrapper.defaultProps = {
   theme: defaultTheme,
 };
 
@@ -127,9 +144,6 @@ const WizardStep = ({ dataTest, title, onClick }: Props): React.Node => {
     if (onChangeStep) onChangeStep(index);
   };
 
-  const iconWidth = parseFloat(theme.orbit.widthIconSmall) + 3.2;
-  const iconHeight = parseFloat(theme.orbit.heightIconSmall) + 3.2;
-
   if (isCompact) {
     return (
       <StyledContainer data-test={dataTest} isCompact={isCompact} status={status}>
@@ -138,7 +152,7 @@ const WizardStep = ({ dataTest, title, onClick }: Props): React.Node => {
           disabled={status === "disabled"}
           type="secondary"
           fullWidth
-          iconLeft={<WizardStepIcon width={iconWidth} height={iconHeight} />}
+          iconLeft={<WizardStepIcon />}
           ariaCurrent={isActive ? "step" : "false"}
           onClick={handleClick}
         >
@@ -154,46 +168,55 @@ const WizardStep = ({ dataTest, title, onClick }: Props): React.Node => {
     );
   }
 
-  return (
-    <StyledContainer data-test={dataTest} isCompact={isCompact} status={status}>
-      <StyledProgressBar status={status} nextStepStatus={nextStepStatus} iconHeight={iconHeight} />
-      <Stack direction="column" align="center" spacing="XSmall">
-        <WizardStepIcon width={iconWidth} height={iconHeight} />
-        <div
-          css={css`
-            padding: ${theme.orbit.paddingBadge};
-          `}
-        >
-          {status === "disabled" ? (
-            <Text as="div" type="secondary" size="small" align="center">
+  const step = (
+    <Stack direction="column" align="center" spacing="XSmall">
+      <WizardStepIcon />
+      <div
+        css={css`
+          padding: ${theme.orbit.paddingBadge};
+        `}
+      >
+        {status === "disabled" ? (
+          <Text as="div" type="secondary" size="small" align="center">
+            {title}
+          </Text>
+        ) : (
+          <StyledLabel active={isActive}>
+            <Text as="span" size="small" align="center">
               {title}
             </Text>
+          </StyledLabel>
+        )}
+      </div>
+    </Stack>
+  );
+
+  return (
+    <StyledContainer data-test={dataTest} isCompact={isCompact} status={status}>
+      <StyledProgressBar status={status} nextStepStatus={nextStepStatus} />
+      <StyledContent>
+        <Stack flex direction="column" align="center">
+          {isActive || status === "disabled" ? (
+            step
           ) : (
-            <div>
-              <StyledLink
-                active={isActive}
-                role="button"
-                tabIndex={0}
-                aria-current={isActive ? "step" : "false"}
-                onClick={event => {
-                  event.currentTarget.blur();
+            <StyledButtonWrapper
+              aria-current={isActive ? "step" : "false"}
+              onClick={event => {
+                event.currentTarget.blur();
+                handleClick(event);
+              }}
+              // keep focus for people who are navigating with the keyboard
+              onKeyDown={event => {
+                if (event.key === "Enter") {
                   handleClick(event);
-                }}
-                // keep focus for people who are navigating with the keyboard
-                onKeyDown={event => {
-                  if (event.key === "Enter") {
-                    handleClick(event);
-                  }
-                }}
-              >
-                <Text as="span" size="small" align="center">
-                  {title}
-                </Text>
-              </StyledLink>
-            </div>
+                }
+              }}
+            >
+              {step}
+            </StyledButtonWrapper>
           )}
-        </div>
-      </Stack>
+        </Stack>
+      </StyledContent>
     </StyledContainer>
   );
 };

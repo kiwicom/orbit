1ee878b890f0680e8ebc2ff8aee7b8d130151064
Matija Marohnić <matija.marohnic@gmail.com>
2021-07-12 13:11:48 +0200

    fix: fix import statement

--- docs/wrap-with-providers.tsx
+++ docs/wrap-with-providers.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ThemeProvider } from "@kiwicom/orbit-components";
 
 import { DevModeProvider } from "./src/hooks/useDevMode";
-import { TableOfContentsProvider } from "./src/components/TableOfContents";
+import { TableOfContentsProvider } from "./src/services/table-of-contents";
 import theme from "./src/theme";
 
 export default function wrapWithProviders({ element }: { element: React.ReactNode }) {

e5dbd557fea7cdeb44bd864dff71e54bdcea2afb
Matija Marohnić <matija.marohnic@gmail.com>
2021-07-12 14:04:52 +0200

    chore: remove unused functions

--- docs/src/utils/common.ts
+++ docs/src/utils/common.ts
@@ -1,13 +1,5 @@
 import React from "react";
 
-export const omitNumbers = (str: string) =>
-  str
-    .split("/")
-    .map(s => s.replace(/^\d+-\s*/g, ""))
-    .join("/");
-
-export const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
-
 export const pascalize = (str: string) =>
   str
     .replace(/-(.)/g, (_match, chartacter: string) => chartacter.toUpperCase())

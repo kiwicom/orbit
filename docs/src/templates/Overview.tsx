4bc2ac6ee551e5f49e99d3679b2fc758bcaee393
vepor <weprous@gmail.com>
2021-07-13 15:12:27 +0200

    docs: fix overview inner spacing

--- docs/src/templates/Overview.tsx
+++ docs/src/templates/Overview.tsx
@@ -25,15 +25,14 @@ interface Props extends PageRendererProps {
 const Overview = ({ location, pageContext }: Props) => {
   const { slug, title, trail, pages } = pageContext;
 
-  const tableOfContents = useTableOfContents()
-
   return (
     <DocLayout location={location} path={slug} title={title} trail={trail} noElevation>
       <Grid
         columns="1fr"
         gap="2rem"
         largeMobile={{ columns: "repeat(2, 1fr)" }}
-        {...(tableOfContents.length > 0 ? { desktop: { columns: "repeat(3, 1fr)" }, largeDesktop: { columns: "repeat(2, 1fr)" }, } : { tablet: { columns: "repeat(3, 1fr)" }, })}
+        desktop={{columns: "repeat(3, 1fr)" }}
+        largeDesktop={{ columns: "repeat(2, 1fr)" }}
       >
         {pages.map(page => (
           <Tile key={page.slug} title={page.title} href={page.slug}>

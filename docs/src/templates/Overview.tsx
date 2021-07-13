0b9187d337e1a698257bacb82e2c881ace2449f3
vepor <weprous@gmail.com>
2021-07-13 15:13:29 +0200

    docs: replace Spectrum chat for Figma community link

--- docs/src/templates/Overview.tsx
+++ docs/src/templates/Overview.tsx
@@ -4,7 +4,6 @@ import { Grid } from "@kiwicom/orbit-components";
 
 import DocLayout from "../components/DocLayout";
 import Tile from "../components/Tile";
-import { useTableOfContents } from "../services/table-of-contents";
 
 interface Props extends PageRendererProps {
   pageContext: {

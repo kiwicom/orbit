101014efb36183f482437f09c7c7eb41b05e8d81
vepor <weprous@gmail.com>
2021-07-13 14:33:31 +0200

    docs: fix overview inner spacing

--- docs/src/templates/Overview.tsx
+++ docs/src/templates/Overview.tsx
@@ -4,6 +4,7 @@ import { Grid } from "@kiwicom/orbit-components";
 
 import DocLayout from "../components/DocLayout";
 import Tile from "../components/Tile";
+import { useTableOfContents } from "../services/table-of-contents";
 
 interface Props extends PageRendererProps {
   pageContext: {
@@ -24,14 +25,15 @@ interface Props extends PageRendererProps {
 const Overview = ({ location, pageContext }: Props) => {
   const { slug, title, trail, pages } = pageContext;
 
+  const [tableOfContents] = useTableOfContents()
+
   return (
     <DocLayout location={location} path={slug} title={title} trail={trail} noElevation>
       <Grid
         columns="1fr"
         gap="2rem"
         largeMobile={{ columns: "repeat(2, 1fr)" }}
-        desktop={{ columns: "repeat(3, 1fr)" }}
-        largeDesktop={{ columns: "repeat(2, 1fr)" }}
+        {...(tableOfContents.length > 0 ? { desktop: { columns: "repeat(3, 1fr)" }, largeDesktop: { columns: "repeat(2, 1fr)" }, } : { tablet: { columns: "repeat(3, 1fr)" }, })}
       >
         {pages.map(page => (
           <Tile key={page.slug} title={page.title} href={page.slug}>

d03ed5adbc7202ac30941bbc9f6b4a8b71bc3d82
vepor <weprous@gmail.com>
2021-07-15 23:17:15 +0200

    feat(tokens): add old and deprecated API for compatibility

--- packages/orbit-design-tokens/src/index.ts
+++ packages/orbit-design-tokens/src/index.ts
@@ -1,6 +1,9 @@
 import createTheme from "./js/createTheme";
 
 export const defaultTheme = createTheme();
+export const defaultTokens = createTheme();
 export { default as fromPlainObject } from "./js/fromPlainObject";
 export { default as transparentColor } from "./js/transparentColor";
+export { default as getTokens } from "./js/getTokens";
+export { default as convertHexToRgba } from "./js/convertHexToRgba";
 export { createTheme };

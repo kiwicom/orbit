bc4ac9f7ecbd1a35faa600a713dad496641d3bbc
Luděk Vepřek <weprous@gmail.com>
2021-07-16 15:02:32 +0200

    Update docs/src/components/DesignTokens/components/DesignTokenValue.tsx
    Co-authored-by: Victor Genaev <vgenaev@gmail.com>

--- docs/src/components/DesignTokens/components/DesignTokenValue.tsx
+++ docs/src/components/DesignTokens/components/DesignTokenValue.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import styled from "styled-components";
 import { Truncate, Tooltip } from "@kiwicom/orbit-components";
 import { Check } from "@kiwicom/orbit-components/icons";

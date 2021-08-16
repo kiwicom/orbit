0d5c631bd18057882b65af3e58929dfe79f6bce9
mainframev <vgenaev@gmail.com>
2021-08-16 16:43:58 +0200

    docs(sandbox): add BadgePrimitive knobs

--- docs/src/__examples__/BadgePrimitive/DEFAULT.tsx
+++ docs/src/__examples__/BadgePrimitive/DEFAULT.tsx
@@ -16,8 +16,21 @@ export default {
       </BadgePrimitive>
     </Stack>
   ),
-  info: {
-    title: "Default badge primitives",
-    description: "By default, badge primitives have padding with no background and ink text.",
-  },
+  knobs: [
+    {
+      name: "icon",
+      type: "icon",
+      defaultValue: "",
+    },
+    {
+      name: "background",
+      type: "text",
+      defaultValue: "linear-gradient(264.15deg, #FC8D3E 0%, #F16E5B 100%)",
+    },
+    {
+      name: "foregroundColor",
+      type: "text",
+      defaultValue: "#fff",
+    },
+  ],
 };

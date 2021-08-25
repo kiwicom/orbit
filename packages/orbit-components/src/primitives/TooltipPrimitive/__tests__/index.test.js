ff1d8a30a2881efa271de2de15eb52db2b7341cd
mainframev <vgenaev@gmail.com>
2021-08-25 13:15:32 +0200

    test(TooltipPrimitive): remove redundant mock

--- packages/orbit-components/src/primitives/TooltipPrimitive/__tests__/index.test.js
+++ packages/orbit-components/src/primitives/TooltipPrimitive/__tests__/index.test.js
@@ -5,10 +5,6 @@ import { render } from "@testing-library/react";
 import Tooltip from "..";
 import Airplane from "../../../icons/Airplane";
 
-jest.mock("../../../hooks/useRandomId", () => ({
-  useRandomId: () => 1,
-}));
-
 describe("Tooltip", () => {
   it("it should match snapshot", () => {
     const content = "Write some message to the user";

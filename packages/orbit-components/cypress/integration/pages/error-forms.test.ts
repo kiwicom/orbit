64f4c99db2ae78a38418a93d48794688dcc63fa5
mainframev <vgenaev@gmail.com>
2021-09-03 21:20:34 +0200

    chore: remove tests

--- packages/orbit-components/cypress/integration/pages/error-forms.test.ts
+++
@@ -1,6 +0,0 @@
-describe("errorForms", () => {
-  it("should have error message", () => {
-    cy.visit("/error-forms");
-    cy.get("input").type("message").should("have.value", "message");
-  });
-});

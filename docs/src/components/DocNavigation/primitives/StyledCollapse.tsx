d00ca699927068828f6b2352806a99f447ec6c5e
mainframev <vgenaev@gmail.com>
2021-09-06 22:00:58 +0200

    docs(Navigation): change spacing between menu items

--- docs/src/components/DocNavigation/primitives/StyledCollapse.tsx
+++ docs/src/components/DocNavigation/primitives/StyledCollapse.tsx
@@ -6,9 +6,11 @@ export const StyledCollapseWrapper = styled.div`
     border-bottom: 0;
   }
 `;
+
 export const StyledCollapseLabel = styled.div`
   font-size: 16px;
 `;
+
 export const StyledCollapseContent = styled.div<{ hasCategories: boolean }>`
   ${({ hasCategories }) => css`
     margin: -10px 0;

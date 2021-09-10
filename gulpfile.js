1f967c85a2c39bd1ac377245935f2e82c20d253f
Matija Marohnić <matija.marohnic@gmail.com>
2021-09-10 21:08:57 +0200

    improve: discontinue orbit.kiwi changelog

--- gulpfile.js
+++ gulpfile.js
@@ -28,30 +28,32 @@ function configureGitHubToken(done) {
 
 async function previewChangelog(done) {
   const packages = await getPackages();
-  const streams = packages.map(pkg => {
-    return conventionalChangelog(
-      {
-        lernaPackage: pkg.name,
-        preset: "angular",
-      },
-      {
-        host: "https://github.com",
-        owner: "kiwicom",
-        repository: "orbit",
-      },
-      {
-        path: path.join("packages", pkg.name.replace("@kiwicom/", "")),
-      },
-    ).pipe(
-      transform((chunk, enc, cb) => {
-        const changelog = chunk.toString();
-        if (changelog.trim().includes("\n")) {
-          console.log(markdownChalk(changelog));
-        }
-        cb();
-      }),
-    );
-  });
+  const streams = packages
+    .filter(pkg => !pkg.name.includes("orbit.kiwi"))
+    .map(pkg => {
+      return conventionalChangelog(
+        {
+          lernaPackage: pkg.name,
+          preset: "angular",
+        },
+        {
+          host: "https://github.com",
+          owner: "kiwicom",
+          repository: "orbit",
+        },
+        {
+          path: path.join("packages", pkg.name.replace("@kiwicom/", "")),
+        },
+      ).pipe(
+        transform((chunk, enc, cb) => {
+          const changelog = chunk.toString();
+          if (changelog.trim().includes("\n")) {
+            console.log(markdownChalk(changelog));
+          }
+          cb();
+        }),
+      );
+    });
 
   mergeStream(...streams)
     .on("done", () => {
@@ -65,7 +67,7 @@ async function previewChangelog(done) {
 function publishPackages() {
   return spawn(
     "yarn",
-    ["lerna", "publish", "--conventional-commits", "--create-release", "github"],
+    ["lerna", "publish", "--no-private", "--conventional-commits", "--create-release", "github"],
     { stdio: "inherit" },
   );
 }

4a67d8c9bc21e087ee01f06540e7e01fb5f9a1e3
mainframev <vgenaev@gmail.com>
2021-09-16 17:12:03 +0200

    docs(Card): update example

--- docs/src/__examples__/Card/DEFAULT.tsx
+++ docs/src/__examples__/Card/DEFAULT.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import { Card, CardSection, Inline } from "@kiwicom/orbit-components";
-import { GenderWoman } from "@kiwicom/orbit-components/icons";
+import { Card, CardSection } from "@kiwicom/orbit-components";
 
 export default {
   Example: () => (
     <Card title="Passenger info" description="All passengers in this itinerary">
-      <CardSection>
-        <Inline>
-          <GenderWoman ariaLabel="female" />
-          Yasmin Karenth
-        </Inline>
-      </CardSection>
+      <CardSection title="Yasmin Karenth" description="1st class" />
     </Card>
   ),
   exampleKnobs: [

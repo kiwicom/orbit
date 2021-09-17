56ab33feff84a7ccfb73f68ff8950268a36a3d67
mainframev <vgenaev@gmail.com>
2021-09-17 19:43:43 +0200

    docs(CarrierLogo): remove redundant examples

--- docs/src/__examples__/CarrierLogo/DEFAULT.tsx
+++ docs/src/__examples__/CarrierLogo/DEFAULT.tsx
@@ -13,9 +13,4 @@ export default {
       ]}
     />
   ),
-  info: {
-    title: "Default carrier logo",
-    description:
-      "By default, carrier logos present a large version of a single logo. When used alone, be sure to pass the name of the carrier. It's best to include the type of carrier to create the right placeholder for missing logos.",
-  },
 };

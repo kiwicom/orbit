d40e6fffccd7d94f6d150eaeb526958ddd70cf14
mainframev <vgenaev@gmail.com>
2021-09-20 13:39:54 +0200

    docs(ServiceLogo): add knobs

--- docs/src/__examples__/ServiceLogo/DEFAULT.tsx
+++ docs/src/__examples__/ServiceLogo/DEFAULT.tsx
@@ -1,11 +1,65 @@
 import React from "react";
 import { ServiceLogo } from "@kiwicom/orbit-components";
 
+export const NAME_OPTIONS = {
+  AIRHELP: "AirHelp",
+  ALIPAY: "Alipay",
+  AMEX: "Amex",
+  AXA: "Axa",
+  AXAASSISTANCE: "AxaAssistance",
+  BOOKING: "Booking",
+  BUSINESSINSIDER: "BusinessInsider",
+  CHINAUNIONPAY: "ChinaUnionPay",
+  DAILYEXPRESS: "DailyExpress",
+  DINERSCLUB: "DinersClub",
+  DISCOVER: "Discover",
+  IATA: "IATA",
+  JCB: "JCB",
+  MAILONLINE: "MailOnline",
+  MAESTRO: "Maestro",
+  MAESTROSHORT: "MaestroShort",
+  MASTERCARD: "MasterCard",
+  MASTERCARDSHORT: "MasterCardShort",
+  MIR: "MIR",
+  MIRROR: "Mirror",
+  NEWYORKTIMES: "NewYorkTimes",
+  NORTOSECURED: "NortonSecured",
+  PAYPAL: "PayPal",
+  RENTALCARS: "RentalCars",
+  SOFORT: "Sofort",
+  TRAVELPULSE: "TravelPulse",
+  TRUSTLY: "Trustly",
+  USATODAY: "UsaToday",
+  VISA: "Visa",
+  VISAHQ: "VisaHQ",
+  ZOOZ: "Zooz",
+  ELO: "Elo",
+};
+
 export default {
   Example: () => <ServiceLogo name="NewYorkTimes" />,
-  info: {
-    title: "Default service logo",
-    description:
-      "By default, carrier logos present a medium version of a logo. The name of the service is included as the alternative text.",
-  },
+  exampleKnobs: [
+    {
+      component: "ServiceLogo",
+      knobs: [
+        {
+          name: "name",
+          type: "select",
+          options: Object.values(NAME_OPTIONS),
+          defaultValue: "",
+        },
+        {
+          name: "size",
+          type: "select",
+          options: ["small", "medium", "large"],
+          defaultValue: "medium",
+        },
+        {
+          name: "grayScale",
+          type: "boolean",
+          defaultValue: false,
+        },
+      ],
+    },
+  ],
 };

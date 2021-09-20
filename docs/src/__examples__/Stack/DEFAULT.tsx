83ec03057f69f9d01917d055c9c668793f6565b4
mainframev <vgenaev@gmail.com>
2021-09-20 14:52:22 +0200

    docs(Stack): add knobs

--- docs/src/__examples__/Stack/DEFAULT.tsx
+++ docs/src/__examples__/Stack/DEFAULT.tsx
@@ -18,9 +18,70 @@ export default {
       </Stack>
     );
   },
-  info: {
-    title: "Default stack",
-    description:
-      "By default, stacks display columns of their children with medium spacing between them. They are also not flexbox containers without a flex property being defined.",
-  },
+  exampleKnobs: [
+    {
+      component: "Stack",
+      knobs: [
+        {
+          name: "justify",
+          type: "select",
+          defaultValue: "",
+          options: ["start", "end", "center", "between", "around"],
+        },
+        {
+          name: "align",
+          type: "select",
+          defaultValue: "",
+          options: ["start", "end", "center", "stretch"],
+        },
+        {
+          name: "inline",
+          type: "boolean",
+          defaultValue: false,
+        },
+        {
+          name: "shrink",
+          type: "boolean",
+          defaultValue: true,
+        },
+        {
+          name: "wrap",
+          type: "boolean",
+          defaultValue: false,
+        },
+        {
+          name: "grow",
+          type: "boolean",
+          defaultValue: true,
+        },
+        {
+          name: "basis",
+          type: "text",
+          defaultValue: "",
+        },
+        {
+          name: "direction",
+          type: "select",
+          defaultValue: "row",
+          options: ["row", "row-reverse", "column", "column-reverse"],
+        },
+        {
+          name: "spacing",
+          type: "select",
+          defaultValue: "medium",
+          options: [
+            "none",
+            "XXXSmall",
+            "XXSmall",
+            "XSmall",
+            "small",
+            "medium",
+            "large",
+            "XLarge",
+            "XXLarge",
+          ],
+        },
+      ],
+    },
+  ],
 };

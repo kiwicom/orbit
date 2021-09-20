58a6f31aff337054fe551c398f7f924ab29cab91
mainframev <vgenaev@gmail.com>
2021-09-20 13:44:59 +0200

    docs(Heading): add knobs

--- docs/src/__examples__/Heading/DEFAULT.tsx
+++ docs/src/__examples__/Heading/DEFAULT.tsx
@@ -3,8 +3,28 @@ import { Heading } from "@kiwicom/orbit-components";
 
 export default {
   Example: () => <Heading>Orbit components</Heading>,
-  info: {
-    title: "Default heading",
-    description: "By default, headings are rendered as divs in the title1 format.",
-  },
+  exampleKnobs: [
+    {
+      component: "Heading",
+      knobs: [
+        {
+          name: "as",
+          type: "select",
+          options: ["h1", "h2", "h3", "h4", "h5", "h6", "div"],
+          defaultValue: "div",
+        },
+        {
+          name: "type",
+          type: "select",
+          options: ["display", "displaySubtitle", "title1", "title2", "title3", "title4", "title5"],
+          defaultValue: "div",
+        },
+        {
+          name: "inverted",
+          type: "boolean",
+          defaultValue: false,
+        },
+      ],
+    },
+  ],
 };

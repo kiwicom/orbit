7a9fff45aa16ad0a9d8747129afa0dd9702439ce
Matija Marohnić <matija.marohnic@gmail.com>
2021-09-27 14:56:22 +0200

    refactor: rewrite viewports ruler
    There were multiple problems with the viewport ruler, the biggest one
    being that it was computing container width too soon, so the initial
    width ended up being 0 because `useMediaQuery` isn't SSR-friendly,
    making the ruler unusable. This refactor uses ResizeObserver instead of
    using the `resize` event of the entire window, and fixes some other bugs
    along the way.

--- docs/src/components/ReactExample/components/ViewportsRuler.tsx
+++ docs/src/components/ReactExample/components/ViewportsRuler.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styled, { css } from "styled-components";
-import useMediaQuery from "@kiwicom/orbit-components/lib/hooks/useMediaQuery";
+import { useMediaQuery, Text } from "@kiwicom/orbit-components";
+import useResizeObserver from "use-resize-observer";
 
 import { QUERIES, CELL_HEIGHT } from "../consts";
 
 interface Props {
-  onChangeSize: (width: number) => void;
+  onChangeSize: (width: number | string) => void;
 }
 
 const StyledViewports = styled.div`
@@ -21,28 +22,26 @@ const StyledViewports = styled.div`
   `}
 `;
 
-const StyledCell = styled(({ className, children, onClick, onMouseEnter }) => (
-  <button type="button" className={className} onClick={onClick} onMouseEnter={onMouseEnter}>
-    {children}
-  </button>
-))`
-  ${({ theme, active, width, index }) => css`
+const StyledCell = styled.button.attrs({ type: "button" })<{
+  $active: boolean;
+  $width: number | string;
+}>`
+  ${({ theme, $active, $width }) => css`
     position: absolute;
     margin-left: auto;
     margin-right: auto;
     left: 0;
     right: 0;
     text-align: center;
-    background: ${active && theme.orbit.paletteCloudLight};
+    background: ${$active && theme.orbit.paletteCloudLight};
     color: ${theme.orbit.paletteInkNormal};
     cursor: pointer;
-    z-index: ${index};
-    width: ${width}px;
+    width: ${typeof $width === "number" ? `${$width}px` : $width};
     height: ${CELL_HEIGHT - 1}px;
     border-right: 1px solid ${theme.orbit.paletteCloudDark};
     border-left: 1px solid ${theme.orbit.paletteCloudDark};
     span {
-      visibility: ${active ? "visible" : "hidden"};
+      visibility: ${$active ? "visible" : "hidden"};
     }
     &:focus {
       outline: none;
@@ -51,62 +50,72 @@ const StyledCell = styled(({ className, children, onClick, onMouseEnter }) => (
   `}
 `;
 
+const StyledLabel = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: grid;
+  place-content: center;
+  pointer-events: none;
+`;
+
 const ViewportsRuler = ({ onChangeSize }: Props) => {
-  const [windowWidth, setWindowWidth] = React.useState(QUERIES.smallMobile);
-  const [activeIdx, setActiveIdx] = React.useState<number | null>(null);
-  const [viewports, setViewports] = React.useState<[string, number][]>([]);
-  const [visibleValue, setVisibleValue] = React.useState("smallMobile (320px)");
-  const ref = React.useRef<HTMLDivElement | null>(null);
+  const [activeViewport, setActiveViewport] = React.useState<string>("smallMobile");
+  const [targetViewport, setTargetViewport] = React.useState<string | null>(null);
+  const [viewports, setViewports] = React.useState<string[]>(Object.keys(QUERIES));
+  const { ref: containerRef, width: containerWidth } = useResizeObserver<HTMLDivElement>();
   const { isLargeMobile } = useMediaQuery();
 
-  const setRuler = React.useCallback(() => {
-    const currentViews = Object.entries(QUERIES)
-      .filter(([, value]) => value <= windowWidth)
-      .concat([["fullWidth", windowWidth]])
-      .reverse();
+  React.useEffect(() => {
+    const newViewports = Object.keys(QUERIES).filter(
+      viewport =>
+        typeof containerWidth !== "number" ||
+        typeof QUERIES[viewport] === "string" ||
+        QUERIES[viewport] < containerWidth,
+    );
 
-    setActiveIdx(currentViews.length - 1);
-
-    setViewports(currentViews);
-  }, [windowWidth, setViewports]);
-
-  const handleResize = React.useCallback(() => {
-    onChangeSize(QUERIES.smallMobile);
-    setRuler();
-    setVisibleValue("smallMobile (320px)");
-    if (ref.current) setWindowWidth(ref.current?.clientWidth);
-  }, [onChangeSize, setRuler]);
+    setViewports(prevViewports =>
+      prevViewports.length === newViewports.length ? prevViewports : newViewports,
+    );
+    setActiveViewport(prevActive =>
+      newViewports.includes(prevActive) ? prevActive : newViewports[newViewports.length - 1],
+    );
+  }, [containerWidth]);
 
   React.useEffect(() => {
-    handleResize();
+    onChangeSize(QUERIES[activeViewport]);
+  }, [activeViewport, onChangeSize]);
 
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [handleResize, windowWidth]);
+  const visibleViewport = targetViewport || activeViewport;
+  const visibleSize = QUERIES[visibleViewport];
 
-  const handleClick = (size: number, idx: number) => {
-    onChangeSize(size);
-    setActiveIdx(idx);
-  };
-
-  return isLargeMobile ? (
-    <StyledViewports ref={ref}>
-      {viewports.map(([name, value], i) => (
-        <StyledCell
-          active={activeIdx === i}
-          key={name}
-          width={value}
-          index={i}
-          onMouseEnter={() => setVisibleValue(`${name} (${value}px)`)}
-          onClick={() => handleClick(value, i)}
-        >
-          <span>{visibleValue}</span>
-        </StyledCell>
-      ))}
-    </StyledViewports>
-  ) : null;
+  return (
+    <div ref={containerRef}>
+      {isLargeMobile && (
+        <StyledViewports>
+          {[...viewports].reverse().map(viewport => (
+            <StyledCell
+              key={viewport}
+              aria-label={`${viewport} (${QUERIES[viewport]})`}
+              $active={targetViewport ? viewport === targetViewport : viewport === activeViewport}
+              $width={QUERIES[viewport]}
+              onMouseEnter={() => setTargetViewport(viewport)}
+              onMouseLeave={() => setTargetViewport(null)}
+              onClick={() => setActiveViewport(viewport)}
+            />
+          ))}
+          <StyledLabel>
+            <Text as="div">
+              {visibleViewport} (
+              {typeof visibleSize === "number" ? `${visibleSize}px` : visibleSize})
+            </Text>
+          </StyledLabel>
+        </StyledViewports>
+      )}
+    </div>
+  );
 };
 
 export default ViewportsRuler;

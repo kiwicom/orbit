c4640fd5459d49bd7c066b9b044892cda70a5685
mainframev <vgenaev@gmail.com>
2021-09-29 14:55:44 +0200

    docs(sandbox): add variants for TextLink

--- docs/src/__examples__/TextLink/TYPES.tsx
+++ docs/src/__examples__/TextLink/TYPES.tsx
@@ -1,23 +1,29 @@
 import React from "react";
-import { Heading, Stack, TextLink } from "@kiwicom/orbit-components";
+import { TextLink } from "@kiwicom/orbit-components";
 
 export default {
-  Example: () => (
-    <Stack flex>
-      <Stack shrink direction="column" spacing="XSmall">
-        <Heading type="title3" as="h3">
-          Primary
-        </Heading>
-        <TextLink href="https://orbit.kiwi">Orbit design system</TextLink>
-      </Stack>
-      <Stack shrink direction="column" spacing="XSmall">
-        <Heading type="title3" as="h3">
-          Secondary
-        </Heading>
-        <TextLink type="secondary" href="https://orbit.kiwi">
-          Orbit design system
-        </TextLink>
-      </Stack>
-    </Stack>
-  ),
+  Example: () => <TextLink href="https://orbit.kiwi">Primary</TextLink>,
+  exampleVariants: [
+    { name: "Primary", code: `() => <TextLink href="https://orbit.kiwi">Primary</TextLink>` },
+    {
+      name: "Secondary",
+      code: `() => <TextLink type="secondary" href="https://orbit.kiwi">Secondary</TextLink>`,
+    },
+    {
+      name: "Info",
+      code: `() => <TextLink type="info" href="https://orbit.kiwi">Info</TextLink>`,
+    },
+    {
+      name: "Success",
+      code: `() => <TextLink type="success" href="https://orbit.kiwi">Success</TextLink>`,
+    },
+    {
+      name: "Warning",
+      code: `() => <TextLink type="warning" href="https://orbit.kiwi">Warning</TextLink>`,
+    },
+    {
+      name: "Critical",
+      code: `() => <TextLink type="critical" href="https://orbit.kiwi">Critical</TextLink>`,
+    },
+  ],
 };

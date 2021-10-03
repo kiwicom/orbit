00b6efd0b01a32155ccfee761c38c6e3f67ee437
mainframev <vgenaev@gmail.com>
2021-10-04 00:32:27 +0200

    test(ReactExample): add tests

--- docs/plugins/sandbox/gatsby/resolvers.js
+++ docs/plugins/sandbox/gatsby/resolvers.js
@@ -2,22 +2,31 @@ const { graphql } = require("gatsby");
 
 const DefaultValue = {
   name: "DefaultValue",
-  description: "DefaultValue",
+  description: "DefaultValue primitives",
   serialize(value) {
+    if (typeof value !== "string" && typeof value !== "boolean" && typeof value !== "number") {
+      throw new Error("Value must be either a String, Boolean or Number");
+    }
+
     return value;
   },
   parseValue(value) {
+    if (typeof value !== "string" && typeof value !== "number" && typeof value !== "boolean") {
+      throw new Error("Value must be either a String, Boolean or Number");
+    }
+
     return value;
   },
   parseLiteral(ast) {
     switch (ast.kind) {
       case graphql.Kind.INT:
         return parseInt(ast.value, 10);
-      case graphql.Kind.OBJECT:
-      case graphql.Kind.LIST:
-        return JSON.stringify(ast.value);
-      default:
+      case graphql.Kind.STRING:
         return ast.value;
+      case graphql.Kind.BOOLEAN:
+        return ast.value;
+      default:
+        throw new Error("Value must be either a String, Int or Boolean");
     }
   },
 };

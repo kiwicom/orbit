5f639f226d1fdc42eed9cf918046bc23a938d972
mainframev <vgenaev@gmail.com>
2021-10-05 19:38:27 +0200

    docs(Badge): tooltip update

--- docs/src/__examples__/Badge/icon.tsx
+++ docs/src/__examples__/Badge/icon.tsx
@@ -4,11 +4,7 @@ import { InformationCircle } from "@kiwicom/orbit-components/icons";
 
 export default {
   Example: () => (
-    <Tooltip
-      renderInPortal={false}
-      content="Get more points by traveling more"
-      placement="top-right"
-    >
+    <Tooltip renderInPortal={false} content="Get more points by traveling more">
       <Badge type="neutral">
         2,435 pts <InformationCircle size="small" ariaLabel="More information" />
       </Badge>

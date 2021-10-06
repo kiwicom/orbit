a71c8426326665b7285d62206306e9648bf09f50
mainframev <vgenaev@gmail.com>
2021-10-06 14:03:35 +0200

    docs(Skeleton): update Skeleton documentation

--- docs/src/__examples__/Skeleton/ROWS.tsx
+++ docs/src/__examples__/Skeleton/ROWS.tsx
@@ -2,20 +2,25 @@ import React from "react";
 import { Skeleton } from "@kiwicom/orbit-components";
 
 export default {
-  Example: () => <Skeleton width={300} height={100} />,
+  Example: () => <Skeleton rows={6} rowOffset={30} rowHeight={21} />,
   exampleKnobs: [
     {
       component: "Skeleton",
       knobs: [
         {
-          name: "width",
+          name: "rows",
           type: "number",
-          defaultValue: 300,
+          defaultValue: 6,
         },
         {
-          name: "height",
+          name: "rowOffset",
           type: "number",
-          defaultValue: 100,
+          defaultValue: 30,
+        },
+        {
+          name: "rowHeight",
+          type: "number",
+          defaultValue: 21,
         },
       ],
     },

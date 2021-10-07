1d817635acdc328bfb88ab37be2d37fe66b622ce
mainframev <vgenaev@gmail.com>
2021-10-07 14:48:52 +0200

    fix(useRandomId): fix missing UIDReset

--- packages/orbit-components/src/ThemeProvider/index.js
+++ packages/orbit-components/src/ThemeProvider/index.js
@@ -1,6 +1,7 @@
 // @flow
 import * as React from "react";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import { UIDReset, UIDFork } from "react-uid";
 
 import Dictionary from "../Dictionary";
 import QueryContext from "./QueryContext";
@@ -13,13 +14,17 @@ const ThemeProvider = ({ theme, dictionary, children }: Props): React.Node => {
 
   return (
     <QueryContext.Provider value={media}>
-      <StyledThemeProvider theme={theme}>
-        {dictionary ? (
-          <Dictionary values={dictionary}>{React.Children.only(children)}</Dictionary>
-        ) : (
-          React.Children.only(children)
-        )}
-      </StyledThemeProvider>
+      <UIDReset>
+        <UIDFork>
+          <StyledThemeProvider theme={theme}>
+            {dictionary ? (
+              <Dictionary values={dictionary}>{React.Children.only(children)}</Dictionary>
+            ) : (
+              React.Children.only(children)
+            )}
+          </StyledThemeProvider>
+        </UIDFork>
+      </UIDReset>
     </QueryContext.Provider>
   );
 };

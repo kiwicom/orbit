df9d5df437ba88668141aa02d743a37f04c70083
Victor Genaev <vgenaev@gmail.com>
2021-10-12 17:43:11 +0200

    feat(Heading): add mediaQuery properties (#3135)
    * feat(Heading): add mediaQuery properties
    
    * fix: fixes after review

--- packages/orbit-components/src/Heading/index.d.ts
+++ packages/orbit-components/src/Heading/index.d.ts
@@ -15,6 +15,10 @@ export type Type =
   | "title5";
 export type As = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "div";
 
+interface MediaQuery extends Common.SpaceAfter {
+  readonly type?: Type;
+}
+
 export interface Props extends Common.Global, Common.SpaceAfter {
   readonly as?: As;
   readonly type?: Type;
@@ -22,6 +26,11 @@ export interface Props extends Common.Global, Common.SpaceAfter {
   readonly inverted?: boolean;
   readonly dataA11ySection?: string;
   readonly id?: string;
+  readonly mediumMobile?: MediaQuery;
+  readonly largeMobile?: MediaQuery;
+  readonly tablet?: MediaQuery;
+  readonly desktop?: MediaQuery;
+  readonly largeDesktop?: MediaQuery;
 }
 
 declare const Heading: React.FunctionComponent<Props>;

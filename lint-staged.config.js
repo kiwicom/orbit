edf8c015682dc9c128255503f136cbf6f4b0db03
Matija Marohnić <matija.marohnic@gmail.com>
2021-10-12 20:10:22 +0200

    chore: lint JSX files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,11 @@
 // @noflow
 
 module.exports = {
-  "*.{js,js.flow,ts}": "eslint --fix --report-unused-disable-directives",
+  "*.{js,js.flow,jsx,jsx.flow,ts}": "eslint --fix --report-unused-disable-directives",
   "*.{md,json,yaml,yml}": "prettier --write",
   "*.mdx": "eslint --fix --report-unused-disable-directives",
   "**/!(snippets)/*.mdx": "remark -q -u validate-links --no-config",
   "packages/orbit-components/**/*.svg": "yarn components check:icons",
   ".browserslistrc": () => ["yarn update-supported-browsers"],
-  "**/__examples__/**/*.js": () => ["yarn components build:examples"],
+  "**/__examples__/**/*.{js,jsx}": () => ["yarn components build:examples"],
 };

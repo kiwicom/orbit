a231fee6084212b4e2e2534199b1a816574162aa
mainframev <vgenaev@gmail.com>
2021-10-15 02:31:22 +0200

    chore(sandbox): use the same theme as in react tab

--- docs/src/components/ReactExample/components/Editor.tsx
+++ docs/src/components/ReactExample/components/Editor.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Editor } from "react-live";
-import dracula from "prism-react-renderer/themes/dracula";
+import theme from "prism-react-renderer/themes/oceanicNext";
 
 interface Props {
   isFullPage?: boolean;
@@ -12,7 +12,7 @@ const SandboxEditor = ({ isFullPage, onChange, code }: Props) => {
   return (
     <Editor
       style={!isFullPage ? { margin: 0, borderRadius: "0 0 12px 12px" } : undefined}
-      theme={dracula}
+      theme={theme}
       onChange={onChange}
       language="jsx"
       code={code}

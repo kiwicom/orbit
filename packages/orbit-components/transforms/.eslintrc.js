84009e81bf79089ee8b54c10f4be845522c5384e
Matija Marohnić <matija.marohnic@gmail.com>
2021-10-20 22:39:25 +0200

    improve: avoid self-referencing file protocol
    We only did this to avoid ESLint errors in codemod fixtures, but it's
    totally unnecessary because that code isn't even being run, only
    transformed, so we can just disable the
    `import/no-extraneous-dependencies` ESLint rule for that directory.

--- packages/orbit-components/transforms/.eslintrc.js
+++ packages/orbit-components/transforms/.eslintrc.js
@@ -3,4 +3,12 @@ module.exports = {
     // jscodeshift testing utility only supports .js extension
     "react/jsx-filename-extension": ["error", { extensions: [".js", ".tsx"] }],
   },
+  overrides: [
+    {
+      files: "__testfixtures__/**",
+      rules: {
+        "import/no-extraneous-dependencies": "off",
+      },
+    },
+  ],
 };

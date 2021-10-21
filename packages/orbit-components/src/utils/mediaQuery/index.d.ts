c0754d985f8a38e7fb434e884b9ebe987a689374
mainframev <vgenaev@gmail.com>
2021-10-21 12:51:49 +0200

    Revert "feat: export Theme type from root (#3125)"
    This reverts commit 0531faa9a34c628eefb3cb9619770b0d43380bd9.

--- packages/orbit-components/src/utils/mediaQuery/index.d.ts
+++ packages/orbit-components/src/utils/mediaQuery/index.d.ts
@@ -2,12 +2,12 @@
 // Project: http://github.com/kiwicom/orbit
 import type { Interpolation } from "styled-components";
 
-import { Theme } from "../../defaultTheme";
+import { ThemeShape } from "../../defaultTheme";
 import { Devices } from "./consts";
 
 type QueryFunction = (style: Interpolation<any>) => Interpolation<any>;
 
 declare const MediaQuery: { [key in Devices]: QueryFunction };
-declare const getBreakpointWidth: (name: string, theme: Theme, pure?: boolean) => string;
+declare const getBreakpointWidth: (name: string, theme: ThemeShape, pure?: boolean) => string;
 
 export { MediaQuery, MediaQuery as default, getBreakpointWidth };

c7af378a24a764c3c6ec5b0c50d853beb7077322
Matija Marohnić <matija.marohnic@gmail.com>
2021-10-26 21:33:11 +0200

    improve: fine-tune size-limit configuration

--- packages/orbit-components/.size-limit.js
+++ packages/orbit-components/.size-limit.js
@@ -1,17 +1,42 @@
+const globby = require("globby");
+const path = require("path");
+
 module.exports = [
   {
-    path: `${__dirname}/lib/**/*.js`,
-    limit: "218 kB",
-    ignore: ["react", "react-dom", "styled-components"],
-    modifyWebpackConfig(config) {
-      // eslint-disable-next-line no-param-reassign
-      config.resolve = {
-        alias: {
-          // resolve to .js rather than .mjs to avoid webpack failing to resolve modules
-          "@adeira/js": require.resolve("@adeira/js/src/index.js"),
-        },
-      };
-      return config;
-    },
+    name: "Orbit",
+    path: `${__dirname}/lib/index.js`,
+    limit: "210 kB",
   },
-];
+  ...globby
+    .sync([`${__dirname}/lib/*/index.js`, `${__dirname}/lib/primitives/*/index.js`])
+    .map(file => ({
+      name: path.basename(path.dirname(file)),
+      path: file,
+    })),
+  {
+    name: "icons",
+    path: `${__dirname}/lib/icons/index.js`,
+  },
+  // hooks we have documented + hooks that should probably be exposed as well
+  ...["useFocusTrap", "useLockScrolling", "useMediaQuery", "useRandomId", "useTheme"].map(hook => ({
+    name: hook,
+    path: `${__dirname}/lib/hooks/${hook}/index.js`,
+  })),
+  ...["mediaQuery", "rtl"].map(utility => ({
+    name: utility,
+    path: `${__dirname}/lib/utils/${utility}/index.js`,
+  })),
+].map(part => ({
+  ...part,
+  ignore: ["react", "react-dom", "styled-components"],
+  modifyWebpackConfig(config) {
+    // eslint-disable-next-line no-param-reassign
+    config.resolve = {
+      alias: {
+        // resolve to .js rather than .mjs to avoid webpack failing to resolve modules
+        "@adeira/js": require.resolve("@adeira/js/src/index.js"),
+      },
+    };
+    return config;
+  },
+}));

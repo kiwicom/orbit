52c2418c06fbd4f8ea4aae6765e29ff48c1b2963
mainframev <vgenaev@gmail.com>
2021-10-27 17:23:12 +0200

    fix(cloneWithTooltip): wrong type declaration

--- packages/orbit-components/src/utils/cloneWithTooltip/index.d.ts
+++ packages/orbit-components/src/utils/cloneWithTooltip/index.d.ts
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 declare const CloneWithTooltip: (
-  tooltip: Record<string, string>,
+  tooltip: React.ReactElement<any>,
   children: React.ReactElement<any>,
 ) => React.ReactElement<any>;
 

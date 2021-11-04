4a15dcc40db4303e7ce2d0bf301bd7231f1a6658
Matija Marohnić <matija.marohnic@gmail.com>
2021-11-04 14:10:35 +0100

    fix(useMediaQuery): optimize, support custom breakpoints, and make it work without context
    In #3138 we moved the entire matchMedia logic to QueryContext, but this
    caused useMediaQuery to depend on the context, where before it was
    possible to use it independently. That breaking change was unnecessary,
    so this change restores this feature.
    
    QueryContext was introduced in #1659 apparently to avoid FOUC, but at
    that time queries were being set after an unnecessary delay which was
    removed in #1982 (which also deletes query object initialization,
    perhaps accidentally), so the initial reaction to that was to remove
    QueryContext altogether, but there is actually still room for it as a
    way to optimize useMediaqQuery by reducing the number of renders because
    newly mounted components can use the context value instead of having to
    initialize the values again. Additional optimization was to keep
    referential equality in case breakpoints haven't changed instead of
    always creating a new object.
    
    Lastly, now that QueryContext has been moved inside styled-component's
    ThemeProvider, useMediaQuery will actually respect the given theme. It
    used to always use the default theme because the value was being set
    outside StyledThemeProvider (this was a problem before #3138), so if
    people using Orbit changed the default breakpoints in ThemeProvider,
    they might run into very unexpected and hard to debug behavior while
    using useMediaQuery.

--- packages/orbit-components/src/ThemeProvider/QueryContext/index.jsx
+++ packages/orbit-components/src/ThemeProvider/QueryContext/index.jsx
@@ -1,13 +1,9 @@
 // @flow
 import * as React from "react";
 
-import useTheme from "../../hooks/useTheme";
-import { getBreakpointWidth } from "../../utils/mediaQuery";
+import typeof ContextType from ".";
 
-import typeof ContextType, { QueryContextProvider as ProviderType } from ".";
-import type { QueryMap, Context } from ".";
-
-const initialValue = {
+export const initialValue = {
   isLargeDesktop: null,
   isDesktop: null,
   isLargeMobile: null,
@@ -18,57 +14,5 @@ const initialValue = {
 
 const QueryContext: ContextType = React.createContext(initialValue);
 QueryContext.displayName = "QueryOrbitContext";
-
-type Query = $Keys<Context>;
-
-const QUERIES: Query[] = Object.keys(initialValue);
-
-export const QueryContextProvider: ProviderType = ({ children }) => {
-  const theme = useTheme();
-
-  const [value, setValue] = React.useState(initialValue);
-
-  React.useEffect(() => {
-    let mqListMap: QueryMap<MediaQueryList>;
-
-    const listener = () => {
-      setValue(prev => {
-        const result = { ...prev };
-        QUERIES.forEach(query => {
-          result[query] = mqListMap[query].matches;
-        });
-        return result;
-      });
-    };
-
-    if (window !== "undefined") {
-      const createMqList = (mediaQuery: string): MediaQueryList => {
-        const mqList = window.matchMedia(mediaQuery);
-        mqList.addEventListener("change", listener);
-        return mqList;
-      };
-
-      mqListMap = {
-        isMediumMobile: createMqList(getBreakpointWidth("mediumMobile", theme)),
-        isLargeMobile: createMqList(getBreakpointWidth("largeMobile", theme)),
-        isTablet: createMqList(getBreakpointWidth("tablet", theme)),
-        isDesktop: createMqList(getBreakpointWidth("desktop", theme)),
-        isLargeDesktop: createMqList(getBreakpointWidth("largeDesktop", theme)),
-        prefersReducedMotion: createMqList("(prefers-reduced-motion: reduce)"),
-      };
-
-      // initialize
-      listener();
-    }
-
-    return () => {
-      QUERIES.forEach(query => {
-        mqListMap[query].removeListener(listener);
-      });
-    };
-  }, [theme]);
-
-  return <QueryContext.Provider value={value}>{children}</QueryContext.Provider>;
-};
 
 export default QueryContext;
--- packages/orbit-components/src/ThemeProvider/index.jsx
+++ packages/orbit-components/src/ThemeProvider/index.jsx
@@ -4,25 +4,25 @@ import { ThemeProvider as StyledThemeProvider } from "styled-components";
 import { UIDReset, UIDFork } from "react-uid";
 
 import Dictionary from "../Dictionary";
-import { QueryContextProvider } from "./QueryContext";
+import QueryContextProvider from "./QueryContext/Provider";
 
 import type { Props } from ".";
 
 const ThemeProvider = ({ theme, dictionary, children }: Props): React.Node => {
   return (
-    <QueryContextProvider>
-      <UIDReset>
-        <UIDFork>
-          <StyledThemeProvider theme={theme}>
+    <UIDReset>
+      <UIDFork>
+        <StyledThemeProvider theme={theme}>
+          <QueryContextProvider>
             {dictionary ? (
               <Dictionary values={dictionary}>{React.Children.only(children)}</Dictionary>
             ) : (
               React.Children.only(children)
             )}
-          </StyledThemeProvider>
-        </UIDFork>
-      </UIDReset>
-    </QueryContextProvider>
+          </QueryContextProvider>
+        </StyledThemeProvider>
+      </UIDFork>
+    </UIDReset>
   );
 };
 

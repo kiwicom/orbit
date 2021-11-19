b5282c5937b12a7788efb8d8adefaf7c96da99b8
Matija Marohnić <matija.marohnic@gmail.com>
2021-11-19 15:18:58 +0100

    chore: stop running unnecessary script

--- scripts/ci-streamline-env.js
+++ scripts/ci-streamline-env.js
@@ -1,6 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 
+/**
+ * necessary for using Streamline icons via the @streamlinehq/* packages
+ *
+ * currently not in use because Streamline's script that was fetching the icons
+ * was sometimes failing, causing the CI to fail, so we temporarily downloaded their icons
+ */
+
 if (process.env.CI) {
   fs.writeFileSync(
     path.join(process.cwd(), "streamlinehq.json"),

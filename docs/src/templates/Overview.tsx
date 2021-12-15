e106021d347153e2be0f9279aa44011b5983bd5c
mainframev <vgenaev@gmail.com>
2021-12-15 18:55:04 +0100

    fix(tracking): protected routes

--- docs/src/templates/Overview.tsx
+++ docs/src/templates/Overview.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { PageRendererProps } from "gatsby";
+import { PageRendererProps, navigate } from "gatsby";
 import { Grid } from "@kiwicom/orbit-components";
 import { sortBy } from "lodash";
 
+import { isLoggedIn } from "../services/auth";
 import DocLayout from "../components/DocLayout";
 import Tile from "../components/Tile";
 import useDevMode from "../hooks/useDevMode";
@@ -27,31 +28,42 @@ interface Props extends PageRendererProps {
 const Overview = ({ location, pageContext }: Props) => {
   const { slug, title, trail, pages } = pageContext;
   const [devMode] = useDevMode();
+  const [render, setRender] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!isLoggedIn() && location.pathname === "/dashboard/tracking/") {
+      navigate(`/dashboard/login/`);
+    } else {
+      setRender(true);
+    }
+  }, [setRender]);
 
   return (
-    <DocLayout location={location} path={slug} title={title} trail={trail} noElevation>
-      <Grid
-        columns="1fr"
-        gap="2rem"
-        largeMobile={{ columns: "repeat(2, 1fr)" }}
-        desktop={{ columns: "repeat(3, 1fr)" }}
-        largeDesktop={{ columns: "repeat(2, 1fr)" }}
-      >
-        {sortBy(pages, ["idx"]).map(
-          ({ title: pageTitle, slug: pageSlug, description, hasReactTab }) => {
-            return (
-              <Tile
-                key={pageSlug}
-                title={pageTitle}
-                href={hasReactTab && devMode ? `${pageSlug}react/` : pageSlug}
-              >
-                {description}
-              </Tile>
-            );
-          },
-        )}
-      </Grid>
-    </DocLayout>
+    render && (
+      <DocLayout location={location} path={slug} title={title} trail={trail} noElevation>
+        <Grid
+          columns="1fr"
+          gap="2rem"
+          largeMobile={{ columns: "repeat(2, 1fr)" }}
+          desktop={{ columns: "repeat(3, 1fr)" }}
+          largeDesktop={{ columns: "repeat(2, 1fr)" }}
+        >
+          {sortBy(pages, ["idx"]).map(
+            ({ title: pageTitle, slug: pageSlug, description, hasReactTab }) => {
+              return (
+                <Tile
+                  key={pageSlug}
+                  title={pageTitle}
+                  href={hasReactTab && devMode ? `${pageSlug}react/` : pageSlug}
+                >
+                  {description}
+                </Tile>
+              );
+            },
+          )}
+        </Grid>
+      </DocLayout>
+    )
   );
 };
 

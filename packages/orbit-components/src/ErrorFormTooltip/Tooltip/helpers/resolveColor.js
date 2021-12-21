8ce16bdc9a00157a6c79a0e8eb4de4119ae2f3ba
Matija Marohnić <matija.marohnic@gmail.com>
2021-12-21 13:33:46 +0100

    fix: fix background color for help tooltip in form components

--- packages/orbit-components/src/ErrorFormTooltip/Tooltip/helpers/resolveColor.js
+++ packages/orbit-components/src/ErrorFormTooltip/Tooltip/helpers/resolveColor.js
@@ -4,7 +4,7 @@ import type { Theme } from "../../../defaultTheme";
 type ResolveColor = ({| isHelp: boolean, theme: Theme |}) => string;
 
 const resolveColor: ResolveColor = ({ isHelp, theme }) => {
-  return isHelp ? theme.orbit.paletteBlueDark : theme.orbit.paletteRedNormal;
+  return isHelp ? theme.orbit.paletteBlueNormal : theme.orbit.paletteRedNormal;
 };
 
 export default resolveColor;

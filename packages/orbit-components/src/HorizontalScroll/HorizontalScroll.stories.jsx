ba09e298f1c13feaf7a8b023a2ca757ecb1a9a43
mainframev <vgenaev@gmail.com>
2022-02-08 14:13:04 +0100

    feat(HorizontalScroll): apply styles only if content is wider than parent width

--- packages/orbit-components/src/HorizontalScroll/HorizontalScroll.stories.jsx
+++ packages/orbit-components/src/HorizontalScroll/HorizontalScroll.stories.jsx
@@ -2,8 +2,10 @@
 import * as React from "react";
 import { select, number, boolean, text } from "@storybook/addon-knobs";
 
+import { SPACINGS } from "../utils/layout/consts";
 import Box from "../Box";
 import Text from "../Text";
+import Stack from "../Stack";
 
 import HorizontalScroll from ".";
 
@@ -12,8 +14,10 @@ export default {
 };
 
 export const Default = (): React.Node => {
+  const spacing = select("Spacing", [null, ...Object.values(SPACINGS)], SPACINGS.XXXSMALL);
+
   return (
-    <HorizontalScroll>
+    <HorizontalScroll spacing={spacing}>
       {Array(...Array(10)).map((_, key) => (
         <Box
           // eslint-disable-next-line react/no-array-index-key
@@ -38,6 +42,43 @@ export const Default = (): React.Node => {
         </Box>
       ))}
     </HorizontalScroll>
+  );
+};
+
+export const NoScroll = (): React.Node => {
+  const spacing = select("Spacing", [null, ...Object.values(SPACINGS)], SPACINGS.XXXSMALL);
+
+  return (
+    <Stack>
+      <Text>
+        Horizontal scroll turns on only if elements inside are wider than parent container
+      </Text>
+      <HorizontalScroll spacing={spacing}>
+        {Array(...Array(5)).map((_, key) => (
+          <Box
+            // eslint-disable-next-line react/no-array-index-key
+            key={key}
+            display="flex"
+            justify="center"
+            minWidth="150px"
+            background="cloudLight"
+            height="full"
+          >
+            <Text size="large" weight="bold" as="div">
+              <div
+                style={{
+                  height: "50px",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                {key}
+              </div>
+            </Text>
+          </Box>
+        ))}
+      </HorizontalScroll>
+    </Stack>
   );
 };
 

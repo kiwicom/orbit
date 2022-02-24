aad7bfdc31b47301809813f591e73f463d49d22e
mainframev <vgenaev@gmail.com>
2022-02-24 16:35:53 +0100

    fix(ChoiceGroup): add forwardRef

--- packages/orbit-components/src/ChoiceGroup/index.jsx
+++ packages/orbit-components/src/ChoiceGroup/index.jsx
@@ -38,80 +38,85 @@ StyledChoiceGroup.defaultProps = {
   theme: defaultTheme,
 };
 
-const ChoiceGroup = ({
-  dataTest,
-  label,
-  labelSize = LABEL_SIZES.NORMAL,
-  labelAs = LABEL_ELEMENTS.H4,
-  error,
-  children,
-  filter,
-  onOnlySelection,
-  onlySelectionText,
-  onChange,
-}: Props): React.Node => {
-  const groupID = useRandomId();
-  const theme = useTheme();
+const ChoiceGroup: React.AbstractComponent<Props, HTMLDivElement> = React.forwardRef(
+  (
+    {
+      dataTest,
+      label,
+      labelSize = LABEL_SIZES.NORMAL,
+      labelAs = LABEL_ELEMENTS.H4,
+      error,
+      children,
+      filter,
+      onOnlySelection,
+      onlySelectionText,
+      onChange,
+    },
+    ref,
+  ): React.Node => {
+    const groupID = useRandomId();
+    const theme = useTheme();
 
-  const handleChange = (ev: SyntheticInputEvent<HTMLInputElement>) => {
-    ev.persist();
-    if (onChange) {
-      onChange(ev);
-    }
-  };
+    const handleChange = (ev: SyntheticInputEvent<HTMLInputElement>) => {
+      ev.persist();
+      if (onChange) {
+        onChange(ev);
+      }
+    };
 
-  const itemProps = {
-    onChange: handleChange,
-    hasError: Boolean(error),
-  };
+    const itemProps = {
+      onChange: handleChange,
+      hasError: Boolean(error),
+    };
 
-  return (
-    <StyledChoiceGroup data-test={dataTest} role="group" aria-labelledby={groupID}>
-      {label && (
-        <Heading id={groupID} type={getHeadingSize(labelSize)} as={labelAs} spaceAfter="medium">
-          {label}
-        </Heading>
-      )}
-      {typeof children === "function" ? (
-        children({
-          // for now a plain <div> is all we need, but we're reserving this space in the API
-          // in case we'll need something more in the future
-          Container: "div",
-          Item: ({ children: itemChildren }) => {
-            return !filter ? (
-              React.cloneElement(React.Children.only(itemChildren), itemProps)
-            ) : (
-              <FilterWrapper
-                child={React.Children.only(itemChildren)}
-                onOnlySelection={onOnlySelection}
-                onlySelectionText={onlySelectionText}
-              >
-                {React.cloneElement(React.Children.only(itemChildren), itemProps)}
-              </FilterWrapper>
-            );
-          },
-          spacing: filter ? "0px" : theme.orbit.spaceXSmall,
-        })
-      ) : (
-        <Stack direction="column" spacing={filter ? "none" : "XSmall"}>
-          {React.Children.map(children, child => {
-            return !filter ? (
-              React.cloneElement(child, itemProps)
-            ) : (
-              <FilterWrapper
-                child={child}
-                onOnlySelection={onOnlySelection}
-                onlySelectionText={onlySelectionText}
-              >
-                {React.cloneElement(child, itemProps)}
-              </FilterWrapper>
-            );
-          })}
-        </Stack>
-      )}
-      {error && <Feedback>{error}</Feedback>}
-    </StyledChoiceGroup>
-  );
-};
+    return (
+      <StyledChoiceGroup ref={ref} data-test={dataTest} role="group" aria-labelledby={groupID}>
+        {label && (
+          <Heading id={groupID} type={getHeadingSize(labelSize)} as={labelAs} spaceAfter="medium">
+            {label}
+          </Heading>
+        )}
+        {typeof children === "function" ? (
+          children({
+            // for now a plain <div> is all we need, but we're reserving this space in the API
+            // in case we'll need something more in the future
+            Container: "div",
+            Item: ({ children: itemChildren }) => {
+              return !filter ? (
+                React.cloneElement(React.Children.only(itemChildren), itemProps)
+              ) : (
+                <FilterWrapper
+                  child={React.Children.only(itemChildren)}
+                  onOnlySelection={onOnlySelection}
+                  onlySelectionText={onlySelectionText}
+                >
+                  {React.cloneElement(React.Children.only(itemChildren), itemProps)}
+                </FilterWrapper>
+              );
+            },
+            spacing: filter ? "0px" : theme.orbit.spaceXSmall,
+          })
+        ) : (
+          <Stack direction="column" spacing={filter ? "none" : "XSmall"}>
+            {React.Children.map(children, child => {
+              return !filter ? (
+                React.cloneElement(child, itemProps)
+              ) : (
+                <FilterWrapper
+                  child={child}
+                  onOnlySelection={onOnlySelection}
+                  onlySelectionText={onlySelectionText}
+                >
+                  {React.cloneElement(child, itemProps)}
+                </FilterWrapper>
+              );
+            })}
+          </Stack>
+        )}
+        {error && <Feedback>{error}</Feedback>}
+      </StyledChoiceGroup>
+    );
+  },
+);
 
 export default ChoiceGroup;

27f58eb1bf695fae17e8c966b8d4f6ea6ef0e7d9
mainframev <vgenaev@gmail.com>
2022-05-10 16:49:36 +0200

    docs(sandbox): refactor sandbox to use page route api

--- docs/plugins/sandbox/gatsby-node.ts
+++ docs/plugins/sandbox/gatsby-node.ts
@@ -1,5 +1,4 @@
-import createPages from "./gatsby/createPages";
 import sourceNodes from "./gatsby/sourceNodes";
 import createSchemaCustomization from "./gatsby/createSchemeCustomization";
 
-export { createPages, sourceNodes, createSchemaCustomization };
+export { sourceNodes, createSchemaCustomization };

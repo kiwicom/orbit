23d9c4cc373635c9b3b943abf20b785ec27552e1
mainframev <vgenaev@gmail.com>
2022-06-05 23:58:40 +0200

    chore: use updated orbit-react-scanner to esm package

--- packages/orbit-tracking/scripts/build.mjs
+++ packages/orbit-tracking/scripts/build.mjs
@@ -1,6 +1,8 @@
 import { $ } from "zx";
 
 (async () => {
+  $.verbose = false;
+
   await $`yarn clean`;
   await $`yarn tsc`;
   await $`yarn tsc-esm-fix --target='dist' --ext='.js' --dirnameVar=false --filenameVar=false`;

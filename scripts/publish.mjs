af0fa7569e45e0a16b417b0d08dd496ccd114eca
Victor Genaev <vgenaev@gmail.com>
2022-06-15 20:56:42 +0200

    chore: update publish script (#3413)
    * chore: update publish script
    
    * chore: move publish script to scripts folder

---
+++ scripts/publish.mjs
@@ -0,0 +1,127 @@
+import { $, path, fetch } from "zx";
+import dotenv from "dotenv-safe";
+import conventionalChangelog from "conventional-changelog";
+import { getPackages } from "@lerna/project";
+import { obj as transform } from "through2";
+import mergeStreams from "merge-stream";
+import markdownChalk from "markdown-chalk";
+
+const SLACK_API = `https://skypicker.slack.com/api/chat.postMessage`;
+const TITLE = `New orbit release 🚀`;
+const CHANNEL = `orbit-react`;
+const COLOR = `#00A58E`;
+
+function installDependencies() {
+  return $.spawn("yarn", ["install"], { stdio: "inherit" });
+}
+
+function publishPackages() {
+  return $.spawn(
+    "yarn",
+    ["lerna", "publish", "--no-private", "--conventional-commits", "--create-release", "github"],
+    { stdio: "inherit" },
+  );
+}
+
+function adjustChangelog(str) {
+  const output = str
+    .replace("Bug fixes", "Bug Fixes 🐛")
+    .replace("Features", "Features 🆕")
+    .replace("BREAKING CHANGES", "BREAKING CHANGES 🚨");
+  return output;
+}
+
+async function postSlackNotification(changelog) {
+  try {
+    const req = await fetch(SLACK_API, {
+      method: "POST",
+      body: JSON.stringify({
+        channel: CHANNEL,
+        attachments: [
+          {
+            title: TITLE,
+            text: adjustChangelog(changelog),
+            color: COLOR,
+          },
+        ],
+      }),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+        Accept: "application/json",
+      },
+    });
+
+    const res = req.json();
+    if (!res.ok) return res.error;
+  } catch (err) {
+    console.error(err);
+  }
+
+  return undefined;
+}
+
+async function configureGitHubToken() {
+  try {
+    dotenv.config({
+      allowEmptyValues: true,
+      example: ".env.example",
+    });
+  } catch (err) {
+    if (/GH_TOKEN|SLACK_TOKEN/g.test(err.message)) {
+      throw new Error(
+        "GitHub token or Slack token is missing in the .env file, Lerna needs it to create GitHub releases.\nLearn how to create one: https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token.",
+      );
+    }
+  }
+}
+
+async function previewChangelog() {
+  const packages = await getPackages();
+  const streams = packages
+    .filter(pkg => !/orbit.kiwi|orbit-tracking|babel-plugin-orbit-components/gm.test(pkg.name))
+    .map(pkg => {
+      return conventionalChangelog(
+        {
+          lernaPackage: pkg.name,
+          preset: "angular",
+        },
+        {
+          host: "https://github.com",
+          owner: "kiwicom",
+          repository: "orbit",
+        },
+        {
+          path: path.join("packages", pkg.name.replace("@kiwicom/", "")),
+        },
+      ).pipe(
+        transform((chunk, _, callback) => {
+          const changelog = chunk.toString();
+          if (changelog.trim().includes("\n")) {
+            // eslint-disable-next-line no-console
+            console.log(markdownChalk(changelog));
+            callback(null, markdownChalk(changelog));
+          }
+        }),
+      );
+    });
+
+  return new Promise((resolve, reject) => {
+    mergeStreams(...streams)
+      .on("data", data => {
+        resolve(data);
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+}
+
+(async () => {
+  await configureGitHubToken();
+  await installDependencies();
+  await previewChangelog()
+    .then(postSlackNotification)
+    .catch(err => console.error(err));
+  await publishPackages();
+})();

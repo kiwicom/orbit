218613f7244f9fa45f277667e6b2ab47cbe1d431
Victor Genaev <vgenaev@gmail.com>
2022-06-15 11:12:40 +0200

    Merge pull request #3288 from kiwicom/fix/buttonmobilestore
    feat(ButtonMobileStore): add lang prop

--- packages/orbit-components/src/ButtonMobileStore/consts.js
+++ packages/orbit-components/src/ButtonMobileStore/consts.js
@@ -3,8 +3,30 @@ export const TYPE_OPTIONS = {
   APPSTORE: "appStore",
   GOOGLEPLAY: "googlePlay",
 };
-export const TYPE_VARIANT = {
-  DARK: "dark",
-  LIGHT: "light",
+
+export const LANGUAGE = {
+  BG: "BG",
+  CS: "CS",
+  DA: "DA",
+  DE: "DE",
+  EN: "EN",
+  ES: "ES",
+  FI: "FI",
+  FR: "FR",
+  HU: "HU",
+  IT: "IT",
+  JA: "JA",
+  KO: "KO",
+  NL: "NL",
+  PL: "PL",
+  PT: "PT",
+  RO: "RO",
+  RU: "RU",
+  SK: "SK",
+  SR: "SR",
+  SV: "SV",
+  TR: "TR",
+  ZH: "ZH",
 };
+
 export const HEIGHT = "40px";

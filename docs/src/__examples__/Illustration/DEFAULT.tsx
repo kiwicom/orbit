6be7363c2212ae55611669b2d0b9ca3379533124
Victor Genaev <vgenaev@gmail.com>
2022-06-17 11:28:49 +0200

    feat(illustration): add AppKiwi illustration (#3416)

--- docs/src/__examples__/Illustration/DEFAULT.tsx
+++ docs/src/__examples__/Illustration/DEFAULT.tsx
@@ -11,6 +11,7 @@ export default {
           name: "name",
           type: "select",
           options: [
+            "AppKiwi",
             "Accommodation",
             "AirHelp",
             "AirportTransport",

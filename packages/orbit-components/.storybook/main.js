e1c198b382c871d880eb914c7876e999c32ac0d0
mainframev <vgenaev@gmail.com>
2022-07-25 15:43:35 +0200

    chore: update commitlint deps

--- packages/orbit-components/.storybook/main.js
+++ packages/orbit-components/.storybook/main.js
@@ -24,18 +24,6 @@ module.exports = {
     // resolve to .js rather than .mjs to avoid webpack failing because of ambiguous imports
     config.resolve.alias["@adeira/js"] = require.resolve("@adeira/js/src/index.js");
     config.resolve.extensions = config.resolve.extensions.filter(ext => ext !== ".mjs");
-
-    config.module.rules.push({
-      test: /\.jsx?$/,
-      use: [
-        {
-          options: { envName: "esm" },
-          loader: require.resolve("babel-loader"),
-        },
-      ],
-      exclude: /node_modules\/(?!(loki)\/).*/, // Loki is not transpilled, throws error in IE 11
-    });
-
     return config;
   },
 };

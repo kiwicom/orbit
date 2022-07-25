e7680e407c612d16245fce669e72a80146843800
Victor Genaev <vgenaev@gmail.com>
2022-07-25 20:37:44 +0200

    fix: storybook (#3537)
    * chore: update commitlint deps
    
    * fix(storybook): fix runtime error

--- packages/orbit-components/.storybook/main.js
+++ packages/orbit-components/.storybook/main.js
@@ -37,6 +37,29 @@ module.exports = {
       exclude: /node_modules\/(?!(loki)\/).*/, // Loki is not transpilled, throws error in IE 11
     });
 
-    return config;
+    const rules = [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                modules: "commonjs",
+              },
+            ],
+          ],
+        },
+      },
+    ];
+
+    return {
+      ...config,
+      module: {
+        rules: [...rules, ...config.module.rules],
+      },
+    };
   },
 };

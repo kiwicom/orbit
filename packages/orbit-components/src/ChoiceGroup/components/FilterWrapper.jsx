7ea4dee8918f9c596c985c44bfd198b47496c712
mainframev <vgenaev@gmail.com>
2022-07-28 11:46:07 +0200

    fix(Checkbox): fix issue with need to click twice on iOS

--- packages/orbit-components/src/ChoiceGroup/components/FilterWrapper.jsx
+++ packages/orbit-components/src/ChoiceGroup/components/FilterWrapper.jsx
@@ -18,29 +18,44 @@ const hoverAndFocus = () => css`
 `;
 
 const StyledContentWrapper = styled.div`
-  box-sizing: border-box;
-  width: 100%;
-  padding-left: 4px;
-  border-radius: 4px;
-  display: flex;
-  align-items: center;
-  height: ${({ theme }) => theme.orbit.heightButtonSmall};
-  ${StyledOnlyButton} {
-    visibility: hidden;
-    opacity: 0;
-  }
+  ${({ disabled }) => css`
+    box-sizing: border-box;
+    width: 100%;
+    padding-left: 4px;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    height: ${({ theme }) => theme.orbit.heightButtonSmall};
 
-  ${({ disabled }) =>
-    !disabled &&
+    ${StyledOnlyButton} {
+      visibility: hidden;
+      opacity: 0;
+    }
+
+    // https://skypicker.slack.com/archives/CAMS40F7B/p1658992944387749
+    ${!disabled &&
     css`
-      /* NOTE: Combined selector &:hover, &:focus-within is not usable here as it renders incorrectly in IE and EDGE */
-      &:hover {
-        ${hoverAndFocus}
+      @media (hover: none) {
+        ${StyledOnlyButton} {
+          visibility: visible;
+          opacity: 0.3;
+          &:hover {
+            opacity: 1;
+          }
+        }
       }
-      &:focus-within {
-        ${hoverAndFocus}
+
+      @media (hover) and (pointer: fine) {
+        &:hover {
+          ${hoverAndFocus};
+        }
+
+        &:focus-within {
+          ${hoverAndFocus}
+        }
       }
     `};
+  `}
 `;
 
 // $FlowFixMe: https://github.com/flow-typed/flow-typed/issues/3653#issuecomment-568539198
@@ -55,6 +70,7 @@ const FilterWrapper: FilterWrapperType = ({
   onlySelectionText,
 }) => {
   const { value, label, disabled } = child.props;
+
   return (
     <StyledContentWrapper disabled={disabled}>
       {children}

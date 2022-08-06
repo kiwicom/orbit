8a290e22d0a3104a78b904e709a813be2b8fabb6
mainframev <vgenaev@gmail.com>
2022-08-06 17:23:41 +0200

    chore: add @babel/preset-typescript to storybook main

--- packages/orbit-components/.storybook/main.js
+++ packages/orbit-components/.storybook/main.js
@@ -44,6 +44,7 @@ module.exports = {
         loader: "babel-loader",
         options: {
           presets: [
+            "@babel/preset-typescript",
             [
               "@babel/preset-env",
               {

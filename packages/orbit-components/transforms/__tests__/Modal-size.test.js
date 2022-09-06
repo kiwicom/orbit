6c8a9dbc175605b7c558a3969b84b105ca8da06c
mainframev <vgenaev@gmail.com>
2022-09-06 19:48:03 +0200

    chore: remove old transforms

--- packages/orbit-components/transforms/__tests__/Modal-size.test.js
+++
@@ -1,5 +0,0 @@
-// @flow
-const { defineTest } = require("jscodeshift/dist/testUtils");
-
-defineTest(__dirname, "Modal-size", null, null, { parser: "flow" });
-defineTest(__dirname, "Modal-size", null, null, { parser: "tsx" });

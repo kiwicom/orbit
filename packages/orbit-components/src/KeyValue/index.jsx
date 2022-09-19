aeeace9e1d423e25e374f4c0c2d5251c65c40c31
Victor Genaev <vgenaev@gmail.com>
2022-09-20 01:04:21 +0200

    feat(KeyValue): new spacing, direction properties (#3576)

--- packages/orbit-components/src/KeyValue/index.jsx
+++ packages/orbit-components/src/KeyValue/index.jsx
@@ -1,23 +1,60 @@
 // @flow
 import * as React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
+import defaultTheme from "../defaultTheme";
+import { SPACINGS } from "../utils/layout/consts";
 import Text from "../Text";
 
 import type { Props } from ".";
 
+const getSpacing = ({ theme }) => ({
+  [SPACINGS.XXXSMALL]: theme.orbit.spaceXXXSmall,
+  [SPACINGS.XXSMALL]: theme.orbit.spaceXXSmall,
+  [SPACINGS.XSMALL]: theme.orbit.spaceXSmall,
+  [SPACINGS.SMALL]: theme.orbit.spaceSmall,
+  [SPACINGS.MEDIUM]: theme.orbit.spaceMedium,
+  [SPACINGS.LARGE]: theme.orbit.spaceLarge,
+  [SPACINGS.XLARGE]: theme.orbit.spaceXLarge,
+  [SPACINGS.XXLARGE]: theme.orbit.spaceXXLarge,
+});
+
+const resolveSpacing = ({ spacing, theme }) => getSpacing({ theme })[spacing];
+
 const StyledWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  ${({ $direction, spacing, theme }) => css`
+    display: flex;
+    flex-direction: ${$direction};
+    align-items: ${$direction !== "column" ? "center" : "flex-start"};
+    gap: ${resolveSpacing({ spacing, theme })};
+  `};
 `;
 
-const KeyValue = ({ dataTest, label, value, size = "normal" }: Props): React.Node => {
+// $FlowFixMe: https://github.com/flow-typed/flow-typed/issues/3653#issuecomment-568539198
+StyledWrapper.defaultProps = {
+  theme: defaultTheme,
+};
+
+const KeyValue = ({
+  dataTest,
+  label,
+  value,
+  spacing,
+  icon,
+  size = "normal",
+  direction = "column",
+}: Props): React.Node => {
   return (
-    <StyledWrapper data-test={dataTest}>
-      <Text type="secondary" size={size === "normal" ? "small" : "normal"}>
-        {label}
+    <StyledWrapper data-test={dataTest} $direction={direction} spacing={spacing}>
+      {label && (
+        <Text type="secondary" size={size === "normal" ? "small" : "normal"}>
+          {label}
+        </Text>
+      )}
+      {icon}
+      <Text size={size} weight="bold">
+        {value}
       </Text>
-      <Text size={size}>{value}</Text>
     </StyledWrapper>
   );
 };

dc5be47340883568317ba4c5e40f26206015ac26
mainframev <vgenaev@gmail.com>
2022-10-11 10:47:05 +0200

    test(TooltipPrimitive): test bubbling on content

--- packages/orbit-components/src/primitives/TooltipPrimitive/__tests__/index.test.jsx
+++ packages/orbit-components/src/primitives/TooltipPrimitive/__tests__/index.test.jsx
@@ -1,6 +1,7 @@
 // @flow
 import * as React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Tooltip from "..";
 import Airplane from "../../../icons/Airplane";
@@ -15,5 +16,54 @@ describe("Tooltip", () => {
     );
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("should call onClick 1 time", () => {
+    const content = "Write some message to the user";
+    const onClick = jest.fn();
+
+    render(
+      <div onClick={onClick} role="button" tabIndex={0} onKeyDown={() => {}}>
+        <Tooltip
+          stopPropagation
+          content={
+            <div onClick={onClick} role="button" tabIndex={0} onKeyDown={() => {}}>
+              {content}
+            </div>
+          }
+        >
+          <div>kek</div>
+        </Tooltip>
+      </div>,
+    );
+
+    userEvent.hover(screen.getByText("kek"));
+    userEvent.click(screen.getByText(content));
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it("should call onClick 2 times", () => {
+    const content = "Write some message to the user";
+    const onClick = jest.fn();
+
+    render(
+      <div onClick={onClick} role="button" tabIndex={0} onKeyDown={() => {}}>
+        <Tooltip
+          content={
+            <div onClick={onClick} role="button" tabIndex={0} onKeyDown={() => {}}>
+              {content}
+            </div>
+          }
+        >
+          <div>kek</div>
+        </Tooltip>
+      </div>,
+    );
+
+    userEvent.hover(screen.getByText("kek"));
+    userEvent.click(screen.getByText(content));
+
+    expect(onClick).toHaveBeenCalledTimes(2);
   });
 });

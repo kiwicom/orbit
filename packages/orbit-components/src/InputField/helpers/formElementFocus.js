8e5f155775a3dc3fcd073d63bef011da33381315
Daniel Sil <daniel.sil@kiwi.com>
2022-10-21 11:22:30 +0200

    feat(InputFile): change focus to native colors

--- packages/orbit-components/src/InputField/helpers/formElementFocus.js
+++ packages/orbit-components/src/InputField/helpers/formElementFocus.js
@@ -1,19 +1,6 @@
 // @flow
-import { css } from "styled-components";
-import { convertHexToRgba } from "@kiwicom/orbit-design-tokens";
+import { defaultFocus } from "../../utils/common";
 
-const formElementFocus = (): any => css`
-  box-shadow: ${({ theme, error }) =>
-    error
-      ? // TODO: UPDATE TOKENS ==> `${theme.orbit.borderWidthInputFocus} ${theme.orbit.borderColorInputFocus}`
-        `inset 0 0 0 1px ${theme.orbit.paletteRedNormal}, 0 0 0 3px ${convertHexToRgba(
-          theme.orbit.paletteRedNormal,
-          15,
-        )};`
-      : `inset 0 0 0 1px ${theme.orbit.borderColorInputFocus}, 0 0 0 3px ${convertHexToRgba(
-          theme.orbit.borderColorInputFocus,
-          15,
-        )};`};
-`;
+const formElementFocus = (): any => defaultFocus;
 
 export default formElementFocus;

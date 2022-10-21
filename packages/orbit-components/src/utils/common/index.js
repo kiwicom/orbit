f4193346cdf2998803e22fcc6a8cb21d2e7f7166
Daniel Sil <daniel.sil@kiwi.com>
2022-10-21 11:17:25 +0200

    feat(utils): add defaultFocus
    This adds the browser default focus styles to be applied in components

--- packages/orbit-components/src/utils/common/index.js
+++ packages/orbit-components/src/utils/common/index.js
@@ -1,2 +1,9 @@
 // @flow
+import { css } from "styled-components";
+
 export const firstToUpper = (str: string): string => str.charAt(0).toUpperCase() + str.slice(1);
+
+export const defaultFocus = (): any => css`
+  outline: 1px solid Highlight;
+  outline: 1px solid -webkit-focus-ring-color;
+`;

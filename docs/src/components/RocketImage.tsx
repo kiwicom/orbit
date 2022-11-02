42af2a388e590e9e876415c8b6e7466726ccdb34
Daniel Sil <daniel.sil@kiwi.com>
2022-11-02 11:26:36 +0100

    docs: have RocketImage to use gatsby-plugin-image
    It also fixes the enter transition

--- docs/src/components/RocketImage.tsx
+++ docs/src/components/RocketImage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
-import Img, { FixedObject } from "gatsby-image";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import styled, { css } from "styled-components";
 import { mediaQueries as mq } from "@kiwicom/orbit-components";
 
@@ -15,62 +15,52 @@ const StyledImageWrapper = styled.div`
     height: 100%;
     overflow: hidden;
   `)}
+
+  [data-main-image] {
+    opacity: 1;
+    transition: none;
+  }
 `;
 
 export default function RocketImage() {
-  const data: {
-    file: {
-      childImageSharp: {
-        fixed: FixedObject;
-        gatsbyImageData: {
-          height: number;
-          layout: string;
-          width: number;
-          images: {
-            fallback: { src: string; srcSet: string };
-            sources: Array<{ srcSet: string; sizes: string; type: string }>;
-          };
-          placeholder: {
-            fallback: string;
-          };
-        };
-      };
-    };
-  } = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
       file(relativePath: { eq: "rocket.png" }) {
         childImageSharp {
-          fixed(width: 810, height: 852) {
-            ...GatsbyImageSharpFixed_withWebp
-          }
+          gatsbyImageData(placeholder: NONE, backgroundColor: "transparent")
         }
       }
     }
   `);
 
+  const image = getImage(data.file);
+
   return (
     <StyledImageWrapper>
-      <Img
-        css={css`
-          position: relative;
-          top: 40px;
-          right: 0;
-          width: 60vw;
-          min-width: 400px;
-          max-width: 850px;
+      {image && (
+        <GatsbyImage
+          alt="orbit-rocket"
+          image={image}
+          css={css`
+            position: relative;
+            top: 40px;
+            right: 0;
+            width: 60vw;
+            min-width: 400px;
+            max-width: 850px;
 
-          &:after {
-            position: absolute;
-            content: "";
-            bottom: 0;
-            width: 100%;
-            height: 300px;
-            background: linear-gradient(0deg, #ffffff 6.03%, rgba(255, 255, 255, 0) 100%);
-          }
-        `}
-        style={{ position: "absolute" }} // to override gatsby-image's position
-        fixed={data.file.childImageSharp.fixed}
-      />
+            &:after {
+              position: absolute;
+              content: "";
+              bottom: 0;
+              width: 100%;
+              height: 300px;
+              background: linear-gradient(0deg, #ffffff 6.03%, rgba(255, 255, 255, 0) 100%);
+            }
+          `}
+          style={{ position: "absolute" }} // to override gatsby-image's position
+        />
+      )}
     </StyledImageWrapper>
   );
 }

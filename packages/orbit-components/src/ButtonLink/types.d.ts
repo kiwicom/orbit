d4832802a4629d2a6ccc5d9c5bda9939d2540ddb
mainframev <vgenaev@gmail.com>
2022-12-01 21:41:57 +0100

    fix(ButtonLink): remove redundant ref type

--- packages/orbit-components/src/ButtonLink/types.d.ts
+++ packages/orbit-components/src/ButtonLink/types.d.ts
@@ -1,14 +1,12 @@
 // Type definitions for @kiwicom/orbit-components
 // Project: http://github.com/kiwicom/orbit
 
-import * as React from "react";
-
 import type * as Common from "../common/types";
 import type { ButtonCommonProps, Size } from "../primitives/ButtonPrimitive/types";
 
 export type Type = "primary" | "secondary" | "critical";
 
-export interface Props extends Common.Globals, Common.Ref, Common.SpaceAfter, ButtonCommonProps {
+export interface Props extends Common.Globals, Common.SpaceAfter, ButtonCommonProps {
   readonly compact?: boolean;
   readonly type?: Type;
   readonly size?: Size;

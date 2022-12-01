eb457941bd6514125ca7ee922d5097f3bdc1cea7
mainframev <vgenaev@gmail.com>
2022-12-01 14:54:24 +0100

    chore: remove createIcon type

--- packages/orbit-components/src/Icon/createIcon.tsx
+++ packages/orbit-components/src/Icon/createIcon.tsx
@@ -1,20 +1,17 @@
 import * as React from "react";
 
 import whiteListProps from "./helpers/whiteListProps";
-import type { Props } from "./types";
 
 import OrbitIcon from ".";
 
-const createIcon = (
-  def: React.ReactNode,
-  viewBox: string,
-  displayName: string,
-): React.ComponentType<Props> => {
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const createIcon = (def: React.ReactNode, viewBox: string, displayName: string) => {
   const icon = props => (
     <OrbitIcon viewBox={viewBox} {...whiteListProps(props)}>
       {def}
     </OrbitIcon>
   );
+
   icon.displayName = displayName;
   return icon;
 };

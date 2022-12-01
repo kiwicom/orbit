4b80564994e67eb21aacde5ec57c74aa378783ff
mainframev <vgenaev@gmail.com>
2022-12-01 19:14:03 +0100

    fix(Select): remove ref type

--- packages/orbit-components/src/Select/types.d.ts
+++ packages/orbit-components/src/Select/types.d.ts
@@ -15,11 +15,7 @@ interface Option {
   readonly disabled?: boolean;
 }
 
-export interface Props
-  extends Common.Globals,
-    Common.Ref<HTMLSelectElement>,
-    Common.SpaceAfter,
-    Common.DataAttrs {
+export interface Props extends Common.Globals, Common.SpaceAfter, Common.DataAttrs {
   readonly id?: string;
   readonly required?: boolean;
   readonly size?: Common.InputSize;

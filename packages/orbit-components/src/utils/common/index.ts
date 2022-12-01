00e17db53d0bf0aa6803c3ac47bfe823c89424cc
mainframev <vgenaev@gmail.com>
2022-12-01 23:28:21 +0100

    feat(utils): add marginUtility

--- packages/orbit-components/src/utils/common/index.ts
+++ packages/orbit-components/src/utils/common/index.ts
@@ -1,9 +1,25 @@
 import type { FlattenSimpleInterpolation } from "styled-components";
 import { css } from "styled-components";
 
+import type { ObjectPropertyUtility } from "../../common/types";
+
 export const firstToUpper = (str: string): string => str.charAt(0).toUpperCase() + str.slice(1);
 
 export const defaultFocus = (): FlattenSimpleInterpolation => css`
   outline: 2px solid Highlight;
   outline: 2px solid -webkit-focus-ring-color;
 `;
+
+export const marginUtility = (
+  margin: string | ObjectPropertyUtility,
+): FlattenSimpleInterpolation => {
+  if (typeof margin === "string") {
+    return css`
+      margin: ${margin};
+    `;
+  }
+
+  return css`
+    margin: ${margin.top} ${margin.right} ${margin.bottom} ${margin.left};
+  `;
+};

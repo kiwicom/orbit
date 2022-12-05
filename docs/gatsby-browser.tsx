a3c66c56129546d5dec2fc2ba9299ec311b78155
mainframev <vgenaev@gmail.com>
2022-12-05 10:56:34 +0100

    docs: use @fontsource

--- docs/gatsby-browser.tsx
+++ docs/gatsby-browser.tsx
@@ -1,5 +1,8 @@
 import "tailwindcss/dist/base.min.css";
 import type { GatsbyBrowser } from "gatsby";
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/500.css";
+import "@fontsource/dm-sans/700.css";
 
 import wrapWithProviders from "./wrapWithProviders";
 

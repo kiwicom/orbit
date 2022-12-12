f101aaadabdcf75b1711237246a40fe5df6dfaad
mainframev <vgenaev@gmail.com>
2022-12-12 11:51:42 +0100

    chore: remove check:icons from lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,6 +5,5 @@ module.exports = {
   "*.{md,json,yaml,yml}": "prettier --write",
   "*.mdx": "eslint --fix --report-unused-disable-directives",
   "**/!(snippets)/*.mdx": "remark -q -u validate-links --no-config",
-  "packages/orbit-components/**/*.svg": "yarn components check:icons",
   ".browserslistrc": () => ["yarn update-supported-browsers"],
 };

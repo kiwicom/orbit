94010b83f37a9c9d9644c02e26af8e72b47349e0
Daniel Sil <daniel.sil@kiwi.com>
2022-12-21 15:14:22 +0000

    feat(Modal): export ModalHeading subcomponent

--- packages/orbit-components/src/Modal/index.tsx
+++ packages/orbit-components/src/Modal/index.tsx
@@ -677,6 +677,6 @@ const Modal = React.forwardRef<Instance, Props>(
 Modal.displayName = "Modal";
 export default Modal;
 
-export { default as ModalHeader } from "./ModalHeader";
+export { default as ModalHeader, ModalHeading } from "./ModalHeader";
 export { default as ModalSection } from "./ModalSection";
 export { default as ModalFooter } from "./ModalFooter";

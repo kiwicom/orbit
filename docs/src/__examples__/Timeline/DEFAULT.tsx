9a71f269dfae7df6f339d761ce4edd027d554df3
mainframev <vgenaev@gmail.com>
2023-01-05 00:08:32 +0100

    docs(examples): remove knobs for TimelineStep

--- docs/src/__examples__/Timeline/DEFAULT.tsx
+++ docs/src/__examples__/Timeline/DEFAULT.tsx
@@ -1,55 +1,21 @@
-import React from "react";
-import { Timeline, TimelineStep } from "@kiwicom/orbit-components";
+import React from 'react'
+import { Timeline, TimelineStep } from '@kiwicom/orbit-components'
 
 export default {
   Example: () => (
     <Timeline>
-      <TimelineStep label="Booked" subLabel="January 3, 10:43" type="success">
+      <TimelineStep label='Booked' subLabel='January 3, 10:43' type='success'>
         You booked the trip and received e-tickets.
       </TimelineStep>
-      <TimelineStep label="Checked in" subLabel="May 3, 8:45" type="success">
+      <TimelineStep label='Checked in' subLabel='May 3, 8:45' type='success'>
         You checked in for the trip and received boarding passes
       </TimelineStep>
-      <TimelineStep label="Board" subLabel="May 4, 8:15" type="warning">
+      <TimelineStep label='Board' subLabel='May 4, 8:15' type='warning'>
         Be at your departure gate at least 30 minutes before boarding.
       </TimelineStep>
-      <TimelineStep label="Arrive" subLabel="May 4, 11:48">
+      <TimelineStep label='Arrive' subLabel='May 4, 11:48'>
         Arrive at your destination
       </TimelineStep>
     </Timeline>
   ),
-  exampleKnobs: [
-    {
-      component: "TimelineStep",
-      knobs: [
-        {
-          name: "type",
-          type: "select",
-          defaultValue: "success",
-          options: ["success", "warning", "critical"],
-        },
-        {
-          name: "direction",
-          type: "select",
-          defaultValue: null,
-          options: ["row", "column"],
-        },
-        {
-          name: "label",
-          type: "text",
-          defaultValue: "Booked",
-        },
-        {
-          name: "asText",
-          type: "boolean",
-          defaultValue: false,
-        },
-        {
-          name: "subLabel",
-          type: "text",
-          defaultValue: "January 3, 10:43",
-        },
-      ],
-    },
-  ],
-};
+}

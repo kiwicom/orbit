d24a702af8300f539aa0bd049af7f5c47d7ee55c
mainframev <vgenaev@gmail.com>
2023-03-01 19:17:00 +0100

    chore(Tabs): fix missing react imports

--- packages/orbit-components/src/Tabs/components/TabPanels/types.d.ts
+++ packages/orbit-components/src/Tabs/components/TabPanels/types.d.ts
@@ -1,3 +1,5 @@
+import type React from "react";
+
 import type { Globals } from "../../../common/types";
 import type { Props as TabPanelProps } from "../TabPanel/types";
 
--- packages/orbit-components/src/Tabs/types.d.ts
+++ packages/orbit-components/src/Tabs/types.d.ts
@@ -1,3 +1,5 @@
+import type React from "react";
+
 import type { Spacing } from "../Stack/types";
 import type { Globals } from "../common/types";
 

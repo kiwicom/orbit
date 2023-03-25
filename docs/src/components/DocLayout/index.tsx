e869295e7ef17dc9355d28c6e736cbd87aed4e27
mainframev <vgenaev@gmail.com>
2023-03-25 20:43:04 +0100

    docs: remove Grid from MdxProvider

--- docs/src/components/DocLayout/index.tsx
+++ docs/src/components/DocLayout/index.tsx
@@ -204,7 +204,6 @@ export default function DocLayout({
                           InlineToken,
                           ReactExample,
                           Container,
-                          Grid,
                           FigmaIframe,
                         }}
                       >

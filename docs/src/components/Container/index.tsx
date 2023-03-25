f595836b0dad48e7deb77c3d7dde3595cd2175c8
Victor Genaev <vgenaev@gmail.com>
2023-03-25 19:33:15 +0100

    docs: add Container component (#3777)

---
+++ docs/src/components/Container/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const StyledContainer = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    padding: ${theme.orbit.spaceXLarge};
+    gap: ${theme.orbit.spaceXLarge};
+    border-radius: ${theme.orbit.borderRadiusLarge};
+    background: ${theme.orbit.paletteCloudLight};
+
+    & > img {
+      max-width: 100%;
+      justify-self: center;
+      background: ${theme.orbit.paletteWhite};
+      border-radius: ${theme.orbit.borderRadiusLarge};
+      padding: ${theme.orbit.spaceXLarge};
+    }
+  `};
+`;
+
+const Container = ({ children }) => {
+  return <StyledContainer>{children}</StyledContainer>;
+};
+
+export default Container;

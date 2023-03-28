4aa3485987a3efa31fb2de421ece395ef8acd031
mainframev <vgenaev@gmail.com>
2023-03-28 14:34:56 +0200

    chore(Tabs): add fullWidth story

--- packages/orbit-components/src/Tabs/Tabs.stories.tsx
+++ packages/orbit-components/src/Tabs/Tabs.stories.tsx
@@ -26,6 +26,28 @@ export const Default = () => {
   );
 };
 
+export const FullWidth = () => {
+  const compact = boolean("compact", false);
+  const fullWidth = boolean("fullWidth", true);
+
+  return (
+    <Tabs onChange={action("onChange")}>
+      <TabList fullWidth={fullWidth} compact={compact}>
+        <Tab>Tab 1</Tab>
+        <Tab type="basic">Tab 2</Tab>
+        <Tab type="medium">Tab 3</Tab>
+        <Tab type="top">Tab 4</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>Tab 1 content</TabPanel>
+        <TabPanel>Tab 2 content</TabPanel>
+        <TabPanel>Tab 3 content</TabPanel>
+        <TabPanel>Tab 4 content</TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+};
+
 export const Compact = () => {
   return (
     <Tabs onChange={action("onChange")}>

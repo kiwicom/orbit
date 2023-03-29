818caf31894cc5d05b656bec8dee814e4cb78bab
Jozef Mikušinec <mik.jozef@gmail.com>
2023-03-29 15:49:18 +0200

    feat(Tabs): add width: 100% to TabPanels (#3793)
    This will make the width: 100% of TabPanel effective
    
    Co-authored-by: jozef.mikusinec <jozef.mikusinec@kiwi.com>

--- packages/orbit-components/src/Tabs/components/TabPanels/index.tsx
+++ packages/orbit-components/src/Tabs/components/TabPanels/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 import type { Props } from "./types";
 import { useTabs, TabPanelProvider } from "../../TabContext";
+
+const StyledTabPanelsWrapper = styled.div`
+  width: 100%;
+`;
 
 const TabPanels = ({ children, dataTest }: Props) => {
   const { selected } = useTabs();
 
   return (
-    <div data-test={dataTest}>
+    <StyledTabPanelsWrapper data-test={dataTest}>
       {React.Children.map(children, (child, idx) => {
         if (!React.isValidElement(child)) return null;
         return (
@@ -16,7 +21,7 @@ const TabPanels = ({ children, dataTest }: Props) => {
           </TabPanelProvider>
         );
       })}
-    </div>
+    </StyledTabPanelsWrapper>
   );
 };
 

25b7509ab17ebe36be322d95439ba9e1bfcc2cfa
Daniel Sil <daniel.sil@kiwi.com>
2023-04-06 13:07:03 +0200

    feat(BadgeListItem): add size prop (#3797)
    * feat(BadgeListItem): add size prop for text size
    
    BadgeListItem now accepts size prop for changing the text size.
    Size can be small (default) or normal.
    
    * fixup! feat(BadgeListItem): add size prop for text size

--- packages/orbit-components/src/BadgeList/BadgeListItem/types.d.ts
+++ packages/orbit-components/src/BadgeList/BadgeListItem/types.d.ts
@@ -5,9 +5,12 @@ import type * as React from "react";
 import type * as Common from "../../common/types";
 
 export type Type = "neutral" | "info" | "success" | "warning" | "critical";
+export type Size = "small" | "normal";
+
 export interface Props extends Common.Globals {
   readonly children: React.ReactNode;
   readonly type?: Type;
+  readonly size?: Size;
   readonly strikeThrough?: boolean;
   readonly icon: React.ReactNode;
 }

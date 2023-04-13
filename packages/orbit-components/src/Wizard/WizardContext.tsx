40343b72f8a5627e10ff411379776dfd2f664a0a
mainframev <vgenaev@gmail.com>
2023-04-13 17:52:33 +0200

    refactor(Wizard): refactor component after eslint update

---
+++ packages/orbit-components/src/Wizard/WizardContext.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+
+export type Status = "available" | "completed" | "disabled";
+
+export interface WizardStepContextValue {
+  index: number;
+  status: Status;
+  isColumnOnDesktop: boolean;
+  nextStepStatus: Status;
+  isCompact: boolean;
+  isLastStep: boolean;
+  isActive: boolean;
+  onChangeStep?: (stepIndex: number) => void | Promise<any>;
+  onClose: () => void;
+}
+
+export const WizardStepContext = React.createContext<WizardStepContextValue>({
+  index: 0,
+  status: "disabled",
+  nextStepStatus: "disabled",
+  isColumnOnDesktop: false,
+  isCompact: false,
+  isLastStep: false,
+  isActive: false,
+  onChangeStep: () => {},
+  onClose: () => {},
+});
+
+export const WizardStepContextProvider = ({
+  index,
+  status,
+  isLastStep,
+  isColumnOnDesktop,
+  nextStepStatus,
+  isCompact,
+  isActive,
+  onChangeStep,
+  onClose,
+  children,
+}) => {
+  const value = React.useMemo(
+    () => ({
+      index,
+      status,
+      isLastStep,
+      isColumnOnDesktop,
+      nextStepStatus,
+      isCompact,
+      isActive,
+      onChangeStep,
+      onClose,
+    }),
+    [
+      index,
+      isActive,
+      isCompact,
+      isColumnOnDesktop,
+      isLastStep,
+      nextStepStatus,
+      onChangeStep,
+      onClose,
+      status,
+    ],
+  );
+
+  return <WizardStepContext.Provider value={value}>{children}</WizardStepContext.Provider>;
+};

2697f32bdf5a55b17247fc0bf408253b7984bcf4
Daniel Sil <daniel.sil@kiwi.com>
2023-04-19 15:21:04 +0200

    chore(Illustration): add type to consts file

--- packages/orbit-components/src/Modal/Modal.stories.tsx
+++ packages/orbit-components/src/Modal/Modal.stories.tsx
@@ -459,7 +459,7 @@ export const FullPreview = () => {
   const size = select("Size", Object.values(SIZES), SIZES.NORMAL);
   const title = text("Title", "Orbit design system");
   const description = text("Description", "Lorem ispum dolor sit amet");
-  const illustration = select("Illustration", Object.values(NAMES), NAMES[0]);
+  const illustration = select("Illustration", NAMES, NAMES[0]);
   const fixed = boolean("fixedFooter", false);
   const suppressed = boolean("suppressed", false);
   const content = text(

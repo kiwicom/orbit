6ff001325622844dd7072957bd583b6f06fd81ff
mainframev <vgenaev@gmail.com>
2023-05-23 14:17:47 +0200

    fix(Drawer): change labelHide to accept string only

--- packages/orbit-components/src/Drawer/components/types.d.ts
+++ packages/orbit-components/src/Drawer/components/types.d.ts
@@ -1,11 +1,9 @@
 // Type definitions for @kiwicom/orbit-components
 // Project: http://github.com/kiwicom/orbit
 
-import type React from "react";
-
 import type * as Common from "../../common/types";
 
 export interface Props {
   readonly onClick?: Common.Callback;
-  readonly title?: React.ReactNode;
+  readonly title?: string;
 }

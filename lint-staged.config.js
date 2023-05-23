0061d17c55831307c56d4ee2399f69af5898b8a5
mainframev <vgenaev@gmail.com>
2023-05-23 11:45:44 +0200

    chore: remove skipLibCheck from ts and add ts-check to lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   "*.{js,flow,ts,tsx}": "eslint --fix --report-unused-disable-directives",
+  "*.{ts,tsx,d.ts}": () => "check:types",
   "*.{md,json,yaml,yml}": "prettier --write",
   "*.mdx": "eslint --fix --report-unused-disable-directives",
   "**/!(snippets)/*.mdx": "remark -q -u validate-links --no-config",

12fca7add4ee2da7d598d5de176ffef1dad5f70d
Robin Cussol <RobinCsl@users.noreply.github.com>
2023-05-23 11:43:36 +0200

    fix(ItinerarySeparator): rename imported Props to avoid naming conflict (#3854)
    Otherwise, TypeScript would report the following error:
    ```
     Import declaration conflicts with local declaration of 'Props'.
    ```

--- packages/orbit-components/src/Itinerary/ItinerarySeparator/types.d.ts
+++ packages/orbit-components/src/Itinerary/ItinerarySeparator/types.d.ts
@@ -1,9 +1,9 @@
 import type * as React from "react";
 
-import type { Props } from "../../Separator/types";
+import type { Props as SeparatorProps } from "../../Separator/types";
 
 export interface Props {
   readonly children?: React.ReactNode;
-  readonly type?: Props["type"];
-  readonly color?: Props["color"];
+  readonly type?: SeparatorProps["type"];
+  readonly color?: SeparatorProps["color"];
 }

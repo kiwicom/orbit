d815e4114f01cdb84e0440a480445c64729fa1a7
mainframev <vgenaev@gmail.com>
2023-05-23 14:11:48 +0200

    fix(Pagination): change label types to string

--- packages/orbit-components/src/Pagination/types.d.ts
+++ packages/orbit-components/src/Pagination/types.d.ts
@@ -1,15 +1,15 @@
 // Type definitions for @kiwicom/orbit-components
 // Project: http://github.com/kiwicom/orbit
 
-import type React from "react";
+import type * as React from "react";
 
 import type * as Common from "../common/types";
 
 export type OnPageChange = (page: number) => void;
 export interface Props extends Common.Globals {
   readonly onPageChange: OnPageChange;
-  readonly labelPrev: React.ReactNode;
-  readonly labelNext: React.ReactNode;
+  readonly labelPrev: string;
+  readonly labelNext: string;
   readonly labelProgress: React.ReactNode;
   readonly pageCount: number;
   readonly selectedPage?: number;

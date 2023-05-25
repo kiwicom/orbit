95c35e5530a2005ee0764876b2b840bda163961d
Daniel Sil <daniel.sil@kiwi.com>
2023-05-25 11:03:11 +0100

    feat(InputField): add new aria-props

--- packages/orbit-components/src/InputField/types.d.ts
+++ packages/orbit-components/src/InputField/types.d.ts
@@ -7,6 +7,7 @@ import type * as Common from "../common/types";
 
 type Type = "text" | "number" | "email" | "password" | "passportid";
 type InputMode = "numeric" | "tel" | "decimal" | "email" | "url" | "search" | "text" | "none";
+type AriaAutoComplete = "inline" | "list" | "both" | "none";
 // InputEvent
 type InputEvent = Common.Event<React.SyntheticEvent<HTMLInputElement>>;
 type KeyboardEvent = Common.Event<React.KeyboardEvent<HTMLInputElement>>;
@@ -17,8 +18,8 @@ export interface Props extends Common.Globals, Common.SpaceAfter, Common.DataAtt
   readonly name?: string;
   readonly label?: Common.Translation;
   readonly inlineLabel?: boolean;
-  readonly value?: string | number | (() => string | number);
-  readonly placeholder?: string | (() => string);
+  readonly value?: string | number;
+  readonly placeholder?: string;
   readonly prefix?: React.ReactNode;
   readonly suffix?: React.ReactNode;
   readonly help?: React.ReactNode;
@@ -33,6 +34,8 @@ export interface Props extends Common.Globals, Common.SpaceAfter, Common.DataAtt
   readonly required?: boolean;
   readonly tabIndex?: string | number;
   readonly readOnly?: boolean;
+  readonly list?: string;
+  readonly role?: string;
   readonly autoComplete?: string;
   readonly autoFocus?: boolean;
   readonly id?: string;
@@ -46,4 +49,9 @@ export interface Props extends Common.Globals, Common.SpaceAfter, Common.DataAtt
   readonly onMouseDown?: InputEvent;
   readonly onKeyDown?: KeyboardEvent;
   readonly onKeyUp?: KeyboardEvent;
+  readonly ariaAutocomplete?: AriaAutoComplete;
+  readonly ariaActiveDescendant?: string;
+  readonly ariaHasPopup?: boolean;
+  readonly ariaExpanded?: boolean;
+  readonly ariaControls?: string;
 }

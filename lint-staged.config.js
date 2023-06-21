3686712b21cfdeaedbf44ce5d66785ee59f692cd
Daniel Sil <daniel.sil@kiwi.com>
2023-06-21 13:12:56 +0200

    chore: remove fix flag from commitlint for mdx

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,7 +4,7 @@ module.exports = {
   "*.{js,flow,ts,tsx}": "eslint --fix --report-unused-disable-directives",
   "*.{ts,tsx,d.ts}": () => "tsc-files --noEmit",
   "*.{md,json,yaml,yml}": "prettier --write",
-  "*.mdx": "eslint --fix --report-unused-disable-directives",
+  "*.mdx": "eslint --report-unused-disable-directives",
   "**/!(snippets)/*.mdx": "remark -q -u validate-links --no-config",
   ".browserslistrc": () => ["yarn update-supported-browsers"],
 };

35b800c2281c2263906bc13a1bb340f99378519e
Daniel Sil <daniel.sil@kiwi.com>
2023-07-04 11:11:56 +0200

    feat(Heading): migrate to Tailwind

---
+++ packages/orbit-components/cypress/integration/pages/heading-media-query-props.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Heading } from "@kiwicom/orbit-components";
+
+export default function HeadingMediaProps() {
+  return (
+    <Heading
+      type="title1"
+      mediumMobile={{ type: "display" }}
+      largeMobile={{ spaceAfter: "small", type: "title2" }}
+      tablet={{ align: "center", type: "title4" }}
+      desktop={{ type: "title5" }}
+      largeDesktop={{ type: "title6" }}
+    >
+      Heading
+    </Heading>
+  );
+}

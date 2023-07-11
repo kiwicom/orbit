8b8f788474e52335fc231ecc8a1d42db50a0602c
mainframev <vgenaev@gmail.com>
2023-07-11 18:23:29 +0200

    chore(tracking): fix missing entry

--- packages/orbit-tracking/tsup.config.js
+++ packages/orbit-tracking/tsup.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/react-scanner.config.ts"],
   format: ["esm", "cjs"],
   dts: true,
   splitting: true,

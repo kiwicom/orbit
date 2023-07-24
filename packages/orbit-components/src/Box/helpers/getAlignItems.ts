9eee20ddaf30d84e13ffd66554905a91d44ec95c
Daniel Sil <daniel.sil@kiwi.com>
2023-07-24 18:02:35 +0200

    feat(Box): migrate to Tailwind
    BREAKING CHANGE: StyledBox removed. `.orbit-box` selector should be used

---
+++ packages/orbit-components/src/Box/helpers/getAlignItems.ts
@@ -0,0 +1,11 @@
+import type { Props } from "../types";
+import type { QUERIES } from "../../utils/mediaQuery/consts";
+import { alignItemsClasses } from "../../common/tailwind/alignItems";
+
+const getAlignItemsClass = (align: Props["align"], viewport?: QUERIES) => {
+  if (!align) return null;
+
+  return viewport ? alignItemsClasses[viewport][align] : alignItemsClasses[align];
+};
+
+export default getAlignItemsClass;

238d8ca54ee55dd25094118256821393b52a88dc
Daniel Sil <daniel.sil@kiwi.com>
2023-07-24 18:02:35 +0200

    feat(Box): migrate to Tailwind
    BREAKING CHANGE: StyledBox removed. `.orbit-box` selector should be used

---
+++ packages/orbit-components/src/Box/helpers/getPadding.ts
@@ -0,0 +1,30 @@
+import type { Props } from "../types";
+import type { QUERIES } from "../../utils/mediaQuery/consts";
+import {
+  paddingBottomClasses,
+  paddingClasses,
+  paddingLeftClasses,
+  paddingRightClasses,
+  paddingTopClasses,
+} from "../../common/tailwind/padding";
+
+const getPaddingClass = (padding: Props["padding"], viewport?: QUERIES) => {
+  if (!padding) return null;
+
+  if (typeof padding === "object") {
+    const { top, bottom, left, right } = padding;
+
+    return [
+      top != null && (viewport ? paddingTopClasses[viewport][top] : paddingTopClasses[top]),
+      bottom != null &&
+        (viewport ? paddingBottomClasses[viewport][bottom] : paddingBottomClasses[bottom]),
+      left != null && (viewport ? paddingLeftClasses[viewport][left] : paddingLeftClasses[left]),
+      right != null &&
+        (viewport ? paddingRightClasses[viewport][right] : paddingRightClasses[right]),
+    ];
+  }
+
+  return viewport ? paddingClasses[viewport][padding] : paddingClasses[padding];
+};
+
+export default getPaddingClass;
---
+++ packages/orbit-components/src/common/tailwind/shrink.ts
@@ -0,0 +1,1 @@
+import { QUERIES } from "../../utils/mediaQuery/consts";

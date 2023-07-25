bc64007aec55d0212b3c01303176e85fe63f4943
Daniel Sil <daniel.sil@kiwi.com>
2023-07-25 14:15:11 +0200

    chore(tailwind): add palette type export

---
+++ packages/orbit-design-tokens/src/js/paletteColors.d.ts
@@ -0,0 +1,88 @@
+/**
+#####################################################
+#                                                   #
+# DO NOT EDIT DIRECTLY, THIS FILE IS AUTO-GENERATED #
+#                                                   #
+#####################################################
+
+If you want to update some tokens:
+- please do so in /definitions folder
+- run @kiwicom/orbit-design-tokens "build" script
+*/
+export type Tokens = {
+  blueLight: string;
+  blueLightHover: string;
+  blueLightActive: string;
+  blueNormal: string;
+  blueNormalHover: string;
+  blueNormalActive: string;
+  blueDark: string;
+  blueDarkHover: string;
+  blueDarkActive: string;
+  blueDarker: string;
+  cloudLight: string;
+  cloudLightHover: string;
+  cloudLightActive: string;
+  cloudNormal: string;
+  cloudNormalHover: string;
+  cloudNormalActive: string;
+  cloudDark: string;
+  cloudDarkHover: string;
+  cloudDarkActive: string;
+  greenLight: string;
+  greenLightHover: string;
+  greenLightActive: string;
+  greenNormal: string;
+  greenNormalHover: string;
+  greenNormalActive: string;
+  greenDark: string;
+  greenDarkHover: string;
+  greenDarkActive: string;
+  greenDarker: string;
+  inkDark: string;
+  inkDarkHover: string;
+  inkDarkActive: string;
+  inkLight: string;
+  inkLightHover: string;
+  inkLightActive: string;
+  inkNormal: string;
+  inkNormalHover: string;
+  inkNormalActive: string;
+  orangeLight: string;
+  orangeLightHover: string;
+  orangeLightActive: string;
+  orangeNormal: string;
+  orangeNormalHover: string;
+  orangeNormalActive: string;
+  orangeDark: string;
+  orangeDarkHover: string;
+  orangeDarkActive: string;
+  orangeDarker: string;
+  productLight: string;
+  productLightHover: string;
+  productLightActive: string;
+  productNormal: string;
+  productNormalHover: string;
+  productNormalActive: string;
+  productDark: string;
+  productDarkHover: string;
+  productDarkActive: string;
+  productDarker: string;
+  redLight: string;
+  redLightHover: string;
+  redLightActive: string;
+  redNormal: string;
+  redNormalHover: string;
+  redNormalActive: string;
+  redDark: string;
+  redDarkHover: string;
+  redDarkActive: string;
+  redDarker: string;
+  socialFacebook: string;
+  socialFacebookHover: string;
+  socialFacebookActive: string;
+  white: string;
+  whiteNormal: string;
+  whiteHover: string;
+  whiteActive: string;
+};

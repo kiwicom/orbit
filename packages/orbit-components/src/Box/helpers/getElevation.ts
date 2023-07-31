fdae83bfe192380fe733b06e01423a49a6041212
mainframev <vgenaev@gmail.com>
2023-07-31 20:28:52 +0200

    feat(Stack): rewrite Stack to tailwind
    test(Stack): test spacings and mediaquery props in cypress
    
    chore(Box): adjust Box types for helpers
    
    chore(Box): unify helper functions with Stack

--- packages/orbit-components/src/Box/helpers/getElevation.ts
+++ packages/orbit-components/src/Box/helpers/getElevation.ts
@@ -2,7 +2,7 @@ import type { Props } from "../types";
 import type { QUERIES } from "../../utils/mediaQuery/consts";
 import { shadowClasses } from "./tailwindClasses";
 
-const getElevationClass = (elevation: Props["elevation"], viewport?: QUERIES) => {
+const getElevationClass = (elevation: Props["elevation"], viewport?: QUERIES): string | null => {
   if (!elevation) return null;
 
   return viewport ? shadowClasses[viewport][elevation] : shadowClasses[elevation];

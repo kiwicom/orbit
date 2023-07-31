23d3c64f42b3f49cc8db7fc8282b659d297830e0
mainframev <vgenaev@gmail.com>
2023-07-31 20:25:37 +0200

    chore(Stack): remove redundant helper functions

--- packages/orbit-components/src/Stack/helpers/__tests__/getBasis.test.ts
+++
@@ -1,8 +0,0 @@
-import getBasis from "../getBasis";
-import theme from "../../../defaultTheme";
-
-describe("#getBasis", () => {
-  it("should set basis", () => {
-    expect(getBasis("200px")({ theme })).toBe("200px");
-  });
-});

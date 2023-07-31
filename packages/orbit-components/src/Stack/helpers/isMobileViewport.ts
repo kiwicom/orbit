6902ba2d9107448226a8f50b38dc0205164af354
mainframev <vgenaev@gmail.com>
2023-07-31 20:25:37 +0200

    chore(Stack): remove redundant helper functions

--- packages/orbit-components/src/Stack/helpers/isMobileViewport.ts
+++
@@ -1,6 +0,0 @@
-import type { Devices } from "../../utils/mediaQuery/types";
-
-const isMobileViewport = (viewport: Devices): boolean =>
-  viewport === "smallMobile" || viewport === "mediumMobile" || viewport === "largeMobile";
-
-export default isMobileViewport;

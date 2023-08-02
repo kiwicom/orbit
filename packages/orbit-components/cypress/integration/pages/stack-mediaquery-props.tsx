2d4e10b8add15568fc9b36f61c20a557ad8e44b9
mainframev <vgenaev@gmail.com>
2023-08-03 01:57:02 +0200

    test(Stack): test spacings and mediaquery props in cypress

---
+++ packages/orbit-components/cypress/integration/pages/stack-mediaquery-props.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Stack } from "@kiwicom/orbit-components";
+
+const commonStyles = {
+  height: "30px",
+  padding: "10px",
+  width: "50px",
+  display: "flex",
+  alignItems: "center",
+};
+
+export default function StackMediaProps() {
+  return (
+    <Stack
+      spacing="none"
+      mediumMobile={{ spacing: "XXXSmall" }}
+      largeMobile={{ spacing: "XXSmall", direction: "column" }}
+      tablet={{ spacing: "XSmall", direction: "row" }}
+      desktop={{ spacing: "small" }}
+      largeDesktop={{ spacing: "medium" }}
+    >
+      <div style={{ background: "blue", ...commonStyles }}>1</div>
+      <div style={{ background: "red", ...commonStyles }}>2</div>
+      <div style={{ background: "cyan", ...commonStyles }}>3</div>
+      <div style={{ background: "magenta", ...commonStyles }}>4</div>
+    </Stack>
+  );
+}

6c228f1b33765f391c9032fa32324c6e342d8a6b
Victor Genaev <vgenaev@gmail.com>
2023-08-08 15:39:23 +0200

    Update docs/tailwind.config.ts
    Co-authored-by: Daniel Sil <daniel.sil@kiwi.com>

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -2,7 +2,10 @@ import { orbitComponentsPreset } from "@kiwicom/orbit-tailwind-preset";
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    path.join(path.dirname(require.resolve("@kiwicom/orbit-components")), "**/*.js"),
+  ],
   presets: [
     orbitComponentsPreset({
       disablePreflight: false,

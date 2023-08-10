e9559a8360db7e9d73346d7e2838700927b907dc
mainframev <vgenaev@gmail.com>
2023-08-10 09:48:27 +0200

    chore(Box): adjust Box types for helpers

--- packages/orbit-components/src/Box/helpers/getDirection.ts
+++ packages/orbit-components/src/Box/helpers/getDirection.ts
@@ -2,7 +2,7 @@ import type { Props } from "../types";
 import type { QUERIES } from "../../utils/mediaQuery/consts";
 import { directionClasses } from "../../common/tailwind/direction";
 
-const getDirectionClass = (direction: Props["direction"], viewport?: QUERIES) => {
+const getDirectionClass = (direction?: Props["direction"], viewport?: QUERIES): string | null => {
   if (!direction) return null;
 
   return viewport ? directionClasses[viewport][direction] : directionClasses[direction];

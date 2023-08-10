9b2b78bb6b831b1306c51b93a21078e7d5768fd6
Daniel Sil <daniel.sil@kiwi.com>
2023-08-10 12:44:32 +0200

    feat(NavigationBar): migrate to Tailwind

---
+++ packages/orbit-components/cypress/integration/pages/navigation-bar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavigationBar } from "@kiwicom/orbit-components";
+
+export default function NavigationBarScrollTest() {
+  return (
+    <div style={{ height: "1000px" }}>
+      <NavigationBar>
+        <div>Navigation Bar</div>
+      </NavigationBar>
+    </div>
+  );
+}

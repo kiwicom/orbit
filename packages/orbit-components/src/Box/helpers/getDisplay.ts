24f3a3c89c312cf9351e366eea739aa4c9ef69e1
mainframev <vgenaev@gmail.com>
2023-08-10 09:48:27 +0200

    chore(Box): adjust Box types for helpers

--- packages/orbit-components/src/Box/helpers/getDisplay.ts
+++ packages/orbit-components/src/Box/helpers/getDisplay.ts
@@ -2,7 +2,7 @@ import type { Props } from "../types";
 import type { QUERIES } from "../../utils/mediaQuery/consts";
 import { displayClasses } from "../../common/tailwind/display";
 
-const getDisplayClass = (display: Props["display"], viewport?: QUERIES) => {
+const getDisplayClass = (display?: Props["display"], viewport?: QUERIES): string | null => {
   if (!display) return null;
 
   return viewport ? displayClasses[viewport][display] : displayClasses[display];

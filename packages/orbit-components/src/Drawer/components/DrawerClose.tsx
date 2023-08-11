ffbe7a9942aa72c78b4705a9a5746e19c1f689d1
mainframev <vgenaev@gmail.com>
2023-08-11 14:59:58 +0200

    feat(Drawer): refactor to tailwind

--- packages/orbit-components/src/Drawer/components/DrawerClose.tsx
+++ packages/orbit-components/src/Drawer/components/DrawerClose.tsx
@@ -1,25 +1,14 @@
 import * as React from "react";
-import styled from "styled-components";
 
 import ButtonLink from "../../ButtonLink";
 import Close from "../../icons/Close";
-import defaultTheme from "../../defaultTheme";
-import { left } from "../../utils/rtl";
 import type { Props } from "./types";
-
-const StyledDrawerClose = styled.div`
-  margin-${left}: ${({ theme }) => theme.orbit.spaceMedium};
-`;
-
-StyledDrawerClose.defaultProps = {
-  theme: defaultTheme,
-};
 
 const DrawerClose = React.forwardRef<HTMLButtonElement, Props>(({ onClick, title }, ref) => {
   return (
-    <StyledDrawerClose>
+    <div className="ms-md">
       <ButtonLink onClick={onClick} iconLeft={<Close />} ref={ref} type="secondary" title={title} />
-    </StyledDrawerClose>
+    </div>
   );
 });
 

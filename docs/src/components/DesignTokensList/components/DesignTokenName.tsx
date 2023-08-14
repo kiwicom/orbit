57c18246a9031f9e3a52a3fe798d2f94195b0e8c
Daniel Sil <daniel.sil@kiwi.com>
2023-08-14 19:50:22 +0200

    docs: adjust tailwind classnames tooltip text (#3963)

--- docs/src/components/DesignTokensList/components/DesignTokenName.tsx
+++ docs/src/components/DesignTokensList/components/DesignTokenName.tsx
@@ -3,10 +3,10 @@ import { Tooltip } from "@kiwicom/orbit-components";
 
 import useCopyToClipboard from "../../../hooks/useCopyToClipboard";
 
-const DesignTokenName = ({ children }) => {
+const DesignTokenName = ({ children, tooltipText = "Click to copy the name of the token." }) => {
   const [isCopied, copy] = useCopyToClipboard(1000);
   return (
-    <Tooltip content="Click to copy the name of the token." placement="top">
+    <Tooltip content={tooltipText} placement="top">
       <button onClick={() => copy(children)} type="button">
         {isCopied ? "Copied!" : children}
       </button>

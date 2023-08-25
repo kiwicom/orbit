db2e9ee9ed8738d8a4c2afdfbfbb5cb4009de05d
oreqizer <oreqizer@gmail.com>
2023-08-25 13:37:02 +0200

    feat(Card): rewrite to Tailwind

---
+++ packages/orbit-components/src/Card/CardSection/components/Expandable.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+import Slide from "../../../utils/Slide";
+import useBoundingRect from "../../../hooks/useBoundingRect";
+
+type Props = {
+  expanded?: boolean;
+  children?: React.ReactNode;
+  slideID: string;
+  labelID: string;
+};
+
+export default function Expandable({ expanded, children, slideID, labelID }: Props) {
+  const [{ height }, ref] = useBoundingRect<HTMLDivElement>({ height: expanded ? null : 0 });
+
+  return (
+    <Slide maxHeight={height} expanded={expanded} id={slideID} ariaLabelledBy={labelID}>
+      <div ref={ref}>{children}</div>
+    </Slide>
+  );
+}

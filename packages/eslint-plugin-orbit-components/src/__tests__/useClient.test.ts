744c3682a6b3b467f6f320856fca686a966ea505
mainframev <vgenaev@gmail.com>
2023-09-04 17:19:41 +0200

    feat(eslint-plugin): add new use-client-directive rule

---
+++ packages/eslint-plugin-orbit-components/src/__tests__/useClient.test.ts
@@ -0,0 +1,31 @@
+import dedent from "dedent";
+import ruleTester from "../ruleTester";
+import useClient from "../rules/useClient";
+
+describe("use-client-directive", () => {
+  ruleTester.run("use-client-directive", useClient, {
+    valid: [
+      {
+        code: dedent`
+          "use client";
+          import React from "react";
+          import styled from 'styled-components';
+          `,
+      },
+    ],
+    invalid: [
+      {
+        code: dedent`
+        import React from "react";
+        import styled from 'styled-components';
+      `,
+        errors: [{ messageId: "error" }],
+        output: dedent`
+        "use client";
+        import React from "react";
+        import styled from 'styled-components';
+        `,
+      },
+    ],
+  });
+});

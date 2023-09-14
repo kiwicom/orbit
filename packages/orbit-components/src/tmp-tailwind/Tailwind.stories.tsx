4c28beef3401a7a61a3e9e466375bcad4fdde4d6
mainframev <vgenaev@gmail.com>
2023-09-14 11:58:48 +0200

    chore: add tailwind folder as temporary storage for refactored components

---
+++ packages/orbit-components/src/tmp-tailwind/Tailwind.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import DummyTailwindComponent from "./DummyTailwindComponent";
+
+export default {
+  title: "Tailwind",
+};
+
+export const DummyTailwindComponentStory = () => {
+  return <DummyTailwindComponent />;
+};

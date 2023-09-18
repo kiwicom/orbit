c3f7adbae87380561174aa7fe9aafd9988b28dc3
Daniel Sil <daniel.sil@kiwi.com>
2023-09-18 15:49:41 +0200

    chore: change type to interface
    Following new lint rules

--- packages/orbit-components/src/Card/CardSection/components/Expandable.tsx
+++ packages/orbit-components/src/Card/CardSection/components/Expandable.tsx
@@ -3,12 +3,12 @@ import * as React from "react";
 import Slide from "../../../utils/Slide";
 import useBoundingRect from "../../../hooks/useBoundingRect";
 
-type Props = {
+interface Props {
   expanded?: boolean;
   children?: React.ReactNode;
   slideID: string;
   labelID: string;
-};
+}
 
 export default function Expandable({ expanded, children, slideID, labelID }: Props) {
   const [{ height }, ref] = useBoundingRect<HTMLDivElement>({ height: expanded ? null : 0 });

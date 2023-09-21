6dd22a057cc62e6207d6d1e1d41221915bc8a388
Victor Genaev <vgenaev@gmail.com>
2023-09-21 19:01:44 +0200

    chore: import jest-dom matchers after update

--- packages/orbit-components/cypress/support/e2e.ts
+++ packages/orbit-components/cypress/support/e2e.ts
@@ -1,5 +1,7 @@
-import "./commands";
+import { configure } from "@testing-library/cypress";
 
 /// <reference types="@testing-library/cypress" />
 /// <reference types="cypress" />
 import "@testing-library/cypress/add-commands";
+
+configure({ testIdAttribute: "data-test" });

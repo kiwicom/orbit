302e0dd259c1eac3a14853298f03605399da2cd7
Victor Genaev <vgenaev@gmail.com>
2023-10-09 11:35:19 +0200

    feat(Hide): migrate to Tailwind
    chore(Layout): remove getViewportHideStyles
    
    test: update snapshots
    
    Update packages/orbit-tailwind-preset/src/presets/components/index.ts
    
    Co-authored-by: Marco Vidal García <marco.vidal@kiwi.com>

--- packages/orbit-components/src/deprecated/Desktop/__tests__/index.test.tsx
+++ packages/orbit-components/src/deprecated/Desktop/__tests__/index.test.tsx
@@ -7,42 +7,8 @@ describe("Desktop", () => {
   it("should output expected styles", () => {
     const { container } = render(<Desktop>kek</Desktop>);
     expect(container.firstChild).toMatchInlineSnapshot(`
-      .c0 {
-        display: none;
-      }
-
-      @media (min-width:414px) {
-        .c0 {
-          display: none;
-        }
-      }
-
-      @media (min-width:576px) {
-        .c0 {
-          display: none;
-        }
-      }
-
-      @media (min-width:768px) {
-        .c0 {
-          display: none;
-        }
-      }
-
-      @media (min-width:992px) {
-        .c0 {
-          display: inline-block;
-        }
-      }
-
-      @media (min-width:1200px) {
-        .c0 {
-          display: inline-block;
-        }
-      }
-
       <div
-        class="c0"
+        class="inline-block sm-mm:hidden mm-lm:hidden lm-tb:hidden tb-de:hidden"
       >
         kek
       </div>
--- packages/orbit-components/src/deprecated/Mobile/__tests__/index.test.tsx
+++ packages/orbit-components/src/deprecated/Mobile/__tests__/index.test.tsx
@@ -7,38 +7,8 @@ describe("Mobile", () => {
   it("should be visible on smallMobile, largeMobile and tablet", () => {
     const { container } = render(<Mobile>kek</Mobile>);
     expect(container.firstChild).toMatchInlineSnapshot(`
-      @media (min-width:414px) {
-        .c0 {
-          display: inline-block;
-        }
-      }
-
-      @media (min-width:576px) {
-        .c0 {
-          display: inline-block;
-        }
-      }
-
-      @media (min-width:768px) {
-        .c0 {
-          display: inline-block;
-        }
-      }
-
-      @media (min-width:992px) {
-        .c0 {
-          display: none;
-        }
-      }
-
-      @media (min-width:1200px) {
-        .c0 {
-          display: none;
-        }
-      }
-
       <div
-        class="c0"
+        class="inline-block de-ld:hidden ld:hidden"
       >
         kek
       </div>

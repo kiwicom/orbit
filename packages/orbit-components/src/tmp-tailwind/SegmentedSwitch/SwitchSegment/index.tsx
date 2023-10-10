b401c18c73a4ebfc1a134eb9ca4dc01953d09350
Marco Vidal Garcia <marco.vidal@kiwi.com>
2023-10-10 17:08:30 +0200

    chore(SegmentedSwitch): migrate to Tailwind
    Closes FEPLT-1706

---
+++ packages/orbit-components/src/tmp-tailwind/SegmentedSwitch/SwitchSegment/index.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import cx from "clsx";
+
+import useClickOutside from "../../../hooks/useClickOutside";
+import type { Props } from "../../../SegmentedSwitch/SwitchSegment/types";
+import useTheme from "../../../hooks/useTheme";
+
+const SwitchSegment = ({
+  value,
+  label,
+  disabled,
+  onChange,
+  setTooltipShown,
+  onFocus,
+  defaultChecked,
+  name,
+}: Props) => {
+  const theme = useTheme();
+  const ref = React.useRef<HTMLInputElement | null>(null);
+  useClickOutside(ref, () => setTooltipShown(false));
+
+  return (
+    <label className="orbit-switch-segment-label relative flex w-full">
+      <input
+        className={cx(
+          "sr-only absolute m-[-1px] h-[1px] w-[1px] overflow-hidden whitespace-nowrap border-0 p-0 focus:outline-none",
+          "peer",
+        )}
+        name={name || "switch-segment"}
+        defaultChecked={defaultChecked}
+        type="radio"
+        role="switch"
+        ref={ref}
+        aria-checked={Boolean(value)}
+        onChange={onChange}
+        onFocus={onFocus}
+        disabled={disabled}
+        value={value}
+      />
+      <div
+        className={cx(
+          "orbit-switch-segment-text",
+          "peer-checked:text-form-element-label-filled-foreground peer-checked:outline-blue-normal peer-checked:z-10 peer-checked:!rounded-[5px] peer-checked:outline peer-checked:outline-2 ",
+          "peer-focus:text-form-element-label-filled-foreground peer-focus:outline-blue-normal peer-focus:z-10 peer-focus:!rounded-[5px] peer-focus:outline peer-focus:outline-2 ",
+          "p-sm font-base text-normal duration-fast box-border flex w-full max-w-full cursor-pointer items-center justify-center border-0 text-center font-medium leading-normal transition-colors ease-in-out",
+          "bg-form-element-background text-form-element-foreground hover:text-form-element-label-filled-foreground",
+        )}
+        style={{ boxShadow: `0 0 0 1px ${theme.orbit.paletteCloudDark}` }}
+      >
+        {label}
+      </div>
+    </label>
+  );
+};
+
+export default SwitchSegment;
---
+++ packages/orbit-components/src/tmp-tailwind/SegmentedSwitch/__tests__/index.test.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import userEvent from "@testing-library/user-event";
+
+import { render, screen } from "../../../test-utils";
+import SegmentedSwitch from "..";
+
+describe("SegmentedSwitch", () => {
+  const user = userEvent.setup();
+
+  it("should have expected DOM output", async () => {
+    const onChange = jest.fn();
+    const onFocus = jest.fn();
+    const label = "label";
+    const dataTest = "test";
+
+    render(
+      <SegmentedSwitch
+        dataTest={dataTest}
+        label={label}
+        onChange={onChange}
+        onFocus={onFocus}
+        options={[
+          { label: "Male", value: "Male" },
+          { label: "Female", value: "Female" },
+        ]}
+      />,
+    );
+
+    expect(screen.getByText(label)).toBeInTheDocument();
+    expect(screen.getByTestId(dataTest)).toBeInTheDocument();
+
+    await user.tab();
+    expect(onFocus).toHaveBeenCalled();
+
+    await user.click(screen.getByLabelText("Female"));
+    expect(screen.getByDisplayValue("Female")).toBeInTheDocument();
+    expect(onChange).toHaveBeenCalled();
+  });
+
+  it("should have error", async () => {
+    const error = "Error message";
+    const label = "Gender";
+
+    render(
+      <SegmentedSwitch
+        label={label}
+        onChange={() => {}}
+        error={error}
+        options={[
+          { label: "Male", value: "Male" },
+          { label: "Female", value: "Female", disabled: true },
+        ]}
+      />,
+    );
+
+    await user.hover(screen.getByText(label));
+    expect(screen.getByText(error)).toBeInTheDocument();
+    expect(screen.getByLabelText("Female")).toBeDisabled();
+  });
+
+  it("should have help", async () => {
+    const help = "Help message";
+
+    render(
+      <SegmentedSwitch
+        label="Gender"
+        onChange={() => {}}
+        help={help}
+        options={[
+          { label: "Male", value: "Male" },
+          { label: "Female", value: "Female" },
+        ]}
+      />,
+    );
+
+    await user.hover(screen.getByText("Gender"));
+    expect(screen.getByText(help)).toBeInTheDocument();
+  });
+});

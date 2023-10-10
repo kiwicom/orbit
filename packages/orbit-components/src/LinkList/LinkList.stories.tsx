8909d8757705e1f72b1c5d9812c10b2d3678ecf3
Victor Genaev <vgenaev@gmail.com>
2023-10-10 15:14:05 +0200

    feat(LinkList): migrate to tailwind
    chore(Stack): remove redundant helper functions

--- packages/orbit-components/src/LinkList/LinkList.stories.tsx
+++ packages/orbit-components/src/LinkList/LinkList.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { select } from "@storybook/addon-knobs";
+import { select, boolean } from "@storybook/addon-knobs";
 
 import { DIRECTIONS, SPACINGS } from "../utils/layout/consts";
 import TextLink from "../TextLink";
@@ -7,19 +7,21 @@ import TextLink from "../TextLink";
 import LinkList from ".";
 
 export default {
-  title: "Linklist",
+  title: "LinkList",
 };
 
 export const Default = () => {
   const direction = select("Direction", Object.values(DIRECTIONS), DIRECTIONS.ROW);
   const spacing = select("Spacing", Object.values(SPACINGS), SPACINGS.MEDIUM);
+  const legacy = boolean("Legacy", false);
+  const indent = boolean("Indent", false);
 
   return (
-    <LinkList direction={direction} spacing={spacing}>
-      <TextLink type="secondary">Flights</TextLink>
-      <TextLink type="secondary">Flights</TextLink>
-      <TextLink type="secondary">Flights</TextLink>
-      <TextLink type="secondary">Flights</TextLink>
+    <LinkList direction={direction} spacing={spacing} legacy={legacy} indent={indent}>
+      <TextLink type="secondary">Flight 1</TextLink>
+      <TextLink type="secondary">Flight 2</TextLink>
+      <TextLink type="secondary">Flight 3</TextLink>
+      <TextLink type="secondary">Flight 4</TextLink>
     </LinkList>
   );
 };

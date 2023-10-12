1ac8deaf5d6c326706410d2ed2c49dd140f36895
oreqizer <oreqizer@gmail.com>
2023-10-12 12:50:17 +0200

    docs(Drawer): add visual test story

--- packages/orbit-components/src/Drawer/Drawer.stories.tsx
+++ packages/orbit-components/src/Drawer/Drawer.stories.tsx
@@ -468,3 +468,17 @@ SmartFaqSearchInRtl.story = {
     info: "Visit Orbit.Kiwi for more detailed guidelines.",
   },
 };
+
+export const VisualTest = () => {
+  return (
+    <Drawer title="Drawer" shown onClose={action("onClose")} actions={<Button>Button</Button>}>
+      Stuffs
+    </Drawer>
+  );
+};
+
+VisualTest.story = {
+  parameters: {
+    chromatic: { disableSnapshot: false },
+  },
+};

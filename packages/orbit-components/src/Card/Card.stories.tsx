8fae5dbf1002ce4ce42d518542fc1cae27fb15d0
oreqizer <oreqizer@gmail.com>
2023-10-12 09:05:12 +0200

    docs(Card): add visual test story

--- packages/orbit-components/src/Card/Card.stories.tsx
+++ packages/orbit-components/src/Card/Card.stories.tsx
@@ -330,3 +330,66 @@ Accessibility.story = {
     info: "This is a preview of component accessibility props",
   },
 };
+
+export const VisualTest = () => {
+  return (
+    <div className="space-y-xs">
+      <Card title="Somebody once" />
+      <Card title="Somebody once" description="Told me the world is gonna roll me" />
+      <Card
+        title="Somebody once"
+        description="Told me the world is gonna roll me"
+        actions={
+          <ButtonLink compact size="small">
+            Sharpest tool
+          </ButtonLink>
+        }
+      />
+
+      <Card title="Somebody once" description="Told me the world is gonna roll me">
+        <CardSection title="I ain't">
+          <Text>The sharpest tool in the shed</Text>
+        </CardSection>
+      </Card>
+      <Card title="Somebody once" description="Told me the world is gonna roll me">
+        <CardSection title="I ain't" expandable>
+          <Text>The sharpest tool in the shed</Text>
+        </CardSection>
+      </Card>
+      <Card title="Somebody once" description="Told me the world is gonna roll me">
+        <CardSection title="I ain't" expandable initialExpanded>
+          <Text>The sharpest tool in the shed</Text>
+        </CardSection>
+      </Card>
+
+      <Card title="Somebody once" description="Told me the world is gonna roll me">
+        <CardSection title="I ain't">
+          <Text>The sharpest tool in the shed</Text>
+        </CardSection>
+        <CardSection title="I ain't" expandable>
+          The sharpest tool in the shed
+        </CardSection>
+        <CardSection title="I ain't" expandable initialExpanded>
+          The sharpest tool in the shed
+        </CardSection>
+        <CardSection
+          expandable
+          title="I ain't"
+          actions={
+            <ButtonLink compact size="small">
+              All star
+            </ButtonLink>
+          }
+        >
+          <Text>The sharpest tool in the shed</Text>
+        </CardSection>
+      </Card>
+    </div>
+  );
+};
+
+VisualTest.story = {
+  parameters: {
+    chromatic: { disableSnapshot: false },
+  },
+};

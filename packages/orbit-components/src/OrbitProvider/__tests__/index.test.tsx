42610480b1f05b45f0cf061e3be0938f05722015
Victor Genaev <vgenaev@gmail.com>
2023-10-13 11:07:11 +0200

    chore(tokens): add tokensToCssVars utility function

---
+++ packages/orbit-components/src/OrbitProvider/__tests__/index.test.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { getTokens } from "@kiwicom/orbit-design-tokens";
+import tokensToCssVars from "@kiwicom/orbit-design-tokens/src/tokensToCssVars";
+
+import OrbitProvider from "..";
+
+const orbitTheme = {
+  palette: {
+    product: {
+      light: "#ff9999",
+      lightHover: "#ff7f7f",
+      lightActive: "#ff6666",
+      normal: "#ff0000",
+      normalHover: "#e50000",
+      normalActive: "#cc0000",
+      dark: "#990000",
+      darkHover: "#720000",
+      darkActive: "#630000",
+      darker: "#530000",
+    },
+  },
+};
+
+describe("OrbitProvider", () => {
+  const ID = "orbit-theme-css-vars";
+  it("should add css variables to the head", () => {
+    render(
+      <OrbitProvider theme={{ orbit: { ...getTokens(orbitTheme) } }} useId={React.useId}>
+        kek
+      </OrbitProvider>,
+    );
+
+    expect(document.getElementById(ID)).toBeInTheDocument();
+    expect(document.getElementById(ID)?.innerText).toBe(
+      tokensToCssVars({ tokens: getTokens(orbitTheme), cssClass: ":root" }),
+    );
+  });
+});
--- packages/orbit-design-tokens/src/index.ts
+++ packages/orbit-design-tokens/src/index.ts
@@ -7,6 +7,7 @@ export const defaultTheme = createTheme();
 export { default as fromPlainObject } from "./fromPlainObject";
 export { default as transparentColor } from "./js/transparentColor";
 export { default as getTokens } from "./getTokens";
+export { default as tokensToCssVars } from "./tokensToCssVars";
 export { default as defaultFoundation } from "./js/defaultFoundation";
 export { default as convertHexToRgba } from "./js/convertHexToRgba";
 export { default as convertRgbaToHex } from "./js/convertRgbaToHex";

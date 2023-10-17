41c5651538fcb605befbc28aefb4742bc0767452
Victor Genaev <vgenaev@gmail.com>
2023-10-17 14:58:35 +0200

    feat(tailwind): remove defaultFoundation export
    BREAKING CHANGE: defaultFoundation export is removed

--- packages/orbit-tailwind-preset/src/index.ts
+++ packages/orbit-tailwind-preset/src/index.ts
@@ -1,6 +1,3 @@
 import orbitComponentsPreset from "./presets/components";
-import defaultFoundation from "./presets/foundation/cssVarsFoundation";
-
-export { defaultFoundation };
 
 export default orbitComponentsPreset;

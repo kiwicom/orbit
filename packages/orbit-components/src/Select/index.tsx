5b731ab4382a9c21f246485510e64e54e2288cdd
Marco Vidal Garcia <marco.vidal@kiwi.com>
2023-10-25 18:04:48 +0200

    feat(Select): migrate to Tailwind
    BREAKING CHANGE: Select component no longer has the `readOnly` prop.
    The `readOnly` prop was passed directly to the native HTML select element as an
    attribute, which is not supported.
    
    > The attribute is not supported or relevant to <select> or input types that ...
    
    https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/readonly

--- packages/orbit-components/src/Select/index.tsx
+++ packages/orbit-components/src/Select/index.tsx
@@ -1,291 +1,19 @@
 "use client";
 
 import * as React from "react";
-import styled, { css } from "styled-components";
+import cx from "clsx";
 
-import type { DataAttrs } from "../common/types";
-import defaultTheme from "../defaultTheme";
 import FormLabel from "../FormLabel";
 import { FakeInput } from "../InputField";
 import ChevronDown from "../icons/ChevronDown";
 import AlertCircle from "../icons/AlertCircle";
 import InformationCircle from "../icons/InformationCircle";
-import ErrorFormTooltip from "../ErrorFormTooltip";
-import { right, left, rtlSpacing } from "../utils/rtl";
-import getSpacingToken from "../common/getSpacingToken";
 import getFieldDataState from "../common/getFieldDataState";
+import ErrorFormTooltip from "../ErrorFormTooltip";
 import useErrorTooltip from "../ErrorFormTooltip/hooks/useErrorTooltip";
-import formElementFocus from "../InputField/helpers/formElementFocus";
-import mq from "../utils/mediaQuery";
 import useRandomId from "../hooks/useRandomId";
 import type { Props } from "./types";
-
-const StyledLabel = styled.label<{ $width: string; spaceAfter?: Props["spaceAfter"] }>`
-  ${({ $width }) => css`
-    position: relative;
-    display: block;
-    width: ${$width};
-    margin-bottom: ${getSpacingToken};
-  `}
-`;
-
-StyledLabel.defaultProps = {
-  theme: defaultTheme,
-};
-
-const StyledInlineLabel = styled.div<{ hasFeedback?: boolean }>`
-  ${({ theme, hasFeedback }) => css`
-    z-index: 3;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    pointer-events: none;
-    justify-content: center;
-    padding: ${rtlSpacing(
-      `0 0 0 ${hasFeedback ? theme.orbit.spaceXXSmall : theme.orbit.spaceSmall}`,
-    )};
-
-    .orbit-form-label {
-      display: inline-block;
-      margin-bottom: 0;
-      font-size: ${theme.orbit.fontSizeInputNormal};
-      line-height: ${theme.orbit.lineHeightTextNormal};
-      text-overflow: ellipsis;
-      white-space: nowrap;
-      overflow: hidden;
-      max-width: 20ch;
-    }
-  `}
-`;
-
-StyledInlineLabel.defaultProps = {
-  theme: defaultTheme,
-};
-
-const StyledIconWrapper = styled.span`
-  display: flex;
-`;
-
-interface StyledSelectType extends Partial<Props>, DataAttrs {
-  className?: string;
-  children: React.ReactNode;
-  error?: React.ReactNode;
-  filled: boolean;
-  ariaDescribedby?: string;
-  ariaInvalid?: boolean;
-}
-
-const StyledSelect = styled(
-  React.forwardRef<HTMLSelectElement, StyledSelectType>(
-    (
-      {
-        className,
-        dataTest,
-        children,
-        value,
-        disabled,
-        name,
-        error,
-        tabIndex,
-        onChange,
-        onFocus,
-        onBlur,
-        id,
-        dataAttrs,
-        readOnly,
-        ariaDescribedby,
-        ariaInvalid,
-      },
-      ref,
-    ) => (
-      <select
-        id={id}
-        data-test={dataTest}
-        data-state={getFieldDataState(!!error)}
-        value={value}
-        className={className}
-        onChange={onChange}
-        onFocus={onFocus}
-        // @ts-expect-error TODO
-        readOnly={readOnly}
-        onBlur={onBlur}
-        disabled={disabled}
-        name={name}
-        tabIndex={tabIndex ? Number(tabIndex) : undefined}
-        ref={ref}
-        aria-describedby={ariaDescribedby}
-        aria-invalid={ariaInvalid}
-        {...dataAttrs}
-      >
-        {children}
-      </select>
-    ),
-  ),
-)`
-  ${({ theme, filled, customValueText, prefix }) => css`
-    appearance: none;
-    background: transparent;
-    cursor: pointer;
-    color: ${filled ? theme.orbit.colorTextInput : theme.orbit.colorPlaceholderInput};
-    font-family: ${theme.orbit.fontFamily};
-    font-size: ${theme.orbit.fontSizeInputNormal};
-    height: ${theme.orbit.heightInputNormal};
-    padding: ${rtlSpacing(
-      prefix
-        ? `0 ${theme.orbit.spaceXXLarge} 0 ${theme.orbit.paddingLeftSelectPrefix}`
-        : `0 ${theme.orbit.spaceXXLarge} 0 ${theme.orbit.spaceSmall}`,
-    )};
-    outline: none;
-    flex: 1 1 20%;
-    width: 100%;
-    height: 100%;
-    border: 0;
-    color: ${customValueText && "transparent !important"};
-    transition: box-shadow ${theme.orbit.durationFast} ease-in-out;
-
-    border-radius: ${theme.orbit.borderRadiusLarge};
-    ${mq.tablet(css`
-      border-radius: ${theme.orbit.borderRadiusNormal};
-    `)};
-
-    > option {
-      color: ${theme.orbit.colorTextInput};
-    }
-
-    &::-ms-expand {
-      display: none;
-    }
-
-    &::-ms-value {
-      background: transparent;
-      color: ${filled ? theme.orbit.colorTextInput : theme.orbit.colorPlaceholderInput};
-      color: ${customValueText && "rgba(255, 255, 255, 0)"};
-    }
-
-    &:focus {
-      outline: none;
-      & ~ ${FakeInput} {
-        ${formElementFocus}
-      }
-    }
-
-    &:disabled {
-      color: ${theme.orbit.colorTextInputDisabled};
-      cursor: not-allowed;
-    }
-
-    ${customValueText &&
-    `
-      &:-webkit-autofill,
-      &:-internal-autofill-selected {
-        -webkit-text-fill-color: transparent;
-      }
-    `}
-  `}
-`;
-
-StyledSelect.defaultProps = {
-  theme: defaultTheme,
-};
-
-export const SelectContainer = styled.div<{ disabled?: boolean; error?: boolean }>`
-  ${({ theme, disabled, error }) => css`
-    position: relative;
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    justify-content: space-between;
-    width: 100%;
-    box-sizing: border-box;
-    height: ${theme.orbit.heightInputNormal};
-    color: ${disabled ? theme.orbit.colorTextInputDisabled : theme.orbit.colorTextInput};
-    cursor: ${disabled ? "not-allowed" : "pointer"};
-
-    &:hover > ${FakeInput} {
-      ${!disabled &&
-      `box-shadow: inset 0 0 0 ${theme.orbit.borderWidthInput} ${
-        error ? theme.orbit.borderColorInputErrorHover : theme.orbit.borderColorInputHover
-      }`};
-    }
-
-    &:focus-within {
-      outline: none;
-
-      ${FakeInput} {
-        ${formElementFocus}
-      }
-    }
-  `}
-`;
-
-SelectContainer.defaultProps = {
-  theme: defaultTheme,
-};
-
-const SelectPrefix = styled.div`
-  ${({ theme }) => css`
-    display: flex;
-    height: 100%;
-    align-items: center;
-    position: absolute;
-    padding: 0 ${theme.orbit.spaceSmall};
-    pointer-events: none;
-    z-index: 3;
-    top: 0;
-  `};
-`;
-
-SelectPrefix.defaultProps = {
-  theme: defaultTheme,
-};
-
-const SelectSuffix = styled.div<{ disabled?: boolean }>`
-  ${({ theme, disabled }) => css`
-    position: absolute;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    top: 0;
-    ${right}: ${theme.orbit.spaceXSmall};
-    color: ${disabled ? theme.orbit.colorTextInputDisabled : theme.orbit.colorTextInput};
-    pointer-events: none;
-    z-index: 3;
-    height: 100%;
-  `};
-`;
-
-SelectSuffix.defaultProps = {
-  theme: defaultTheme,
-};
-
-const StyledCustomValue = styled.div<{ $filled?: boolean; $disabled?: boolean; $prefix?: boolean }>`
-  ${({ theme, $filled, $disabled, $prefix }) => css`
-    color: ${($disabled && theme.orbit.paletteInkLight) ||
-    ($filled ? theme.orbit.colorTextInput : theme.orbit.colorPlaceholderInput)};
-    display: flex;
-    align-items: center;
-    font-family: ${theme.orbit.fontFamily};
-    font-size: ${theme.orbit.fontSizeInputNormal};
-    z-index: 3;
-    position: absolute;
-    height: 100%;
-    top: 0;
-    ${left}: ${$prefix ? "48px" : theme.orbit.spaceSmall};
-    bottom: 0;
-    pointer-events: none;
-  `}
-`;
-
-StyledCustomValue.defaultProps = {
-  theme: defaultTheme,
-};
-
-const StyledSelectWrapper = styled.div`
-  height: 100%;
-  width: 100%;
-  position: relative;
-  z-index: 3;
-`;
+import { spaceAfterClasses } from "../common/tailwind/spaceAfter";
 
 const Select = React.forwardRef<HTMLSelectElement, Props>((props, ref) => {
   const {
@@ -312,7 +40,6 @@ const Select = React.forwardRef<HTMLSelectElement, Props>((props, ref) => {
     customValueText,
     insideInputGroup,
     dataAttrs,
-    readOnly,
   } = props;
   const filled = !(value == null || value === "");
 
@@ -336,7 +63,11 @@ const Select = React.forwardRef<HTMLSelectElement, Props>((props, ref) => {
   const shown = tooltipShown || tooltipShownHover;
 
   return (
-    <StyledLabel spaceAfter={spaceAfter} ref={inputRef} $width={width}>
+    <label
+      className={cx("relative block", spaceAfter && spaceAfterClasses[spaceAfter])}
+      ref={inputRef}
+      style={{ width }}
+    >
       {label && !inlineLabel && (
         <FormLabel
           filled={!!filled}
@@ -352,23 +83,41 @@ const Select = React.forwardRef<HTMLSelectElement, Props>((props, ref) => {
           {label}
         </FormLabel>
       )}
-      <SelectContainer
-        className="orbit-select-container"
+      <div
         ref={label ? null : labelRef}
-        disabled={disabled}
-        error={!!error}
+        className={cx(
+          "orbit-select-container",
+          "relative flex flex-row items-center justify-between",
+          "h-form-box-normal box-border w-full",
+          disabled
+            ? "text-form-element-disabled-foreground cursor-not-allowed"
+            : "text-form-element-filled-foreground cursor-pointer",
+          !disabled &&
+            (error
+              ? "[&_.orbit-input-field-fake-input]:hover:shadow-form-element-error-hover"
+              : "[&_.orbit-input-field-fake-input]:hover:shadow-form-element-hover"),
+          "focus-within:outline-none",
+          "[&_.orbit-input-field-fake-input]:focus-within:outline-blue-normal [&_.orbit-input-field-fake-input]:focus-within:outline [&_.orbit-input-field-fake-input]:focus-within:outline-2",
+        )}
       >
         {label && inlineLabel && (
-          <StyledInlineLabel hasFeedback={!!(error || help)} ref={labelRef}>
+          <div
+            className={cx(
+              "pointer-events-none z-[3] flex h-full items-center justify-center",
+              error || help ? "ps-xxs" : "ps-sm",
+              "[&_.orbit-form-label]:text-normal [&_.orbit-form-label]:mb-0 [&_.orbit-form-label]:inline-block [&_.orbit-form-label]:max-w-[20ch] [&_.orbit-form-label]:overflow-hidden [&_.orbit-form-label]:text-ellipsis [&_.orbit-form-label]:whitespace-nowrap [&_.orbit-form-label]:leading-normal",
+            )}
+            ref={labelRef}
+          >
             {help && !error && (
-              <StyledIconWrapper ref={iconRef}>
+              <span className="flex" ref={iconRef}>
                 <InformationCircle color="info" size="small" />
-              </StyledIconWrapper>
+              </span>
             )}
             {error && (
-              <StyledIconWrapper ref={iconRef}>
+              <span className="flex" ref={iconRef}>
                 <AlertCircle color="critical" size="small" />
-              </StyledIconWrapper>
+              </span>
             )}
             <FormLabel
               filled={!!value}
@@ -379,35 +128,59 @@ const Select = React.forwardRef<HTMLSelectElement, Props>((props, ref) => {
             >
               {label}
             </FormLabel>
-          </StyledInlineLabel>
+          </div>
         )}
-        <StyledSelectWrapper>
-          {prefix && <SelectPrefix>{prefix}</SelectPrefix>}
-          {customValueText && (
-            <StyledCustomValue $disabled={disabled} $filled={filled} $prefix={!!prefix}>
-              {customValueText}
-            </StyledCustomValue>
+
+        <div className="relative z-[3] h-full w-full">
+          {prefix && (
+            <div className="px-sm pointer-events-none absolute top-0 z-[3] flex h-full items-center">
+              {prefix}
+            </div>
           )}
-          <StyledSelect
-            dataTest={dataTest}
+          {customValueText && (
+            <div
+              className={cx(
+                (disabled && "text-form-element-disabled-foreground") || filled
+                  ? "text-form-element-filled-foreground"
+                  : "text-form-element-foreground",
+                "text-form-element-normal font-base pointer-events-none absolute bottom-0 top-0 z-[3] flex items-center",
+                prefix ? "ps-[48px]" : "ps-sm",
+              )}
+            >
+              {customValueText}
+            </div>
+          )}
+          <select
+            className={cx(
+              "cursor-pointer appearance-none bg-transparent outline-none",
+              filled ? "text-form-element-filled-foreground" : "text-form-element-foreground",
+              "font-base text-form-element-normal",
+              "pe-xxl",
+              prefix ? "ps-[48px]" : "ps-sm",
+              "shrink grow basis-1/5",
+              "h-full w-full",
+              "border-0",
+              customValueText && "!text-transparent",
+              "duration-fast transition-shadow ease-in-out",
+              "rounded-large tb:rounded-normal",
+              "[&>option]:text-form-element-filled-foreground",
+              "disabled:text-form-element-disabled-foreground disabled:cursor-not-allowed",
+            )}
+            data-test={dataTest}
+            data-state={getFieldDataState(!!error)}
             disabled={disabled}
-            error={error}
             value={value == null ? "" : value}
-            prefix={prefix}
             name={name}
             onFocus={handleFocus}
             onBlur={onBlur}
             onChange={onChange}
-            filled={filled}
-            customValueText={customValueText}
             tabIndex={tabIndex ? Number(tabIndex) : undefined}
             id={selectId}
-            readOnly={readOnly}
             required={required}
             ref={ref}
-            dataAttrs={dataAttrs}
-            ariaDescribedby={shown ? `${selectId}-feedback` : undefined}
-            ariaInvalid={error ? true : undefined}
+            aria-describedby={shown ? `${selectId}-feedback` : undefined}
+            aria-invalid={error ? true : undefined}
+            {...dataAttrs}
           >
             {placeholder && (
               <option label={placeholder.toString()} value="">
@@ -423,14 +196,20 @@ const Select = React.forwardRef<HTMLSelectElement, Props>((props, ref) => {
                 {option.label}
               </option>
             ))}
-          </StyledSelect>
-        </StyledSelectWrapper>
-
-        <SelectSuffix disabled={disabled}>
+          </select>
+        </div>
+        <div
+          className={cx(
+            "end-xs pointer-events-none absolute top-0 z-[3] flex h-full items-center justify-center",
+            disabled
+              ? "text-form-element-disabled-foreground"
+              : "text-form-element-filled-foreground",
+          )}
+        >
           <ChevronDown color="secondary" />
-        </SelectSuffix>
+        </div>
         <FakeInput disabled={disabled} error={error} />
-      </SelectContainer>
+      </div>
       {!insideInputGroup && hasTooltip && (
         <ErrorFormTooltip
           id={`${selectId}-feedback`}
@@ -443,7 +222,7 @@ const Select = React.forwardRef<HTMLSelectElement, Props>((props, ref) => {
           referenceElement={inlineLabel ? iconRef : inputRef}
         />
       )}
-    </StyledLabel>
+    </label>
   );
 });
 

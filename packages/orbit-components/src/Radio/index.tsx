6f66d543e16b0f2f8e1545222d3b278f2be051f2
Marco Vidal Garcia <marco.vidal@kiwi.com>
2023-10-25 15:55:41 +0200

    feat(Radio): migrate to Tailwind

--- packages/orbit-components/src/Radio/index.tsx
+++ packages/orbit-components/src/Radio/index.tsx
@@ -1,207 +1,11 @@
 "use client";
 
-import * as React from "react";
-import styled, { css } from "styled-components";
+import cx from "clsx";
+import React from "react";
 
-import defaultTheme from "../defaultTheme";
-import { rtlSpacing } from "../utils/rtl";
-import getFieldDataState from "../common/getFieldDataState";
 import cloneWithTooltip from "../utils/cloneWithTooltip";
-import media from "../utils/mediaQuery";
-import { defaultFocus } from "../utils/common";
+import getFieldDataState from "../common/getFieldDataState";
 import type { Props } from "./types";
-
-const getBorderColor =
-  () =>
-  ({
-    theme,
-    hasError,
-    disabled,
-    checked,
-  }: {
-    theme: typeof defaultTheme;
-    hasError?: boolean;
-    disabled?: boolean;
-    checked?: boolean;
-  }) => {
-    if (disabled) return theme.orbit.paletteCloudNormal;
-    if (checked) return theme.orbit.paletteBlueNormal;
-    if (hasError && !disabled && !checked) return theme.orbit.borderColorCheckboxRadioError;
-
-    return theme.orbit.borderColorCheckboxRadio;
-  };
-
-const getBackground =
-  () =>
-  ({
-    theme,
-    disabled,
-    checked,
-  }: {
-    theme: typeof defaultTheme;
-    disabled?: boolean;
-    checked?: boolean;
-  }) => {
-    if (disabled) return theme.orbit.paletteCloudNormal;
-
-    return checked ? theme.orbit.paletteBlueNormal : theme.orbit.backgroundInput;
-  };
-
-const getGlyphBackground = ({
-  theme,
-  checked,
-  disabled,
-}: {
-  theme: typeof defaultTheme;
-  checked: Props["checked"];
-  disabled: Props["disabled"];
-}) => {
-  if (disabled && checked) return theme.orbit.paletteCloudLight;
-  if (disabled) return theme.orbit.paletteCloudNormal;
-  return theme.orbit.paletteWhite;
-};
-
-const Glyph = styled.span<{ disabled?: boolean; checked?: boolean }>`
-  ${({ theme, checked, disabled }) => css`
-    visibility: hidden;
-    width: 8px;
-    height: 8px;
-    background-color: ${getGlyphBackground({ theme, disabled, checked })};
-    border-radius: ${theme.orbit.borderRadiusCircle};
-    flex-shrink: 0;
-  `};
-`;
-
-Glyph.defaultProps = {
-  theme: defaultTheme,
-};
-
-const StyledIconContainer = styled.div<{ disabled?: boolean; checked?: boolean }>`
-  ${({ theme }) => css`
-    position: relative;
-    box-sizing: border-box;
-    flex: 0 0 auto;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: ${getBackground};
-    height: ${theme.orbit.heightCheckbox};
-    width: ${theme.orbit.widthCheckbox};
-    border-radius: ${theme.orbit.borderRadiusCircle};
-    transform: scale(1);
-    transition: all ${theme.orbit.durationFast} ease-in-out;
-  `};
-`;
-
-StyledIconContainer.defaultProps = {
-  theme: defaultTheme,
-};
-
-const StyledTextContainer = styled.div<{ disabled?: boolean }>`
-  ${({ theme, disabled }) => css`
-    display: flex;
-    flex-direction: column;
-    font-weight: ${theme.orbit.fontWeightMedium};
-    margin: ${rtlSpacing(`0 0 0 ${theme.orbit.spaceXSmall}`)};
-    flex: 1;
-    opacity: ${disabled ? theme.orbit.opacityRadioButtonDisabled : "1"};
-  `};
-`;
-
-StyledTextContainer.defaultProps = {
-  theme: defaultTheme,
-};
-
-const StyledInfo = styled.span`
-  ${({ theme }) => css`
-    font-size: ${theme.orbit.fontSizeFormFeedback};
-    color: ${theme.orbit.colorInfoCheckBoxRadio};
-    line-height: ${theme.orbit.lineHeightTextSmall};
-  `};
-`;
-
-StyledInfo.defaultProps = {
-  theme: defaultTheme,
-};
-
-const StyledLabelText = styled.span`
-  ${({ theme }) => css`
-    font-weight: ${theme.orbit.fontWeightMedium};
-    font-size: ${theme.orbit.fontSizeFormLabel};
-    color: ${theme.orbit.colorFormLabel};
-    line-height: ${theme.orbit.heightCheckbox};
-
-    .orbit-text {
-      font-weight: ${theme.orbit.fontWeightMedium};
-      font-size: ${theme.orbit.fontSizeFormLabel};
-      color: ${theme.orbit.colorFormLabel};
-      line-height: ${theme.orbit.heightCheckbox};
-    }
-  `};
-`;
-
-StyledLabelText.defaultProps = {
-  theme: defaultTheme,
-};
-
-const StyledInput = styled.input`
-  position: absolute;
-  opacity: 0;
-
-  &:checked + ${StyledIconContainer} > ${Glyph} {
-    visibility: visible;
-  }
-
-  &:focus + ${StyledIconContainer} {
-    ${defaultFocus};
-
-    ${media.largeMobile(css`
-      border-width: 1px;
-    `)}
-  }
-`;
-
-StyledInput.defaultProps = {
-  theme: defaultTheme,
-};
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const StyledLabel = styled(({ disabled, theme, type, hasError, ...props }) => (
-  <label {...props}>{props.children}</label>
-))`
-  ${({ theme, disabled }) => css`
-    font-family: ${theme.orbit.fontFamily};
-    display: flex;
-    width: 100%;
-    flex-direction: row;
-    align-items: self-start;
-    cursor: ${disabled ? "not-allowed" : "pointer"};
-    position: relative;
-
-    ${StyledIconContainer} {
-      border: 2px solid ${getBorderColor};
-    }
-
-    &:hover ${StyledIconContainer} {
-      border-color: ${!disabled && theme.orbit.paletteBlueLightActive};
-    }
-
-    &:active ${StyledIconContainer} {
-      border-color: ${disabled ? getBorderColor : theme.orbit.paletteBlueNormal};
-      transform: ${!disabled && `scale(${theme.orbit.modifierScaleCheckboxRadioActive})`};
-    }
-
-    ${media.largeMobile(css`
-      ${StyledIconContainer} {
-        border: 1px solid ${getBorderColor};
-      }
-    `)}
-  `};
-`;
-
-StyledLabel.defaultProps = {
-  theme: defaultTheme,
-};
 
 const Radio = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
   const {
@@ -215,15 +19,32 @@ const Radio = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
     info,
     readOnly,
     id,
-    tabIndex,
+    tabIndex = 0,
     dataTest,
     tooltip,
   } = props;
+
   return (
-    <StyledLabel disabled={disabled} hasError={hasError} checked={checked}>
-      <StyledInput
+    <label
+      htmlFor={id}
+      className={cx(
+        "font-base items-self-start text-form-element-label-foreground relative flex w-full",
+        disabled ? "cursor-not-allowed" : "cursor-pointer",
+        !disabled && [
+          !checked && "bg-form-element-background",
+          !checked &&
+            (hasError
+              ? "[&>.orbit-radio-icon-container]:border-form-element-error [&>.orbit-radio-icon-container]:hover:border-form-element-error-hover [&>.orbit-radio-icon-container]:active:border-form-element-error "
+              : "[&>.orbit-radio-icon-container]:border-form-element [&>.orbit-radio-icon-container]:hover:border-form-element-hover [&>.orbit-radio-icon-container]:active:border-form-element-active"),
+          checked &&
+            "[&>.orbit-radio-icon-container]:border-form-element-focus [&>.orbit-radio-icon-container]:bg-blue-normal active:border-form-element-focus",
+        ],
+      )}
+    >
+      <input
         data-test={dataTest}
         data-state={getFieldDataState(hasError)}
+        className="peer absolute opacity-0"
         value={value}
         type="radio"
         disabled={disabled}
@@ -231,23 +52,54 @@ const Radio = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
         id={id}
         onChange={onChange}
         name={name}
-        tabIndex={tabIndex ? Number(tabIndex) : undefined}
+        tabIndex={Number(tabIndex)}
         ref={ref}
         readOnly={readOnly}
       />
       {cloneWithTooltip(
         tooltip,
-        <StyledIconContainer disabled={disabled} checked={checked}>
-          <Glyph disabled={disabled} checked={checked} />
-        </StyledIconContainer>,
+        <div
+          className={cx(
+            "orbit-radio-icon-container",
+            "relative box-border",
+            "flex flex-none items-center justify-center",
+            "rounded-circle h-icon-medium w-icon-medium",
+            "duration-fast scale-100 transition-all ease-in-out",
+            "lm:border border-[2px] border-solid",
+            "active:scale-95",
+            "peer-focus:outline-blue-normal peer-focus:outline peer-focus:outline-2",
+            disabled && "bg-form-element-disabled-background border-transparent",
+          )}
+        >
+          <span
+            className={cx(
+              "h-xs w-xs rounded-circle",
+              disabled ? "bg-cloud-light" : "bg-form-element-background",
+              checked ? "visible" : "invisible",
+            )}
+          />
+        </div>,
       )}
       {(label || info) && (
-        <StyledTextContainer disabled={disabled}>
-          {label && <StyledLabelText>{label}</StyledLabelText>}
-          {info && <StyledInfo>{info}</StyledInfo>}
-        </StyledTextContainer>
+        <div
+          className={cx(
+            "ms-xs flex flex-1 flex-col font-medium",
+            disabled ? "opacity-50" : "opacity-1",
+          )}
+        >
+          {label && (
+            <span className="text-normal text-primary-foreground [&_.orbit-text]:text-normal font-medium leading-normal [&_.orbit-text]:font-medium [&_.orbit-text]:leading-normal">
+              {label}
+            </span>
+          )}
+          {info && (
+            <span className="text-small leading-small text-form-element-label-filled-foreground">
+              {info}
+            </span>
+          )}
+        </div>
       )}
-    </StyledLabel>
+    </label>
   );
 });
 

ad518aad111c91cf1c136a6856749c0e84a0f49d
oreqizer <oreqizer@gmail.com>
2023-10-26 14:16:02 +0200

    chore(TileGroup): adjust colors and hover states

--- packages/orbit-components/src/TileGroup/index.tsx
+++ packages/orbit-components/src/TileGroup/index.tsx
@@ -12,8 +12,8 @@ const TileGroup = ({ children, dataTest, id, as }: Props) => {
     <Component
       className={cx(
         "shadow-action rounded-normal [&_.orbit-slide]:bg-white-normal m-0 w-full p-0",
-        "[&_.orbit-tile-wrapper]:first:rounded-t-normal [&_.orbit-tile-wrapper]:last:rounded-b-normal [&_.orbit-tile-wrapper]:not-last:border-b [&_.orbit-tile-wrapper]:not-last:border-cloud-normal",
-        "[&_.orbit-tile-wrapper]:duration-fast [&_.orbit-tile-wrapper]:hover:bg-white-normal [&_.orbit-tile-wrapper]:focus:bg-white-normal [&_.orbit-tile-wrapper]:rounded-none [&_.orbit-tile-wrapper]:shadow-none [&_.orbit-tile-wrapper]:transition-colors [&_.orbit-tile-wrapper]:ease-in-out",
+        "first:[&_.orbit-tile-wrapper]:rounded-t-normal last:[&_.orbit-tile-wrapper]:rounded-b-normal not-last:[&_.orbit-tile-wrapper]:border-b not-last:[&_.orbit-tile-wrapper]:border-cloud-normal",
+        "[&_.orbit-tile-wrapper]:duration-fast hover:[&_.orbit-tile-wrapper]:bg-cloud-normal focus:[&_.orbit-tile-wrapper]:bg-cloud-normal [&_.orbit-tile-wrapper]:rounded-none [&_.orbit-tile-wrapper]:shadow-none [&_.orbit-tile-wrapper]:transition-colors [&_.orbit-tile-wrapper]:ease-in-out",
       )}
       data-test={dataTest}
       id={id}

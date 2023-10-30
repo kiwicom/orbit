23474b3156e22a17aec21317aa1185beef0b3757
Victor Genaev <vgenaev@gmail.com>
2023-10-30 14:56:36 +0100

    feat(Separator): migrate to Tailwind
    BREAKING CHANGE:
    'color' prop now accepts Tailwind classname instead of a token,
    e.g. 'paletteBlueNormal' -> 'border-blue-normal'
    
    'indent' prop renamed to 'sideOffset'
    
    'type' prop now by default is 'solid', instead of 'none'

--- packages/orbit-components/src/Separator/Separator.stories.tsx
+++ packages/orbit-components/src/Separator/Separator.stories.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
-import { select, text } from "@storybook/addon-knobs";
+import { select } from "@storybook/addon-knobs";
 
 import SPACINGS_AFTER from "../common/getSpacingToken/consts";
 import RenderInRtl from "../utils/rtl/RenderInRtl";
-import type { PaletteTokens, Indent } from "./types";
+import type { BorderColorClass, SideOffset } from "./types";
+import defaultTheme from "../defaultTheme";
 
 import Separator from ".";
 
@@ -12,6 +13,8 @@ export default {
 };
 
 export const Default = () => <Separator />;
+
+const kebabCase = (str: string) => str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
 
 Default.story = {
   parameters: {
@@ -22,12 +25,25 @@ Default.story = {
 export const Playground = () => {
   const spaceAfter = select("spaceAfter", Object.values(SPACINGS_AFTER), SPACINGS_AFTER.LARGEST);
   const align = select("align", ["left", "right", "center"], "left");
-  const indentOptions: Indent[] = ["none", "small", "medium", "large", "XLarge", "XXLarge"];
-  const type = select("type", ["solid", "dashed", "dotted", "double", "none"], "none");
-  const color = text("color", "paletteBlueNormal") as PaletteTokens;
-  const indent = select("indent", indentOptions, "none");
+  const sideOffsetOptions: SideOffset[] = ["none", "small", "medium", "large", "XLarge", "XXLarge"];
+  const type = select("type", ["solid", "dashed", "dotted", "double", "none"], "solid");
+  const color = select(
+    "color",
+    Object.keys(defaultTheme.orbit)
+      .filter(t => t.startsWith("palette"))
+      .map(c => kebabCase(c.replace("palette", "border"))),
+    "",
+  ) as BorderColorClass;
+
+  const sideOffset = select("sideOffset", sideOffsetOptions, "none");
   return (
-    <Separator color={color} align={align} indent={indent} spaceAfter={spaceAfter} type={type} />
+    <Separator
+      color={color}
+      align={align}
+      sideOffset={sideOffset}
+      spaceAfter={spaceAfter}
+      type={type}
+    />
   );
 };
 
@@ -50,16 +66,16 @@ export const VisualTest = () => {
       <Separator type="dotted" />
       <Separator type="dashed" />
       <Separator type="double" />
-      <Separator color="paletteRedNormal" type="solid" />
-      <Separator color="paletteBlueNormal" type="solid" />
-      <Separator color="paletteProductNormal" indent="small" type="solid" />
-      <Separator color="paletteProductNormal" indent="medium" type="solid" />
-      <Separator color="paletteProductNormal" indent="large" type="solid" />
-      <Separator color="paletteProductNormal" indent="XLarge" type="solid" />
-      <Separator color="paletteProductNormal" indent="XXLarge" type="solid" />
-      <Separator align="left" indent="large" color="paletteBundleBasic" type="solid" />
-      <Separator align="right" indent="large" color="paletteBundleBasic" type="solid" />
-      <Separator align="center" indent="XXLarge" color="paletteBundleBasic" type="solid" />
+      <Separator color="border-red-normal" type="solid" />
+      <Separator color="border-blue-normal" type="solid" />
+      <Separator color="border-product-normal" sideOffset="small" type="solid" />
+      <Separator color="border-product-normal" sideOffset="medium" type="solid" />
+      <Separator color="border-product-normal" sideOffset="large" type="solid" />
+      <Separator color="border-product-normal" sideOffset="XLarge" type="solid" />
+      <Separator color="border-product-normal" sideOffset="XXLarge" type="solid" />
+      <Separator align="left" sideOffset="large" color="border-bundle-basic" type="solid" />
+      <Separator align="right" sideOffset="large" color="border-bundle-basic" type="solid" />
+      <Separator align="center" sideOffset="XXLarge" color="border-bundle-basic" type="solid" />
       <Separator spaceAfter="smallest" />
       <Separator spaceAfter="small" />
       <Separator spaceAfter="medium" />

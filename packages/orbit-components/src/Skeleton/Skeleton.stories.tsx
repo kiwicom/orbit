50338cd5e8e2fa26ee56acf01d1d9721f2e5ded7
Marco Vidal Garcia <marco.vidal@kiwi.com>
2023-10-31 10:22:42 +0100

    chore(Skeleton): use more meaningful rtl story

--- packages/orbit-components/src/Skeleton/Skeleton.stories.tsx
+++ packages/orbit-components/src/Skeleton/Skeleton.stories.tsx
@@ -67,7 +67,14 @@ export const Playground = () => {
 
 export const RTL = () => (
   <RenderInRtl>
-    <Skeleton rows={5} rowOffset={30} />
+    <Skeleton height="100px" width="500px" viewBox="0 0 500 100">
+      <rect x="48" y="8" rx="3" ry="3" width="88" height="6" />
+      <rect x="48" y="26" rx="3" ry="3" width="52" height="6" />
+      <rect x="0" y="56" rx="3" ry="3" width="410" height="6" />
+      <rect x="0" y="72" rx="3" ry="3" width="380" height="6" />
+      <rect x="0" y="88" rx="3" ry="3" width="178" height="6" />
+      <circle cx="20" cy="20" r="20" />
+    </Skeleton>
   </RenderInRtl>
 );
 

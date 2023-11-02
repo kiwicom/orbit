0663ffb65083a7888dc9ca4a9b52dbb9198872d7
oreqizer <oreqizer@gmail.com>
2023-11-02 12:33:56 +0100

    feat(Table): migrate TableBody to Tailwind

--- packages/orbit-components/src/Table/TableBody/index.tsx
+++ packages/orbit-components/src/Table/TableBody/index.tsx
@@ -1,27 +1,11 @@
-"use client";
-
 import * as React from "react";
-import styled from "styled-components";
 
 import type * as Common from "../../common/types";
-import defaultTheme from "../../defaultTheme";
 
-export const StyledTableBody = styled(({ children, className, dataTest }) => (
-  <tbody className={className} data-test={dataTest}>
+const TableBody = ({ children, dataTest }: React.PropsWithChildren<Common.Globals>) => (
+  <tbody className="border-b-cloud-normal w-full whitespace-nowrap border-b" data-test={dataTest}>
     {children}
   </tbody>
-))`
-  width: 100%;
-  white-space: nowrap;
-  border-bottom: 1px solid ${({ theme }) => theme.orbit.borderColorTable};
-`;
-
-StyledTableBody.defaultProps = {
-  theme: defaultTheme,
-};
-
-const TableBody: React.FC<React.PropsWithChildren<Common.Globals>> = ({ children, dataTest }) => (
-  <StyledTableBody dataTest={dataTest}>{children}</StyledTableBody>
 );
 
 export default TableBody;

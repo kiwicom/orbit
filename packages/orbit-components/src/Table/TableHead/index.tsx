ef0a54e2ce7641497a2e2c5ab5a58926a32ebe8f
oreqizer <oreqizer@gmail.com>
2023-11-02 16:02:12 +0100

    feat(Table): migrate TableHead to Tailwind

--- packages/orbit-components/src/Table/TableHead/index.tsx
+++ packages/orbit-components/src/Table/TableHead/index.tsx
@@ -1,31 +1,14 @@
-"use client";
-
 import * as React from "react";
-import styled from "styled-components";
 
 import type * as Common from "../../common/types";
-import defaultTheme from "../../defaultTheme";
-import { StyledTableCell } from "../TableCell";
 
-export const StyledTableHead = styled(({ children, className, dataTest }) => (
-  <thead className={className} data-test={dataTest}>
+const TableHead = ({ children, dataTest }: React.PropsWithChildren<Common.Globals>) => (
+  <thead
+    className="border-b-cloud-dark w-full whitespace-nowrap border-b font-bold"
+    data-test={dataTest}
+  >
     {children}
   </thead>
-))`
-  border-bottom: 1px solid ${({ theme }) => theme.orbit.borderColorTableHead};
-  width: 100%;
-  white-space: nowrap;
-  ${StyledTableCell} {
-    font-weight: ${({ theme }) => theme.orbit.fontWeightTableHead};
-  }
-`;
-
-StyledTableHead.defaultProps = {
-  theme: defaultTheme,
-};
-
-const TableHead: React.FC<React.PropsWithChildren<Common.Globals>> = ({ children, dataTest }) => (
-  <StyledTableHead dataTest={dataTest}>{children}</StyledTableHead>
 );
 
 export default TableHead;

1eb242c7e7e9cf93f8a0368d94ac4095d20322e7
oreqizer <oreqizer@gmail.com>
2023-11-02 16:04:08 +0100

    feat(Table): migrate TableRow to Tailwind

--- packages/orbit-components/src/Table/TableRow/index.tsx
+++ packages/orbit-components/src/Table/TableRow/index.tsx
@@ -1,22 +1,11 @@
-"use client";
-
 import * as React from "react";
-import styled from "styled-components";
 
 import type * as Common from "../../common/types";
 
-export const StyledTableRow = styled(({ children, className, dataTest }) => (
-  <tr className={className} data-test={dataTest}>
+const TableRow = ({ dataTest, children }: React.PropsWithChildren<Common.Globals>) => (
+  <tr className="box-border w-full whitespace-nowrap" data-test={dataTest}>
     {children}
   </tr>
-))`
-  box-sizing: border-box;
-  width: 100%;
-  white-space: nowrap;
-`;
-
-const TableRow: React.FC<React.PropsWithChildren<Common.Globals>> = ({ dataTest, children }) => (
-  <StyledTableRow dataTest={dataTest}>{children}</StyledTableRow>
 );
 
 export default TableRow;

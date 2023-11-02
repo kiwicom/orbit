c81929281fdd3221dc32d8c9788fd41a825aad17
oreqizer <oreqizer@gmail.com>
2023-11-02 15:55:20 +0100

    feat(Table): migrate TableFooter to Tailwind

--- packages/orbit-components/src/Table/TableFooter/index.tsx
+++ packages/orbit-components/src/Table/TableFooter/index.tsx
@@ -1,28 +1,11 @@
-"use client";
-
 import * as React from "react";
-import styled from "styled-components";
 
 import type * as Common from "../../common/types";
-import defaultTheme from "../../defaultTheme";
 
-export const StyledTableFooter = styled(({ children, className, dataTest }) => (
-  <tfoot className={className} data-test={dataTest}>
+const TableFooter = ({ children, dataTest }: React.PropsWithChildren<Common.Globals>) => (
+  <tfoot className="bg-cloud-normal w-full whitespace-nowrap font-bold" data-test={dataTest}>
     {children}
   </tfoot>
-))`
-  width: 100%;
-  white-space: nowrap;
-  background: ${({ theme }) => theme.orbit.paletteCloudNormal};
-  font-weight: bold;
-`;
-
-StyledTableFooter.defaultProps = {
-  theme: defaultTheme,
-};
-
-const TableFooter: React.FC<React.PropsWithChildren<Common.Globals>> = ({ children, dataTest }) => (
-  <StyledTableFooter dataTest={dataTest}>{children}</StyledTableFooter>
 );
 
 export default TableFooter;

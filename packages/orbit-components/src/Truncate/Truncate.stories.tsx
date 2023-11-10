dffb82fd967ece1766d7a660a93cd59e57b6fef2
Marco Vidal Garcia <marco.vidal@kiwi.com>
2023-11-10 12:56:16 +0100

    test(Truncate): add visual tests

--- packages/orbit-components/src/Truncate/Truncate.stories.tsx
+++ packages/orbit-components/src/Truncate/Truncate.stories.tsx
@@ -95,3 +95,33 @@ Rtl.story = {
     info: "This is a preview of this component in RTL setup.",
   },
 };
+
+export const VisualTest = () => {
+  const content =
+    "Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Nulla pulvinar eleifend sem. Sed ac dolor sit amet purus malesuada congue. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam.";
+
+  return (
+    <div className="space-y-sm">
+      <Truncate maxWidth="20%">
+        <Text>{content}</Text>
+      </Truncate>
+      <Stack direction="row">
+        <Truncate maxWidth="20%">
+          <Text>{content}</Text>
+        </Truncate>
+        <Truncate maxWidth="50%">
+          <Text>{content}</Text>
+        </Truncate>
+        <Truncate>
+          <Text>{content}</Text>
+        </Truncate>
+      </Stack>
+    </div>
+  );
+};
+
+VisualTest.story = {
+  parameters: {
+    chromatic: { disableSnapshot: false },
+  },
+};

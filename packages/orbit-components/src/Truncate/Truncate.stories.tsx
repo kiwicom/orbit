e2bb6cb591f0b910b129dc4758ac9c68451d3940
Marco Vidal Garcia <marco.vidal@kiwi.com>
2023-11-10 12:56:16 +0100

    test(Truncate): add visual tests

--- packages/orbit-components/src/Truncate/Truncate.stories.tsx
+++ packages/orbit-components/src/Truncate/Truncate.stories.tsx
@@ -4,6 +4,7 @@ import { text } from "@storybook/addon-knobs";
 import RenderInRtl from "../utils/rtl/RenderInRtl";
 import Stack from "../Stack";
 import Text from "../Text";
+import Heading from "../Heading";
 
 import Truncate from ".";
 
@@ -93,5 +94,38 @@ Rtl.story = {
 
   parameters: {
     info: "This is a preview of this component in RTL setup.",
+  },
+};
+
+export const VisualTest = () => {
+  const content =
+    "Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Nulla pulvinar eleifend sem. Sed ac dolor sit amet purus malesuada congue. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam.";
+
+  return (
+    <div className="space-y-sm">
+      <Truncate maxWidth="20%">
+        <Text>{content}</Text>
+      </Truncate>
+      <Stack direction="row">
+        <Truncate maxWidth="20%">
+          <Text>{content}</Text>
+        </Truncate>
+        <Truncate maxWidth="50%">
+          <Text>{content}</Text>
+        </Truncate>
+        <Truncate>
+          <Text>{content}</Text>
+        </Truncate>
+      </Stack>
+      <Truncate maxWidth="60%">
+        <Heading>This is a Heading component inside a Truncate</Heading>
+      </Truncate>
+    </div>
+  );
+};
+
+VisualTest.story = {
+  parameters: {
+    chromatic: { disableSnapshot: false },
   },
 };

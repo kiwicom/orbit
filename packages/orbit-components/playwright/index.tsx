e76dcf24845837a51470b5ff7c3986b246316864
oreqizer <oreqizer@gmail.com>
2023-11-21 12:03:54 +0100

    test: add playwright random ID wrapper

--- packages/orbit-components/playwright/index.tsx
+++ packages/orbit-components/playwright/index.tsx
@@ -1,1 +1,14 @@
 import "./index.css";
+
+import * as React from "react";
+import { beforeMount } from "@playwright/experimental-ct-react/hooks";
+
+import RandomIdProvider from "../src/OrbitProvider/RandomId/Provider";
+
+beforeMount(async ({ App }) => {
+  return (
+    <RandomIdProvider useId={React.useId}>
+      <App />
+    </RandomIdProvider>
+  );
+});

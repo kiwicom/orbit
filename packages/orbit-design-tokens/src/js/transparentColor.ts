3ee9dfa881f98755fde6fe0d735d574013448d9f
Victor Genaev <vgenaev@gmail.com>
2023-11-23 16:30:17 +0100

    chore(tokens): update transparent color function

--- packages/orbit-design-tokens/src/js/transparentColor.ts
+++ packages/orbit-design-tokens/src/js/transparentColor.ts
@@ -1,11 +1,16 @@
 import { parseToRgba, rgba, guard } from "color2k";
 
+const isHex = (color: string): boolean => color.startsWith("#");
+const isRgb = (color: string): boolean => color.startsWith("rgb");
+
 const transparentColor = (color: string, opacity: number): string => {
   // tailwind package is using css variables
   if (color.match(/var\(/)) {
     const regex = /var\(([^,]+),\s*([^)]+)\)/;
     const [, , colorValue] = color.match(regex);
-    return transparentColor(colorValue, opacity);
+    const value = colorValue as string;
+    const isValidColor = isHex(value) || isRgb(value);
+    return transparentColor(isValidColor ? value : `rgb(${value})`, opacity);
   }
 
   const [R, G, B] = parseToRgba(color);

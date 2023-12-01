6d6c357f3dd70458f47a84f814c86acdae70ad1f
Victor Genaev <vgenaev@gmail.com>
2023-12-01 14:09:52 +0100

    test(Stepper): add visual test

---
+++ packages/orbit-components/src/Stepper/Stepper.ct-story.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import type { Props } from "./types";
+
+import Stepper from ".";
+
+export default function StepperStory(props: Props) {
+  return <Stepper {...props} />;
+}
---
+++ packages/orbit-components/src/Stepper/Stepper.ct.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { test, expect } from "@playwright/experimental-ct-react";
+
+import StepperStory from "./Stepper.ct-story";
+
+test.describe("visual Stepper", () => {
+  test("default Stepper", async ({ mount }) => {
+    const component = await mount(
+      <StepperStory titleIncrement="increment" titleDecrement="decrement" />,
+    );
+
+    await expect(component).toHaveScreenshot();
+  });
+
+  test("Stepper disabled", async ({ mount }) => {
+    const component = await mount(
+      <StepperStory titleIncrement="increment" titleDecrement="decrement" disabled />,
+    );
+
+    await expect(component).toHaveScreenshot();
+  });
+
+  test("Stepper disabled increment", async ({ mount }) => {
+    const component = await mount(
+      <StepperStory
+        titleIncrement="increment"
+        titleDecrement="decrement"
+        maxValue={10}
+        defaultValue={10}
+        minValue={0}
+      />,
+    );
+
+    await expect(component).toHaveScreenshot();
+  });
+
+  test("Stepper disabled decrement", async ({ mount }) => {
+    const component = await mount(
+      <StepperStory
+        titleIncrement="increment"
+        titleDecrement="decrement"
+        maxValue={10}
+        defaultValue={0}
+        minValue={0}
+      />,
+    );
+
+    await expect(component).toHaveScreenshot();
+  });
+});

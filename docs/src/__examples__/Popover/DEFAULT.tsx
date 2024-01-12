a9598970cb6b0963b86c4762dda8f706218ef2ec
Daniel Sil <daniel.sil@kiwi.com>
2024-01-12 17:06:12 +0100

    docs: fix Popover example

--- docs/src/__examples__/Popover/DEFAULT.tsx
+++ docs/src/__examples__/Popover/DEFAULT.tsx
@@ -1,34 +1,43 @@
 import React from "react";
-import { Popover, Button, ButtonLink, Stack } from "@kiwicom/orbit-components";
+import {
+  Popover,
+  Button,
+  ButtonLink,
+  Stack,
+  OrbitProvider,
+  defaultTheme,
+} from "@kiwicom/orbit-components";
 import { QuestionCircle } from "@kiwicom/orbit-components/icons";
 
 export default {
   Example: () => (
-    <Popover
-      renderInPortal={false}
-      content={
-        <Stack spacing="small">
-          <ButtonLink
-            external
-            type="secondary"
-            fullWidth
-            href="https://orbit.kiwi/components/popover/react/"
-          >
-            Reference
-          </ButtonLink>
-          <ButtonLink
-            external
-            type="secondary"
-            fullWidth
-            href="https://orbit.kiwi/components/popover/"
-          >
-            Guidelines
-          </ButtonLink>
-        </Stack>
-      }
-    >
-      <Button circled title="Help" iconLeft={<QuestionCircle />} />
-    </Popover>
+    <OrbitProvider theme={defaultTheme} useId={React.useId}>
+      <Popover
+        renderInPortal={false}
+        content={
+          <Stack spacing="small">
+            <ButtonLink
+              external
+              type="secondary"
+              fullWidth
+              href="https://orbit.kiwi/components/popover/react/"
+            >
+              Reference
+            </ButtonLink>
+            <ButtonLink
+              external
+              type="secondary"
+              fullWidth
+              href="https://orbit.kiwi/components/popover/"
+            >
+              Guidelines
+            </ButtonLink>
+          </Stack>
+        }
+      >
+        <Button circled title="Help" iconLeft={<QuestionCircle />} />
+      </Popover>
+    </OrbitProvider>
   ),
   exampleKnobs: [
     {
--- docs/src/__examples__/Popover/TRIGGER.tsx
+++ docs/src/__examples__/Popover/TRIGGER.tsx
@@ -1,35 +1,44 @@
 import React from "react";
-import { Popover, Button, ButtonLink, Stack } from "@kiwicom/orbit-components";
+import {
+  Popover,
+  Button,
+  ButtonLink,
+  Stack,
+  OrbitProvider,
+  defaultTheme,
+} from "@kiwicom/orbit-components";
 import { ChevronDown } from "@kiwicom/orbit-components/icons";
 
 export default {
   Example: () => (
-    <Popover
-      renderInPortal={false}
-      content={
-        <Stack spacing="small">
-          <ButtonLink
-            external
-            type="secondary"
-            fullWidth
-            href="https://orbit.kiwi/components/popover/react/"
-          >
-            Reference
-          </ButtonLink>
-          <ButtonLink
-            external
-            type="secondary"
-            fullWidth
-            href="https://orbit.kiwi/components/popover/"
-          >
-            Guidelines
-          </ButtonLink>
-        </Stack>
-      }
-    >
-      <Button iconRight={<ChevronDown />} type="secondary">
-        Learn more
-      </Button>
-    </Popover>
+    <OrbitProvider theme={defaultTheme} useId={React.useId}>
+      <Popover
+        renderInPortal={false}
+        content={
+          <Stack spacing="small">
+            <ButtonLink
+              external
+              type="secondary"
+              fullWidth
+              href="https://orbit.kiwi/components/popover/react/"
+            >
+              Reference
+            </ButtonLink>
+            <ButtonLink
+              external
+              type="secondary"
+              fullWidth
+              href="https://orbit.kiwi/components/popover/"
+            >
+              Guidelines
+            </ButtonLink>
+          </Stack>
+        }
+      >
+        <Button iconRight={<ChevronDown />} type="secondary">
+          Learn more
+        </Button>
+      </Popover>
+    </OrbitProvider>
   ),
 };

fc4181e97d67b36d0f4b6fbeb517af34c24a171f
Victor Genaev <vgenaev@gmail.com>
2024-01-16 15:51:35 +0100

    test(BadgeList): replace chromatic visual test with playwright

---
+++ packages/orbit-components/src/BadgeList/BadgeList.ct-story.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import BadgeListItem from "./BadgeListItem";
+import { TYPE_OPTIONS, SIZE_OPTIONS } from "./consts";
+import KiwicomGuarantee from "../icons/KiwicomGuarantee";
+
+import BadgeList from ".";
+
+export default function BadgeListVisualStory() {
+  return (
+    <div className="space-y-md">
+      <div className="space-y-xs">
+        {Object.values(TYPE_OPTIONS).map(type => (
+          <BadgeList>
+            <BadgeListItem icon={<KiwicomGuarantee />} type={type}>
+              List type {type} item #1
+            </BadgeListItem>
+            <BadgeListItem icon={<KiwicomGuarantee />} type={type}>
+              List type {type} item #2
+            </BadgeListItem>
+          </BadgeList>
+        ))}
+      </div>
+
+      {Object.values(SIZE_OPTIONS).map(size => (
+        <BadgeList>
+          <BadgeListItem icon={<KiwicomGuarantee />} size={size}>
+            List size {size} item #1
+          </BadgeListItem>
+          <BadgeListItem icon={<KiwicomGuarantee />} size={size}>
+            List size {size} item #2
+          </BadgeListItem>
+        </BadgeList>
+      ))}
+    </div>
+  );
+}

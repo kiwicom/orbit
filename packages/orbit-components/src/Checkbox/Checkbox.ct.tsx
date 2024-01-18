a791cb15c1988330edfb3fabdddf72f88658924a
Victor Genaev <vgenaev@gmail.com>
2024-01-18 12:02:10 +0100

    test(Checkbox): replace chromatic visual test with playwright

---
+++ packages/orbit-components/src/Checkbox/Checkbox.ct.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { test, expect } from "@playwright/experimental-ct-react";
+
+import CheckboxStory from "./Checkbox.ct-story";
+
+test.describe("visual Checkbox", () => {
+  test("Checkbox", async ({ mount }) => {
+    const component = await mount(<CheckboxStory />);
+
+    await expect(component).toHaveScreenshot();
+  });
+});

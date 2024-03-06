f8abe40074afe23096e52d8fc5fd55e9607ce28c
Vojta Bulant <vojtech@bulant.cz>
2024-03-06 14:41:03 +0100

    chore(Illustration): migrate IllustrationPrimitiveList to tailwind

--- packages/orbit-components/src/primitives/IllustrationPrimitive/IllustrationPrimitiveList.tsx
+++ packages/orbit-components/src/primitives/IllustrationPrimitive/IllustrationPrimitiveList.tsx
@@ -1,80 +1,24 @@
 import React from "react";
-import styled, { css } from "styled-components";
-
-import defaultTheme from "../../defaultTheme";
-import mq from "../../utils/mediaQuery";
 
 import IllustrationPrimitive from ".";
 
-const List = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: space-between;
-  border-radius: ${({ theme }) => theme.orbit.borderRadiusLarge};
-`;
-
-List.defaultProps = {
-  theme: defaultTheme,
-};
-
-const Container = styled.div`
-  ${({ theme }) => css`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    align-content: center;
-    width: 100%;
-    min-height: 80px;
-    background-color: white;
-    margin-bottom: ${theme.orbit.spaceLarge};
-    border-radius: ${theme.orbit.borderRadiusLarge};
-    border: ${theme.orbit.borderWidthCard} ${theme.orbit.borderStyleCard}
-      ${theme.orbit.paletteCloudNormal};
-    padding: ${theme.orbit.spaceLarge};
-    ${mq.largeMobile(css`
-      flex-direction: row;
-    `)};
-  `}
-`;
-
-Container.defaultProps = {
-  theme: defaultTheme,
-};
-
-const IllustrationJSX = styled.div`
-  ${({ theme }) => css`
-    font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;
-    font-size: 12px;
-    line-height: ${theme.orbit.lineHeightTextSmall};
-    color: ${theme.orbit.paletteInkNormal};
-    background-color: ${theme.orbit.paletteCloudLight};
-    border: ${theme.orbit.borderWidthCard} ${theme.orbit.borderStyleCard}
-      ${theme.orbit.paletteCloudNormal};
-    margin-top: ${theme.orbit.spaceMedium};
-    padding: ${theme.orbit.spaceXXSmall} ${theme.orbit.spaceXSmall};
-    ${mq.largeMobile(css`
-      margin: 0;
-    `)};
-  `}
-`;
-
-IllustrationJSX.defaultProps = {
-  theme: defaultTheme,
-};
-
 const IllustrationPrimitiveList = (props: { images: string[]; nameOfComponent: string }) => {
   return (
-    <List>
+    <div>
       {props.images.map(illustration => {
         return (
-          <Container key={illustration}>
+          <div
+            className="bg-white-normal mb-lg rounded-large border-cloud-normal p-lg lm:flex-row flex min-h-[80px] w-full flex-col items-center border border-solid"
+            key={illustration}
+          >
             <IllustrationPrimitive name={illustration} />
-            <IllustrationJSX>{`<${props.nameOfComponent} name="${illustration}" />`}</IllustrationJSX>
-          </Container>
+            <code className="border-cloud-normal mt-md lm:mt-0 py-xxs px-xs leading-small text-ink-normal bg-cloud-light border border-solid text-[12px]">
+              {`<${props.nameOfComponent} name="${illustration}" />`}
+            </code>
+          </div>
         );
       })}
-    </List>
+    </div>
   );
 };
 

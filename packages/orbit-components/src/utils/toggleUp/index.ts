fe2ed3024a4f61c0ae08ed6273f30926fdd118c8
Marco Vidal Garcia <marco.vidal@kiwi.com>
2024-03-12 11:57:20 +0100

    feat: deprecate toggleUp and toggleDown utilities
    These utilities are not being used by anyone.

--- packages/orbit-components/src/utils/toggleUp/index.ts
+++ packages/orbit-components/src/utils/toggleUp/index.ts
@@ -1,5 +1,9 @@
 import { keyframes } from "styled-components";
 
+/**
+ * @deprecated This utility is deprecated and will be removed in future versions.
+ * Please use an alternative solution.
+ */
 const toggleUp = (contentHeight: number) => keyframes`
   0% { max-height: ${contentHeight}px; opacity: 1 }
   100% { max-height: 0; opacity: 0 }

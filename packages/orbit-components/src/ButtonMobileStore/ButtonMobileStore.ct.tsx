e017087a67cb3b8db84d6d9a4db5c32071d718fc
Daniel Sil <daniel.sil@kiwi.com>
2024-03-13 16:48:24 +0100

    test(ButtonMobileStore): wait for images to load

--- packages/orbit-components/src/ButtonMobileStore/ButtonMobileStore.ct.tsx
+++ packages/orbit-components/src/ButtonMobileStore/ButtonMobileStore.ct.tsx
@@ -4,8 +4,9 @@ import { test, expect } from "@playwright/experimental-ct-react";
 import ButtonMobileStoreStory from "./ButtonMobileStore.ct-story";
 
 test.describe("visual ButtonMobileStoreStory", () => {
-  test("ButtonMobileStoreStory", async ({ mount }) => {
+  test("ButtonMobileStoreStory", async ({ mount, page }) => {
     const component = await mount(<ButtonMobileStoreStory />);
+    page.waitForRequest(/images.kiwi.com/);
 
     await expect(component).toHaveScreenshot();
   });

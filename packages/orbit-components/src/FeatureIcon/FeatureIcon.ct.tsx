fcd2ff6a7d32f6168c71d9f8ed85ad4389effbdf
Daniel Sil <daniel.sil@kiwi.com>
2024-03-13 17:06:20 +0100

    test(FeatureIcon): wait for images to load

--- packages/orbit-components/src/FeatureIcon/FeatureIcon.ct.tsx
+++ packages/orbit-components/src/FeatureIcon/FeatureIcon.ct.tsx
@@ -4,8 +4,9 @@ import { test, expect } from "@playwright/experimental-ct-react";
 import FeatureIconStory from "./FeatureIcon.ct-story";
 
 test.describe("visual FeatureIcon", () => {
-  test("FeatureIcon", async ({ mount }) => {
+  test("FeatureIcon", async ({ mount, page }) => {
     const component = await mount(<FeatureIconStory />);
+    page.waitForRequest(/images.kiwi.com/);
 
     await expect(component).toHaveScreenshot();
   });

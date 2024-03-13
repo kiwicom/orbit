8ee6255efb7fb18598a757f0bcb24c34a2cc31c2
Daniel Sil <daniel.sil@kiwi.com>
2024-03-13 17:03:12 +0100

    test(CarrierLogo): wait for images to load

--- packages/orbit-components/src/CarrierLogo/CarrierLogo.ct.tsx
+++ packages/orbit-components/src/CarrierLogo/CarrierLogo.ct.tsx
@@ -4,8 +4,9 @@ import { test, expect } from "@playwright/experimental-ct-react";
 import CarrierLogoStory from "./CarrierLogo.ct-story";
 
 test.describe("visual CarrierLogo", () => {
-  test("CarrierLogo", async ({ mount }) => {
+  test("CarrierLogo", async ({ mount, page }) => {
     const component = await mount(<CarrierLogoStory />);
+    page.waitForRequest(/images.kiwi.com/);
 
     await expect(component).toHaveScreenshot();
   });

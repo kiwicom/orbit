123ca9a81955166738dd0acf432fd6334038b328
Daniel Sil <daniel.sil@kiwi.com>
2024-03-13 17:21:06 +0100

    test(ServiceLogo): wait for images to load

--- packages/orbit-components/src/ServiceLogo/ServiceLogo.ct.tsx
+++ packages/orbit-components/src/ServiceLogo/ServiceLogo.ct.tsx
@@ -4,8 +4,9 @@ import { test, expect } from "@playwright/experimental-ct-react";
 import ServiceLogoStory from "./ServiceLogo.ct-story";
 
 test.describe("visual ServiceLogo", () => {
-  test("default", async ({ mount }) => {
+  test("default", async ({ mount, page }) => {
     const component = await mount(<ServiceLogoStory />);
+    page.waitForRequest(/images.kiwi.com/);
 
     await expect(component).toHaveScreenshot();
   });

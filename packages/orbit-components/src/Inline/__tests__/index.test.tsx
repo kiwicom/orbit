1ae877c2d61112c80f1ab88c1106a4b72fe2183f
Daniel Sil <daniel.sil@kiwi.com>
2024-03-19 12:59:26 +0100

    chore: add eslint-disable for expected custom class in test

--- packages/orbit-components/src/Inline/__tests__/index.test.tsx
+++ packages/orbit-components/src/Inline/__tests__/index.test.tsx
@@ -8,6 +8,7 @@ describe("#Inline", () => {
     const dataTest = "test";
 
     render(
+      // eslint-disable-next-line tailwindcss/no-custom-classname
       <Inline align="center" justify="start" dataTest={dataTest} id="ID" className="CLASS">
         <div>kek</div>
         <div>bur</div>

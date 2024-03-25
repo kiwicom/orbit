7cf5ca78afedfd0a56d3914d975d5839e51b0ad3
Daniel Sil <daniel.sil@kiwi.com>
2024-03-25 17:40:48 +0100

    feat: remove styled-components dependency

--- packages/orbit-components/babel.config.js
+++ packages/orbit-components/babel.config.js
@@ -5,7 +5,6 @@ module.exports = {
     [require.resolve("@babel/preset-typescript"), { isTSX: true, allExtensions: true }],
   ],
   plugins: [
-    require.resolve("babel-plugin-styled-components"),
     [
       require.resolve("@babel/plugin-transform-runtime"),
       { version: require("@babel/runtime/package.json").version },
@@ -17,7 +16,6 @@ module.exports = {
         [require.resolve("@babel/preset-env"), { modules: false, bugfixes: true, loose: true }],
       ],
       plugins: [
-        require.resolve("babel-plugin-styled-components"),
         [
           require.resolve("@babel/plugin-transform-runtime"),
           { useESModules: true, version: require("@babel/runtime/package.json").version },
@@ -25,10 +23,7 @@ module.exports = {
       ],
     },
     test: {
-      plugins: [
-        require.resolve("babel-plugin-styled-components"),
-        require.resolve("babel-plugin-require-context-hook"),
-      ],
+      plugins: [require.resolve("babel-plugin-require-context-hook")],
     },
   },
 };

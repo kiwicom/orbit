1e5610db8d5f89c0ee7907c6a1d143ba0e5d5a56
Daniel Sil <daniel.sil@kiwi.com>
2024-03-28 15:03:55 +0100

    fix(Icon): icon components props are now correctly typed

--- packages/orbit-components/src/Icon/createIcon.tsx
+++ packages/orbit-components/src/Icon/createIcon.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 
 import whiteListProps from "./helpers/whiteListProps";
+import type { Props } from "./types";
 
 import OrbitIcon from ".";
 
 const createIcon = (def: React.ReactNode, viewBox: string, displayName: string) => {
-  const icon = props => (
+  const icon = (props: Props) => (
     <OrbitIcon viewBox={viewBox} {...whiteListProps(props)}>
       {def}
     </OrbitIcon>

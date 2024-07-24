0968adde7628128907ec72a3e1d8d1536f0d57f4
Dominika Hustinova <domi.hustinova@gmail.com>
2024-07-24 20:00:40 +0200

    chore: migrate Badge story from CSF2 to CSF3

--- packages/orbit-components/src/Badge/Badge.stories.tsx
+++ packages/orbit-components/src/Badge/Badge.stories.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import * as Icons from "../icons";
 import { TYPE_OPTIONS } from "./consts";
@@ -6,338 +7,282 @@ import RenderInRtl from "../utils/rtl/RenderInRtl";
 
 import Badge from ".";
 
-const getIcon = (source: string | null) => source && Icons[source];
+type BadgePropsAndCustomArgs = React.ComponentProps<typeof Badge> & {
+  content?: string;
+};
 
-export default {
+const meta: Meta<BadgePropsAndCustomArgs> = {
   title: "Badge",
-};
+  component: Badge,
 
-export const Default = ({ content, icon }: { content: string; icon: string }) => {
-  const Icon = getIcon(icon);
-
-  return <Badge icon={Icon && <Icon />}>{content}</Badge>;
-};
-
-Default.story = {
   parameters: {
     info: "Check Orbit.Kiwi for more detailed design guidelines.",
   },
-};
 
-Default.args = {
-  content: "Badge",
-  icon: "Airplane",
-};
+  args: {
+    content: "Badge",
+    icon: "Airplane",
+  },
 
-Default.argTypes = {
-  icon: {
-    options: Object.keys(Icons),
-    control: {
-      type: "select",
+  argTypes: {
+    icon: {
+      options: Object.keys(Icons),
+      control: {
+        type: "select",
+      },
+    },
+    type: {
+      options: Object.values(TYPE_OPTIONS),
+      control: {
+        type: "select",
+      },
     },
   },
 };
 
-export const Neutral = ({ content }) => {
-  return (
-    <Badge type={TYPE_OPTIONS.NEUTRAL} icon={<Icons.Sightseeing />}>
-      {content}
-    </Badge>
-  );
-};
+export default meta;
+type Story = StoryObj<BadgePropsAndCustomArgs>;
 
-Neutral.story = {
-  parameters: {
-    info: "Check Orbit.Kiwi for more detailed design guidelines.",
-  },
-};
+const getIcon = (source: string | null) => source && Icons[source];
 
-Neutral.args = {
-  content: "Badge",
-};
+export const Default: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
 
-export const Info = ({ content }) => {
-  return (
-    <Badge type={TYPE_OPTIONS.INFO} icon={<Icons.InformationCircle />}>
-      {content}
-    </Badge>
-  );
-};
-
-Info.story = {
-  parameters: {
-    info: "Check Orbit.Kiwi for more detailed design guidelines.",
-  },
-};
-
-Info.args = {
-  content: "Badge",
-};
-
-export const InfoSubtle = ({ content }) => {
-  return (
-    <Badge type={TYPE_OPTIONS.INFO_SUBTLE} icon={<Icons.Sightseeing />}>
-      {content}
-    </Badge>
-  );
-};
-
-InfoSubtle.story = {
-  parameters: {
-    info: "Check Orbit.Kiwi for more detailed design guidelines.",
-  },
-};
-
-InfoSubtle.args = {
-  content: "Badge",
-};
-
-export const Success = ({ content }) => {
-  return (
-    <Badge type={TYPE_OPTIONS.SUCCESS} icon={<Icons.CheckCircle />}>
-      {content}
-    </Badge>
-  );
-};
-
-Success.story = {
-  parameters: {
-    info: "Check Orbit.Kiwi for more detailed design guidelines.",
-  },
-};
-
-Success.args = {
-  content: "Badge",
-};
-
-export const SuccessSubtle = ({ content }) => {
-  return (
-    <Badge type={TYPE_OPTIONS.SUCCESS_SUBTLE} icon={<Icons.CheckCircle />}>
-      {content}
-    </Badge>
-  );
-};
-
-SuccessSubtle.story = {
-  parameters: {
-    info: "Check Orbit.Kiwi for more detailed design guidelines.",
-  },
-};
-
-SuccessSubtle.args = {
-  content: "Badge",
-};
-
-export const Warning = ({ content }) => {
-  return (
-    <Badge type={TYPE_OPTIONS.WARNING} icon={<Icons.Clock />}>
-      {content}
-    </Badge>
-  );
-};
-
-Warning.story = {
-  parameters: {
-    info: "Check Orbit.Kiwi for more detailed design guidelines.",
-  },
-};
-
-Warning.args = {
-  content: "Badge",
-};
-
-export const WarningSubtle = ({ content }) => {
-  return (
-    <Badge type={TYPE_OPTIONS.WARNING_SUBTLE} icon={<Icons.Clock />}>
-      {content}
-    </Badge>
-  );
-};
-
-WarningSubtle.story = {
-  parameters: {
-    info: "Check Orbit.Kiwi for more detailed design guidelines.",
-  },
-};
-
-WarningSubtle.args = {
-  content: "Badge",
-};
-
-export const Critical = ({ content }) => {
-  return (
-    <Badge type={TYPE_OPTIONS.CRITICAL} icon={<Icons.Passport />}>
-      {content}
-    </Badge>
-  );
-};
-
-Critical.story = {
-  parameters: {
-    info: "Check Orbit.Kiwi for more detailed design guidelines.",
-  },
-};
-
-Critical.args = {
-  content: "Badge",
-};
-
-export const CriticalSubtle = ({ content }) => {
-  return (
-    <Badge type={TYPE_OPTIONS.CRITICAL_SUBTLE} icon={<Icons.Sightseeing />}>
-      {content}
-    </Badge>
-  );
-};
-
-CriticalSubtle.story = {
-  parameters: {
-    info: "Check Orbit.Kiwi for more detailed design guidelines.",
-  },
-};
-
-CriticalSubtle.args = {
-  content: "Badge",
-};
-
-export const Dark = ({ content }) => {
-  return (
-    <Badge type={TYPE_OPTIONS.DARK} icon={<Icons.Sightseeing />}>
-      {content}
-    </Badge>
-  );
-};
-
-Dark.story = {
-  parameters: {
-    info: "Check Orbit.Kiwi for more detailed design guidelines.",
-  },
-};
-
-Dark.args = {
-  content: "Badge",
-};
-
-export const White = ({ content }) => {
-  return (
-    <div style={{ backgroundColor: "#46515e", padding: "10px" }}>
-      <Badge type={TYPE_OPTIONS.WHITE} icon={<Icons.Sightseeing />}>
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
         {content}
       </Badge>
-    </div>
-  );
-};
-
-White.story = {
-  parameters: {
-    info: "Check Orbit.Kiwi for more detailed design guidelines.",
+    );
   },
 };
 
-White.args = {
-  content: "Badge",
+export const Neutral: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
+
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        {content}
+      </Badge>
+    );
+  },
+  args: { type: TYPE_OPTIONS.NEUTRAL, icon: "Sightseeing" },
 };
 
-export const BadgeWithTranslatedNode = () => (
-  <Badge icon={<Icons.Airplane />}>
-    <span>Content should </span>
-    <span>be</span>
-    <span> with space</span>
-  </Badge>
-);
+export const Info: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
 
-BadgeWithTranslatedNode.story = {
-  name: "Badge with translated node",
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        {content}
+      </Badge>
+    );
+  },
+  args: { type: TYPE_OPTIONS.INFO, icon: "InformationCircle" },
 };
 
-export const Playground = ({ content, type, dataTest, Icon, ariaLabel }) => {
-  return (
-    <Badge type={type} icon={Icon && <Icon />} dataTest={dataTest} ariaLabel={ariaLabel}>
-      {content}
-    </Badge>
-  );
+export const InfoSubtle: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
+
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        {content}
+      </Badge>
+    );
+  },
+
+  args: { type: TYPE_OPTIONS.INFO_SUBTLE, icon: "Sightseeing" },
 };
 
-Playground.story = {
+export const Success: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
+
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        {content}
+      </Badge>
+    );
+  },
+  args: { type: TYPE_OPTIONS.SUCCESS, icon: "CheckCircle" },
+};
+
+export const SuccessSubtle: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
+
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        {content}
+      </Badge>
+    );
+  },
+
+  args: { type: TYPE_OPTIONS.SUCCESS, icon: "CheckCircle" },
+};
+
+export const Warning: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
+
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        {content}
+      </Badge>
+    );
+  },
+
+  args: { type: TYPE_OPTIONS.WARNING, icon: "Clock" },
+};
+
+export const WarningSubtle: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
+
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        {content}
+      </Badge>
+    );
+  },
+
+  args: { type: TYPE_OPTIONS.WARNING_SUBTLE, icon: "Clock" },
+};
+
+export const Critical: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
+
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        {content}
+      </Badge>
+    );
+  },
+
+  args: { type: TYPE_OPTIONS.CRITICAL, icon: "Passport" },
+};
+
+export const CriticalSubtle: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
+
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        {content}
+      </Badge>
+    );
+  },
+
+  args: { type: TYPE_OPTIONS.CRITICAL_SUBTLE, icon: "Sightseeing" },
+};
+
+export const Dark: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
+
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        {content}
+      </Badge>
+    );
+  },
+
+  args: { type: TYPE_OPTIONS.DARK, icon: "Sightseeing" },
+};
+
+export const White: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
+
+    return (
+      <div style={{ backgroundColor: "#46515e", padding: "10px" }}>
+        <Badge {...args} icon={Icon && <Icon />}>
+          {content}
+        </Badge>
+      </div>
+    );
+  },
+
+  args: { type: TYPE_OPTIONS.WHITE, icon: "Sightseeing" },
+};
+
+export const BadgeWithTranslatedNode: Story = {
+  render: ({ icon, ...args }) => {
+    const Icon = getIcon(icon as string);
+
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        <span>Content should </span>
+        <span>be</span>
+        <span> with space</span>
+      </Badge>
+    );
+  },
+
+  parameters: { controls: { exclude: ["content"] } },
+};
+
+export const Playground: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
+
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        {content}
+      </Badge>
+    );
+  },
+
   parameters: {
     info: "You can try all possible configurations of this component. However, check Orbit.Kiwi for more detailed design guidelines.",
   },
-};
 
-Playground.args = {
-  content: "Badge",
-  type: TYPE_OPTIONS.INFO_SUBTLE,
-  dataTest: "test",
-  ariaLabel: "test",
-  Icon: getIcon("Airplane"),
-};
-
-Playground.argTypes = {
-  type: {
-    options: Object.values(TYPE_OPTIONS),
-    control: {
-      type: "select",
-    },
-  },
-  Icon: {
-    options: Object.keys(Icons),
-    control: {
-      type: "select",
-    },
+  args: {
+    type: TYPE_OPTIONS.INFO_SUBTLE,
+    ariaLabel: "test",
   },
 };
 
-export const Accessibility = ({
-  content,
-  icon,
-  ariaLabel,
-}: {
-  content: string;
-  icon: string;
-  ariaLabel: string;
-}) => {
-  const Icon = getIcon(icon);
+export const Accessibility: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
 
-  return (
-    <Badge icon={Icon && <Icon />} ariaLabel={ariaLabel}>
-      {content}
-    </Badge>
-  );
-};
+    return (
+      <Badge {...args} icon={Icon && <Icon />}>
+        {content}
+      </Badge>
+    );
+  },
 
-Accessibility.story = {
   parameters: {
-    info: "This is a preview of component accessibility props",
+    info: "This is a preview of component accessibility props.",
+  },
+
+  args: {
+    ariaLabel: "test",
   },
 };
 
-Accessibility.args = {
-  content: "Badge",
-  icon: "Airplane",
-  ariaLabel: "test",
-};
+export const Rtl: Story = {
+  render: ({ content, icon, ...args }) => {
+    const Icon = getIcon(icon as string);
 
-Accessibility.argTypes = {
-  icon: {
-    options: Object.keys(Icons),
-    control: {
-      type: "select",
-    },
+    return (
+      <RenderInRtl>
+        <Badge {...args} icon={Icon && <Icon />}>
+          {content}
+        </Badge>
+      </RenderInRtl>
+    );
   },
-};
-
-export const Rtl = () => (
-  <RenderInRtl>
-    <Badge type="info" icon={<Icons.Airplane />}>
-      Badge
-    </Badge>
-  </RenderInRtl>
-);
-
-Rtl.story = {
-  name: "RTL",
 
   parameters: {
     info: "This is a preview of this component in RTL setup.",
   },
+
+  args: { type: TYPE_OPTIONS.INFO },
 };

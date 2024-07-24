5fabfdbf19aca3a0bca069a4bbe80bbb7d35e8d2
Dominika Hustinova <domi.hustinova@gmail.com>
2024-07-24 18:11:53 +0200

    chore: migrate Truncate story from CSF2 to CSF3

--- packages/orbit-components/src/Truncate/Truncate.stories.tsx
+++ packages/orbit-components/src/Truncate/Truncate.stories.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import RenderInRtl from "../utils/rtl/RenderInRtl";
 import Stack from "../Stack";
@@ -6,27 +7,34 @@ import Text from "../Text";
 
 import Truncate from ".";
 
-export default {
+type TruncatePropsAndCustomArgs = React.ComponentProps<typeof Truncate> & { content?: string };
+
+const meta: Meta<TruncatePropsAndCustomArgs> = {
   title: "Truncate",
+  component: Truncate,
+
+  args: {
+    content:
+      "Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Nulla pulvinar eleifend sem. Sed ac dolor sit amet purus malesuada congue. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam.",
+    maxWidth: "20%",
+  },
 };
 
-export const Default = ({ content }) => {
-  return <Truncate>{content}</Truncate>;
-};
+export default meta;
+type Story = StoryObj<TruncatePropsAndCustomArgs>;
 
-Default.story = {
+export const Default: Story = {
+  render: ({ content, maxWidth }) => {
+    return <Truncate maxWidth={maxWidth}>{content}</Truncate>;
+  },
+
   parameters: {
     info: "This is the default configuration of this component. Visit Orbit.Kiwi for more detailed guidelines.",
   },
 };
 
-Default.args = {
-  content:
-    "Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Nulla pulvinar eleifend sem. Sed ac dolor sit amet purus malesuada congue. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam.",
-};
-
-export const InStack = ({ content, maxWidth }) => {
-  return (
+export const InStack: Story = {
+  render: ({ content, maxWidth }) => (
     <Stack direction="row">
       <Truncate maxWidth={maxWidth}>
         <Text>{content}</Text>
@@ -35,43 +43,31 @@ export const InStack = ({ content, maxWidth }) => {
         <Text>{content}</Text>
       </Truncate>
     </Stack>
-  );
-};
+  ),
 
-InStack.story = {
   parameters: {
     info: "This is the configuration of this component in combination with Stack. Visit Orbit.Kiwi for more detailed guidelines.",
   },
 };
 
-InStack.args = {
-  content:
-    "Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Nulla pulvinar eleifend sem. Sed ac dolor sit amet purus malesuada congue. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam.",
-  maxWidth: "20%",
-};
-
-export const Playground = ({ content, maxWidth }) => {
-  return (
+export const Playground: Story = {
+  render: ({ content, maxWidth }) => (
     <Truncate maxWidth={maxWidth}>
       <Text>{content}</Text>
     </Truncate>
-  );
-};
+  ),
 
-Playground.story = {
   parameters: {
     info: "This is the configuration of this component in combination with Stack. Visit Orbit.Kiwi for more detailed guidelines.",
   },
+
+  args: {
+    maxWidth: "none",
+  },
 };
 
-Playground.args = {
-  content:
-    "Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Nulla pulvinar eleifend sem. Sed ac dolor sit amet purus malesuada congue. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam.",
-  maxWidth: "none",
-};
-
-export const Rtl = ({ content, maxWidth }) => {
-  return (
+export const Rtl: Story = {
+  render: ({ content, maxWidth }) => (
     <RenderInRtl>
       <Stack direction="row">
         <Truncate maxWidth={maxWidth}>
@@ -82,19 +78,9 @@ export const Rtl = ({ content, maxWidth }) => {
         </Truncate>
       </Stack>
     </RenderInRtl>
-  );
-};
-
-Rtl.story = {
-  name: "RTL",
+  ),
 
   parameters: {
     info: "This is a preview of this component in RTL setup.",
   },
-};
-
-Rtl.args = {
-  content:
-    "Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Nulla pulvinar eleifend sem. Sed ac dolor sit amet purus malesuada congue. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Pellentesque pretium lectus id turpis. Etiam dictum tincidunt diam.",
-  maxWidth: "20%",
 };

dddb57de5bbeaea28d7b143d0b855cbae3a6464e
Dominika Hustinova <domi.hustinova@gmail.com>
2024-07-25 09:48:00 +0200

    chore: migrate Breadcrumbs story from CSF2 to CSF3

--- packages/orbit-components/src/Breadcrumbs/Breadcrumbs.stories.tsx
+++ packages/orbit-components/src/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
 import { SPACINGS_AFTER } from "../common/consts";
@@ -6,32 +7,41 @@ import RenderInRtl from "../utils/rtl/RenderInRtl";
 
 import Breadcrumbs, { BreadcrumbsItem } from ".";
 
-export default {
+type BreadcrumbsPropsAndCustomArgs = React.ComponentProps<typeof Breadcrumbs> &
+  React.ComponentProps<typeof BreadcrumbsItem>;
+
+const meta: Meta<BreadcrumbsPropsAndCustomArgs> = {
   title: "Breadcrumbs",
-};
+  component: Breadcrumbs,
 
-export const Default = () => (
-  <Breadcrumbs onGoBack={action("onGoBack")}>
-    <BreadcrumbsItem href="https://kiwi.com" onClick={action("clicked")}>
-      Kiwi.com
-    </BreadcrumbsItem>
-    <BreadcrumbsItem href="#1" onClick={action("clicked")}>
-      1. Level
-    </BreadcrumbsItem>
-    <BreadcrumbsItem onClick={action("clicked")}>2. Level</BreadcrumbsItem>
-    <BreadcrumbsItem href="#3">3. Level</BreadcrumbsItem>
-    <BreadcrumbsItem>4. Level</BreadcrumbsItem>
-  </Breadcrumbs>
-);
-
-Default.story = {
   parameters: {
-    info: "Some description about this type of component. ",
+    controls: {
+      exclude: ["goBackTitle"],
+    },
   },
 };
 
-export const Playground = ({ spaceAfter, href, withGoBack, backHref }) => {
-  return (
+export default meta;
+type Story = StoryObj<BreadcrumbsPropsAndCustomArgs>;
+
+export const Default = {
+  render: args => (
+    <Breadcrumbs {...args} onGoBack={action("onGoBack")}>
+      <BreadcrumbsItem href="https://kiwi.com" onClick={action("clicked")}>
+        Kiwi.com
+      </BreadcrumbsItem>
+      <BreadcrumbsItem href="#1" onClick={action("clicked")}>
+        1. Level
+      </BreadcrumbsItem>
+      <BreadcrumbsItem onClick={action("clicked")}>2. Level</BreadcrumbsItem>
+      <BreadcrumbsItem href="#3">3. Level</BreadcrumbsItem>
+      <BreadcrumbsItem>4. Level</BreadcrumbsItem>
+    </Breadcrumbs>
+  ),
+};
+
+export const Playground: Story = {
+  render: ({ spaceAfter, href, withGoBack, backHref }) => (
     <Breadcrumbs
       backHref={backHref}
       onGoBack={withGoBack ? action("onGoBack") : undefined}
@@ -53,69 +63,57 @@ export const Playground = ({ spaceAfter, href, withGoBack, backHref }) => {
         4. Level
       </BreadcrumbsItem>
     </Breadcrumbs>
-  );
-};
+  ),
 
-Playground.story = {
-  parameters: {
-    info: "Some description about this type of component. ",
+  args: {
+    spaceAfter: SPACINGS_AFTER.SMALL,
+    href: "https://kiwi.com",
+    withGoBack: true,
+    backHref: "",
   },
-};
 
-Playground.args = {
-  spaceAfter: SPACINGS_AFTER.SMALL,
-  href: "https://kiwi.com",
-  withGoBack: true,
-  backHref: "",
-};
-
-Playground.argTypes = {
-  spaceAfter: {
-    options: Object.values(SPACINGS_AFTER),
-    control: {
-      type: "select",
+  argTypes: {
+    spaceAfter: {
+      options: Object.values(SPACINGS_AFTER),
+      control: {
+        type: "select",
+      },
     },
   },
 };
 
-export const Rtl = ({ href }) => {
-  return (
+export const Rtl: Story = {
+  render: args => (
     <RenderInRtl>
-      <Breadcrumbs onGoBack={action("onGoBack")}>
-        <BreadcrumbsItem id="rocket" href={href} onClick={action("clicked")}>
+      <Breadcrumbs {...args} onGoBack={action("onGoBack")}>
+        <BreadcrumbsItem id="rocket" href="https://kiwi.com" onClick={action("clicked")}>
           Kiwi.com
         </BreadcrumbsItem>
-        <BreadcrumbsItem id="rocket2" href={href} onClick={action("clicked")}>
+        <BreadcrumbsItem id="rocket2" href="https://kiwi.com" onClick={action("clicked")}>
           1. Level
         </BreadcrumbsItem>
-        <BreadcrumbsItem id="rocket3" href={href} onClick={action("clicked")}>
+        <BreadcrumbsItem id="rocket3" href="https://kiwi.com" onClick={action("clicked")}>
           2. Level
         </BreadcrumbsItem>
-        <BreadcrumbsItem id="rocket4" href={href} onClick={action("clicked")}>
+        <BreadcrumbsItem id="rocket4" href="https://kiwi.com" onClick={action("clicked")}>
           3. Level
         </BreadcrumbsItem>
-        <BreadcrumbsItem id="rocket5" href={href} onClick={action("clicked")}>
+        <BreadcrumbsItem id="rocket5" href="https://kiwi.com" onClick={action("clicked")}>
           4. Level
         </BreadcrumbsItem>
       </Breadcrumbs>
     </RenderInRtl>
-  );
+  ),
 };
 
-Rtl.story = {
-  name: "RTL",
-};
-
-Rtl.args = {
-  href: "https://kiwi.com",
-};
-
-export const BackLink = () => <Breadcrumbs backHref="https://www.kiwi.com">{null}</Breadcrumbs>;
-
-BackLink.story = {
-  name: "Back link",
+export const BackLink: Story = {
+  render: args => (
+    <Breadcrumbs {...args} backHref="https://www.kiwi.com">
+      {null}
+    </Breadcrumbs>
+  ),
 
   parameters: {
-    info: "Render the back button as a link. ",
+    info: "Render the back button as a link.",
   },
 };

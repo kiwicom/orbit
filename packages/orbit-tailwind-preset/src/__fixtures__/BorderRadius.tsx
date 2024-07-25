218e9e7761639fc89800ebd497c78e2e596a6646
Daniel Sil <daniel.sil@kiwi.com>
2024-07-25 16:51:07 +0200

    feat(tailwind): add new rounded- classes and deprecate some others
    New classes: rounded-50, rounded-100, rounded-150, rounded-300, rounded-400
    Deprecated: rounded-small, rounded-normal, rounded-large

--- packages/orbit-tailwind-preset/src/__fixtures__/BorderRadius.tsx
+++ packages/orbit-tailwind-preset/src/__fixtures__/BorderRadius.tsx
@@ -3,10 +3,18 @@ import React from "react";
 const BorderRadius = () => {
   return (
     <div>
+      <div className="rounded-50">50</div>
+      <div className="rounded-100">100</div>
+      <div className="rounded-150">150</div>
+      <div className="rounded-300">300</div>
+      <div className="rounded-400">400</div>
+      <div className="rounded-none">none</div>
+      <div className="rounded-full">full</div>
+      {/* Deprecated values */}
       <div className="rounded-small">small</div>
       <div className="rounded-normal">normal</div>
       <div className="rounded-large">large</div>
-      <div className="rounded-full">full</div>
+      <div className="rounded-circle">circle</div>
     </div>
   );
 };

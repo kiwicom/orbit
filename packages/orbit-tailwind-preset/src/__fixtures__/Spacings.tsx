3d919f0be1b19c8c4d056a79325ff337b39d6c31
Daniel Sil <daniel.sil@kiwi.com>
2024-08-01 17:21:42 +0200

    feat(tailwind): add new spacing classes

--- packages/orbit-tailwind-preset/src/__fixtures__/Spacings.tsx
+++ packages/orbit-tailwind-preset/src/__fixtures__/Spacings.tsx
@@ -5,6 +5,19 @@ const Spacings = () => {
     <div>
       <div className="px-0">space 0</div>
       <div className="px-none">space none</div>
+      <div className="px-px">space 1</div>
+      <div className="px-50">space50</div>
+      <div className="px-100">space100</div>
+      <div className="px-150">space150</div>
+      <div className="px-200">space200</div>
+      <div className="px-300">space300</div>
+      <div className="px-400">space400</div>
+      <div className="px-500">space500</div>
+      <div className="px-600">space600</div>
+      <div className="px-800">space800</div>
+      <div className="px-1000">space1000</div>
+      <div className="px-1200">space1200</div>
+      <div className="px-1600">space1600</div>
       <div className="px-xxxs">spaceXXXSmall</div>
       <div className="px-xxs">spaceXXSmall</div>
       <div className="px-xs">spaceXSmall</div>
--- packages/orbit-tailwind-preset/src/__tests__/spacing.test.tsx
+++ packages/orbit-tailwind-preset/src/__tests__/spacing.test.tsx
@@ -5,6 +5,19 @@ import { render, screen } from "../testUtils";
 import Spacings from "../__fixtures__/Spacings";
 
 const SPACINGS = [
+  "space50",
+  "space100",
+  "space150",
+  "space200",
+  "space300",
+  "space400",
+  "space500",
+  "space600",
+  "space800",
+  "space1000",
+  "space1200",
+  "space1600",
+  // Deprecated
   "spaceXXXSmall",
   "spaceXXSmall",
   "spaceXSmall",

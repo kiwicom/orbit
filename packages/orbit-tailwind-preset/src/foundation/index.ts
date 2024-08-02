e56a3598b3bec9abf69609a390736ae4d43d6122
Marco Vidal Garcia <marco.vidal@kiwi.com>
2024-08-02 16:44:09 +0200

    feat(tailwind): add new shadow- classes and deprecate some others
    New classes:
    - shadow-level1, shadow-level2, shadow-level3, shadow-level3-reverse, shadow-level4
    
    Deprecated:
    - shadow-action, shadow-action-active, shadow-raised, shadow-raised-reverse, shadow-overlay
    - bg-elevation-action, bg-elevation-action-active, bg-elevation-raised, bg-elevation-raised-reverse,
      bg-elevation-overlay

--- packages/orbit-tailwind-preset/src/foundation/index.ts
+++ packages/orbit-tailwind-preset/src/foundation/index.ts
@@ -17,11 +17,16 @@ const config = (tokens: typeof defaultTokens): Config => {
         none: "none",
         fixed: tokens.elevationFixedBoxShadow,
         "fixed-reverse": tokens.elevationFixedReverseBoxShadow,
-        raised: tokens.elevationRaisedBoxShadow,
-        "raised-reverse": tokens.elevationRaisedReverseBoxShadow,
-        action: tokens.elevationActionBoxShadow,
-        "action-active": tokens.elevationActionActiveBoxShadow,
-        overlay: tokens.elevationOverlayBoxShadow,
+        raised: tokens.elevationRaisedBoxShadow, // deprecated
+        "raised-reverse": tokens.elevationRaisedReverseBoxShadow, // deprecated
+        action: tokens.elevationActionBoxShadow, // deprecated
+        "action-active": tokens.elevationActionActiveBoxShadow, // deprecated
+        overlay: tokens.elevationOverlayBoxShadow, // deprecated
+        level1: tokens.elevationLevel1BoxShadow,
+        level2: tokens.elevationLevel2BoxShadow,
+        level3: tokens.elevationLevel3BoxShadow,
+        "level3-reverse": tokens.elevationLevel3ReverseBoxShadow,
+        level4: tokens.elevationLevel4BoxShadow,
       },
       transitionDuration: {
         fast: tokens.durationFast,

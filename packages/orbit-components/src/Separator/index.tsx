008317beb222e2c08961b9e0971052a46540e62d
Daniel Sil <daniel.sil@kiwi.com>
2024-08-05 15:19:28 +0200

    feat(Separator): add new values to sideOffset prop and deprecate old ones
    The new sideOffset values are already available. The old ones are deprecated.

--- packages/orbit-components/src/Separator/index.tsx
+++ packages/orbit-components/src/Separator/index.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import cx from "clsx";
 
-import type { Props, Align } from "./types";
+import type { Props, Align, SideOffset } from "./types";
 import { getSpaceAfterClasses } from "../common/tailwind";
 
 const BORDER_TYPE_CLASSES = {
@@ -21,33 +21,53 @@ const enum Indent {
   large = "large",
   XLarge = "XLarge",
   XXLarge = "XXLarge",
+  THREE_HUNDRED = "300",
+  FOUR_HUNDRED = "400",
+  SIX_HUNDRED = "600",
+  EIGHT_HUNDRED = "800",
+  ONE_THOUSAND = "1000",
 }
 
-function getSideOffsetAmount(indent: keyof typeof Indent, align: Align) {
+function getSideOffsetAmount(indent: SideOffset, align: Align) {
   const classes = {
     left: {
       [Indent.none]: "pe-0",
-      [Indent.small]: "pe-300",
-      [Indent.medium]: "pe-400",
-      [Indent.large]: "pe-600",
-      [Indent.XLarge]: "pe-800",
-      [Indent.XXLarge]: "pe-1000",
+      [Indent.THREE_HUNDRED]: "pe-300",
+      [Indent.small]: "pe-300", // deprecated
+      [Indent.FOUR_HUNDRED]: "pe-400",
+      [Indent.medium]: "pe-400", // deprecated
+      [Indent.SIX_HUNDRED]: "pe-600",
+      [Indent.large]: "pe-600", // deprecated
+      [Indent.EIGHT_HUNDRED]: "pe-800",
+      [Indent.XLarge]: "pe-800", // deprecated
+      [Indent.ONE_THOUSAND]: "pe-1000",
+      [Indent.XXLarge]: "pe-1000", // deprecated
     },
     right: {
       [Indent.none]: "ps-0",
-      [Indent.small]: "ps-300",
-      [Indent.medium]: "ps-400",
-      [Indent.large]: "ps-600",
-      [Indent.XLarge]: "ps-800",
-      [Indent.XXLarge]: "ps-1000",
+      [Indent.THREE_HUNDRED]: "ps-300",
+      [Indent.small]: "ps-300", // deprecated
+      [Indent.FOUR_HUNDRED]: "ps-400",
+      [Indent.medium]: "ps-400", // deprecated
+      [Indent.SIX_HUNDRED]: "ps-600",
+      [Indent.large]: "ps-600", // deprecated
+      [Indent.EIGHT_HUNDRED]: "ps-800",
+      [Indent.XLarge]: "ps-800", // deprecated
+      [Indent.ONE_THOUSAND]: "ps-1000",
+      [Indent.XXLarge]: "ps-1000", // deprecated
     },
     center: {
       [Indent.none]: "px-0",
-      [Indent.small]: "px-300",
-      [Indent.medium]: "px-400",
-      [Indent.large]: "px-600",
-      [Indent.XLarge]: "px-800",
-      [Indent.XXLarge]: "px-1000",
+      [Indent.THREE_HUNDRED]: "px-300",
+      [Indent.small]: "px-300", // deprecated
+      [Indent.FOUR_HUNDRED]: "px-400",
+      [Indent.medium]: "px-400", // deprecated
+      [Indent.SIX_HUNDRED]: "px-600",
+      [Indent.large]: "px-600", // deprecated
+      [Indent.EIGHT_HUNDRED]: "px-800",
+      [Indent.XLarge]: "px-800", // deprecated
+      [Indent.ONE_THOUSAND]: "px-1000",
+      [Indent.XXLarge]: "px-1000", // deprecated
     },
   };
 

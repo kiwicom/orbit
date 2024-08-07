2aaf4017ecf715846b8de5a456ed765dbec084dc
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-08-07 16:36:10 +0200

    chore(Dialog): migrate from CSF2 to CSF3

--- packages/orbit-components/src/Dialog/Dialog.stories.tsx
+++ packages/orbit-components/src/Dialog/Dialog.stories.tsx
@@ -1,81 +1,74 @@
 import * as React from "react";
 import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from "@storybook/react/";
 
 import Button from "../Button";
 import ButtonLink from "../ButtonLink";
 import Illustration from "../Illustration";
 import { NAMES } from "../Illustration/consts.mts";
+import type { Name } from "../Illustration/types";
 
 import Dialog from ".";
 
-export default {
+const meta: Meta<typeof Dialog> = {
   title: "Dialog",
-};
+  component: Dialog,
 
-export const Default = ({ title, description, dataTest }) => {
-  return (
-    <Dialog
-      title={title}
-      description={description}
-      dataTest={dataTest}
-      primaryAction={<Button type="critical">Log out</Button>}
-      renderInPortal={false}
-    />
-  );
-};
-
-Default.story = {
   parameters: {
     info: "Visit Orbit.Kiwi for more detailed guidelines.",
   },
+
+  args: {
+    title: "Log out",
+    description: "Are you sure you want to log out now?",
+    illustration: "NoNotification",
+    maxWidth: NaN,
+    renderInPortal: false,
+    lockScrolling: false,
+    onClose: action("onClose"),
+  },
+
+  argTypes: {
+    illustration: {
+      options: NAMES,
+      control: {
+        type: "select",
+      },
+    },
+    maxWidth: {
+      control: {
+        type: "number",
+      },
+    },
+  },
 };
 
-Default.args = {
-  title: "Log out",
-  description: "Are you sure you want to log out now?",
-  dataTest: "test",
-};
+export default meta;
+type Story = StoryObj<typeof Dialog>;
 
-export const Playground = ({ title, maxWidth, description, illustration, dataTest }) => {
-  return (
+export const Default: Story = {
+  render: args => (
     <Dialog
-      title={title}
-      maxWidth={maxWidth}
-      description={description}
-      illustration={<Illustration name={illustration} size="small" />}
-      dataTest={dataTest}
-      onClose={action("onClose")}
+      {...args}
+      illustration={false}
+      primaryAction={<Button type="critical">Log out</Button>}
+    />
+  ),
+
+  parameters: {
+    controls: {
+      exclude: ["illustration"],
+    },
+  },
+};
+
+export const Playground: Story = {
+  render: ({ illustration, ...args }) => (
+    <Dialog
+      {...args}
+      illustration={<Illustration name={illustration as Name} size="small" />}
       primaryAction={<Button type="critical">Log out</Button>}
       secondaryAction={<ButtonLink type="secondary">Cancel</ButtonLink>}
-      renderInPortal={false}
     />
-  );
-};
-
-Playground.story = {
-  parameters: {
-    info: "Visit Orbit.Kiwi for more detailed guidelines.",
-  },
-};
-
-Playground.args = {
-  title: "Log out",
-  description: "Are you sure you want to log out now?",
-  illustration: "NoNotification",
-  dataTest: "test",
-  maxWidth: NaN,
-};
-
-Playground.argTypes = {
-  illustration: {
-    options: NAMES,
-    control: {
-      type: "select",
-    },
-  },
-  maxWidth: {
-    control: {
-      type: "number",
-    },
-  },
+  ),
 };

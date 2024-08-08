cb240cc74155af30028a4dd9dc37a5568e9e22e4
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-08-08 16:25:38 +0200

    chore(Hide): migrate from CSF2 to CSF3

--- packages/orbit-components/src/Hide/Hide.stories.tsx
+++ packages/orbit-components/src/Hide/Hide.stories.tsx
@@ -1,70 +1,65 @@
 import * as React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 
-import Separator from "../Separator";
-import ChevronBackward from "../icons/ChevronBackward";
 import type { Devices } from "../utils/mediaQuery";
 
 import Hide from ".";
 
-export default {
+type HidePropsAndCustomArgs = React.ComponentProps<typeof Hide> & {
+  largeDesktop: boolean;
+  desktop: boolean;
+  tablet: boolean;
+  largeMobile: boolean;
+  mediumMobile: boolean;
+  smallMobile: boolean;
+};
+
+const meta: Meta<HidePropsAndCustomArgs> = {
   title: "Hide",
-};
-
-export const WithSeparator = ({ block }) => {
-  return (
-    <Hide on={["largeMobile"]} block={block}>
-      <Separator />
-    </Hide>
-  );
-};
-
-WithSeparator.story = {
+  component: Hide,
   parameters: {
-    info: "Configuration with Separator, for separator to work correctly block property has to be set.",
+    info: "Check Orbit.Kiwi for more detailed design guidelines.",
+    controls: {
+      exclude: ["on"],
+    },
+  },
+
+  args: {
+    largeDesktop: false,
+    desktop: false,
+    tablet: false,
+    largeMobile: false,
+    mediumMobile: false,
+    smallMobile: false,
+    block: false,
   },
 };
 
-WithSeparator.args = {
-  block: true,
-};
+export default meta;
 
-export const Playground = ({
-  largeDesktop,
-  desktop,
-  tablet,
-  largeMobile,
-  mediumMobile,
-  smallMobile,
-  block,
-}) => {
-  const on = [
-    largeDesktop && "largeDesktop",
-    desktop && "desktop",
-    tablet && "tablet",
-    largeMobile && "largeMobile",
-    mediumMobile && "mediumMobile",
-    smallMobile && "smallMobile",
-  ].filter(item => typeof item === "string") as Devices[];
+type Story = StoryObj<HidePropsAndCustomArgs>;
 
-  return (
-    <Hide on={on} block={block}>
-      <ChevronBackward />
-    </Hide>
-  );
-};
+export const Playground: Story = {
+  render: ({ block, ...args }) => {
+    const { largeDesktop, desktop, tablet, largeMobile, mediumMobile, smallMobile } = args;
 
-Playground.story = {
+    const on = [
+      largeDesktop && "largeDesktop",
+      desktop && "desktop",
+      tablet && "tablet",
+      largeMobile && "largeMobile",
+      mediumMobile && "mediumMobile",
+      smallMobile && "smallMobile",
+    ].filter(item => typeof item === "string") as Devices[];
+
+    return (
+      <Hide block={block} on={on}>
+        <div style={{ height: 20, minWidth: "200px", width: "100%", background: "darkViolet" }} />
+      </Hide>
+    );
+  },
+
   parameters: {
     info: "You can try all possible configurations of this component. However, check Orbit.Kiwi for more detailed design guidelines.",
   },
-};
-
-Playground.args = {
-  largeDesktop: false,
-  desktop: false,
-  tablet: false,
-  largeMobile: false,
-  mediumMobile: false,
-  smallMobile: false,
-  block: false,
 };

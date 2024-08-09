0938f1e8ea46852f565a9acea739cd7a75920dad
Dominika Hustinova <domi.hustinova@gmail.com>
2024-08-09 16:14:58 +0200

    chore(Illustration): migrate stories from CSF2 to CSF3

--- packages/orbit-components/src/Illustration/Illustration.stories.tsx
+++ packages/orbit-components/src/Illustration/Illustration.stories.tsx
@@ -1,58 +1,54 @@
 import * as React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import { NAMES } from "./consts.mts";
 import { SIZE_OPTIONS } from "../primitives/IllustrationPrimitive";
 import IllustrationPrimitiveList from "../primitives/IllustrationPrimitive/IllustrationPrimitiveList";
-import type { Name } from "./types";
 
 import Illustration from ".";
 
-export default {
+const meta: Meta<typeof Illustration> = {
   title: "Illustration",
-};
+  component: Illustration,
 
-export const Playground = ({ size, name, dataTest, alt }) => {
-  return (
-    <Illustration size={size} name={name} dataTest={dataTest} margin={{ bottom: 12 }} alt={alt} />
-  );
-};
-
-Playground.story = {
   parameters: {
     info: "Explore our new set of illustrations for Kiwi.com.",
   },
-};
 
-Playground.args = {
-  size: SIZE_OPTIONS.MEDIUM,
-  name: "Accommodation" as Name,
-  dataTest: "test",
-  alt: "",
-};
-
-Playground.argTypes = {
-  size: {
-    options: Object.values(SIZE_OPTIONS),
-    control: {
-      type: "select",
-    },
+  args: {
+    size: SIZE_OPTIONS.MEDIUM,
+    name: "Accommodation",
+    margin: { bottom: 12 },
+    alt: "",
   },
-  name: {
-    options: NAMES,
-    control: {
-      type: "select",
+
+  argTypes: {
+    size: {
+      options: Object.values(SIZE_OPTIONS),
+      control: {
+        type: "select",
+      },
+    },
+    name: {
+      options: NAMES,
+      control: {
+        type: "select",
+      },
     },
   },
 };
 
-export const ListOfAllIllustrations = () => {
-  return <IllustrationPrimitiveList nameOfComponent="Illustration" images={NAMES} />;
-};
+export default meta;
+type Story = StoryObj<typeof Illustration>;
 
-ListOfAllIllustrations.story = {
-  name: "List of all Illustrations",
+export const Playground: Story = {};
+
+export const ListOfAllIllustrations = {
+  render: () => <IllustrationPrimitiveList nameOfComponent="Illustration" images={NAMES} />,
 
   parameters: {
-    info: "Explore our new set of illustrations for Kiwi.com.",
+    controls: {
+      disable: true,
+    },
   },
 };

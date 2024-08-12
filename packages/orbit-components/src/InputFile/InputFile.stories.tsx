669a3b44014223f61b7ff2c8435eb38edac1b613
Dominika Hustinova <domi.hustinova@gmail.com>
2024-08-12 10:17:56 +0200

    chore(InputFile): migrate stories from CSF2 to CSF3

--- packages/orbit-components/src/InputFile/InputFile.stories.tsx
+++ packages/orbit-components/src/InputFile/InputFile.stories.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
 import RenderInRtl from "../utils/rtl/RenderInRtl";
@@ -6,203 +7,115 @@ import { SPACINGS_AFTER } from "../common/consts";
 
 import InputFile from ".";
 
-export default {
+const meta: Meta<typeof InputFile> = {
   title: "InputFile",
-};
+  component: InputFile,
 
-export const Default = ({ label }) => {
-  return (
-    <InputFile
-      label={label}
-      onChange={action("onChange")}
-      onFocus={action("onFocus")}
-      onBlur={action("onBlur")}
-      onRemoveFile={action("removeFile")}
-    />
-  );
-};
-
-Default.story = {
   parameters: {
-    info: "You can try all possible configurations of this component. However, check Orbit.Kiwi for more detailed design guidelines.",
+    controls: {
+      exclude: ["onChange", "onFocus", "onBlur", "onRemoveFile"],
+    },
+  },
+
+  args: {
+    label: "Label",
+    onChange: action("onChange"),
+    onFocus: action("onFocus"),
+    onBlur: action("onBlur"),
+    onRemoveFile: action("removeFile"),
   },
 };
 
-Default.args = {
-  label: "Label",
-};
+export default meta;
+type Story = StoryObj<typeof InputFile>;
 
-export const FilledWithFile = ({ label, fileName }) => {
-  return (
-    <InputFile
-      label={label}
-      fileName={fileName}
-      onChange={action("onChange")}
-      onFocus={action("onFocus")}
-      onBlur={action("onBlur")}
-      onRemoveFile={action("removeFile")}
-    />
-  );
-};
+export const Default: Story = {};
 
-FilledWithFile.story = {
-  name: "Filled with file",
-
-  parameters: {
-    info: "You can try all possible configurations of this component. However, check Orbit.Kiwi for more detailed design guidelines.",
+export const FilledWithFile: Story = {
+  args: {
+    fileName: "file.png",
   },
 };
 
-FilledWithFile.args = {
-  label: "Label",
-  fileName: "file.png",
-};
-
-export const WithHelp = ({ label }) => {
-  return (
-    <InputFile
-      label={label}
-      help={
-        <div>
-          Supported files: <strong>PNG, JPG, PDF</strong>
-        </div>
-      }
-      onChange={action("onChange")}
-      onFocus={action("onFocus")}
-      onBlur={action("onBlur")}
-      onRemoveFile={action("removeFile")}
-    />
-  );
-};
-
-WithHelp.story = {
-  name: "With help",
-
+export const WithHelp: Story = {
   parameters: {
-    info: "You can try all possible configurations of this component. However, check Orbit.Kiwi for more detailed design guidelines.",
+    controls: {
+      exclude: ["onChange", "onFocus", "onBlur", "onRemoveFile", "help"],
+    },
+  },
+
+  args: {
+    help: (
+      <div>
+        Supported files: <strong>PNG, JPG, PDF</strong>
+      </div>
+    ),
   },
 };
 
-WithHelp.args = {
-  label: "Label",
-};
-
-export const WithError = ({ label, error }) => {
-  return (
-    <InputFile
-      label={label}
-      error={error}
-      onChange={action("onChange")}
-      onFocus={action("onFocus")}
-      onBlur={action("onBlur")}
-      onRemoveFile={action("removeFile")}
-    />
-  );
-};
-
-WithError.story = {
-  name: "With error",
-
+export const WithError: Story = {
   parameters: {
-    info: "You can try all possible configurations of this component. However, check Orbit.Kiwi for more detailed design guidelines.",
+    controls: {
+      exclude: ["onChange", "onFocus", "onBlur", "onRemoveFile", "error"],
+    },
+  },
+
+  args: {
+    error: (
+      <div>
+        <strong>Error message</strong> (explain how to solve it)
+      </div>
+    ),
   },
 };
 
-WithError.args = {
-  label: "Label",
-  error: "Error message (explain how to solve it)",
-};
-
-export const Playground = ({
-  label,
-  multiple,
-  disabled,
-  buttonLabel,
-  name,
-  placeholder,
-  fileName,
-  error,
-  help,
-  allowedFileTypes,
-  dataTest,
-  spaceAfter,
-  required,
-  width,
-}) => {
-  return (
-    <InputFile
-      label={label}
-      multiple={multiple}
-      buttonLabel={buttonLabel}
-      name={name}
-      disabled={disabled}
-      placeholder={placeholder}
-      fileName={fileName}
-      width={width}
-      error={error}
-      help={help}
-      required={required}
-      dataTest={dataTest}
-      allowedFileTypes={allowedFileTypes}
-      onChange={action("onChange")}
-      onFocus={action("onFocus")}
-      onBlur={action("onBlur")}
-      onRemoveFile={action("removeFile")}
-      spaceAfter={spaceAfter}
-    />
-  );
-};
-
-Playground.story = {
+export const Playground: Story = {
   parameters: {
     info: "You can try all possible configurations of this component. However, check Orbit.Kiwi for more detailed design guidelines.",
   },
-};
 
-Playground.args = {
-  label: "Label",
-  multiple: false,
-  disabled: false,
-  buttonLabel: "Please select file",
-  name: "fileInput",
-  placeholder: "No file has been selected yet",
-  fileName: "",
-  error: "No file has been selected yet",
-  help: "",
-  allowedFileTypes: ["media/*", "image/*"].join(","),
-  dataTest: "test",
-  spaceAfter: SPACINGS_AFTER.SMALL,
-  required: false,
-  width: "",
-};
+  args: {
+    multiple: false,
+    disabled: false,
+    buttonLabel: "Please select file",
+    name: "fileInput",
+    placeholder: "No file has been selected yet",
+    fileName: "",
+    error: "No file has been selected yet",
+    help: "",
+    allowedFileTypes: ["media/*", "image/*"].join(","),
+    spaceAfter: SPACINGS_AFTER.SMALL,
+    required: false,
+    width: "",
+    helpClosable: true,
+    tabIndex: "",
+    insideInputGroup: false,
+    id: "",
+  },
 
-Playground.argTypes = {
-  spaceAfter: {
-    options: Object.values(SPACINGS_AFTER),
-    control: {
-      type: "select",
+  argTypes: {
+    spaceAfter: {
+      options: Object.values(SPACINGS_AFTER),
+      control: {
+        type: "select",
+      },
     },
   },
 };
 
-export const Rtl = ({ label, error, help }) => {
-  return (
+export const Rtl: Story = {
+  render: args => (
     <RenderInRtl>
-      <InputFile label={label} error={error} help={help} />
+      <InputFile {...args} />
     </RenderInRtl>
-  );
-};
-
-Rtl.story = {
-  name: "RTL",
+  ),
 
   parameters: {
     info: "This is a preview of this component in RTL setup.",
   },
-};
 
-Rtl.args = {
-  label: "Label",
-  error: "Error message (explain how to solve it)",
-  help: "",
+  args: {
+    error: "Error message (explain how to solve it)",
+    help: "",
+  },
 };

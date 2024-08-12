2d0d5d581be690c937bf57b4749ebad981f0d45d
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-08-12 11:55:59 +0200

    chore(List): migrate from CSF2 to CSF3

--- packages/orbit-components/src/List/List.stories.tsx
+++ packages/orbit-components/src/List/List.stories.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import * as Icons from "../icons";
 import { SIZES, TYPES } from "./consts";
@@ -12,137 +13,128 @@ import List, { ListItem } from ".";
 
 const getIcon = (source: string | null) => source && Icons[source];
 
-export default {
+type ListPropsAndCustomArgs = React.ComponentProps<typeof List> &
+  React.ComponentProps<typeof ListItem> & { iconColor: string };
+
+const meta: Meta<ListPropsAndCustomArgs> = {
   title: "List",
-};
+  component: List,
 
-export const Default = () => (
-  <List>
-    <ListItem>
-      24,000 locations <TextLink href="#">around</TextLink> the globe
-    </ListItem>
-    <ListItem>
-      Lowest price car rental in <strong>Warsaw</strong>
-    </ListItem>
-    <ListItem>From 3 star budget to 5 star luxury</ListItem>
-  </List>
-);
-
-Default.story = {
   parameters: {
-    info: "List groups related information together and make content more scalable and organized.",
+    info: "List groups related information together and make content more scalable and organized. Check Orbit.Kiwi for more detailed design guidelines.",
+  },
+
+  args: {
+    type: TYPES.PRIMARY,
+    size: SIZES.NORMAL,
+    spaceAfter: SPACINGS_AFTER.MEDIUM,
+  },
+
+  argTypes: {
+    size: {
+      options: Object.values(SIZES),
+      control: {
+        type: "select",
+      },
+    },
+    type: {
+      options: Object.values(TYPES),
+      control: {
+        type: "select",
+      },
+    },
+    spaceAfter: {
+      options: Object.values(SPACINGS_AFTER),
+      control: {
+        type: "select",
+      },
+    },
   },
 };
 
-export const DifferentTypeAndSize = ({ size, type }) => {
-  return (
-    <List size={size} type={type}>
+export default meta;
+
+type Story = StoryObj<ListPropsAndCustomArgs>;
+
+export const Default: Story = {
+  render: () => (
+    <List>
+      <ListItem>
+        24,000 locations <TextLink href="#">around</TextLink> the globe
+      </ListItem>
+      <ListItem>
+        Lowest price car rental in <strong>Warsaw</strong>
+      </ListItem>
+      <ListItem>From 3 star budget to 5 star luxury</ListItem>
+    </List>
+  ),
+
+  parameters: {
+    controls: {
+      disable: true,
+    },
+  },
+};
+
+export const DifferentTypeAndSize: Story = {
+  render: args => (
+    <List {...args}>
       <ListItem>
         Gain peace of mind before you travel. No stress about what could go wrong.
       </ListItem>
       <ListItem>Customise your coverage to suit your needs and your budget.</ListItem>
       <ListItem>Feel safe in the hands of AXA Assistance, the travel insurance experts.</ListItem>
     </List>
-  );
-};
+  ),
 
-DifferentTypeAndSize.story = {
-  name: "Different type and size",
-
-  parameters: {
-    info: "List groups related information together and make content more scalable and organized.",
+  args: {
+    size: SIZES.LARGE,
+    type: TYPES.SECONDARY,
   },
 };
 
-DifferentTypeAndSize.args = {
-  size: SIZES.SMALL,
-  type: TYPES.SECONDARY,
-};
+export const WithLabelAndIcon: Story = {
+  render: ({ icon, iconColor, children, ...args }) => {
+    const Icon = typeof icon === "string" && getIcon(icon);
 
-DifferentTypeAndSize.argTypes = {
-  size: {
-    options: Object.values(SIZES),
-    control: {
-      type: "select",
+    return (
+      <List {...args}>
+        <ListItem label="Kiwi.com services" icon={<Icon color={iconColor} />}>
+          {children}
+        </ListItem>
+        <ListItem label="Kiwi.com services" icon={<Icon color={iconColor} />}>
+          {children}
+        </ListItem>
+        <ListItem icon={<Icon color={iconColor} />}>{children}</ListItem>
+      </List>
+    );
+  },
+
+  args: {
+    icon: "Check",
+    iconColor: ICON_COLORS.SUCCESS,
+    children: "24,000 locations around the globe",
+  },
+
+  argTypes: {
+    icon: {
+      options: Object.keys(Icons),
+      control: {
+        type: "select",
+      },
     },
-  },
-  type: {
-    options: Object.values(TYPES),
-    control: {
-      type: "select",
-    },
-  },
-};
-export const WithLabels = ({ type, size, icon, iconColor, content, spaceAfter, dataTest }) => {
-  const Icon = getIcon(icon);
-
-  return (
-    <List size={size} type={type} dataTest={dataTest} spaceAfter={spaceAfter}>
-      <ListItem label="Kiwi.com services" icon={Icon && <Icon size="small" color={iconColor} />}>
-        {content}
-      </ListItem>
-      <ListItem label="Kiwi.com services" icon={Icon && <Icon size="small" color={iconColor} />}>
-        {content}
-      </ListItem>
-      <ListItem icon={Icon && <Icon size="small" color={iconColor} />}>{content}</ListItem>
-    </List>
-  );
-};
-
-WithLabels.story = {
-  name: "With labels",
-
-  parameters: {
-    info: "List groups related information together and make content more scalable and organized.",
-  },
-};
-
-WithLabels.args = {
-  type: TYPES.PRIMARY,
-  size: SIZES.NORMAL,
-  icon: "Check",
-  iconColor: ICON_COLORS.SUCCESS,
-  content: "24,000 locations around the globe",
-  spaceAfter: SPACINGS_AFTER.MEDIUM,
-  dataTest: "test",
-};
-
-WithLabels.argTypes = {
-  type: {
-    options: Object.values(TYPES),
-    control: {
-      type: "select",
-    },
-  },
-  size: {
-    options: Object.values(SIZES),
-    control: {
-      type: "select",
-    },
-  },
-  icon: {
-    options: Object.keys(Icons),
-    control: {
-      type: "select",
-    },
-  },
-  iconColor: {
-    options: Object.values(ICON_COLORS),
-    control: {
-      type: "select",
-    },
-  },
-  spaceAfter: {
-    options: Object.values(SPACINGS_AFTER),
-    control: {
-      type: "select",
+    iconColor: {
+      options: Object.values(ICON_COLORS),
+      control: {
+        type: "select",
+      },
     },
   },
 };
 
-export const WithCarrier = ({ size, type }) => {
-  return (
-    <List size={size} type={type}>
+export const WithCarrier: Story = {
+  render: args => (
+    <List {...args}>
       <ListItem icon={<CarrierLogo carriers={[{ code: "FR", name: "Ryanair" }]} />}>
         Airline: Ryanair
       </ListItem>
@@ -150,115 +142,61 @@ export const WithCarrier = ({ size, type }) => {
       <ListItem icon={<Icons.Trip />}>PNR: TEST0X0</ListItem>
       <ListItem icon={<Icons.Airplane />}>Airbus A320 (320)</ListItem>
     </List>
-  );
-};
+  ),
 
-WithCarrier.story = {
-  name: "With carrier",
-
-  parameters: {
-    info: "List groups related information together and make content more scalable and organized.",
+  args: {
+    size: SIZES.SMALL,
+    type: TYPES.SECONDARY,
   },
 };
 
-WithCarrier.args = {
-  size: SIZES.SMALL,
-  type: TYPES.SECONDARY,
-};
+export const Playground: Story = {
+  render: ({ icon, iconColor, children, ...args }) => {
+    const Icon = typeof icon === "string" && getIcon(icon);
 
-WithCarrier.argTypes = {
-  size: {
-    options: Object.values(SIZES),
-    control: {
-      type: "select",
-    },
+    return (
+      <List {...args}>
+        {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          Array(...Array(3)).map((_, idx) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <ListItem key={idx} icon={<Icon color={iconColor} />}>
+              {children}
+            </ListItem>
+          ))
+        }
+      </List>
+    );
   },
-  type: {
-    options: Object.values(TYPES),
-    control: {
-      type: "select",
-    },
+
+  args: {
+    ...WithLabelAndIcon.args,
+    id: "list-id",
   },
-};
 
-export const Playground = ({ size, type, icon, iconColor, content, spaceAfter, dataTest }) => {
-  const Icon = getIcon(icon);
-
-  return (
-    <List size={size} type={type} dataTest={dataTest} spaceAfter={spaceAfter}>
-      <ListItem icon={Icon && <Icon color={iconColor} />}>{content}</ListItem>
-      <ListItem icon={Icon && <Icon color={iconColor} />}>{content}</ListItem>
-      <ListItem icon={Icon && <Icon color={iconColor} />}>{content}</ListItem>
-    </List>
-  );
-};
-
-Playground.story = {
-  parameters: {
-    info: "You can try all possible configurations of this component. However, check Orbit.Kiwi for more detailed design guidelines.",
+  argTypes: {
+    ...WithLabelAndIcon.argTypes,
   },
 };
 
-Playground.args = {
-  size: SIZES.NORMAL,
-  type: TYPES.PRIMARY,
-  icon: "Check",
-  iconColor: ICON_COLORS.SUCCESS,
-  content: "24,000 locations around the globe",
-  spaceAfter: SPACINGS_AFTER.SMALL,
-  dataTest: "test",
-};
-
-Playground.argTypes = {
-  size: {
-    options: Object.values(SIZES),
-    control: {
-      type: "select",
-    },
-  },
-  type: {
-    options: Object.values(TYPES),
-    control: {
-      type: "select",
-    },
-  },
-  icon: {
-    options: Object.keys(Icons),
-    control: {
-      type: "select",
-    },
-  },
-  iconColor: {
-    options: Object.values(ICON_COLORS),
-    control: {
-      type: "select",
-    },
-  },
-  spaceAfter: {
-    options: Object.values(SPACINGS_AFTER),
-    control: {
-      type: "select",
-    },
-  },
-};
-
-export const Rtl = () => (
-  <RenderInRtl>
-    <List size="small" type="secondary">
-      <ListItem icon={<CarrierLogo carriers={[{ code: "FR", name: "Ryanair" }]} />}>
-        Airline: Ryanair
-      </ListItem>
-      <ListItem icon={<Icons.InformationCircle />}>Flight no: FR 1337</ListItem>
-      <ListItem icon={<Icons.Trip />}>PNR: TEST0X0</ListItem>
-      <ListItem icon={<Icons.Airplane />}>Airbus A320 (320)</ListItem>
-    </List>
-  </RenderInRtl>
-);
-
-Rtl.story = {
-  name: "RTL",
+export const Rtl: Story = {
+  render: () => (
+    <RenderInRtl>
+      <List size="small" type="secondary">
+        <ListItem icon={<CarrierLogo carriers={[{ code: "FR", name: "Ryanair" }]} />}>
+          Airline: Ryanair
+        </ListItem>
+        <ListItem icon={<Icons.InformationCircle />}>Flight no: FR 1337</ListItem>
+        <ListItem icon={<Icons.Trip />}>PNR: TEST0X0</ListItem>
+        <ListItem icon={<Icons.Airplane />}>Airbus A320 (320)</ListItem>
+      </List>
+    </RenderInRtl>
+  ),
 
   parameters: {
     info: "This is a preview of this component in RTL setup.",
+    controls: {
+      disable: true,
+    },
   },
 };

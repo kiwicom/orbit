954bc7402f3630feaf63f9adf25a38690b532403
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-08-12 15:36:21 +0200

    chore(Loading): migrate from CSF2 to CSF3

--- packages/orbit-components/src/Loading/Loading.stories.tsx
+++ packages/orbit-components/src/Loading/Loading.stories.tsx
@@ -1,123 +1,136 @@
 import * as React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import TYPE_OPTIONS from "./consts";
-import Card from "../Card";
-import CardSection from "../Card/CardSection";
-import Illustration from "../Illustration";
-import Button from "../Button";
 import RenderInRtl from "../utils/rtl/RenderInRtl";
+import Text from "../Text";
+import Card, { CardSection } from "../Card";
+import Illustration from "../Illustration";
 
 import Loading from ".";
 
-export default {
+const meta: Meta<typeof Loading> = {
   title: "Loading",
-};
-
-export const Default = () => <Loading />;
-
-Default.story = {
-  parameters: {
-    info: "This is the default configuration of this component. Visit Orbit.Kiwi for more detailed guidelines.",
-  },
-};
-
-export const ButtonLoading = () => <Button loading>Default button</Button>;
-
-ButtonLoading.story = {
-  name: "Button loading",
+  component: Loading,
 
   parameters: {
-    info: "This is the default configuration of this component. Visit Orbit.Kiwi for more detailed guidelines.",
+    info: "Check Orbit.Kiwi for more detailed design guidelines.",
   },
-};
 
-export const CardLoading = ({ title, description, loading, loadingText }) => {
-  return (
-    <Card loading={loading} title={title} description={description}>
-      <Loading loading={loading} type="boxLoader" text={loadingText}>
-        <CardSection>
-          <Illustration name="EnjoyApp" size="medium" />
-        </CardSection>
-      </Loading>
-    </Card>
-  );
-};
-
-CardLoading.story = {
-  name: "Card loading",
-
-  parameters: {
-    info: "This is the default configuration of this component. Visit Orbit.Kiwi for more detailed guidelines.",
+  args: {
+    loading: true,
+    type: TYPE_OPTIONS.PAGE_LOADER,
   },
-};
 
-CardLoading.args = {
-  title: "Card with title",
-  description: "",
-  loading: true,
-  loadingText: "Please wait, Card content is loading...",
-};
-
-export const InlineLoader = ({ loadingText }) => {
-  return <Loading type={TYPE_OPTIONS.INLINE_LOADER} text={loadingText} />;
-};
-
-InlineLoader.story = {
-  name: "Inline loader",
-
-  parameters: {
-    info: "This configuration of this component is without any height, width or paddings. Visit Orbit.Kiwi for more detailed guidelines.",
-  },
-};
-
-InlineLoader.args = {
-  loadingText: "Please wait, content of the page is loading...",
-};
-
-export const CustomLoader = ({ customSize }) => {
-  return <Loading customSize={customSize} />;
-};
-
-CustomLoader.args = {
-  customSize: 50,
-};
-
-export const Playground = ({ type, loadingText, loading, dataTest }) => {
-  return <Loading loading={loading} type={type} text={loadingText} dataTest={dataTest} />;
-};
-
-Playground.story = {
-  parameters: {
-    info: "This is the default configuration of this component. Visit Orbit.Kiwi for more detailed guidelines.",
-  },
-};
-
-Playground.args = {
-  type: TYPE_OPTIONS.PAGE_LOADER,
-  loadingText: "Please wait, content of the page is loading...",
-  loading: true,
-  dataTest: "test",
-};
-
-Playground.argTypes = {
-  type: {
-    options: Object.values(TYPE_OPTIONS),
-    control: {
-      type: "select",
+  argTypes: {
+    type: {
+      options: Object.values(TYPE_OPTIONS),
+      control: {
+        type: "select",
+      },
     },
   },
 };
 
-export const Rtl = () => (
-  <RenderInRtl>
-    <Loading loading type="pageLoader" text="Please wait, content of the page is loading..." />
-  </RenderInRtl>
-);
+export default meta;
 
-Rtl.story = {
-  name: "RTL",
+type Story = StoryObj<typeof Loading>;
+
+export const Default: Story = {
+  parameters: {
+    controls: {
+      disable: true,
+    },
+  },
+};
+
+export const DefaultWithChildren: Story = {
+  render: args => (
+    <Loading {...args}>
+      <Text>Loading prop is false.</Text>
+    </Loading>
+  ),
+
+  parameters: {
+    controls: {
+      exclude: ["children", "customSize", "type"],
+    },
+  },
+
+  args: {
+    text: "Please wait, content is loading...",
+  },
+
+  argTypes: {
+    text: {
+      control: {
+        type: "text",
+      },
+    },
+  },
+};
+
+export const CardLoading: Story = {
+  render: ({ loading }) => {
+    return (
+      <Card loading={loading} title="Card title" description="Card description">
+        <Loading loading={loading} type="boxLoader">
+          <CardSection>
+            <Illustration name="EnjoyApp" size="medium" />
+          </CardSection>
+        </Loading>
+      </Card>
+    );
+  },
+
+  parameters: {
+    info: "Example of usage of Loading component inside Card. Check Orbit.Kiwi for more detailed guidelines.",
+    controls: {
+      exclude: ["children", "customSize", "text", "type"],
+    },
+  },
+};
+
+export const Playground: Story = {
+  render: ({ customSize, ...args }) => (
+    <div className="stroke-blue-dark">
+      <Loading {...args} customSize={customSize === 0 ? undefined : customSize} />
+    </div>
+  ),
+
+  args: {
+    ...DefaultWithChildren.args,
+    customSize: 50,
+    id: "loader-id",
+  },
+
+  argTypes: {
+    ...DefaultWithChildren.argTypes,
+  },
+
+  parameters: {
+    info: "Playground of Loading component. Type buttonLoader inherits parent's stroke color. For visualisation of undefined value for customSize arg, set value number 0. Visit Orbit.Kiwi for more detailed guidelines.",
+    controls: {
+      exclude: ["children", "loading"],
+    },
+  },
+};
+
+export const Rtl: Story = {
+  render: args => (
+    <RenderInRtl>
+      <Loading {...args} />
+    </RenderInRtl>
+  ),
+
+  args: {
+    type: TYPE_OPTIONS.INLINE_LOADER,
+  },
 
   parameters: {
     info: "This is a preview of this component in RTL setup.",
+    controls: {
+      disable: true,
+    },
   },
 };

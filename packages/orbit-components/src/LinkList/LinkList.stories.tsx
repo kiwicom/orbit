e2b854d54ea9f1b6517363a68e60ef2e7b1442a7
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-08-12 11:55:41 +0200

    chore(LinkList): migrate from CSF2 to CSF3

--- packages/orbit-components/src/LinkList/LinkList.stories.tsx
+++ packages/orbit-components/src/LinkList/LinkList.stories.tsx
@@ -1,43 +1,53 @@
 import * as React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import { DIRECTIONS, SPACINGS } from "../utils/layout/consts";
 import TextLink from "../TextLink";
 
 import LinkList from ".";
 
-export default {
+const meta: Meta<typeof LinkList> = {
   title: "LinkList",
+  component: LinkList,
+
+  parameters: {
+    info: "LinkList component. Check Orbit.Kiwi for more detailed design guidelines.",
+  },
+
+  args: {
+    direction: DIRECTIONS.ROW,
+    spacing: SPACINGS.FOUR_HUNDRED,
+    legacy: false,
+    indent: false,
+  },
+
+  argTypes: {
+    direction: {
+      options: Object.values(DIRECTIONS),
+      control: {
+        type: "select",
+      },
+    },
+    spacing: {
+      options: Object.values(SPACINGS),
+      control: {
+        type: "select",
+      },
+    },
+  },
 };
 
-export const Default = ({ direction, spacing, legacy, indent }) => {
-  return (
-    <LinkList direction={direction} spacing={spacing} legacy={legacy} indent={indent}>
+export default meta;
+
+type Story = StoryObj<typeof LinkList>;
+
+export const Playground: Story = {
+  render: args => (
+    <LinkList {...args}>
       <TextLink type="secondary">Flight 1</TextLink>
       <TextLink type="secondary">Flight 2</TextLink>
       <TextLink type="secondary">Flight 3</TextLink>
       <TextLink type="secondary">Flight 4</TextLink>
     </LinkList>
-  );
-};
-
-Default.args = {
-  direction: DIRECTIONS.ROW,
-  spacing: SPACINGS.FOUR_HUNDRED,
-  legacy: false,
-  indent: false,
-};
-
-Default.argTypes = {
-  direction: {
-    options: Object.values(DIRECTIONS),
-    control: {
-      type: "select",
-    },
-  },
-  spacing: {
-    options: Object.values(SPACINGS),
-    control: {
-      type: "select",
-    },
-  },
+  ),
 };

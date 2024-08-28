b3adffe1dc80c280fca4150afe94d10a13ad39fd
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-08-28 10:00:45 +0300

    feat(tailwind): add new rounded-200 class
    New class: rounded-200

--- packages/orbit-tailwind-preset/src/__fixtures__/BorderRadius.tsx
+++ packages/orbit-tailwind-preset/src/__fixtures__/BorderRadius.tsx
@@ -6,6 +6,7 @@ const BorderRadius = () => {
       <div className="rounded-50">50</div>
       <div className="rounded-100">100</div>
       <div className="rounded-150">150</div>
+      <div className="rounded-200">200</div>
       <div className="rounded-300">300</div>
       <div className="rounded-400">400</div>
       <div className="rounded-none">none</div>

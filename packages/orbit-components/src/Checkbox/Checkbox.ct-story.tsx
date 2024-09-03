f5bdc3f119b5f5635a2c88b128177693e0c598e9
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-09-03 17:07:14 +0200

    test(Checkbox): add visual tests for active tooltip

--- packages/orbit-components/src/Checkbox/Checkbox.ct-story.tsx
+++ packages/orbit-components/src/Checkbox/Checkbox.ct-story.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import Text from "../Text";
+import Tooltip from "../Tooltip";
 
 import Checkbox from ".";
 
-export default function CheckboxStory() {
+export function CheckboxStory() {
   return (
     <div className="space-y-200 flex flex-col">
       <Checkbox label="Check this box" />
@@ -12,8 +12,19 @@ export default function CheckboxStory() {
       <Checkbox label="Check this box" checked />
       <Checkbox label="Check this box" disabled />
       <Checkbox label="Check this box" disabled checked />
-      <Checkbox label="Check this box" disabled tooltip={<Text>Tooltip</Text>} />
       <Checkbox label="Check this box" hasError />
+    </div>
+  );
+}
+
+export function CheckboxWithTooltipStory() {
+  return (
+    <div className="space-y-200 lm:pt-1200 lm:min-h-800 flex min-h-[700px] flex-col">
+      <Checkbox
+        label="Check this box"
+        tooltip={<Tooltip content="Tooltip content is visible" placement="top" />}
+        checked
+      />
     </div>
   );
 }

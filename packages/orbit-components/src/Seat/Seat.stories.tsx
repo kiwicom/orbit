905b9191ac49a39e005935aceee153b1b73659e9
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-09-06 14:28:30 +0200

    chore(Seat): migrate from CSF2 to CSF3

--- packages/orbit-components/src/Seat/Seat.stories.tsx
+++ packages/orbit-components/src/Seat/Seat.stories.tsx
@@ -1,178 +1,82 @@
 import * as React from "react";
 import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { SIZE_OPTIONS } from "./consts";
-import Stack from "../Stack";
+import { SIZE_OPTIONS, TYPES } from "./consts";
 import SeatLegend from "./components/SeatLegend";
 
 import Seat from ".";
 
-export const Default = () => <Seat type="default" />;
-
-export const Mixed = ({ label, size }) => {
-  return (
-    <Stack direction="column">
-      <Stack align="end">
-        <Seat size={size} onClick={action("onClick")} label={label} price="12$" />
-        <Seat
-          size={size}
-          onClick={action("onClick")}
-          label={label}
-          price="12$"
-          type="unavailable"
-        />
-        <Seat
-          size={size}
-          onClick={action("onClick")}
-          label={label}
-          price="12$"
-          selected
-          type="legroom"
-        />
-      </Stack>
-      <Stack align="end">
-        <Seat
-          size={size}
-          onClick={action("onClick")}
-          label={label}
-          price="12$"
-          type="unavailable"
-        />
-        <Seat
-          size={size}
-          onClick={action("onClick")}
-          label={label}
-          price="12$"
-          selected
-          type="legroom"
-        />
-        <Seat size={size} onClick={action("onClick")} label={label} price="12$" />
-      </Stack>
-      <Stack align="end">
-        <Seat
-          size={size}
-          onClick={action("onClick")}
-          label={label}
-          price="12$"
-          selected
-          type="legroom"
-        />
-        <Seat size={size} onClick={action("onClick")} label={label} price="12$" />
-        <Seat
-          size={size}
-          onClick={action("onClick")}
-          label={label}
-          price="12$"
-          type="unavailable"
-        />
-      </Stack>
-    </Stack>
-  );
-};
-
-Mixed.args = {
-  label: "XY",
-  size: SIZE_OPTIONS.MEDIUM,
-};
-
-Mixed.argTypes = {
-  size: {
-    options: Object.values(SIZE_OPTIONS),
-    control: {
-      type: "select",
-    },
-  },
-};
-
-export const Selected = ({ size, dataTest, label }) => {
-  return (
-    <Stack direction="row">
-      <Seat size={size} onClick={action("onClick")} label={label} selected dataTest={dataTest} />
-      <Seat
-        size={size}
-        onClick={action("onClick")}
-        label={label}
-        selected
-        type="legroom"
-        dataTest={dataTest}
-      />
-      <Seat size={size} onClick={action("onClick")} label={label} selected dataTest={dataTest} />
-    </Stack>
-  );
-};
-
-Selected.args = {
-  size: SIZE_OPTIONS.MEDIUM,
-  dataTest: "test",
-  label: "XY",
-};
-
-Selected.argTypes = {
-  size: {
-    options: Object.values(SIZE_OPTIONS),
-    control: {
-      type: "select",
-    },
-  },
-};
-
-export const Playground = ({ dataTest, size, label, price, selected }) => {
-  return (
-    <Stack direction="row">
-      <Seat size={size} label={label} price={price} selected={selected} dataTest={dataTest} />
-      <Seat
-        size={size}
-        label={label}
-        price={price}
-        selected={selected}
-        onClick={action("onClick")}
-        type="legroom"
-        dataTest={dataTest}
-      />
-      <Seat
-        size={size}
-        label={label}
-        price={price}
-        onClick={action("onClick")}
-        selected={selected}
-        type="unavailable"
-        dataTest={dataTest}
-      />
-    </Stack>
-  );
-};
-
-Playground.args = {
-  dataTest: "test",
-  size: SIZE_OPTIONS.MEDIUM,
-  label: "XY",
-  price: "$12",
-  selected: false,
-};
-
-Playground.argTypes = {
-  size: {
-    options: Object.values(SIZE_OPTIONS),
-    control: {
-      type: "select",
-    },
-  },
-};
-
-export const Legend = () => {
-  return (
-    <Stack direction="column">
-      <SeatLegend label="Extra legroom ($ 5.99 – $ 12.98)" />
-      <SeatLegend type="legroom" label="Standard ($ 5.99 – $ 12.98)" />
-      <SeatLegend type="unavailable" label="Unavailable" />
-    </Stack>
-  );
-};
-
-export default {
+const meta: Meta<typeof Seat> = {
   title: "Seat",
-  includeStories: ["Default", "Selected", "Mixed", "Legend", "Playground"],
+  component: Seat,
+
   parameters: {
-    info: "Visit Orbit.Kiwi for more detailed guidelines.",
+    info: "Seat components stories. Visit Orbit.Kiwi for more detailed guidelines.",
+    controls: {
+      exclude: ["onClick"],
+    },
+  },
+
+  args: {
+    label: "XY",
+    type: TYPES.DEFAULT,
+  },
+
+  argTypes: {
+    type: {
+      options: Object.values(TYPES),
+      control: {
+        type: "select",
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Seat>;
+
+export const Default: Story = {
+  parameters: {
+    controls: {
+      exclude: ["type", "size", "title", "description", "selected", "onClick"],
+    },
+  },
+};
+
+export const Playground: Story = {
+  args: {
+    id: "seat-id",
+    size: SIZE_OPTIONS.MEDIUM,
+    price: "$12",
+    selected: false,
+    title: "25D",
+    description: "Extra legroom seat",
+    onClick: action("onClick"),
+  },
+
+  argTypes: {
+    size: {
+      options: Object.values(SIZE_OPTIONS),
+      control: {
+        type: "select",
+      },
+    },
+  },
+};
+
+export const Legend: Story = {
+  render: args => <SeatLegend {...args} />,
+
+  parameters: {
+    info: "SeatLegend component. Check Orbit.Kiwi for more detailed guidelines.",
+    controls: {
+      exclude: ["size", "title", "description", "selected", "onClick"],
+    },
+  },
+
+  args: {
+    label: "Extra legroom ($ 5.99 – $ 12.98)",
   },
 };

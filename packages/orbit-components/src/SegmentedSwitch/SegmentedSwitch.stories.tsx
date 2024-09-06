d29c0b2af14aa5216d7ee75211863cd6786e2623
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-09-06 14:28:46 +0200

    chore(SegmentedSwitch): migrate from CSF2 to CSF3

--- packages/orbit-components/src/SegmentedSwitch/SegmentedSwitch.stories.tsx
+++ packages/orbit-components/src/SegmentedSwitch/SegmentedSwitch.stories.tsx
@@ -1,78 +1,64 @@
 import * as React from "react";
 import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from "@storybook/react";
 
+import { SPACINGS_AFTER } from "../common/consts";
 import RenderInRtl from "../utils/rtl/RenderInRtl";
 
 import SegmentedSwitch from ".";
 
-export default {
+const meta: Meta<typeof SegmentedSwitch> = {
   title: "SegmentedSwitch",
-};
+  component: SegmentedSwitch,
 
-export const Default = ({ label, help, error, maxWidth, showTooltip }) => {
-  return (
-    <SegmentedSwitch
-      label={label}
-      onChange={action("onChange")}
-      onFocus={action("onFocus")}
-      showTooltip={showTooltip}
-      help={help}
-      error={error}
-      maxWidth={maxWidth}
-      options={[
-        { label: "Male", value: "Male" },
-        { label: "Female", value: "Female" },
-      ]}
-    />
-  );
-};
-
-Default.story = {
   parameters: {
-    info: "Check Orbit.Kiwi for more detailed design guidelines.",
+    info: "SegmentedSwitch component. Check Orbit.Kiwi for more detailed guidelines.",
+    controls: {
+      exclude: ["onChange", "onFocus"],
+    },
+  },
+
+  args: {
+    label: "Gender",
+    help: "When Chuck Norris plays dodgeball, the balls dodge him.",
+    error: "Chuck Norris makes onions cry.",
+    maxWidth: "20%",
+    showTooltip: false,
+    options: [
+      { label: "Male", value: "Male" },
+      { label: "Female", value: "Female" },
+    ],
+    onChange: action("onChange"),
+    onFocus: action("onFocus"),
+    spaceAfter: SPACINGS_AFTER.NORMAL,
+  },
+
+  argTypes: {
+    spaceAfter: {
+      options: Object.values(SPACINGS_AFTER),
+      control: {
+        type: "select",
+      },
+    },
   },
 };
 
-Default.args = {
-  label: "Gender",
-  help: "When Chuck Norris plays dodgeball, the balls dodge him.",
-  error: "Chuck Norris makes onions cry.",
-  maxWidth: "20%",
-  showTooltip: false,
-};
+export default meta;
 
-export const Rtl = ({ label, help, error, maxWidth, showTooltip }) => {
-  return (
-    <RenderInRtl>
-      <SegmentedSwitch
-        label={label}
-        onChange={action("onChange")}
-        onFocus={action("onFocus")}
-        showTooltip={showTooltip}
-        help={help}
-        error={error}
-        maxWidth={maxWidth}
-        options={[
-          { label: "Male", value: "Male" },
-          { label: "Female", value: "Female" },
-        ]}
-      />
-    </RenderInRtl>
-  );
-};
+type Story = StoryObj<typeof SegmentedSwitch>;
 
-Rtl.story = {
-  name: "RTL",
+export const Playground: Story = {};
+
+export const Rtl: Story = {
+  render: args => {
+    return (
+      <RenderInRtl>
+        <SegmentedSwitch {...args} />
+      </RenderInRtl>
+    );
+  },
 
   parameters: {
     info: "This is a preview of this component in RTL setup.",
   },
-};
-
-Rtl.args = {
-  label: "Gender",
-  help: "When Chuck Norris plays dodgeball, the balls dodge him.",
-  error: "Chuck Norris makes onions cry.",
-  maxWidth: "",
-  showTooltip: false,
 };

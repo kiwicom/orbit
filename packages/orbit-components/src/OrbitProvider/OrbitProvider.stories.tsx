6ea9e0e53b19338d50f50228d994f39045a474c0
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-09-06 14:28:38 +0200

    chore(OrbitProvider): migrate from CSF2 to CSF3

--- packages/orbit-components/src/OrbitProvider/OrbitProvider.stories.tsx
+++ packages/orbit-components/src/OrbitProvider/OrbitProvider.stories.tsx
@@ -1,50 +1,68 @@
 import * as React from "react";
 import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import Button from "../Button";
 import getTokens from "../getTokens";
 
 import OrbitProvider from ".";
 
-export function OwnTheme({ orbitTheme, customTheme }) {
-  return (
+interface OrbitThemeCustomProps {
+  orbitTheme: {
+    palette: {
+      [key: string]: {
+        [colorName: string]: string;
+      };
+    };
+  };
+  customTheme: {
+    [key: string]: string;
+  };
+}
+
+type OrbitProviderPropsAndCustomArgs = React.ComponentProps<typeof OrbitProvider> &
+  OrbitThemeCustomProps;
+
+const meta: Meta<OrbitProviderPropsAndCustomArgs> = {
+  title: "OrbitProvider",
+  component: OrbitProvider,
+
+  parameters: {
+    info: "Example how OrbitProvider component can be used. Visit Orbit.Kiwi for more detailed guidelines.",
+  },
+
+  args: {
+    orbitTheme: {
+      palette: {
+        product: {
+          light: "#ff9999",
+          lightHover: "#ff7f7f",
+          lightActive: "#ff6666",
+          normal: "#ff0000",
+          normalHover: "#e50000",
+          normalActive: "#cc0000",
+          dark: "#990000",
+          darkHover: "#720000",
+          darkActive: "#630000",
+          darker: "#530000",
+        },
+      },
+    },
+    customTheme: { black: "#000" },
+  },
+};
+
+type Story = StoryObj<OrbitProviderPropsAndCustomArgs>;
+
+export default meta;
+
+export const Default: Story = {
+  render: ({ orbitTheme, customTheme }) => (
     <OrbitProvider
       theme={{ orbit: { ...getTokens(orbitTheme), ...customTheme } }}
       useId={React.useId}
     >
       <Button onClick={action("onClick")}>Hello World!</Button>
     </OrbitProvider>
-  );
-}
-
-OwnTheme.story = {
-  name: "Own theme",
-
-  parameters: {
-    info: "This is the default configuration of this component. Visit Orbit.Kiwi for more detailed guidelines.",
-  },
-};
-
-OwnTheme.args = {
-  orbitTheme: {
-    palette: {
-      product: {
-        light: "#ff9999",
-        lightHover: "#ff7f7f",
-        lightActive: "#ff6666",
-        normal: "#ff0000",
-        normalHover: "#e50000",
-        normalActive: "#cc0000",
-        dark: "#990000",
-        darkHover: "#720000",
-        darkActive: "#630000",
-        darker: "#530000",
-      },
-    },
-  },
-  customTheme: { black: "#000" },
-};
-
-export default {
-  title: "OrbitProvider",
+  ),
 };

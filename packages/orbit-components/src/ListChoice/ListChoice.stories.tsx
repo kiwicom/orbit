6aa7338c20a0e9a3e6298426387378fb3f9ac63c
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-09-19 15:54:34 +0200

    chore(ListChoice): migrate from CSF2 to CSF3

--- packages/orbit-components/src/ListChoice/ListChoice.stories.tsx
+++ packages/orbit-components/src/ListChoice/ListChoice.stories.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { useArgs } from "@storybook/preview-api";
 import { action } from "@storybook/addon-actions";
 
 import Button from "../Button";
@@ -7,239 +9,173 @@ import RenderInRtl from "../utils/rtl/RenderInRtl";
 
 import ListChoice from ".";
 
+Button.displayName = "Button";
+ListChoice.displayName = "ListChoice";
+
 const getIcon = (source: string | null) => source && Icons[source];
 
-export default {
+const meta: Meta<typeof ListChoice> = {
   title: "ListChoice",
-};
-
-export const Default = ({ title, description, icon }) => {
-  const Icon = getIcon(icon);
-
-  return (
-    <ListChoice
-      title={title}
-      description={description}
-      icon={Icon && <Icon />}
-      onClick={action("onClick")}
-    />
-  );
-};
-
-Default.story = {
-  parameters: {
-    info: "Some description about this type of ListChoice in general.",
-  },
-};
-
-Default.args = {
-  title: "Choice Title",
-  description: "Further description",
-  icon: "Accommodation",
-};
-
-Default.argTypes = {
-  icon: {
-    options: Object.keys(Icons),
-    control: {
-      type: "select",
-    },
-  },
-};
-
-export const MultipleChoices = () => (
-  <>
-    <ListChoice
-      title="Choice Title"
-      description="Further description"
-      selectable
-      icon={<Icons.Accommodation />}
-      onClick={action("onClick")}
-    />
-    <ListChoice
-      title="Choice Title"
-      description="Further description"
-      selectable
-      selected
-      disabled
-      icon={<Icons.Accommodation />}
-      onClick={action("onClick")}
-    />
-    <ListChoice
-      title="Choice Title"
-      description="Further description"
-      selectable
-      icon={<Icons.Accommodation />}
-      onClick={action("onClick")}
-    />
-  </>
-);
-
-MultipleChoices.story = {
-  name: "Multiple choices",
+  component: ListChoice,
 
   parameters: {
-    info: "Some description about this type of ListChoice in general.",
+    info: "Check Orbit.Kiwi for more detailed design guidelines.",
+  },
+
+  args: {
+    title: "Choice Title",
   },
 };
 
-export const WithAction = ({ title, description, disabled, icon, dataTest }) => {
-  const Icon = getIcon(icon);
+export default meta;
 
-  return (
-    <ListChoice
-      title={title}
-      description={description}
-      disabled={disabled}
-      icon={Icon && <Icon />}
-      action={
-        <Button
-          onClick={action("onClick")}
-          iconLeft={<Icons.Plus />}
-          size="small"
-          type="primarySubtle"
-        />
-      }
-      onClick={action("onClick")}
-      dataTest={dataTest}
-    />
-  );
-};
+type Story = StoryObj<typeof ListChoice>;
 
-WithAction.args = {
-  title: "Choice Title",
-  description: "Further description",
-  disabled: false,
-  icon: "Accommodation",
-  dataTest: "test",
-};
-
-WithAction.argTypes = {
-  icon: {
-    options: Object.keys(Icons),
-    control: {
-      type: "select",
-    },
-  },
-};
-
-export const Playground = ({
-  title,
-  description,
-  selectable,
-  selected,
-  disabled,
-  icon,
-  dataTest,
-}) => {
-  const Icon = getIcon(icon);
-
-  return (
-    <ListChoice
-      title={title}
-      description={description}
-      selectable={selectable}
-      selected={selected}
-      disabled={disabled}
-      icon={Icon && <Icon />}
-      onClick={action("onClick")}
-      dataTest={dataTest}
-    />
-  );
-};
-
-Playground.story = {
+export const Default: Story = {
   parameters: {
-    info: "Some description about this type of InputField in general.",
-  },
-};
-
-Playground.args = {
-  title: "Choice Title",
-  description: "Further description",
-  selectable: true,
-  selected: false,
-  disabled: false,
-  icon: "Accommodation",
-  dataTest: "test",
-};
-
-Playground.argTypes = {
-  icon: {
-    options: Object.keys(Icons),
-    control: {
-      type: "select",
+    info: "Minimal setup with title, uncontrolled, not selectable. Check Orbit.Kiwi for more detailed design guidelines.",
+    controls: {
+      exclude: ["tabIndex"],
     },
   },
 };
 
-export const Controlled = ({ icon }) => {
-  const [selected, setSelected] = React.useState(false);
+export const MultipleChoices: Story = {
+  render: ({ icon, ...args }) => {
+    const Icon = typeof icon === "string" && getIcon(icon);
 
-  const Icon = getIcon(icon);
+    return (
+      <>
+        {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          Array(...Array(3)).map((_, idx) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <ListChoice key={idx} {...args} icon={<Icon />} />
+          ))
+        }
+      </>
+    );
+  },
 
-  return (
-    <ListChoice
-      title="Controlled ListChoice"
-      description="This ListChoice is controlled by React.useState"
-      icon={Icon && <Icon />}
-      selectable
-      selected={selected}
-      onClick={e => {
-        action("onClick")(e);
-        setSelected(!selected);
-      }}
-    />
-  );
-};
+  args: {
+    description: "Further description",
+    icon: "Accommodation",
+    selectable: true,
+    selected: false,
+    disabled: false,
+    tabIndex: 0,
+    role: "list",
+    onClick: action("onClick"),
+  },
 
-Controlled.args = {
-  icon: "Accommodation",
-};
+  argTypes: {
+    icon: {
+      options: Object.keys(Icons),
+      control: {
+        type: "select",
+      },
+    },
+  },
 
-Controlled.argTypes = {
-  icon: {
-    options: Object.keys(Icons),
-    control: {
-      type: "select",
+  parameters: {
+    controls: {
+      exclude: ["onClick"],
     },
   },
 };
 
-export const Rtl = () => (
-  <RenderInRtl>
-    <>
+export const WithAction: Story = {
+  render: ({ icon, ...args }) => {
+    const Icon = typeof icon === "string" && getIcon(icon);
+    const IconButton = getIcon("Plus");
+
+    return (
       <ListChoice
-        title="Choice Title"
-        description="Further description"
-        selectable
-        icon={<Icons.Accommodation />}
-        onClick={action("onClick")}
+        {...args}
+        icon={<Icon />}
+        action={
+          <Button
+            onClick={action("onClick")}
+            iconLeft={<IconButton />}
+            size="small"
+            type="primarySubtle"
+          />
+        }
       />
-      <ListChoice
-        title="Choice Title"
-        description="Further description"
-        selectable
-        selected
-        disabled
-        icon={<Icons.Accommodation />}
-        onClick={action("onClick")}
-      />
-      <ListChoice
-        title="Choice Title"
-        description="Further description"
-        selectable
-        icon={<Icons.Accommodation />}
-        onClick={action("onClick")}
-      />
-    </>
-  </RenderInRtl>
-);
+    );
+  },
 
-Rtl.story = {
-  name: "RTL",
+  args: {
+    ...MultipleChoices.args,
+    selectable: false,
+  },
+
+  argTypes: {
+    ...MultipleChoices.argTypes,
+  },
 
   parameters: {
-    info: "Some description about this type of ListChoice.",
+    controls: {
+      exclude: ["onClick"],
+    },
+  },
+};
+
+export const Playground: Story = {
+  render: function Render({ icon, ...args }) {
+    const [{ selected }, updateArgs] = useArgs();
+
+    function onClick() {
+      updateArgs({ selected: !selected });
+    }
+
+    const Icon = typeof icon === "string" && getIcon(icon);
+
+    return <ListChoice {...args} onClick={() => onClick()} icon={<Icon />} />;
+  },
+
+  args: {
+    ...MultipleChoices.args,
+    id: "listchoice-id",
+  },
+
+  argTypes: {
+    ...MultipleChoices.argTypes,
+  },
+
+  parameters: {
+    controls: {
+      exclude: ["onClick"],
+    },
+  },
+};
+
+export const Rtl: Story = {
+  render: ({ icon, ...args }) => {
+    const Icon = typeof icon === "string" && getIcon(icon);
+
+    return (
+      <RenderInRtl>
+        <>
+          <ListChoice {...args} icon={<Icon />} />
+          <ListChoice {...args} icon={<Icon />} selected disabled />
+          <ListChoice {...args} icon={<Icon />} />
+        </>
+      </RenderInRtl>
+    );
+  },
+
+  args: {
+    ...MultipleChoices.args,
+  },
+
+  argTypes: {
+    ...MultipleChoices.argTypes,
+  },
+
+  parameters: {
+    controls: {
+      disable: true,
+    },
   },
 };

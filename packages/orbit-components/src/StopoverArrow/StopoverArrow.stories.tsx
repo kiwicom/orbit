c84ab09850f831f53e1df3453242ad2c65da32a7
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-09-24 18:24:15 +0200

    chore(StopoverArrow): migrate from CSF2 to CSF3

--- packages/orbit-components/src/StopoverArrow/StopoverArrow.stories.tsx
+++ packages/orbit-components/src/StopoverArrow/StopoverArrow.stories.tsx
@@ -1,65 +1,48 @@
 import * as React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import STOPS from "./consts";
 import RenderInRtl from "../utils/rtl/RenderInRtl";
 
 import StopoverArrow from ".";
 
-export default {
+const meta: Meta<typeof StopoverArrow> = {
   title: "StopoverArrow",
+  component: StopoverArrow,
+
+  args: {
+    stops: STOPS.ZERO,
+    id: "ID",
+  },
+
+  argTypes: {
+    stops: {
+      options: Object.values(STOPS),
+      control: {
+        type: "select",
+      },
+    },
+  },
 };
 
-export const Playground = ({ stops, dataTest, id }) => {
-  return <StopoverArrow stops={stops} dataTest={dataTest} id={id} />;
-};
+type Story = StoryObj<typeof StopoverArrow>;
 
-Playground.story = {
+export default meta;
+
+export const Playground: Story = {
   parameters: {
     info: "You can try all possible configurations of this component. However, check Orbit.Kiwi for more detailed design guidelines.",
   },
 };
 
-Playground.args = {
-  stops: STOPS.ZERO,
-  dataTest: "test",
-  id: "ID",
-};
-
-Playground.argTypes = {
-  stops: {
-    options: Object.values(STOPS),
-    control: {
-      type: "select",
-    },
-  },
-};
-
-export const InRtl = ({ stops, dataTest }) => {
-  return (
+export const Rtl: Story = {
+  render: args => (
     <RenderInRtl>
-      <StopoverArrow stops={stops} dataTest={dataTest} />
+      <StopoverArrow {...args} />
     </RenderInRtl>
-  );
-};
-
-InRtl.story = {
-  name: "In RTL",
+  ),
 
   parameters: {
-    info: "You can try all possible configurations of this component. However, check Orbit.Kiwi for more detailed design guidelines.",
-  },
-};
-
-InRtl.args = {
-  stops: STOPS.THREE,
-  dataTest: "test",
-};
-
-InRtl.argTypes = {
-  stops: {
-    options: Object.values(STOPS),
-    control: {
-      type: "select",
-    },
+    info: "This is a preview of StopoverArrow component in RTL setup.",
   },
 };

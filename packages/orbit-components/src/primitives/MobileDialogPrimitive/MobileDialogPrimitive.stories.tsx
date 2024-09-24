13dbe8e2f273d05dcc4e3c6ab8efa4381c7c0980
Dominika Hustinova <domi.hustinova@gmail.com>
2024-09-24 13:25:09 +0200

    chore(MobileDialogPrimitive): migrate stories from CSF2 to CSF3

--- packages/orbit-components/src/primitives/MobileDialogPrimitive/MobileDialogPrimitive.stories.tsx
+++ packages/orbit-components/src/primitives/MobileDialogPrimitive/MobileDialogPrimitive.stories.tsx
@@ -1,49 +1,50 @@
 import * as React from "react";
+import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from "@storybook/react";
 
-import Button from "../../Button";
+import Alert from "../../Alert";
+import Stack from "../../Stack";
+import Text from "../../Text";
 
-import TooltipPrimitive from ".";
+import MobileDialogPrimitive from ".";
 
-export default {
+const meta: Meta<typeof MobileDialogPrimitive> = {
   title: "MobileDialogPrimitive",
-};
+  component: MobileDialogPrimitive,
 
-export const Playground = ({
-  content,
-  dataTest,
-  tabIndex,
-  enabled,
-  stopPropagation,
-  removeUnderlinedText,
-  labelClose,
-}) => {
-  return (
-    <TooltipPrimitive
-      content={content}
-      dataTest={dataTest}
-      tabIndex={tabIndex}
-      labelClose={labelClose}
-      enabled={enabled}
-      stopPropagation={stopPropagation}
-      removeUnderlinedText={removeUnderlinedText}
-    >
-      <Button>Open Mobile Dialog</Button>
-    </TooltipPrimitive>
-  );
-};
-
-Playground.story = {
   parameters: {
-    info: "You can try all possible configurations of this component. However, check Orbit.Kiwi for more detailed design guidelines.",
+    info: "You can try all possible configurations of this component. Visit Orbit.Kiwi for more detailed guidelines.",
+    controls: {
+      exclude: ["onShow"],
+    },
+  },
+
+  args: {
+    labelClose: "Close",
+    enabled: true,
+    onShow: action("onShow"),
+    renderInPortal: true,
+    tabIndex: 0,
+    content: "Write your text here.",
+    stopPropagation: true,
+    removeUnderlinedText: false,
+    block: false,
+    lockScrolling: false,
   },
 };
 
-Playground.args = {
-  content: "Write your text here.",
-  dataTest: "test",
-  tabIndex: 0,
-  enabled: true,
-  stopPropagation: true,
-  removeUnderlinedText: false,
-  labelClose: "Close",
+export default meta;
+type Story = StoryObj<typeof MobileDialogPrimitive>;
+
+export const Playground: Story = {
+  render: args => (
+    <Alert icon title="Lorem ipsum dolor sit amet" type="warning">
+      <Stack spacing="none">
+        Excepteur sint occaecat cupidatat non proident.{" "}
+        <MobileDialogPrimitive {...args}>
+          <Text>Aliquam erat volutpat.</Text>
+        </MobileDialogPrimitive>
+      </Stack>
+    </Alert>
+  ),
 };

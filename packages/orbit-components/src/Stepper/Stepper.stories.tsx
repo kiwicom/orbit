966bee6cc5611af2d17c920656dfbb3cb207141e
Sarka Chwastkova <chwastkova.s@gmail.com>
2024-09-24 16:55:30 +0200

    chore(Stepper): migrate from CSF2 to CSF3

--- packages/orbit-components/src/Stepper/Stepper.stories.tsx
+++ packages/orbit-components/src/Stepper/Stepper.stories.tsx
@@ -1,136 +1,98 @@
 import * as React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
+import RenderInRtl from "../utils/rtl/RenderInRtl";
 import StatelessStepper from "./StepperStateless";
 
 import Stepper from ".";
 
-export default {
+const meta: Meta<typeof Stepper> = {
   title: "Stepper",
+  component: Stepper,
 };
 
-export const Default = ({ titleIncrement, titleDecrement }) => {
-  return (
-    <Stepper
-      titleIncrement={titleIncrement}
-      titleDecrement={titleDecrement}
-      onChange={action("onChange")}
-    />
-  );
-};
+export default meta;
 
-Default.story = {
+type Story = StoryObj<typeof Stepper>;
+
+export const Default: Story = {
   parameters: {
-    info: "Some description about this type of Stepper in general.",
+    info: "Default settings of Stepper component. Check Orbit.Kiwi for more detailed guidelines.",
+    controls: {
+      disable: true,
+    },
   },
 };
 
-Default.args = {
-  titleIncrement: "Add a passenger",
-  titleDecrement: "Remove a passenger",
-};
+export const Playground: Story = {
+  args: {
+    id: "stepper-ID",
+    name: "Passengers number",
+    step: 1,
+    minValue: 0,
+    maxValue: 20,
+    defaultValue: 0,
+    active: false,
+    disabled: false,
+    maxWidth: 120,
+    titleIncrement: "Add a passenger",
+    titleDecrement: "Remove a passenger",
+    onChange: action("onChange"),
+    onFocus: action("onFocus"),
+    onBlur: action("onBlur"),
+  },
 
-export const Stateless = ({
-  min,
-  max,
-  value,
-  name,
-  disabled,
-  dataTest,
-  disabledIncrement,
-  disabledDecrement,
-  titleIncrement,
-  titleDecrement,
-  active,
-}) => {
-  return (
-    <StatelessStepper
-      dataTest={dataTest}
-      value={value}
-      active={active}
-      name={name}
-      maxValue={max}
-      minValue={min}
-      disabled={disabled}
-      disabledIncrement={disabledIncrement}
-      disabledDecrement={disabledDecrement}
-      titleIncrement={titleIncrement}
-      titleDecrement={titleDecrement}
-      onFocus={action("onFocus")}
-      onBlur={action("onBlur")}
-      onChange={action("onChange")}
-      onKeyDown={action("onKeyDown")}
-      onIncrement={action("onIncrement")}
-      onDecrement={action("onDecrement")}
-    />
-  );
-};
-
-Stateless.story = {
   parameters: {
-    info: "Here you can try StatelessStepper component with additional functionality.",
+    info: "Playground of Stepper component. Check Orbit.Kiwi for more detailed guidelines.",
+    controls: {
+      exclude: ["onFocus", "onBlur", "onChange"],
+    },
   },
 };
 
-Stateless.args = {
-  min: 1,
-  max: 10,
-  value: "2 adults",
-  name: "name",
-  disabled: false,
-  dataTest: "test",
-  disabledIncrement: false,
-  disabledDecrement: false,
-  titleIncrement: "Add a passenger",
-  titleDecrement: "Remove a passenger",
-  active: false,
-};
+export const Stateless: Story & StoryObj<typeof StatelessStepper> = {
+  render: args => <StatelessStepper {...args} />,
 
-export const Playground = ({
-  min,
-  max,
-  step,
-  defaultValue,
-  name,
-  disabled,
-  dataTest,
-  titleIncrement,
-  titleDecrement,
-  active,
-}) => {
-  return (
-    <Stepper
-      defaultValue={defaultValue}
-      step={step}
-      active={active}
-      name={name}
-      maxValue={max}
-      minValue={min}
-      disabled={disabled}
-      dataTest={dataTest}
-      titleIncrement={titleIncrement}
-      titleDecrement={titleDecrement}
-      onChange={action("onChange")}
-      onFocus={action("onFocus")}
-      onBlur={action("onBlur")}
-    />
-  );
-};
-Playground.story = {
+  args: {
+    ...Playground.args,
+    defaultValue: undefined,
+    onChange: undefined,
+    value: 0,
+    disabledIncrement: false,
+    disabledDecrement: false,
+    onKeyDown: action("onKeyDown"),
+    onIncrement: action("onIncrement"),
+    onDecrement: action("onDecrement"),
+  },
+
   parameters: {
-    info: "Here you can try Stepper component with additional functionality.",
+    info: "Stepper stateless component. Check Orbit.Kiwi for more detailed guidelines.",
+    controls: {
+      exclude: [
+        "onFocus",
+        "onBlur",
+        "onChange",
+        "onKeyDown",
+        "onIncrement",
+        "onDecrement",
+        "defaultValue",
+      ],
+    },
   },
 };
 
-Playground.args = {
-  min: 1,
-  max: 10,
-  step: 2,
-  defaultValue: 4,
-  name: "name",
-  disabled: false,
-  dataTest: "test",
-  titleIncrement: "Add a passenger",
-  titleDecrement: "Remove a passenger",
-  active: false,
+export const Rtl: Story = {
+  render: () => (
+    <RenderInRtl>
+      <Stepper />
+    </RenderInRtl>
+  ),
+
+  parameters: {
+    info: "This is a preview of Stepper component in RTL setup.",
+    controls: {
+      disable: true,
+    },
+  },
 };

8aa69cf85538187b5fe2e298aba4835e70ec7c94
Daniel Sil <daniel.sil@kiwi.com>
2024-11-14 12:38:28 +0100

    chore: adapt to new Heading props

--- packages/orbit-components/cypress/integration/pages/media-queries.tsx
+++ packages/orbit-components/cypress/integration/pages/media-queries.tsx
@@ -5,12 +5,36 @@ export default function MediaQueries() {
   const query = useMediaQuery();
   return (
     <>
-      {query.isMediumMobile && <Heading type="title0">Medium mobile</Heading>}
-      {query.isLargeMobile && <Heading type="title0">Large mobile</Heading>}
-      {query.isTablet && <Heading type="title0">Tablet</Heading>}
-      {query.isDesktop && <Heading type="title0">Desktop</Heading>}
-      {query.isLargeDesktop && <Heading type="title0">Large desktop</Heading>}
-      {query.prefersReducedMotion && <Heading type="title0">Reduced motion</Heading>}
+      {query.isMediumMobile && (
+        <Heading role="heading" level={1} type="title0">
+          Medium mobile
+        </Heading>
+      )}
+      {query.isLargeMobile && (
+        <Heading role="heading" level={1} type="title0">
+          Large mobile
+        </Heading>
+      )}
+      {query.isTablet && (
+        <Heading role="heading" level={1} type="title0">
+          Tablet
+        </Heading>
+      )}
+      {query.isDesktop && (
+        <Heading role="heading" level={1} type="title0">
+          Desktop
+        </Heading>
+      )}
+      {query.isLargeDesktop && (
+        <Heading role="heading" level={1} type="title0">
+          Large desktop
+        </Heading>
+      )}
+      {query.prefersReducedMotion && (
+        <Heading role="heading" level={1} type="title0">
+          Reduced motion
+        </Heading>
+      )}
     </>
   );
 }

f065631d8cd460fb1d064e424f9d9dbd86c71ff7
Daniel Sil <daniel.sil@kiwi.com>
2024-12-19 17:41:05 +0100

    feat: remove deprecated left and right util functions
    BREAKING CHANGE: these functions were deprecated and are now removed.

--- packages/orbit-components/src/utils/rtl/index.ts
+++ packages/orbit-components/src/utils/rtl/index.ts
@@ -1,10 +1,4 @@
-import type { Theme } from "../../defaultTheme";
-import type { RtlSpacing, LeftToRight } from "./types";
-
-const leftToRight: LeftToRight =
-  (left: string, right: string) =>
-  ({ theme }: { theme: Theme }) =>
-    theme.rtl ? right : left;
+import type { RtlSpacing } from "./types";
 
 export const rtlSpacing: RtlSpacing =
   value =>
@@ -13,13 +7,3 @@ export const rtlSpacing: RtlSpacing =
     const parts = value.split(" ").filter(part => !Number.isNaN(parseFloat(part)) && part);
     return parts.length === 4 ? [parts[0], parts[3], parts[2], parts[1]].join(" ") : value;
   };
-
-/**
- * @deprecated This function is deprecated. Use `start` properties instead.
- */
-export const left = leftToRight("left", "right");
-
-/**
- * @deprecated This function is deprecated. Use `end` properties instead.
- */
-export const right = leftToRight("right", "left");
--- packages/orbit-components/src/utils/rtl/types.d.ts
+++ packages/orbit-components/src/utils/rtl/types.d.ts
@@ -1,5 +1,3 @@
 import type { ThemeProps } from "../../defaultTheme";
 
-export type LeftToRight = (left: string, right: string) => (theme: ThemeProps) => string;
-
 export type RtlSpacing = (value: string) => (theme: ThemeProps) => string;

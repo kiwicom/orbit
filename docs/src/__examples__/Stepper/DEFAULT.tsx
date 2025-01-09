22c13f441c443e748613571f488801521847c4a2
Sarka Chwastkova <chwastkova.s@gmail.com>
2025-01-09 16:24:51 +0100

    docs: add accessibility docs for Stepper(StepperStateless) component

--- docs/src/__examples__/Stepper/DEFAULT.tsx
+++ docs/src/__examples__/Stepper/DEFAULT.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { Heading, Stepper, Stack } from "@kiwicom/orbit-components";
+import { Heading, Stepper, Stack, OrbitProvider, defaultTheme } from "@kiwicom/orbit-components";
 import { Passengers } from "@kiwicom/orbit-components/icons";
 
 export default {
   Example: () => (
-    <Stack align="center" spacing="400" desktop={{ spacing: "600" }}>
-      <Heading type="title4">
-        <Stack align="center">
-          <Passengers />
-          Travelers
-        </Stack>
-      </Heading>
-      <div style={{ maxWidth: "8em" }}>
-        <Stepper defaultValue={2} maxValue={10} minValue={1} />
-      </div>
-    </Stack>
+    <OrbitProvider theme={defaultTheme} useId={React.useId}>
+      <Stack align="center" spacing="400" desktop={{ spacing: "600" }}>
+        <Heading type="title4">
+          <Stack align="center">
+            <Passengers />
+            Travelers
+          </Stack>
+        </Heading>
+        <div style={{ maxWidth: "8em" }}>
+          <Stepper defaultValue={2} maxValue={10} minValue={1} />
+        </div>
+      </Stack>
+    </OrbitProvider>
   ),
   exampleKnobs: [
     {

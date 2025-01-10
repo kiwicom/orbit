6aa6810fde0d65b83b9b7b381e69a7c8e23ef049
Daniel Sil <daniel.sil@kiwi.com>
2025-01-10 16:08:55 +0100

    chore: adjust playwright test wrapper
    CSS vars were not being injected in the test environment

--- packages/orbit-components/playwright/index.tsx
+++ packages/orbit-components/playwright/index.tsx
@@ -2,10 +2,23 @@ import "./index.css";
 
 import * as React from "react";
 import { beforeMount } from "@playwright/experimental-ct-react/hooks";
+import { tokensToCssVars } from "@kiwicom/orbit-design-tokens";
 
 import RandomIdProvider from "../src/OrbitProvider/RandomId/Provider";
+import { getCssVarsForWL } from "../src/OrbitProvider";
+import defaultTheme from "../src/defaultTheme";
 
 beforeMount(async ({ App }) => {
+  const ID = "orbit-theme-css-vars";
+  const style = document.createElement("style");
+  style.setAttribute("id", ID);
+  style.innerText = tokensToCssVars({
+    tokens: getCssVarsForWL(defaultTheme.orbit),
+    cssClass: ":root",
+  });
+  const head = document.getElementsByTagName("head")[0];
+  head.appendChild(style);
+
   return (
     <RandomIdProvider useId={React.useId}>
       <App />

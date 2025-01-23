kiwicom/orbit

commit 5c4981f68f01301ca8c49cdebbca9abac706eb48

chore: adapt components to new tokens

packages/orbit-components/src/Accordion/AccordionSection/components/SectionFooter.tsx
@@ -10,7 +10,7 @@ const AccordionSectionFooter = ({ children, dataTest }: Props) => (
   <div
     // the border-radius is calculated based on the border-radius and border-width of the AccordionSection component
     // outer border-radius is 4px, border-width is 1px so inner border-radius is 4px - 1px = 3px
-    className="p-600 rounded-b-normal shadow-level2 sticky bottom-0 flex"
+    className="p-600 rounded-b-100 shadow-level2 sticky bottom-0 flex"
     data-test={dataTest && `${dataTest}Footer`}
   >
     {children}

packages/orbit-components/config/generateEmailIcons.mts
@@ -8,9 +8,9 @@ const DIR = path.join(__dirname, "../orbit-email-icons");
 const sizesToGenerate = [32, 48];
 const colors = [
   ["white", defaultTokens.paletteWhite],
-  ["secondary", defaultTokens.colorIconSecondary],
-  ["tertiary", defaultTokens.colorIconTertiary],
-  ["primary", defaultTokens.colorIconPrimary],
+  ["secondary", defaultTokens.iconSecondaryForeground],
+  ["tertiary", defaultTokens.iconTertiaryForeground],
+  ["primary", defaultTokens.iconPrimaryForeground],
   ["warning", defaultTokens.paletteOrangeNormal],
   ["error", defaultTokens.paletteRedNormal],
   ["success", defaultTokens.paletteGreenNormal],

packages/orbit-components/cypress/integration/pages/box-mediaquery-props.tsx
@@ -13,7 +13,7 @@ export default function BoxMediaProps() {
       }}
       largeMobile={{
         textAlign: "center",
-        borderRadius: "large",
+        borderRadius: "150",
         overflow: "scroll",
         shrink: 0,
         color: "orangeDark",

packages/orbit-components/src/Alert/AlertButton/helpers/getAlertButtonTypeToken.ts
@@ -19,31 +19,31 @@ type GetAlertButtonTypeToken = (name: TokenName, type: Type, theme: Theme) => st
 const getAlertButtonTypeToken: GetAlertButtonTypeToken = (name, type, theme) => {
   const tokens = {
     [TOKENS.backgroundButton]: {
-      [TYPE_OPTIONS.INFO]: theme.orbit.backgroundButtonInfo,
-      [TYPE_OPTIONS.SUCCESS]: theme.orbit.backgroundButtonSuccess,
-      [TYPE_OPTIONS.WARNING]: theme.orbit.backgroundButtonWarning,
-      [TYPE_OPTIONS.CRITICAL]: theme.orbit.backgroundButtonCritical,
+      [TYPE_OPTIONS.INFO]: theme.orbit.buttonInfoBackground,
+      [TYPE_OPTIONS.SUCCESS]: theme.orbit.buttonSuccessBackground,
+      [TYPE_OPTIONS.WARNING]: theme.orbit.buttonWarningBackground,
+      [TYPE_OPTIONS.CRITICAL]: theme.orbit.buttonCriticalBackground,
       [TYPE_OPTIONS.INFO_SUBTLE]: convertHexToRgba(theme.orbit.paletteBlueNormal, 12),
       [TYPE_OPTIONS.SUCCESS_SUBTLE]: convertHexToRgba(theme.orbit.paletteGreenNormal, 12),
       [TYPE_OPTIONS.WARNING_SUBTLE]: convertHexToRgba(theme.orbit.paletteOrangeNormal, 12),
       [TYPE_OPTIONS.CRITICAL_SUBTLE]: convertHexToRgba(theme.orbit.paletteRedNormal, 12),
-      [TYPE_OPTIONS.SECONDARY]: theme.orbit.backgroundButtonSecondary,
+      [TYPE_OPTIONS.SECONDARY]: theme.orbit.buttonSecondaryBackground,
     },
     [TOKENS.backgroundButtonHover]: {
-      [TYPE_OPTIONS.INFO]: theme.orbit.backgroundButtonInfoHover,
-      [TYPE_OPTIONS.SUCCESS]: theme.orbit.backgroundButtonSuccessHover,
-      [TYPE_OPTIONS.WARNING]: theme.orbit.backgroundButtonWarningHover,
-      [TYPE_OPTIONS.CRITICAL]: theme.orbit.backgroundButtonCriticalHover,
+      [TYPE_OPTIONS.INFO]: theme.orbit.buttonInfoBackgroundHover,
+      [TYPE_OPTIONS.SUCCESS]: theme.orbit.buttonSuccessBackgroundHover,
+      [TYPE_OPTIONS.WARNING]: theme.orbit.buttonWarningBackgroundHover,
+      [TYPE_OPTIONS.CRITICAL]: theme.orbit.buttonCriticalBackgroundHover,
       [TYPE_OPTIONS.INFO_SUBTLE]: convertHexToRgba(theme.orbit.paletteBlueNormal, 18),
       [TYPE_OPTIONS.SUCCESS_SUBTLE]: convertHexToRgba(theme.orbit.paletteGreenNormal, 18),
       [TYPE_OPTIONS.WARNING_SUBTLE]: convertHexToRgba(theme.orbit.paletteOrangeNormal, 18),
       [TYPE_OPTIONS.CRITICAL_SUBTLE]: convertHexToRgba(theme.orbit.paletteRedNormal, 18),
     },
     [TOKENS.backgroundButtonActive]: {
-      [TYPE_OPTIONS.INFO]: theme.orbit.backgroundButtonInfoActive,
-      [TYPE_OPTIONS.SUCCESS]: theme.orbit.backgroundButtonSuccessActive,
-      [TYPE_OPTIONS.WARNING]: theme.orbit.backgroundButtonWarningActive,
-      [TYPE_OPTIONS.CRITICAL]: theme.orbit.backgroundButtonCriticalActive,
+      [TYPE_OPTIONS.INFO]: theme.orbit.buttonInfoBackgroundActive,
+      [TYPE_OPTIONS.SUCCESS]: theme.orbit.buttonSuccessBackgroundActive,
+      [TYPE_OPTIONS.WARNING]: theme.orbit.buttonWarningBackgroundActive,
+      [TYPE_OPTIONS.CRITICAL]: theme.orbit.buttonCriticalBackgroundActive,
       [TYPE_OPTIONS.INFO_SUBTLE]: convertHexToRgba(theme.orbit.paletteBlueNormal, 24),
       [TYPE_OPTIONS.SUCCESS_SUBTLE]: convertHexToRgba(theme.orbit.paletteGreenNormal, 24),
       [TYPE_OPTIONS.WARNING_SUBTLE]: convertHexToRgba(theme.orbit.paletteOrangeNormal, 24),
@@ -60,10 +60,10 @@ const getAlertButtonTypeToken: GetAlertButtonTypeToken = (name, type, theme) => 
       [TYPE_OPTIONS.CRITICAL_SUBTLE]: theme.orbit.paletteRedLight,
     },
     [TOKENS.colorTextButton]: {
-      [TYPE_OPTIONS.INFO]: theme.orbit.colorTextButtonInfo,
-      [TYPE_OPTIONS.SUCCESS]: theme.orbit.colorTextButtonSuccess,
-      [TYPE_OPTIONS.WARNING]: theme.orbit.colorTextButtonWarning,
-      [TYPE_OPTIONS.CRITICAL]: theme.orbit.colorTextButtonCritical,
+      [TYPE_OPTIONS.INFO]: theme.orbit.buttonInfoForeground,
+      [TYPE_OPTIONS.SUCCESS]: theme.orbit.buttonSuccessForeground,
+      [TYPE_OPTIONS.WARNING]: theme.orbit.buttonWarningForeground,
+      [TYPE_OPTIONS.CRITICAL]: theme.orbit.buttonCriticalForeground,
       [TYPE_OPTIONS.INFO_SUBTLE]: theme.orbit.paletteBlueDark,
       [TYPE_OPTIONS.SUCCESS_SUBTLE]: theme.orbit.paletteGreenDark,
       [TYPE_OPTIONS.WARNING_SUBTLE]: theme.orbit.paletteOrangeDark,
@@ -71,10 +71,10 @@ const getAlertButtonTypeToken: GetAlertButtonTypeToken = (name, type, theme) => 
       [TYPE_OPTIONS.SECONDARY]: theme.orbit.paletteInkDark,
     },
     [TOKENS.colorTextButtonHover]: {
-      [TYPE_OPTIONS.INFO]: theme.orbit.colorTextButtonInfo,
-      [TYPE_OPTIONS.SUCCESS]: theme.orbit.colorTextButtonSuccess,
-      [TYPE_OPTIONS.WARNING]: theme.orbit.colorTextButtonWarning,
-      [TYPE_OPTIONS.CRITICAL]: theme.orbit.colorTextButtonCritical,
+      [TYPE_OPTIONS.INFO]: theme.orbit.buttonInfoForeground,
+      [TYPE_OPTIONS.SUCCESS]: theme.orbit.buttonSuccessForeground,
+      [TYPE_OPTIONS.WARNING]: theme.orbit.buttonWarningForeground,
+      [TYPE_OPTIONS.CRITICAL]: theme.orbit.buttonCriticalForeground,
       [TYPE_OPTIONS.INFO_SUBTLE]: theme.orbit.paletteBlueDarkHover,
       [TYPE_OPTIONS.SUCCESS_SUBTLE]: theme.orbit.paletteGreenDarkHover,
       [TYPE_OPTIONS.WARNING_SUBTLE]: theme.orbit.paletteOrangeDarkHover,
@@ -82,10 +82,10 @@ const getAlertButtonTypeToken: GetAlertButtonTypeToken = (name, type, theme) => 
       [TYPE_OPTIONS.SECONDARY]: theme.orbit.paletteInkDark,
     },
     [TOKENS.colorTextButtonActive]: {
-      [TYPE_OPTIONS.INFO]: theme.orbit.colorTextButtonInfo,
-      [TYPE_OPTIONS.SUCCESS]: theme.orbit.colorTextButtonSuccess,
-      [TYPE_OPTIONS.WARNING]: theme.orbit.colorTextButtonWarning,
-      [TYPE_OPTIONS.CRITICAL]: theme.orbit.colorTextButtonCritical,
+      [TYPE_OPTIONS.INFO]: theme.orbit.buttonInfoForeground,
+      [TYPE_OPTIONS.SUCCESS]: theme.orbit.buttonSuccessForeground,
+      [TYPE_OPTIONS.WARNING]: theme.orbit.buttonWarningForeground,
+      [TYPE_OPTIONS.CRITICAL]: theme.orbit.buttonCriticalForeground,
       [TYPE_OPTIONS.INFO_SUBTLE]: theme.orbit.paletteBlueDarkActive,
       [TYPE_OPTIONS.SUCCESS_SUBTLE]: theme.orbit.paletteGreenDarkActive,
       [TYPE_OPTIONS.WARNING_SUBTLE]: theme.orbit.paletteOrangeDarkActive,

packages/orbit-components/src/ButtonLink/helpers/getButtonLinkIconForeground.ts
@@ -25,9 +25,9 @@ const getButtonLinkIconForeground: getButtonLinkIconForegroundType = ({ type, th
   if (compact && type === TYPES.SECONDARY) {
     return {
       foreground: getButtonLinkTypeToken(TOKENS.foreground, type, theme),
-      foregroundHover: theme.orbit.colorTextButtonLinkSecondaryCompactHover,
-      foregroundActive: theme.orbit.colorTextButtonLinkSecondaryCompactHover,
-      foregroundFocus: theme.orbit.colorTextButtonLinkSecondaryCompactActive,
+      foregroundHover: theme.orbit.paletteProductNormalHover,
+      foregroundActive: theme.orbit.paletteProductNormalHover,
+      foregroundFocus: theme.orbit.paletteProductNormalActive,
     };
   }
 

packages/orbit-components/src/ButtonLink/helpers/getButtonLinkStyles.ts
@@ -38,9 +38,9 @@ const getButtonLinkStyles: GetButtonLinkStylesType = ({ type, theme, compact }) 
     if (type === TYPES.SECONDARY) {
       return {
         ...commonStyles,
-        foregroundHover: theme.orbit.colorTextButtonLinkSecondaryCompactHover,
-        foregroundActive: theme.orbit.colorTextButtonLinkSecondaryCompactHover,
-        foregroundFocus: theme.orbit.colorTextButtonLinkSecondaryCompactActive,
+        foregroundHover: theme.orbit.paletteProductNormalHover,
+        foregroundActive: theme.orbit.paletteProductNormalHover,
+        foregroundFocus: theme.orbit.paletteProductNormalActive,
       };
     }
     return commonStyles;

packages/orbit-components/src/ButtonMobileStore/index.tsx
@@ -31,7 +31,7 @@ const ButtonMobileStore = ({
 
   return (
     <a
-      className="inline-block h-[40px]"
+      className="h-1000 inline-block"
       href={href}
       target="_blank"
       rel="noopener noreferrer"

packages/orbit-components/src/ChoiceGroup/components/Feedback.tsx
@@ -23,7 +23,7 @@ const FormFeedback = (props: Props) => {
         {
           "--max-height": Math.floor(
             parseFloat(defaultTheme.orbit.lineHeightText) *
-              parseInt(defaultTheme.orbit.fontSizeFormFeedback, 10),
+              parseInt(defaultTheme.orbit.fontSizeTextSmall, 10),
           ),
         } as React.CSSProperties
       }

commit 62508c993e4e91236063aa29d75d982c04bade4e

fix(Accordion): make SectionFooter's border radius 3px
This regression was introduced in 5c4981f68f01301ca8c49cdebbca9abac706eb48

packages/orbit-components/src/Accordion/AccordionSection/components/SectionFooter.tsx
@@ -10,7 +10,7 @@ const AccordionSectionFooter = ({ children, dataTest }: Props) => (
   <div
     // the border-radius is calculated based on the border-radius and border-width of the AccordionSection component
     // outer border-radius is 4px, border-width is 1px so inner border-radius is 4px - 1px = 3px
-    className="p-600 bg-white-normal rounded-b-100 shadow-level2 sticky bottom-0 flex"
+    className="p-600 bg-white-normal shadow-level2 sticky bottom-0 flex rounded-b-[3px]"
     data-test={dataTest && `${dataTest}Footer`}
   >
     {children}
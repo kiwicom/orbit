62508c993e4e91236063aa29d75d982c04bade4e
Robin Cussol <me@robincussol.com>
2025-01-23 11:01:09 +0100

    fix(Accordion): make SectionFooter's border radius 3px
    This regression was introduced in 5c4981f68f01301ca8c49cdebbca9abac706eb48

--- packages/orbit-components/src/Accordion/AccordionSection/components/SectionFooter.tsx
+++ packages/orbit-components/src/Accordion/AccordionSection/components/SectionFooter.tsx
@@ -10,7 +10,7 @@ const AccordionSectionFooter = ({ children, dataTest }: Props) => (
   <div
     // the border-radius is calculated based on the border-radius and border-width of the AccordionSection component
     // outer border-radius is 4px, border-width is 1px so inner border-radius is 4px - 1px = 3px
-    className="p-600 bg-white-normal rounded-b-100 shadow-level2 sticky bottom-0 flex"
+    className="p-600 bg-white-normal shadow-level2 sticky bottom-0 flex rounded-b-[3px]"
     data-test={dataTest && `${dataTest}Footer`}
   >
     {children}

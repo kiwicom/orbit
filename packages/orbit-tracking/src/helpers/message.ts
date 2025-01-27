e02783e86c0e679b4235324e0de2ca6118e5265c
Daniel Sil <daniel.sil@kiwi.com>
2025-01-27 12:36:39 +0100

    chore: remove orbit-tracking package

--- packages/orbit-tracking/src/helpers/message.ts
+++
@@ -1,4 +0,0 @@
-import { chalk } from "zx";
-
-export const errorMessage = (str: string) => console.error(chalk.red(str));
-export const infoMessage = (str: string) => console.info(chalk.magenta.bold(str));
--- packages/orbit-tracking/tsup.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["src/index.ts", "src/react-scanner.config.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  splitting: true,
-  clean: true,
-});

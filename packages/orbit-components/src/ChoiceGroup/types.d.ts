c9a77a2df8ee12478907cb64e62d30dd6be2a053
Daniel Sil <daniel.sil@kiwi.com>
2025-02-27 17:03:33 +0100

    fix(ChoiceGroup): the label now renders as a div by default
    It was rendering as an h4, causing problems with heading hierarchies

--- packages/orbit-components/src/ChoiceGroup/types.d.ts
+++ packages/orbit-components/src/ChoiceGroup/types.d.ts
@@ -6,7 +6,7 @@ import type * as React from "react";
 import type * as Common from "../common/types";
 
 export type Size = "normal" | "large";
-type Element = "h2" | "h3" | "h4" | "h5" | "h6";
+type Element = "h2" | "h3" | "h4" | "h5" | "h6" | "div";
 
 export interface Props extends Common.Globals {
   readonly children:

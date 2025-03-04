e8b5c8e0d7ef41f851d537b625910ebb5784e00a
Sarka Chwastkova <chwastkova.s@gmail.com>
2025-03-04 10:47:46 +0100

    feat(Accordion): remove default Open button
    BREAKING CHANGE: The Accordion component no longer renders an Open
    button by default. If it is needed, you should pass it via actions prop.

--- packages/orbit-components/src/Accordion/AccordionSection/components/SectionHeader.tsx
+++ packages/orbit-components/src/Accordion/AccordionSection/components/SectionHeader.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import cx from "clsx";
 
-import Button from "../../../Button";
 import type * as Common from "../../../common/types";
 
 interface Props extends Common.Globals {
@@ -44,26 +43,10 @@ const AccordionSectionHeader = ({
     [onExpand],
   );
 
-  const handleButtonClick = React.useCallback(
-    (e: React.SyntheticEvent<HTMLButtonElement | HTMLAnchorElement>) => {
-      e.stopPropagation();
-      onExpand?.();
-    },
-    [onExpand],
-  );
-
   const content = (
     <>
       <div className="flex grow items-center">{children}</div>
-      {!expanded && expandable && (
-        <div className="ms-600 flex">
-          {actions || (
-            <Button onClick={handleButtonClick} type="secondary">
-              Open
-            </Button>
-          )}
-        </div>
-      )}
+      {!expanded && expandable && actions && <div className="ms-600 flex">{actions}</div>}
     </>
   );
 

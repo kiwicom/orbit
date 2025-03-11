a44bc12ad4d1bd147383bc32f973c8b1e1a77995
Sarka Chwastkova <chwastkova.s@gmail.com>
2025-03-11 16:06:59 +0100

    feat(AirportIllustration): set empty string for alt when a value is not provided

--- packages/orbit-components/src/AirportIllustration/index.tsx
+++ packages/orbit-components/src/AirportIllustration/index.tsx
@@ -5,8 +5,8 @@ import React from "react";
 import type { Props } from "./types";
 import IllustrationPrimitive, { SIZE_OPTIONS } from "../primitives/IllustrationPrimitive";
 
-const AirportIllustration = ({ size = SIZE_OPTIONS.MEDIUM, ...props }: Props) => (
-  <IllustrationPrimitive {...props} size={size} />
+const AirportIllustration = ({ size = SIZE_OPTIONS.MEDIUM, alt = "", ...props }: Props) => (
+  <IllustrationPrimitive {...props} alt={alt} size={size} />
 );
 
 export default AirportIllustration;

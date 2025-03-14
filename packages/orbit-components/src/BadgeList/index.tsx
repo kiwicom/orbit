efa57ba203f6c9994d13602b386b30e19e8d223d
Marco Vidal Garcia <marco.vidal@kiwi.com>
2025-03-14 17:51:48 +0100

    feat(BadgeList): add aria labels to list and icons
    closes FEPLT-2257

--- packages/orbit-components/src/BadgeList/index.tsx
+++ packages/orbit-components/src/BadgeList/index.tsx
@@ -4,9 +4,9 @@ import * as React from "react";
 
 import type { Props } from "./types";
 
-const BadgeList = ({ children, dataTest, id }: Props) => {
+const BadgeList = ({ children, dataTest, id, ariaLabel }: Props) => {
   return (
-    <ul className="m-0 flex flex-col p-0" data-test={dataTest} id={id}>
+    <ul className="m-0 flex flex-col p-0" data-test={dataTest} id={id} aria-label={ariaLabel}>
       {children}
     </ul>
   );

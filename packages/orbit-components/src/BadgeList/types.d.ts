8dc8b0005163a5aeb7b6c8f4326b050f7445e39f
Marco Vidal Garcia <marco.vidal@kiwi.com>
2025-03-19 16:31:34 +0100

    feat(BadgeList): add spacing prop

--- packages/orbit-components/src/BadgeList/types.d.ts
+++ packages/orbit-components/src/BadgeList/types.d.ts
@@ -4,8 +4,10 @@
 import type * as React from "react";
 
 import type * as Common from "../common/types";
+import type { Spacing } from "../Stack/types";
 
 export interface Props extends Common.Globals {
   readonly children: React.ReactNode;
   readonly ariaLabel?: string;
+  readonly spacing?: Spacing;
 }

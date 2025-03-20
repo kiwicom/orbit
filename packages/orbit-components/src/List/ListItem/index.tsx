986fc90f1f244dc6bd24995203ecda0e404a6f75
Sarka Chwastkova <chwastkova.s@gmail.com>
2025-03-20 17:17:21 +0100

    fixup! fix(ListItem): add aria-hidden attribute to ignore bullet by VO

--- packages/orbit-components/src/List/ListItem/index.tsx
+++ packages/orbit-components/src/List/ListItem/index.tsx
@@ -5,17 +5,14 @@ import * as React from "react";
 import CircleSmall from "../../icons/CircleSmall";
 import type { Props } from "./types";
 
-const ListItem = ({ label, children, icon = <CircleSmall />, dataTest }: Props) => {
+const ListItem = ({ label, children, icon = <CircleSmall ariaHidden />, dataTest }: Props) => {
   return (
     <li
       data-test={dataTest}
       className="orbit-list-item font-base mb-100 flex last:m-0 last-of-type:m-0"
     >
       {icon && (
-        <div
-          className="orbit-list-item-icon me-200 [&>.orbit-carrier-logo]:size-icon-small [&>.orbit-carrier-logo>img]:size-icon-small flex flex-none"
-          aria-hidden
-        >
+        <div className="orbit-list-item-icon me-200 [&>.orbit-carrier-logo]:size-icon-small [&>.orbit-carrier-logo>img]:size-icon-small flex flex-none">
           {icon}
         </div>
       )}

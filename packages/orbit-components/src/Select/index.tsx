0ff7c2ffd509eb49031e547c8955d6a110127202
Dominika Hustinova <domi.hustinova@gmail.com>
2025-03-24 15:50:49 +0100

    chore(Select): hide chevron icon from screen readers

--- packages/orbit-components/src/Select/index.tsx
+++ packages/orbit-components/src/Select/index.tsx
@@ -218,7 +218,7 @@ const Select = React.forwardRef<HTMLSelectElement, Props>((props, ref) => {
                 : "text-form-element-filled-foreground",
             )}
           >
-            <ChevronDown color="secondary" />
+            <ChevronDown color="secondary" ariaHidden />
           </div>
           <FakeInput disabled={disabled} error={error} />
         </div>

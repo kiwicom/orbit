e2758f675bf632f44ebcbef8a15183827eccc258
Daniel Sil <daniel.sil@kiwi.com>
2025-04-03 17:11:22 +0200

    feat(Checkbox): remove tooltip prop
    BREAKING CHANGE: Checkbox component no longer supports tooltip prop.
    This use case is no longer valid in Orbit.

--- packages/orbit-components/src/Checkbox/Checkbox.ct-story.tsx
+++ packages/orbit-components/src/Checkbox/Checkbox.ct-story.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import Tooltip from "../Tooltip";
-
 import Checkbox from ".";
 
 export function CheckboxStory() {
@@ -13,18 +11,6 @@ export function CheckboxStory() {
       <Checkbox label="Check this box" disabled />
       <Checkbox label="Check this box" disabled checked />
       <Checkbox label="Check this box" hasError />
-    </div>
-  );
-}
-
-export function CheckboxWithTooltipStory() {
-  return (
-    <div className="space-y-200 lm:pt-1200 lm:min-h-800 flex min-h-[700px] flex-col">
-      <Checkbox
-        label="Check this box"
-        tooltip={<Tooltip content="Tooltip content is visible" placement="top" />}
-        checked
-      />
     </div>
   );
 }
--- packages/orbit-components/src/Checkbox/Checkbox.ct.tsx
+++ packages/orbit-components/src/Checkbox/Checkbox.ct.tsx
@@ -1,63 +1,11 @@
 import * as React from "react";
 import { test, expect } from "@playwright/experimental-ct-react";
-import { defaultTokens } from "@kiwicom/orbit-design-tokens";
 
-import { CheckboxStory, CheckboxWithTooltipStory } from "./Checkbox.ct-story";
-
-const skipIfViewportSmallerThan = (viewport, width, message) => {
-  test.skip(viewport !== null && viewport.width < width, message);
-};
-
-const skipIfViewportLargerThanOrEqual = (viewport, width, message) => {
-  test.skip(viewport !== null && viewport.width >= width, message);
-};
+import { CheckboxStory } from "./Checkbox.ct-story";
 
 test.describe("visual Checkbox", () => {
-  const { breakpointLargeMobile } = defaultTokens;
-
   test("Checkbox", async ({ mount }) => {
     const component = await mount(<CheckboxStory />);
-    await expect(component).toHaveScreenshot();
-  });
-
-  test("Checkbox - checked with tooltip on desktop", async ({ mount, viewport }) => {
-    /**
-     * This command skips the test if the viewport width is smaller than largeMobile.
-     * Condition inside is based on the viewport width,
-     * which is specified for each of the device in playwright-ct.config.ts file.
-     * Using that we can distinguish mobileSmall, mobileMedium devices and larger devices.
-     */
-    skipIfViewportSmallerThan(
-      viewport,
-      breakpointLargeMobile,
-      "This feature is largeMobile, tablet and desktop only",
-    );
-
-    const component = await mount(<CheckboxWithTooltipStory />);
-
-    await component.getByRole("button").hover();
-    await expect(component).toHaveScreenshot();
-  });
-
-  test("Checkbox - checked with tooltip on mobile", async ({ mount, viewport, page }) => {
-    /**
-     * This command skips the test if the viewport width is bigger than largeMobile.
-     * Condition inside is based on the viewport width,
-     * which is specified for each of the device in playwright-ct.config.ts file.
-     * Using that we can distinguish mobileSmall, mobileMedium devices and larger devices.
-     */
-    skipIfViewportLargerThanOrEqual(
-      viewport,
-      breakpointLargeMobile,
-      "This feature is small and medium mobile only",
-    );
-
-    const component = await mount(<CheckboxWithTooltipStory />);
-    const tooltip = await page.getByRole("tooltip");
-
-    await component.getByRole("button").click();
-
-    await expect(tooltip).toBeVisible();
     await expect(component).toHaveScreenshot();
   });
 });

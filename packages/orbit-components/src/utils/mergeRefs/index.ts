5b4cb1a05f9648c5d52b3281dfa5db4258463708
Sarka Chwastkova <chwastkova.s@gmail.com>
2025-04-07 18:03:51 +0200

    chore: remove unused directives

--- packages/orbit-components/src/utils/mergeRefs/index.ts
+++ packages/orbit-components/src/utils/mergeRefs/index.ts
@@ -8,7 +8,6 @@ export default function mergeRefs<T = HTMLElement>(
       if (typeof ref === "function") {
         ref(value);
       } else if (ref != null && typeof ref !== "string") {
-        // @ts-expect-error expected to be mutable
         // eslint-disable-next-line no-param-reassign
         ref.current = value;
       }

a94cdcd185938dd695be71ca85b7462df52899e6
Sarka Chwastkova <chwastkova.s@gmail.com>
2025-04-11 15:30:32 +0200

    fix(BadgeList): remove margin values, set default spacing

--- packages/orbit-components/src/BadgeList/index.tsx
+++ packages/orbit-components/src/BadgeList/index.tsx
@@ -5,8 +5,15 @@ import cx from "clsx";
 
 import type { Props } from "./types";
 import { getSpacingClasses } from "../common/tailwind";
+import { SPACINGS } from "../utils/layout/consts";
 
-const BadgeList = ({ children, dataTest, id, ariaLabel, spacing }: Props) => {
+const BadgeList = ({
+  children,
+  dataTest,
+  id,
+  ariaLabel,
+  spacing = SPACINGS.ONE_HUNDRED,
+}: Props) => {
   return (
     <ul
       className={cx(

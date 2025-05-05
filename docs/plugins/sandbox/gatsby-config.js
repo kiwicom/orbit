c43810d32c3451508626a87f3942f7dede299d75
Sarka Chwastkova <chwastkova.s@gmail.com>
2025-05-05 17:33:12 +0200

    docs: fix missing graphql data

--- docs/plugins/sandbox/gatsby-config.js
+++ docs/plugins/sandbox/gatsby-config.js
@@ -1,1 +1,4 @@
-module.exports = require("./gatsby-config.ts");
+const { useGatsbyConfig } = require("gatsby-plugin-ts-config");
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+module.exports = useGatsbyConfig(() => require("./gatsby-config.ts"));

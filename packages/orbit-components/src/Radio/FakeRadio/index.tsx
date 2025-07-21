fd6920b726647e32a38c8093fe60c61ca1bc5e62
Sarka Chwastkova <chwastkova.s@gmail.com>
2025-07-21 09:00:33 +0200

    fix(Radio): remove fullwidth property for FakeRadio

--- packages/orbit-components/src/Radio/FakeRadio/index.tsx
+++ packages/orbit-components/src/Radio/FakeRadio/index.tsx
@@ -13,49 +13,33 @@ const FakeRadio = ({
   return (
     <div
       className={cx(
-        "font-base text-form-element-label-foreground relative flex w-full [align-items:self-start]",
-        "[&_.orbit-radio-icon-container]:has-[:checked]:border-2 [&_.orbit-radio-icon-container_span]:has-[:checked]:visible",
-        "[&_.orbit-radio-icon-container]:has-[:focus]:outline-blue-normal [&_.orbit-radio-icon-container]:has-[:focus]:outline [&_.orbit-radio-icon-container]:has-[:focus]:outline-2",
-        disabled
-          ? [
-              "cursor-not-allowed",
-              "[&_.orbit-radio-icon-container]:bg-cloud-light [&_.orbit-radio-icon-container]:border-cloud-dark",
-            ]
-          : [
-              "cursor-pointer",
-              "[&_.orbit-radio-icon-container]:has-[:checked]:border-form-element-focus [&_.orbit-radio-icon-container]:has-[:checked]:active:border-form-element-focus [&_.orbit-radio-icon-container]:has-[:checked]:bg-white-normal [&_.orbit-radio-icon-container]:bg-form-element-background",
-              !checked &&
-                hasError &&
-                "[&_.orbit-radio-icon-container]:border-form-element-error [&_.orbit-radio-icon-container]:hover:border-form-element-error-hover [&_.orbit-radio-icon-container]:active:border-form-element-error",
-              !hasError &&
-                "[&_.orbit-radio-icon-container]:border-form-element [&_.orbit-radio-icon-container]:hover:border-form-element-hover [&_.orbit-radio-icon-container]:active:border-form-element-active",
-              checked &&
-                !hasError &&
-                "[&_.orbit-radio-icon-container]:border-form-element-focus active:border-form-element-focus [&_.orbit-radio-icon-container]:bg-white-normal",
-            ],
+        "relative box-border",
+        "flex flex-none items-center justify-center",
+        "size-icon-medium rounded-full",
+        "duration-fast scale-100 transition-all ease-in-out",
+        "border-solid",
+        checked ? "border-2" : "border",
+        disabled && "bg-cloud-light border-cloud-dark cursor-not-allowed",
+        !disabled && [
+          "bg-form-element-background cursor-pointer active:scale-95",
+          checked &&
+            "border-form-element-focus hover:border-form-element-focus active:border-form-element-focus",
+          !checked &&
+            hasError &&
+            "border-form-element-error hover:border-form-element-error-hover active:border-form-element-error",
+          !checked &&
+            !hasError &&
+            "border-cloud-dark hover:border-cloud-dark-hover active:border-cloud-dark-active",
+        ],
       )}
     >
-      <div
+      <span
         className={cx(
-          "orbit-radio-icon-container",
-          "relative box-border",
-          "flex flex-none items-center justify-center",
-          "size-icon-medium rounded-full",
-          "duration-fast scale-100 transition-all ease-in-out",
-          "border-solid",
-          checked ? "border-2" : "border",
-          !disabled && "active:scale-95",
-          hasError && "border-blue-normal",
+          "size-[10px] rounded-full",
+          disabled ? "bg-cloud-dark" : "bg-blue-normal",
+          checked ? "visible" : "invisible",
         )}
-      >
-        <span
-          className={cx(
-            "size-[10px] rounded-full",
-            disabled ? "bg-cloud-dark" : "bg-blue-normal",
-            checked ? "visible" : "invisible",
-          )}
-        />
-      </div>
+      />
     </div>
   );
 };

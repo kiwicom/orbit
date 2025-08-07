c2472b5ff30c2cb3804caaf3bd8d422e9c988881
Sarka Chwastkova <chwastkova.s@gmail.com>
2025-08-07 10:40:08 +0200

    feat(Checkbox): update focus and hover styles

--- packages/orbit-components/src/Checkbox/index.tsx
+++ packages/orbit-components/src/Checkbox/index.tsx
@@ -36,26 +36,35 @@ const Checkbox = (props: Props) => {
         "font-base",
         "flex flex-row",
         "relative w-full",
-        "[&_.orbit-checkbox-icon-container]:hover:shadow-none",
-        "[&_.orbit-checkbox-icon-container]:has-[:focus]:outline-blue-normal [&_.orbit-checkbox-icon-container]:has-[:focus]:outline [&_.orbit-checkbox-icon-container]:has-[:focus]:outline-2",
         "[&_.orbit-checkbox-icon-container>svg]:has-[:checked]:visible",
         disabled
           ? [
-              "cursor-not-allowed",
-              "[&_.orbit-checkbox-icon-container]:bg-form-element-disabled-background",
+              "[&_.orbit-checkbox-icon-container]:bg-form-element-disabled-background [&_.orbit-checkbox-icon-container]:border-cloud-dark cursor-not-allowed",
               "[&_.orbit-checkbox-icon-container]:has-[:checked]:bg-cloud-dark",
+              "[&_.orbit-checkbox-icon-container]:has-[:focus]:outline-0 [&_.orbit-checkbox-icon-container]:has-[:hover]:outline-0",
               checked && "[&_.orbit-checkbox-icon-container]:bg-cloud-dark",
             ]
           : [
               "cursor-pointer",
-              "[&_.orbit-checkbox-icon-container]:has-[:checked]:border-blue-normal [&_.orbit-checkbox-icon-container]:has-[:checked]:bg-blue-normal [&_.orbit-checkbox-icon-container]:has-[:checked]:hover:border-blue-dark [&_.orbit-checkbox-icon-container]:has-[:checked]:hover:bg-blue-dark",
-              checked &&
-                !hasError &&
-                "[&_.orbit-checkbox-icon-container]:bg-blue-normal [&_.orbit-checkbox-icon-container]:border-blue-normal [&_.orbit-checkbox-icon-container]:hover:bg-blue-dark [&_.orbit-checkbox-icon-container]:hover:border-blue-dark",
-              !checked && "[&_.orbit-checkbox-icon-container]:bg-form-element-background",
-              !checked && hasError
-                ? "[&_.orbit-checkbox-icon-container]:border-form-element-error"
-                : "[&_.orbit-checkbox-icon-container]:border-form-element-border-color [&_.orbit-checkbox-icon-container]:hover:border-blue-light-active [&_.orbit-checkbox-icon-container]:active:border-form-element-focus",
+              "[&_.orbit-checkbox-icon-container]:has-[:focus]:outline [&_.orbit-checkbox-icon-container]:has-[:focus]:outline-2",
+              "[&_.orbit-checkbox-icon-container]:has-[:hover]:outline [&_.orbit-checkbox-icon-container]:has-[:hover]:outline-2",
+              hasError
+                ? [
+                    "[&_.orbit-checkbox-icon-container]:border-form-element-error",
+                    "[&_.orbit-checkbox-icon-container]:has-[:focus]:outline-blue-normal [&_.orbit-checkbox-icon-container]:has-[:focus]:border-red-normal",
+                    "[&_.orbit-checkbox-icon-container]:has-[:hover]:outline-red-light [&_.orbit-checkbox-icon-container]:has-[:hover]:border-red-normal",
+                  ]
+                : [
+                    "[&_.orbit-checkbox-icon-container]:border-form-element-border-color",
+                    "[&_.orbit-checkbox-icon-container]:has-[:focus]:outline-blue-light [&_.orbit-checkbox-icon-container]:has-[:focus]:border-blue-normal",
+                    "[&_.orbit-checkbox-icon-container]:has-[:hover]:outline-blue-light [&_.orbit-checkbox-icon-container]:has-[:hover]:border-blue-normal",
+                  ],
+              checked
+                ? [
+                    "[&_.orbit-checkbox-icon-container]:bg-blue-normal [&_.orbit-checkbox-icon-container]:border-blue-normal",
+                    "[&_.orbit-checkbox-icon-container]:has-[:checked]:bg-blue-normal [&_.orbit-checkbox-icon-container]:has-[:checked]:border-blue-normal",
+                  ]
+                : "[&_.orbit-checkbox-icon-container]:bg-form-element-background",
             ],
       )}
     >
